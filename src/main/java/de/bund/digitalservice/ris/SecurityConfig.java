2c12c6ad1e5ff80dd465f51f98da59010427555e
Klaus Hartl <klaus.hartl@gmail.com>
2022-04-14 17:05:10 +0200

    Relax spring security default configuration

---
+++ src/main/java/de/bund/digitalservice/ris/SecurityConfig.java
@@ -0,0 +1,16 @@
+package de.bund.digitalservice.ris;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+  @Bean
+  public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+    return http.authorizeExchange().anyExchange().permitAll().and().csrf().disable().build();
+  }
+}
