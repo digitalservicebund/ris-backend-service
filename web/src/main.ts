233cc8310339fa79924ed4ddb78834744f11a2fd
Benjamin Degenhart <benjamin.degenhart@digitalservice.bund.de>
2022-05-04 11:16:50 +0200

    Dummy change

--- web/src/main.ts
+++ web/src/main.ts
@@ -3,6 +3,6 @@ import "./style.css"
 const app = document.querySelector<HTMLDivElement>("#app")!
 
 app.innerHTML = `
-  <h1>Hello DigitalService!</h1>
+  <h1>Hello DigitalService :)</h1>
   <a href="https://vitejs.dev/guide/features.html" target="_blank">Vite Documentation</a>
 `
