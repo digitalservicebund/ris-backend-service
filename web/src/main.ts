50ef67573aa817085a04a8daee27bfd7c613b85c
Florian <florian.zechmeister@digitalservice.bund.de>
2022-05-13 06:59:31 +0200

    Install tailwindcss
    Addresses RISDEV-33

--- web/src/main.ts
+++ web/src/main.ts
@@ -1,8 +1,6 @@
 import { createApp } from "vue"
 import App from "./App.vue"
 
-import "./style.css"
+import "./index.css"
 
-const app = createApp(App)
-
-app.mount("#app")
+createApp(App).mount("#app")
