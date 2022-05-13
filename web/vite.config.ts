380049deb7bb4047542fbec235f3e7bd168564a7
jördis punknoir <joerdis.schuller@digitalservice.bund.de>
2022-05-13 11:46:46 +0200

    Revert "Fix vite config"
    This reverts commit 8462c919ab710327aa41745ffab3b3aa7322a12c.

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -4,15 +4,7 @@ import { defineConfig } from "vite"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  build: {
-    outDir: "../src/main/resources/static",
-  },
-  test: {
-    globals: true,
-    environment: "jsdom",
-    include: ["test/**/*.ts"],
-    exclude: ["test/e2e/**/*.ts", "test/a11y/**/*.ts"],
-  },
+
   // [vuestic-ui] Add alias for ~normalize.css.
   resolve: {
     alias: [{ find: /^~(.*)$/, replacement: "$1" }],
