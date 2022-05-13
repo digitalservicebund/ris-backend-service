7774434b832bd2f0105fb299c530c2c2fbe675f2
jördis punknoir <joerdis.schuller@digitalservice.bund.de>
2022-05-13 11:20:43 +0200

    Add vuestic

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,15 +1,12 @@
 import vue from "@vitejs/plugin-vue"
 import { defineConfig } from "vite"
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  build: {
-    outDir: "../src/main/resources/static",
-  },
-  test: {
-    globals: true,
-    environment: "jsdom",
-    include: ["test/**/*.ts"],
-    exclude: ["test/e2e/**/*.ts", "test/a11y/**/*.ts"],
+
+  // [vuestic-ui] Add alias for ~normalize.css.
+  resolve: {
+    alias: [{ find: /^~(.*)$/, replacement: "$1" }],
   },
 })
