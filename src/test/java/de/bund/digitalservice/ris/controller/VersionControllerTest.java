2e3fb45ad8ebe67f2f0be2590a9b05ca08bf340b
Benjamin Degenhart <benjamin.degenhart@digitalservice.bund.de>
2022-05-16 17:32:38 +0200

    Add test for rudimentary version info
    Addresses RISDEV-28

---
+++ src/test/java/de/bund/digitalservice/ris/controller/VersionControllerTest.java
@@ -0,0 +1,29 @@
+package de.bund.digitalservice.ris.controller;
+
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+@ActiveProfiles("test")
+class VersionControllerTest {
+
+  @Autowired private WebTestClient webTestClient;
+
+  @Test
+  void shouldGetVersionInfo() {
+    this.webTestClient
+        .get()
+        .uri("/api/v1/version")
+        .exchange()
+        .expectBody()
+        .jsonPath("$.version")
+        .isEqualTo("0.0.1")
+        .jsonPath("$.commit")
+        .isEqualTo("abcdef");
+  }
+}
