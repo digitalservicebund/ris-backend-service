53f6d4a5f856f85f8eb83ba5c98ed09acf3e35af
Benjamin Degenhart <benjamin.degenhart@digitalservice.bund.de>
2022-05-16 23:18:10 +0200

    Fallback value is mandatory

--- src/main/java/de/bund/digitalservice/ris/service/VersionService.java
+++ src/main/java/de/bund/digitalservice/ris/service/VersionService.java
@@ -4,6 +4,7 @@ import de.bund.digitalservice.ris.datamodel.VersionInfo;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Objects;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
@@ -14,14 +15,14 @@ import reactor.core.publisher.Mono;
 @Slf4j
 public class VersionService {
 
-  @Value("${api.version.commit-sha-file}")
+  @Value("${api.version.commit-sha-file:#{null}}")
   private Path COMMIT_SHA_FILE;
 
   public Mono<ResponseEntity<Object>> generateVersionInfo() {
     VersionInfo info = new VersionInfo();
     info.setVersion("0.0.1");
     String commitSha = "Commit SHA not available";
-    if (COMMIT_SHA_FILE.toFile().exists()) {
+    if (Objects.nonNull(COMMIT_SHA_FILE) && COMMIT_SHA_FILE.toFile().exists()) {
       try {
         commitSha = Files.readString(COMMIT_SHA_FILE);
       } catch (IOException ignored) {
