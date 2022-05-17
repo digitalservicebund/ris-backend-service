29034731a16f3a5cdcfdcd6529df70a201f80a78
jördis punknoir <joerdis.schuller@digitalservice.bund.de>
2022-05-17 18:48:28 +0200

    Add vuestic theming

--- web/src/services/vuestic-ui/global-config.ts
+++ web/src/services/vuestic-ui/global-config.ts
@@ -1,14 +1,9 @@
 import {COLOR_THEMES} from "./themes";
-import {
-    colorBackgroundButtonPrimaryDefault, colorTextButtonPrimaryDefault
-} from "@digitalservice4germany/style-dictionary";
 
 export default {
     ...COLOR_THEMES,
     components: {
         VaButton: {
-            color: colorBackgroundButtonPrimaryDefault,
-            textColor: colorTextButtonPrimaryDefault,
             rounded: false,
             round: false,
             flat: false,
