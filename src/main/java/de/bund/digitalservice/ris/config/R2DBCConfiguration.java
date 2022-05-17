bc102e2bc9f8ebe9b12bb3fdfa3d25d698b7ea1a
Benjamin Degenhart <benjamin.degenhart@digitalservice.bund.de>
2022-05-17 19:10:40 +0200

    Enable the h2-console on a different port
    Code via https://github.com/siru-dduck/spring-webflux-guide/blob/406d4a5dc818a0d393915b6d55526b17e352aeb5/src/main/java/siru/springwebflux/h2/H2Config.java
    
    Addresses RISDEV-26

--- src/main/java/de/bund/digitalservice/ris/config/R2DBCConfiguration.java
+++ src/main/java/de/bund/digitalservice/ris/config/R2DBCConfiguration.java
@@ -1,19 +1,41 @@
 package de.bund.digitalservice.ris.config;
 
 import io.r2dbc.spi.ConnectionFactory;
+import lombok.extern.slf4j.Slf4j;
+import org.h2.tools.Server;
 import org.springframework.boot.r2dbc.ConnectionFactoryBuilder;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.ContextClosedEvent;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.data.r2dbc.config.AbstractR2dbcConfiguration;
 import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
 
 @Configuration
 @EnableR2dbcRepositories
+@Slf4j
 public class R2DBCConfiguration extends AbstractR2dbcConfiguration {
-  @Bean
+
+  private Server webServer;
+
+  @Override
   public ConnectionFactory connectionFactory() {
     return ConnectionFactoryBuilder.withUrl(
             "r2dbc:h2:mem:///testdb?options=DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE")
         .build();
+  }
+
+  @EventListener(ContextRefreshedEvent.class)
+  public void start() throws java.sql.SQLException {
+    log.info("Starting h2-console at port 8078");
+    // Settings to use on localhost:8078 --> Generic H2 (Server), jdbc:h2:memo:testdb
+    this.webServer =
+        org.h2.tools.Server.createWebServer("-webPort", "8078", "-tcpAllowOthers").start();
+  }
+
+  @EventListener(ContextClosedEvent.class)
+  public void stop() {
+    log.info("Stopping h2-console at port 8078");
+    this.webServer.stop();
   }
 }
