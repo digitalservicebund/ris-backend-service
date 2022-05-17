c7514f856f47a0f1123e4fd632031e8a7fff5a99
Benjamin Degenhart <benjamin.degenhart@digitalservice.bund.de>
2022-05-17 13:50:38 +0200

    Add test for VersionService
    Addresses RISDEV-28

---
+++ src/test/java/de/bund/digitalservice/ris/service/VersionServiceTest.java
@@ -0,0 +1,30 @@
+package de.bund.digitalservice.ris.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import de.bund.digitalservice.ris.datamodel.VersionInfo;
+import java.util.Objects;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import reactor.test.StepVerifier;
+
+@SpringBootTest
+class VersionServiceTest {
+
+  @Autowired private VersionService service;
+
+  @Test
+  public void testGenerateVersionInfo() {
+    StepVerifier.create(service.generateVersionInfo())
+        .consumeNextWith(
+            responseEntity -> {
+              assertNotNull(responseEntity);
+              assertTrue(responseEntity.getStatusCode().is2xxSuccessful());
+              VersionInfo versionInfo = (VersionInfo) responseEntity.getBody();
+              assertEquals(Objects.requireNonNull(versionInfo).getVersion(), "0.0.1");
+              assertNotNull(versionInfo.getCommitSHA());
+            })
+        .verifyComplete();
+  }
+}
