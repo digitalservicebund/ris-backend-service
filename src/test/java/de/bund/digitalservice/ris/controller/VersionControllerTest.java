32ef59ca65c9cd686f5ceb2107c184e985a816ca
Benjamin Degenhart <benjamin.degenhart@digitalservice.bund.de>
2022-05-17 13:16:49 +0200

    No body check in controller test, no test profile
    Addresses RISDEV-28

--- src/test/java/de/bund/digitalservice/ris/controller/VersionControllerTest.java
+++ src/test/java/de/bund/digitalservice/ris/controller/VersionControllerTest.java
@@ -1,29 +1,28 @@
 package de.bund.digitalservice.ris.controller;
 
-import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+import static org.springframework.security.test.web.reactive.server.SecurityMockServerConfigurers.csrf;
 
+import de.bund.digitalservice.ris.service.VersionService;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest(webEnvironment = RANDOM_PORT)
-@ActiveProfiles("test")
+@ExtendWith(SpringExtension.class)
+@WebFluxTest(controllers = VersionController.class)
+@WithMockUser
 class VersionControllerTest {
 
-  @Autowired private WebTestClient webTestClient;
+  @Autowired private WebTestClient webClient;
+
+  @MockBean private VersionService service;
 
   @Test
-  void shouldGetVersionInfo() {
-    this.webTestClient
-        .get()
-        .uri("/api/v1/version")
-        .exchange()
-        .expectBody()
-        .jsonPath("$.version")
-        .isEqualTo("0.0.1")
-        .jsonPath("$.commitSHA")
-        .exists();
+  void testGetVersion() {
+    webClient.mutateWith(csrf()).get().uri("/api/v1/version").exchange().expectStatus().isOk();
   }
 }
