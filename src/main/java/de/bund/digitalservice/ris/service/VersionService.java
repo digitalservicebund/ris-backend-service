f23053ea12871408aea90e035b2eef229a413221
Benjamin Degenhart <benjamin.degenhart@digitalservice.bund.de>
2022-05-17 14:25:37 +0200

    Fallback value if env var is not available
    Addresses RISDEV-28

--- src/main/java/de/bund/digitalservice/ris/service/VersionService.java
+++ src/main/java/de/bund/digitalservice/ris/service/VersionService.java
@@ -1,6 +1,7 @@
 package de.bund.digitalservice.ris.service;
 
 import de.bund.digitalservice.ris.datamodel.VersionInfo;
+import java.util.Objects;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -13,7 +14,8 @@ public class VersionService {
   public Mono<ResponseEntity<Object>> generateVersionInfo() {
     VersionInfo info = new VersionInfo();
     info.setVersion("0.0.1");
-    info.setCommitSHA(System.getenv("COMMIT_SHA"));
+    info.setCommitSHA(
+        Objects.requireNonNullElse(System.getenv("COMMIT_SHA"), "commit SHA not available"));
     return Mono.just(info).map(ResponseEntity::ok);
   }
 }
