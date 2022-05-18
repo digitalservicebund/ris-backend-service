6f4582b5745071020c7afff68cfda2aac711e4b9
Benjamin Degenhart <benjamin.degenhart@digitalservice.bund.de>
2022-05-18 22:31:14 +0200

    Clean up commit SHA attempts, it works now
    Addresses RISDEV-28

--- src/main/java/de/bund/digitalservice/ris/service/VersionService.java
+++ src/main/java/de/bund/digitalservice/ris/service/VersionService.java
@@ -1,7 +1,6 @@
 package de.bund.digitalservice.ris.service;
 
 import de.bund.digitalservice.ris.datamodel.VersionInfo;
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import lombok.extern.slf4j.Slf4j;
@@ -17,13 +16,11 @@ public class VersionService {
   public Mono<ResponseEntity<Object>> generateVersionInfo() {
     VersionInfo info = new VersionInfo();
     info.setVersion("0.0.1");
-    String commitSha = "Commit SHA is not available";
+    String commitSha = "commit SHA not available";
     try {
-      File file = ResourceUtils.getFile("classpath:.commit-sha");
-      commitSha = "Error when trying to read commit SHA file";
-      commitSha = Files.readAllLines(file.toPath()).get(0);
-    } catch (IOException e) {
-      commitSha = "Error when trying to read commit SHA";
+      commitSha =
+          Files.readAllLines(ResourceUtils.getFile("classpath:.commit-sha").toPath()).get(0);
+    } catch (IOException ignored) {
     }
     info.setCommitSHA(commitSha);
     return Mono.just(info).map(ResponseEntity::ok);
