15599fe7e6790bd9b5c80ae5a4ebbd2d93d25bbe
Florian <florian.zechmeister@digitalservice.bund.de>
2022-05-19 16:36:28 +0200

    Remove tailwindcss
    Vuetify bring everything we need.

--- web/tailwind.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-  presets: [require("@digitalservice4germany/style-dictionary/tailwind")],
-}
