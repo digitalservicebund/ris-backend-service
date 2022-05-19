1d8b61896e1c897d8908aa17a6284f81959d7ff3
jördis punknoir <joerdis.schuller@digitalservice.bund.de>
2022-05-19 13:08:42 +0200

    Fix basic theming

--- web/src/plugins/vuetify.ts
+++ web/src/plugins/vuetify.ts
@@ -6,11 +6,22 @@ import "vuetify/styles"
 import { createVuetify } from "vuetify"
 import * as components from "vuetify/components"
 import * as directives from "vuetify/directives"
-import { COLOR_THEMES } from "./themes"
 
 export const risLightTheme = {
   dark: false,
-  ...COLOR_THEMES,
+  colors: {
+    white: "#FFFFFF",
+    black: "#000000",
+    blue800: "#004B76",
+    background: "#FFFFFF",
+    surface: "#FFFFFF",
+    primary: "#6200EE",
+    secondary: "#03DAC6",
+    error: "#B00020",
+    info: "#2196F3",
+    success: "#4CAF50",
+    warning: "#FB8C00",
+  },
 }
 
 export default createVuetify({
