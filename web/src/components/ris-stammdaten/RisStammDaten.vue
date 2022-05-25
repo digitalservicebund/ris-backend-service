9094be90c545e0f4b3f1811caacf66ddf64cb859
jördis punknoir <joerdis.schuller@digitalservice.bund.de>
2022-05-25 16:04:09 +0200

    Add base stammdaten form RISDEV-65

---
+++ web/src/components/ris-stammdaten/RisStammDaten.vue
@@ -0,0 +1,48 @@
+<script lang="ts" setup></script>
+
+<template>
+  <v-form>
+    <v-container>
+      <v-row><h3>Stammdaten</h3></v-row>
+      <v-row>
+        <v-col cols="12" md="4">
+          <v-text-field label="Vorgang"></v-text-field>
+        </v-col>
+        <v-col cols="12" md="4">
+          <v-text-field label="Entscheidungsdatum"></v-text-field>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="12" md="4">
+          <v-text-field label="Gerichtssitz"></v-text-field>
+        </v-col>
+        <v-col cols="12" md="4">
+          <v-text-field label="Gerichtstyp"></v-text-field>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="12" md="4">
+          <v-text-field label="Aktenzeichen"></v-text-field>
+        </v-col>
+        <v-col cols="12" md="4">
+          <v-text-field label="Spruchkörper"></v-text-field>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="12" md="4">
+          <v-text-field label="Eingangsart"></v-text-field>
+        </v-col>
+        <v-col cols="12" md="4">
+          <v-text-field label="ECLI-Nummer"></v-text-field>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="12" md="4">
+          <v-text-field label="Rechtskraft"></v-text-field>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-form>
+</template>
+
+<style lang="scss"></style>
