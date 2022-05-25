c87e3ca6fb806f55d3b574b09e0a4214fd4f83f2
jördis punknoir <joerdis.schuller@digitalservice.bund.de>
2022-05-25 16:47:21 +0200

    Revert "Remove a11y test"
    This reverts commit 1d2b636f3a29cf001f0e50c0795578a5d1af943e.

--- web/test/a11y/example.spec.ts
+++ web/test/a11y/example.spec.ts
@@ -1,10 +1,10 @@
-// import { test } from "@playwright/test"
-// import { injectAxe, checkA11y } from "axe-playwright"
-//
-// test.describe("basic example a11y test", () => {
-//   test("test start page", async ({ page }) => {
-//     await page.goto("http://localhost:4173/")
-//     await injectAxe(page)
-//     await checkA11y(page)
-//   })
-// })
+import { test } from "@playwright/test"
+import { injectAxe, checkA11y } from "axe-playwright"
+
+test.describe("basic example a11y test", () => {
+  test("test start page", async ({ page }) => {
+    await page.goto("http://localhost:4173/")
+    await injectAxe(page)
+    await checkA11y(page)
+  })
+})
