214b2d451731494757147e02d4fd2ae5b10caa6d
jördis punknoir <joerdis.schuller@digitalservice.bund.de>
2022-05-25 15:16:34 +0200

    Remove unnecessary observer mock from test

--- web/src/components/hello-world/__tests__/HelloWorld.spec.ts
+++ web/src/components/hello-world/__tests__/HelloWorld.spec.ts
@@ -1,12 +1,9 @@
 import { mount } from "@vue/test-utils"
-import { describe, it, expect, vitest } from "vitest"
+import { describe, it, expect } from "vitest"
 import { createVuetify } from "vuetify"
 import * as components from "vuetify/components"
 import * as directives from "vuetify/directives"
-import useResizeObserverMock from "../../../../test/test-helper/useResizeObserverMock"
 import HelloWorld from "../HelloWorld.vue"
-
-vitest.mock("use-resize-observer", () => useResizeObserverMock)
 
 describe("HelloWorld", () => {
   const vuetify = createVuetify({ components, directives })
