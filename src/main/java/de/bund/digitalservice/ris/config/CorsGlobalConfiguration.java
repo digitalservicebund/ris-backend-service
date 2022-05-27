04b645948a722a0ef8d85cd172ef7f2f3637e0c0
Benjamin Degenhart <benjamin.degenhart@digitalservice.bund.de>
2022-05-27 17:08:26 +0200

    Re-enable local serving from 8080
    This tag here seems to have been unnecessary and removing it enables local serving of static content at 8080, this wasn't possible with the tag

--- src/main/java/de/bund/digitalservice/ris/config/CorsGlobalConfiguration.java
+++ src/main/java/de/bund/digitalservice/ris/config/CorsGlobalConfiguration.java
@@ -3,11 +3,9 @@ package de.bund.digitalservice.ris.config;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.config.CorsRegistry;
-import org.springframework.web.reactive.config.EnableWebFlux;
 import org.springframework.web.reactive.config.WebFluxConfigurer;
 
 @Configuration
-@EnableWebFlux
 @ConditionalOnProperty(name = "application.cors.enable", havingValue = "true")
 public class CorsGlobalConfiguration implements WebFluxConfigurer {
 
