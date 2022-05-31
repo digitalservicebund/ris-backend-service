2288596ba42cf4970f46d4f52e491425676d4c66
Benjamin Degenhart <benjamin.degenhart@digitalservice.bund.de>
2022-05-31 17:40:28 +0200

    Add new fields to schema.sql
    RISDEV-26

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,5 +1,22 @@
 CREATE TABLE IF NOT EXISTS doc_unit (
     id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
     s3path VARCHAR(255),
-    filetype VARCHAR(30)
+    filetype VARCHAR(30),
+    aktenzeichen VARCHAR(255),
+    entscheidungsdatum VARCHAR(255),
+    gericht VARCHAR(255),
+    dokumenttyp VARCHAR(255),
+    gerichtssitz VARCHAR(255),
+    spruchkoerper VARCHAR(255),
+    vorgang VARCHAR(255),
+    eclinummer VARCHAR(255),
+    eingangsart VARCHAR(255),
+    tenor VARCHAR(255),
+    gruende VARCHAR(255),
+    tatbestand VARCHAR(255),
+    entscheidungsgruende VARCHAR(255),
+    abweichendemeinung VARCHAR(255),
+    sonstigerlangtext VARCHAR(255),
+    gliederung VARCHAR(255),
+    berichtigung VARCHAR(255)
 );
