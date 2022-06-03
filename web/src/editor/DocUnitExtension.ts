885538ebb5f21d0fdc95dc80e7c103d3a21869d9
Ralph von Petersdorff-Campen <ralph.vonpetersdorffcampen@digitalservice.bund.de>
2022-06-03 17:09:29 +0200

    Fix prettier and tsc
    Address RISDEV-57

--- web/src/editor/DocUnitExtension.ts
+++ web/src/editor/DocUnitExtension.ts
@@ -1,14 +1,18 @@
 import { Extension, mergeAttributes, Node } from "@tiptap/vue-3"
 
 interface DocUnitOptions {
-  types: string[],
+  types: string[]
+}
+
+interface RandnummerOptions {
+  HTMLAttributes: Record<string, unknown>
 }
 
 export const DocUnitParagraphExtension = Extension.create<DocUnitOptions>({
-  name: 'docUnit',
+  name: "docUnit",
   addOptions() {
     return {
-      types: ['paragraph'],
+      types: ["paragraph"],
     }
   },
   addGlobalAttributes() {
@@ -18,106 +22,66 @@ export const DocUnitParagraphExtension = Extension.create<DocUnitOptions>({
         attributes: {
           fontSize: {
             default: null,
-            parseHTML: element => element.style.fontSize.replace(/['"]+/g, ''),
-            renderHTML: attributes => {
+            parseHTML: (element) =>
+              element.style.fontSize.replace(/['"]+/g, ""),
+            renderHTML: (attributes) => {
               if (!attributes.fontSize) {
                 return {}
               }
 
               return {
-                style: `font-size: ${attributes.fontSize}`
+                style: `font-size: ${attributes.fontSize}`,
               }
-            }
+            },
           },
           textAlign: {
             default: null,
-            parseHTML: element => element.style.textAlign.replace(/['"]+/g, ''),
-            renderHTML: attributes => {
+            parseHTML: (element) =>
+              element.style.textAlign.replace(/['"]+/g, ""),
+            renderHTML: (attributes) => {
               if (!attributes.textAlign) {
                 return {}
               }
 
               return {
-                style: `text-align: ${attributes.textAlign}`
+                style: `text-align: ${attributes.textAlign}`,
               }
-            }
+            },
           },
-        }
-      }
+        },
+      },
     ]
-  }
-})
-
-/*
-export const DocUnitDivExtension = Extension.create<DocUnitOptions>({
-  name: 'docUnit',
-  addOptions() {
-    return {
-      types: ['randnummer'],
-    }
   },
-  addGlobalAttributes() {
-    return [
-      {
-        types: this.options.types,
-        attributes: {
-          randnummer: {
-            default: null,
-            parseHTML: element => element.getAttribute('class'),
-            renderHTML: attributes => {
-              if (attributes['class'] !== 'randnummer') {
-                return {}
-              }
-
-              return {
-                style: 'display: flex; margin-bottom: 10px'
-              }
-            }
-          }
-        }
-      }
-    ]
-  }
 })
-*/
-
-interface RandnummerOptions {
-  HTMLAttributes: Record<string, any>,
-}
 
 export const Randnummer = Node.create<RandnummerOptions>({
-  name: 'randnummer',
+  name: "randnummer",
   priority: 1000,
-  group: 'block',
-  content: 'inline*',
+  group: "block",
+  content: "inline*",
   addAttributes() {
-    return { number: '1' }
+    return { number: "1" }
   },
   addOptions() {
     return {
       HTMLAttributes: {
-        style: 'display: flex; margin-bottom: 10px'
+        style: "display: flex; margin-bottom: 10px",
       },
     }
   },
   parseHTML() {
-    return [
-      { tag: 'randnummer' },
-    ]
+    return [{ tag: "randnummer" }]
   },
   renderHTML({ HTMLAttributes }) {
-    return [ 
-      'div', 
-      mergeAttributes(this.options.HTMLAttributes, HTMLAttributes), 
+    return [
+      "div",
+      mergeAttributes(this.options.HTMLAttributes, HTMLAttributes),
       [
-        'div',
-        { style: 'padding-top: 20px; min-width: 40px;' },
-        HTMLAttributes.number.toString()
+        "div",
+        { style: "padding-top: 20px; min-width: 40px;" },
+        HTMLAttributes.number.toString(),
       ],
-      [
-        'div',
-        0 
-      ],
+      ["div", 0],
     ]
   },
 })
