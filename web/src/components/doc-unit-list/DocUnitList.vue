1887c8f5377588fb28f95e9886732e9902b2e234
Benjamin Degenhart <benjamin.degenhart@digitalservice.bund.de>
2022-06-09 23:44:42 +0200

    Use v-table for docUnit list
    RISDEV-105

--- web/src/components/doc-unit-list/DocUnitList.vue
+++ web/src/components/doc-unit-list/DocUnitList.vue
@@ -10,13 +10,34 @@ onMounted(() => {
 </script>
 
 <template>
-  <ul v-if="!docUnitsStore.isEmpty()">
-    <li v-for="docUnit in docUnitsStore.getAll()" :key="docUnit.id">
-      <router-link :to="{ name: 'Rubriken', params: { id: docUnit.id } }"
-        >{{ docUnit.id }}, {{ docUnit.s3path }},
-        {{ docUnit.filetype }}</router-link
-      >
-    </li>
-  </ul>
+  <v-table v-if="!docUnitsStore.isEmpty()">
+    <thead>
+      <tr>
+        <th class="text-center">Dok.-Nummer</th>
+        <th class="text-center">Angelegt am</th>
+        <th class="text-center">Aktenzeichen</th>
+        <th class="text-center">Dokumente</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="docUnit in docUnitsStore.getAll()" :key="docUnit.id">
+        <td>
+          <router-link :to="{ name: 'Rubriken', params: { id: docUnit.id } }">
+            {{ docUnit.id }}
+          </router-link>
+        </td>
+        <td>-</td>
+        <td>{{ docUnit.aktenzeichen }}</td>
+        <td>
+          <router-link
+            v-if="docUnit.filename"
+            :to="{ name: 'Dokumente', params: { id: docUnit.id } }"
+          >
+            {{ docUnit.filename }}
+          </router-link>
+        </td>
+      </tr>
+    </tbody>
+  </v-table>
   <span v-else>No doc units found</span>
 </template>
