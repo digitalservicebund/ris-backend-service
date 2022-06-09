15f6826b153e0958fd9fdca7839b542749ffd23e
Benjamin Degenhart <benjamin.degenhart@digitalservice.bund.de>
2022-06-09 15:33:32 +0200

    Fix failing e2e test
    RISDEV-105

--- web/test/e2e/example.spec.ts
+++ web/test/e2e/example.spec.ts
@@ -3,6 +3,6 @@ import { test, expect } from "@playwright/test"
 test.describe("basic example test", () => {
   test("basic test", async ({ page }) => {
     await page.goto("http://localhost:4173/")
-    await expect(page.locator("text=DigitalService")).toBeVisible()
+    await expect(page.locator("text=NeuRIS")).toBeVisible()
   })
 })
