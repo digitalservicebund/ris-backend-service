4c31148704e8fb78e667407bb418eb2471a4d80d
Ralph von Petersdorff-Campen <ralph.vonpetersdorffcampen@digitalservice.bund.de>
2022-06-09 12:06:31 +0200

    Second try to fix code secure alert
    Addresses RISDEV-57

--- src/main/java/de/bund/digitalservice/ris/config/ConverterConfiguration.java
+++ src/main/java/de/bund/digitalservice/ris/config/ConverterConfiguration.java
@@ -11,13 +11,21 @@ import org.springframework.context.annotation.Configuration;
 public class ConverterConfiguration {
   @Bean
   public DocumentBuilderFactory documentBuilderFactory() {
-    DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+    DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+
     try {
-      dbFactory.setFeature(XMLConstants.FEATURE_SECURE_PROCESSING, true);
+      factory.setFeature(XMLConstants.FEATURE_SECURE_PROCESSING, true);
+      factory.setFeature("http://apache.org/xml/features/disallow-doctype-decl", true);
+      factory.setFeature("http://xml.org/sax/features/external-general-entities", false);
+      factory.setFeature("http://xml.org/sax/features/external-parameter-entities", false);
     } catch (ParserConfigurationException e) {
       throw new BeanCreationException(
           "DocumentBuilderFactory", "Couldn't create DocumentBuilderFactory!", e);
     }
-    return dbFactory;
+
+    factory.setAttribute(XMLConstants.ACCESS_EXTERNAL_DTD, "");
+    factory.setAttribute(XMLConstants.ACCESS_EXTERNAL_SCHEMA, "");
+
+    return factory;
   }
 }
