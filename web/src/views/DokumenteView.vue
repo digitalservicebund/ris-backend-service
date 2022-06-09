e73d60e260b6eab34e3d9539a3e9554aaa6853c1
Benjamin Degenhart <benjamin.degenhart@digitalservice.bund.de>
2022-06-09 21:43:32 +0200

    Use RouteHelper in DokumenteView too
    RISDEV-105

--- web/src/views/DokumenteView.vue
+++ web/src/views/DokumenteView.vue
@@ -1,7 +1,12 @@
 <script lang="ts" setup>
+import RouteHelper from "../components/RouteHelper.vue"
 import UploadFile from "../components/upload-file/UploadFile.vue"
+import { useDocUnitsStore } from "../store"
+
+const store = useDocUnitsStore()
 </script>
 
 <template>
-  <UploadFile />
+  <RouteHelper />
+  <UploadFile v-if="store.hasSelected()" />
 </template>
