2a877b2d282d1f077a7dc6ef762afc08d6991a45
Benjamin Degenhart <benjamin.degenhart@digitalservice.bund.de>
2022-06-12 20:47:00 +0200

    Temp fix OTC URI not available at bean creation

--- src/main/java/de/bund/digitalservice/ris/config/OtcObsConfig.java
+++ src/main/java/de/bund/digitalservice/ris/config/OtcObsConfig.java
@@ -4,7 +4,6 @@ import de.bund.digitalservice.ris.utils.S3AsyncMockClient;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.time.Duration;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -15,8 +14,6 @@ import software.amazon.awssdk.services.s3.S3AsyncClient;
 
 @Configuration
 public class OtcObsConfig {
-  @Value("${otc.obs.url}:https://obs.eu-de.otc.t-systems.com")
-  private String otcObsUrlString;
 
   @Bean
   @Profile({"production", "staging"})
@@ -25,7 +22,7 @@ public class OtcObsConfig {
         NettyNioAsyncHttpClient.builder().writeTimeout(Duration.ZERO).maxConcurrency(64).build();
 
     return S3AsyncClient.builder()
-        .endpointOverride(new URI(otcObsUrlString))
+        .endpointOverride(new URI("https://obs.eu-de.otc.t-systems.com"))
         .region(Region.of("eu-de"))
         .httpClient(httpClient)
         .build();
