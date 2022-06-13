96b9128d00e2bead542b3f4008a585aacc39f9f4
Benjamin Degenhart <benjamin.degenhart@digitalservice.bund.de>
2022-06-13 11:49:27 +0200

    Re-add repaired @Value for otc.obs.url
    Original change was in 37877493db1b732e7a5092bc431ebf7cc6c740bc

--- src/main/java/de/bund/digitalservice/ris/config/OtcObsConfig.java
+++ src/main/java/de/bund/digitalservice/ris/config/OtcObsConfig.java
@@ -4,6 +4,7 @@ import de.bund.digitalservice.ris.utils.S3AsyncMockClient;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.time.Duration;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -14,6 +15,8 @@ import software.amazon.awssdk.services.s3.S3AsyncClient;
 
 @Configuration
 public class OtcObsConfig {
+  @Value("${otc.obs.url:https://obs.eu-de.otc.t-systems.com}")
+  private String otcObsUrlString;
 
   @Bean
   @Profile({"production", "staging"})
@@ -22,7 +25,7 @@ public class OtcObsConfig {
         NettyNioAsyncHttpClient.builder().writeTimeout(Duration.ZERO).maxConcurrency(64).build();
 
     return S3AsyncClient.builder()
-        .endpointOverride(new URI("https://obs.eu-de.otc.t-systems.com"))
+        .endpointOverride(new URI(otcObsUrlString))
         .region(Region.of("eu-de"))
         .httpClient(httpClient)
         .build();
