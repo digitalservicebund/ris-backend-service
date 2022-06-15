58cdd8ad26f3a75720020668593cc8c6e78b157e
Benjamin Degenhart <benjamin.degenhart@digitalservice.bund.de>
2022-06-15 13:16:14 +0200

    Outcomment e2e test of homepage

--- web/test/e2e/example.spec.ts
+++ web/test/e2e/example.spec.ts
@@ -1,8 +1,9 @@
-import { test, expect } from "@playwright/test"
+import { test } from "@playwright/test"
 
 test.describe("basic example test", () => {
   test("basic test", async ({ page }) => {
     await page.goto("http://localhost:4173/")
-    await expect(page.locator("text=NeuRIS")).toBeVisible()
+    // homepage forwards to /rechtsprechung TODO
+    // await expect(page.locator("text=NeuRIS")).toBeVisible()
   })
 })
