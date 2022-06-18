2e4f3c14d7f4a0b50ccbce50a9ba27336752ee49
Benjamin Degenhart <benjamin.degenhart@digitalservice.bund.de>
2022-06-18 23:40:32 +0200

    Improve styling of doc info panel
    RISDEV-105

--- web/src/components/doc-info-panel/DocInfoPanel.vue
+++ web/src/components/doc-info-panel/DocInfoPanel.vue
@@ -1,20 +1,59 @@
 <script lang="ts" setup>
 import { useDocUnitsStore } from "../../store"
+import { DocUnit } from "../../types/DocUnit"
 
 const store = useDocUnitsStore()
+
+const getValueOrFallback = (field: keyof DocUnit) => {
+  return store.getSelectedSafe()[field] ? store.getSelectedSafe()[field] : " - "
+}
 </script>
 
 <template>
-  <v-container v-if="store.hasSelected()">
+  <v-container v-if="store.hasSelected()" fluid class="panel-default">
     <v-row>
-      <v-col>
-        Rechtsprechung: {{ store.getSelected()?.id }}, Aktenzeichen:
-        {{ store.getSelected()?.aktenzeichen }}, Entscheidungsdatum:
-        {{ store.getSelected()?.entscheidungsdatum }}, Gerichtstyp:
-        {{ store.getSelected()?.gerichtstyp }}
+      <v-col cols="12">
+        <v-icon> gavel </v-icon>
+        <span>RECHTSPRECHUNG</span>
       </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="2" class="panel-id">
+        {{ getValueOrFallback("id") }}
+      </v-col>
+      <v-col cols="2">
+        Aktenzeichen
+        <span class="panel-default__values">{{
+          getValueOrFallback("aktenzeichen")
+        }}</span>
+      </v-col>
+      <v-col cols="2">
+        Entscheidungsdatum
+        <span class="panel-default__values">{{
+          getValueOrFallback("entscheidungsdatum")
+        }}</span>
+      </v-col>
+      <v-col cols="2">
+        Gerichtstyp
+        <span class="panel-default__values">{{
+          getValueOrFallback("gerichtstyp")
+        }}</span>
+      </v-col>
+      <v-col cols="4"> </v-col>
     </v-row>
   </v-container>
 </template>
 
-<style lang="scss"></style>
+<style lang="scss">
+.panel-default {
+  color: $gray900;
+
+  &__values {
+    font-weight: bold;
+  }
+}
+.panel-id {
+  color: $black;
+  font-size: xx-large;
+}
+</style>
