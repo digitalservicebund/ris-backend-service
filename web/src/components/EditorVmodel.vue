2a3e62bcb847055307c67b6697e54a11bb75a50e
Benjamin Degenhart <benjamin.degenhart@digitalservice.bund.de>
2022-06-19 17:28:06 +0200

    Prepare editor icons, not wired up yet
    RISDEV-105

--- web/src/components/EditorVmodel.vue
+++ web/src/components/EditorVmodel.vue
@@ -56,15 +56,41 @@ watch(
     editor.commands.setContent(value, false)
   }
 )
+
+const showButtons = () => {
+  return props.editable // && in focus
+}
 </script>
 
 <template>
-  <editor-content :editor="editor" :class="'ProseMirror__' + props.fieldSize" />
+  <v-container fluid>
+    <v-row v-if="showButtons()">
+      <v-col cols="1"><v-icon>format_bold</v-icon></v-col>
+      <v-col cols="1"><v-icon>format_italic</v-icon></v-col>
+      <v-col cols="1"><v-icon>format_underlined</v-icon></v-col>
+      <v-col cols="1"><v-icon>strikethrough_s</v-icon></v-col>
+      <v-col cols="1">Heading</v-col>
+      <v-col cols="1"><v-icon>list</v-icon></v-col>
+      <v-col cols="3" />
+      <v-col cols="1"><v-icon>open_in_full</v-icon></v-col>
+      <v-col cols="1">Vergrößern</v-col>
+    </v-row>
+    <v-row v-if="showButtons()"><v-col></v-col></v-row>
+    <v-divider v-if="showButtons()" color="black"></v-divider>
+    <v-row>
+      <v-col cols="12">
+        <editor-content
+          :editor="editor"
+          :class="'ProseMirror__' + props.fieldSize"
+        />
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <style lang="scss">
 .ProseMirror {
-  background: #eee;
+  // background: #eee;
   color: #000;
   padding: 0.75rem 1rem;
   border-radius: 0.5rem;
