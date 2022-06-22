ae7d73dda3b6a8b73c1af15acee3821f594c89eb
Klaus Hartl <klaus.hartl@gmail.com>
2022-06-22 11:06:38 +0200

    Adapt archunit test to use JUnit annotations
    This way running single tests from within IntelliJ are supported,
    despite having slightly more boilerplate. Writing tests though will be
    no different from all other unit or integration tests.

--- src/test/java/de/bund/digitalservice/ris/ArchitectureFitnessTest.java
+++ src/test/java/de/bund/digitalservice/ris/ArchitectureFitnessTest.java
@@ -1,15 +1,30 @@
 package de.bund.digitalservice.ris;
 
-import static com.tngtech.archunit.library.dependencies.SlicesRuleDefinition.slices;
-
-import com.tngtech.archunit.junit.AnalyzeClasses;
-import com.tngtech.archunit.junit.ArchTest;
+import com.tngtech.archunit.core.domain.JavaClasses;
+import com.tngtech.archunit.core.importer.ClassFileImporter;
 import com.tngtech.archunit.lang.ArchRule;
+import com.tngtech.archunit.library.dependencies.SlicesRuleDefinition;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 
-@AnalyzeClasses(packages = "de.bund.digitalservice")
 class ArchitectureFitnessTest {
 
-  @ArchTest
-  static final ArchRule preventPackageImportCycles =
-      slices().matching("de.bund.digitalservice.(**)").should().beFreeOfCycles();
+  static final String DOMAIN_LAYER_PACKAGES = "de.bund.digitalservice.ris.domain..";
+
+  static JavaClasses classes;
+
+  @BeforeAll
+  static void setUp() {
+    classes = new ClassFileImporter().importPackages("de.bund.digitalservice.ris");
+  }
+
+  @Test
+  void preventPackageImportCycles() {
+    ArchRule rule =
+        SlicesRuleDefinition.slices()
+            .matching("de.bund.digitalservice.(**)")
+            .should()
+            .beFreeOfCycles();
+    rule.check(classes);
+  }
 }
