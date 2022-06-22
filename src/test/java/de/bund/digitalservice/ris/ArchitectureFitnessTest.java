7722742219ae3df1cbe3fa6a063eceea859c389b
Klaus Hartl <klaus.hartl@gmail.com>
2022-06-22 11:56:00 +0200

    Add ArchUnit tests related to ports & adapters
    The disabled one concerning dependencies of the domain package needs to
    be made passing.

--- src/test/java/de/bund/digitalservice/ris/ArchitectureFitnessTest.java
+++ src/test/java/de/bund/digitalservice/ris/ArchitectureFitnessTest.java
@@ -2,29 +2,83 @@ package de.bund.digitalservice.ris;
 
 import com.tngtech.archunit.core.domain.JavaClasses;
 import com.tngtech.archunit.core.importer.ClassFileImporter;
+import com.tngtech.archunit.core.importer.ImportOption.Predefined;
 import com.tngtech.archunit.lang.ArchRule;
+import com.tngtech.archunit.lang.syntax.ArchRuleDefinition;
+import com.tngtech.archunit.library.dependencies.SliceRule;
 import com.tngtech.archunit.library.dependencies.SlicesRuleDefinition;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
 
 class ArchitectureFitnessTest {
 
+  static final String ADAPTER_LAYER_PACKAGES = "de.bund.digitalservice.ris.adapter..";
   static final String DOMAIN_LAYER_PACKAGES = "de.bund.digitalservice.ris.domain..";
 
   static JavaClasses classes;
 
   @BeforeAll
   static void setUp() {
-    classes = new ClassFileImporter().importPackages("de.bund.digitalservice.ris");
+    classes =
+        new ClassFileImporter()
+            .withImportOption(Predefined.DO_NOT_INCLUDE_TESTS)
+            .importPackages("de.bund.digitalservice.ris");
   }
 
   @Test
-  void preventPackageImportCycles() {
-    ArchRule rule =
+  void packagesShouldBeFreeOfCycles() {
+    SliceRule rule =
         SlicesRuleDefinition.slices()
             .matching("de.bund.digitalservice.(**)")
             .should()
             .beFreeOfCycles();
+    rule.check(classes);
+  }
+
+  @Test
+  @Disabled("Domain services need to be refactored to not depend on outside packages")
+  void domainClassesShouldOnlyDependOnDomainOrStdLibClasses() {
+    ArchRule rule =
+        ArchRuleDefinition.classes()
+            .that()
+            .resideInAPackage(DOMAIN_LAYER_PACKAGES)
+            .should()
+            .onlyDependOnClassesThat()
+            .resideInAnyPackage(DOMAIN_LAYER_PACKAGES, "java..");
+    rule.check(classes);
+  }
+
+  @Test
+  void controllerClassesShouldNotResideOutsideOfAdapterPackage() {
+    ArchRule rule =
+        ArchRuleDefinition.noClasses()
+            .that()
+            .areAnnotatedWith(Controller.class)
+            .or()
+            .areAnnotatedWith(RestController.class)
+            .should()
+            .resideOutsideOfPackage(ADAPTER_LAYER_PACKAGES);
+    rule.check(classes);
+  }
+
+  @Test
+  void controllerClassesShouldNotDependOnEachOther() {
+    ArchRule rule =
+        ArchRuleDefinition.noClasses()
+            .that()
+            .haveSimpleNameEndingWith("Controller")
+            .should()
+            .dependOnClassesThat()
+            .resideInAPackage(ADAPTER_LAYER_PACKAGES)
+            .andShould()
+            .dependOnClassesThat()
+            .areAnnotatedWith(Controller.class)
+            .andShould()
+            .dependOnClassesThat()
+            .areAnnotatedWith(RestController.class);
     rule.check(classes);
   }
 }
