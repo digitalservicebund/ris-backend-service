bad8aca7a666c241cd83b2cefd36f9384627660e
Klaus Hartl <klaus.hartl@gmail.com>
2022-06-23 11:41:28 +0200

    Remove no longer required security relax config
    Environments are now being protected via ingress, and thus we don't need
    Spring security for this in any way.

--- src/main/java/de/bund/digitalservice/ris/config/SecurityConfig.java
+++ src/main/java/de/bund/digitalservice/ris/config/SecurityConfig.java
@@ -1,6 +1,5 @@
 package de.bund.digitalservice.ris.config;
 
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
@@ -9,7 +8,6 @@ import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
 @EnableWebFluxSecurity
-@ConditionalOnProperty(name = "application.security.relax", havingValue = "true")
 public class SecurityConfig {
   @Bean
   public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
