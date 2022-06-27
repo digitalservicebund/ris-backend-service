f19b915c5b2ba67115c617b82af8bfc9042d8def
Benjamin Degenhart <benjamin.degenhart@digitalservice.bund.de>
2022-06-27 15:10:40 +0200

    Remove unused DocUnitRepository method

--- src/main/java/de/bund/digitalservice/ris/domain/DocUnitRepository.java
+++ src/main/java/de/bund/digitalservice/ris/domain/DocUnitRepository.java
@@ -1,13 +1,7 @@
 package de.bund.digitalservice.ris.domain;
 
-import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Component;
-import reactor.core.publisher.Flux;
 
 @Component
-public interface DocUnitRepository extends ReactiveCrudRepository<DocUnit, Long> {
-
-  @Query("select id, s3path, filetype from DOC_UNIT where filetype = $1")
-  Flux<DocUnit> findByFileType(String filetype);
-}
+public interface DocUnitRepository extends ReactiveCrudRepository<DocUnit, Long> {}
