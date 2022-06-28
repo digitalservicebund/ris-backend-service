486ba55b8aed38199923783c39bca00037754d29
Benjamin Degenhart <benjamin.degenhart@digitalservice.bund.de>
2022-06-28 14:43:02 +0200

    Ensure documentnumber to be unique
    RISDEV-42
    
    Co-Authored-By: rvp-c <104769777+rvp-c@users.noreply.github.com>

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS doc_unit (
     id Long AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    documentnumber VARCHAR(14) NOT NULL,
+    documentnumber VARCHAR(14) NOT NULL UNIQUE,
     creationtimestamp TIMESTAMP WITH TIME ZONE NOT NULL,
     fileuploadtimestamp TIMESTAMP WITH TIME ZONE,
     s3path VARCHAR(255),
