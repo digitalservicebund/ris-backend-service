b216103835bb1b49d84681e4d0528ce74f8078ee
Klaus Hartl <klaus.hartl@gmail.com>
2022-07-01 18:06:55 +0200

    Add missing postgres connection information
    These are less sensitive and are read from a Kubernetes ConfigMap.
    
    Addresses RISDEV-76

--- src/main/java/de/bund/digitalservice/ris/config/PostgresConfig.java
+++ src/main/java/de/bund/digitalservice/ris/config/PostgresConfig.java
@@ -10,4 +10,10 @@ public class PostgresConfig {
 
   @Value("${database.password:test}")
   private String password;
+
+  @Value("${database.host:localhost}")
+  private String host;
+
+  @Value("${database.database:neuris}")
+  private String database;
 }
