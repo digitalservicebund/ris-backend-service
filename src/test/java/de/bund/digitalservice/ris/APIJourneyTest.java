2ef76876dc75e8f01db82e80dd6cbac7eb219326
Klaus Hartl <klaus.hartl@gmail.com>
2022-07-07 17:40:14 +0200

    Add completed upload journey test back in

--- src/test/java/de/bund/digitalservice/ris/APIJourneyTest.java
+++ src/test/java/de/bund/digitalservice/ris/APIJourneyTest.java
@@ -1,15 +1,17 @@
 package de.bund.digitalservice.ris;
 
+import java.nio.file.Paths;
 import java.util.UUID;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.PathResource;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.web.reactive.function.BodyInserters;
 
 @ExtendWith(SpringExtension.class)
 @TestPropertySource(locations = "classpath:application.properties")
@@ -44,7 +46,6 @@ class APIJourneyTest {
   }
 
   @Test
-  @Disabled
   void docUnitFileUploadAPITest() {
     // Create documentation unit
     DocUnitResponse response =
@@ -69,7 +70,10 @@ class APIJourneyTest {
         .uri("/api/v1/docunits/{uuid}/file", response.uuid())
         .headers(headers -> headers.setBasicAuth(stagingUser, stagingPassword))
         .contentType(MediaType.APPLICATION_JSON)
-        .bodyValue("")
+        .body(
+            BodyInserters.fromResource(
+                new PathResource(
+                    Paths.get("src", "test", "resources", "fixtures", "attachment.docx"))))
         .exchange()
         .expectStatus()
         .isCreated();
