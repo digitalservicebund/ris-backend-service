2e35edca05f32f62fd822b0412d3f06e1d2a30bc
benjaminaaron <benjamin.degenhart@digitalservice.bund.de>
2022-07-10 17:39:59 +0200

    Add e2e test for storing formatted text input
    RISDEV-175

--- web/test/e2e/docunit-store-changes.spec.ts
+++ web/test/e2e/docunit-store-changes.spec.ts
@@ -20,18 +20,15 @@ test.describe("save changes and verify it persists", () => {
 
   test("save core data change", async () => {
     await navigateToRubriken(page, documentNumber)
+    // using the first field: Aktenzeichen
 
     await page.locator("id=aktenzeichen").fill("abc")
     await page.locator("button >> text=Speichern").first().click()
 
     await page.waitForTimeout(500) // give server time to process, otherwise this test gets flaky
-
-    await page.goto("/")
-    await page.reload() // to make sure the data needs to come fresh from the server
-    await page.goto("/")
+    await pageReload(page) // to make sure the data needs to come fresh from the server
 
     // 1. verify that the change is visible in docunit list on /rechtsprechung
-
     const aktenzeichenColumn = page
       .locator("tr", {
         has: page.locator(
@@ -39,16 +36,41 @@ test.describe("save changes and verify it persists", () => {
         ),
       })
       .locator("td:nth-child(3)")
-
     expect(await aktenzeichenColumn.innerText()).toBe("abc")
 
     // 2. verify that the change is visible in Rubriken
-
     await navigateToRubriken(page, documentNumber)
-
     expect(await page.inputValue("id=aktenzeichen")).toBe("abc")
   })
+
+  test("save formatted text input data change", async () => {
+    await navigateToRubriken(page, documentNumber)
+    // using the first field: Entscheidungsname
+
+    const boldButton = await page
+      .locator("id=entscheidungsname_btns >> div")
+      .first()
+    await boldButton.click()
+    let editorField = await page.locator("id=entscheidungsname_editor >> div")
+    await editorField.type("this is bold text")
+    await page.locator("button >> text=Speichern").last().click()
+
+    await pageReload(page)
+
+    // verify that the change is visible in Rubriken
+    await navigateToRubriken(page, documentNumber)
+    editorField = await page.locator("id=entscheidungsname_editor >> div")
+    expect(await editorField.innerHTML()).toBe(
+      "<p><strong>this is bold text</strong></p>"
+    )
+  })
 })
+
+export const pageReload = async (page: Page) => {
+  await page.goto("/")
+  await page.reload()
+  await page.goto("/")
+}
 
 export const navigateToRubriken = async (
   page: Page,
