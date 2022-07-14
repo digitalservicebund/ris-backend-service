2c80222a2b567e86a9bed40d5c9cf9e253d182a2
Hien Tran <ht@Hiens-MBP.fritz.box>
2022-07-14 09:05:18 +0200

    Refactor docunit-copy-paste-formatted RISDEV-175

--- web/test/e2e/docunit-copy-paste-formatted.spec.ts
+++ web/test/e2e/docunit-copy-paste-formatted.spec.ts
@@ -1,74 +1,64 @@
-import { test, Page, expect } from "@playwright/test"
-import { deleteDocUnit, generateDocUnit } from "./docunit-lifecycle.spec"
+import { test, expect } from "@playwright/test"
 import { uploadTestfile } from "./docunit-odoc-upload-delete.spec"
 import { pageReload } from "./docunit-store-changes.spec"
-import { getAuthenticatedPage } from "./e2e-utils"
+import { authenticate, generateDocUnit, deleteDocUnit } from "./e2e-utils"
 
+// copy-paste only works in chromium
+// see https://github.com/microsoft/playwright/issues/13037#issuecomment-1078208810
+// and https://github.com/microsoft/playwright/issues/8114#issue-964733495
 test.describe("test copy-pasting formatted content from odoc to KurzLangtexte-field", () => {
-  // SETUP
-
   let documentNumber: string
-  let page: Page
 
   test.beforeAll(async ({ browser, browserName }) => {
     if (browserName !== "chromium") return
 
-    page = await getAuthenticatedPage(browser)
+    authenticate(browser)
+  })
+
+  test("generate doc unit", async ({ page, browserName }) => {
+    if (browserName !== "chromium") return
 
     documentNumber = await generateDocUnit(page)
   })
 
-  test.afterAll(async ({ browserName }) => {
+  test.afterAll(async ({ page, browserName }) => {
     if (browserName !== "chromium") return
 
     await deleteDocUnit(page, documentNumber)
   })
 
-  // TESTS
-
-  test("copy-paste from odoc panel to Langtext-field", async ({
-    browserName,
-  }) => {
-    // copy-paste only works in chromium
-    // see https://github.com/microsoft/playwright/issues/13037#issuecomment-1078208810
-    // and https://github.com/microsoft/playwright/issues/8114#issue-964733495
+  test("copy-paste from odoc to text-field", async ({ page, browserName }) => {
     if (browserName !== "chromium") return
 
-    await page.goto("/")
+    await page.goto("/jurisdiction")
     await page
-      .locator(`a[href*="/rechtsprechung/${documentNumber}/dokumente"]`)
+      .locator(`a[href*="/jurisdiction/docunit/${documentNumber}/files"]`)
       .click()
-
     await uploadTestfile(page, "some-formatting.docx")
     await page.waitForSelector(
       ".fileviewer-info-panel-value >> text=some-formatting.docx"
     )
     await page.waitForSelector("text=Headline")
-
     await page
       .locator(`a[href*="/rechtsprechung/${documentNumber}/rubriken"]`)
       .first()
       .click()
+
+    await page.goto(`/jurisdiction/docunit/${documentNumber}/categories`)
     await page.locator("id=odoc-open-element").click()
 
     let editorField = await page.locator("id=gruende_editor >> div")
-
     const odocEditor = await page.locator("id=odoc_editor >> div")
+   
     let odocContent = await odocEditor.innerHTML()
-
-    await odocEditor.focus()
-    await odocEditor.selectText()
     const platform = await page.evaluate(() => navigator.platform)
     const modifier = platform.toLowerCase().includes("mac") ? "Meta" : "Control"
 
     await page.keyboard.press(`${modifier}+KeyC`)
     await editorField.click()
     await page.keyboard.press(`${modifier}+KeyV`)
-
     await page.locator("button >> text=Speichern").last().click()
-
     await pageReload(page)
-
     await page
       .locator(`a[href*="/rechtsprechung/${documentNumber}/rubriken"]`)
       .first()
@@ -76,6 +66,7 @@ test.describe("test copy-pasting formatted content from odoc to KurzLangtexte-fi
 
     editorField = await page.locator("id=gruende_editor >> div")
     let editorFieldContent = await editorField.innerHTML()
+
     // This removal of the first tag is necessary because the original docx has an empty <p> tag with a font size
     // defined. The editor however seems to be stripping away the font size within this empty <p> tag upon pasting.
     // This makes the two HTML strings not equal. However, for the purpose of this comparison, it is sufficient
