dd2125c5ce61bd907bc06fb984c4f398c8f0fa78
Leonie Koch <lk.leonie.koch@gmail.com>
2022-07-14 12:18:57 +0200

    Add on dragover tests to upload files
    RISDEV-175

--- web/test/e2e/docunit-upload-delete.spec.ts
+++ web/test/e2e/docunit-upload-delete.spec.ts
@@ -28,6 +28,48 @@ test.describe("upload an original document to a doc unit and delete it again", (
     await deleteDocUnit(page, documentNumber)
   })
 
+  test("drag over docx file in upload area", async ({ page }) => {
+    const documentNumber = await generateDocUnit(page)
+    const docx = await fs.promises.readFile(
+      "./test/e2e/testfiles/sample.docx",
+      "utf-8"
+    )
+    const dataTransfer = await page.evaluateHandle((docx) => {
+      const data = new DataTransfer()
+      const file = new File([`${docx}`], "sample.docx", {
+        type: "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+      })
+      data.items.add(file)
+      return data
+    }, docx)
+    await page.dispatchEvent(".upload-drop-area", "dragover", { dataTransfer })
+    await expect(
+      await page.locator("text=Datei in diesen Bereich ziehen")
+    ).toBeVisible()
+    await deleteDocUnit(page, documentNumber)
+  })
+
+  test("drag over non-docx file in upload area", async ({ page }) => {
+    const documentNumber = await generateDocUnit(page)
+    const png = await fs.promises.readFile(
+      "./test/e2e/testfiles/sample.png",
+      "utf-8"
+    )
+    const dataTransfer = await page.evaluateHandle((png) => {
+      const data = new DataTransfer()
+      const file = new File([`${png}`], "sample.png", {
+        type: "image/png",
+      })
+      data.items.add(file)
+      return data
+    }, png)
+    await page.dispatchEvent(".upload-drop-area", "dragover", { dataTransfer })
+    await expect(
+      await page.locator("text=Datei wird nicht unterstützt.")
+    ).toBeVisible()
+    await deleteDocUnit(page, documentNumber)
+  })
+
   test("drop docx file in upload area", async ({ page }) => {
     const documentNumber = await generateDocUnit(page)
     const docx = await fs.promises.readFile(
