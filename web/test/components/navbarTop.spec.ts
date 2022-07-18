49c662904ba257da61760f9b4027bc94a149dc8f
Hien Tran <me@hientran.dev>
2022-07-18 14:02:17 +0200

    Refactor navbarTop remove css selector
    RISDEV-175

--- web/test/components/navbarTop.spec.ts
+++ web/test/components/navbarTop.spec.ts
@@ -5,11 +5,8 @@ import * as components from "vuetify/components"
 import * as directives from "vuetify/directives"
 import NavbarTop from "../../src/components/NavbarTop.vue"
 
-const NAVBAR_TOP_CLASS_NAME = "topbar-main"
-const LOGO_CLASS_NAME = "topbar-logo-circle"
-const HEADER_TEXT_CLASS_NAME = "topbar-header"
+const HEADER_TEXT_SUB_TEXT = "Rechtsinformationssystem"
 const HEADER_TEXT = " Rechtsinformationssystem des Bundes [Platzhalter] "
-const NAVBAR_TOP_LINK_CLASS_NAME = "topbar-link-box"
 const NAVBAR_TOP_LINK_TEXT = "Rechtsprechung"
 const NAVBAR_TOP_LINK_URL = "/jurisdiction"
 
@@ -27,34 +24,15 @@ describe("navbar top", () => {
   })
 
   test("navbar top should be rendered without error", async () => {
-    const { container } = render(NavbarTop, {
+    render(NavbarTop, {
       props: {},
       global: { plugins: [vuetify, router] },
     })
 
-    expect(
-      container.getElementsByClassName(NAVBAR_TOP_CLASS_NAME).length
-    ).toEqual(1)
-
-    expect(container.getElementsByClassName(LOGO_CLASS_NAME).length).toEqual(1)
-
-    expect(
-      container.getElementsByClassName(HEADER_TEXT_CLASS_NAME).length
-    ).toBe(1)
-
-    const header_text = container.getElementsByClassName(
-      HEADER_TEXT_CLASS_NAME
-    )[0]
-    expect(header_text.textContent).toEqual(HEADER_TEXT)
-
-    const headerText = container.getElementsByClassName(
-      HEADER_TEXT_CLASS_NAME
-    )[0]
-    expect(headerText.textContent).toEqual(HEADER_TEXT)
-
-    expect(
-      container.getElementsByClassName(NAVBAR_TOP_LINK_CLASS_NAME).length
-    ).toBe(1)
+    const header_text = screen
+      .getByText(HEADER_TEXT_SUB_TEXT, { exact: false })
+      .closest("div")
+    expect(header_text?.textContent).toEqual(HEADER_TEXT)
 
     const navLink = screen.getByText(NAVBAR_TOP_LINK_TEXT).closest("a")
     expect(navLink?.href).contains(NAVBAR_TOP_LINK_URL)
