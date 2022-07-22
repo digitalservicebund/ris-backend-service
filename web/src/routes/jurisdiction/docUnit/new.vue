347e73235bf85dfd91ea928c42e4cfd60472349a
Florian <florian.zechmeister@digitalservice.bund.de>
2022-07-22 13:28:37 +0200

    Display status of docUnit creation

--- web/src/routes/jurisdiction/docUnit/new.vue
+++ web/src/routes/jurisdiction/docUnit/new.vue
@@ -1,12 +1,15 @@
 <script setup lang="ts">
-import { onBeforeMount } from "vue"
+import { onBeforeMount, ref } from "vue"
 import { useRouter } from "vue-router"
+import DocUnit from "@/domain/docUnit"
 import docUnitService from "@/services/docUnitService"
 
 const router = useRouter()
+const status = ref<string | DocUnit>("Dokumentationseinheit wird erstellt...")
 
 onBeforeMount(async () => {
   const newDocUnit = await docUnitService.createNew("KO", "RE")
+  status.value = newDocUnit
   await router.replace({
     name: "jurisdiction-docUnit-:documentNumber-files",
     params: { documentNumber: newDocUnit.documentnumber },
@@ -15,5 +18,5 @@ onBeforeMount(async () => {
 </script>
 
 <template>
-  <div></div>
+  <div>{{ status }}</div>
 </template>
