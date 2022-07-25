7db700e3c79a7e30fee85e5e9110de6598734d34
Klaus Hartl <klaus.hartl@gmail.com>
2022-07-25 14:52:26 +0200

    Add spotless setup for sql files

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,12 @@
-INSERT INTO document_number_counter (nextnumber, currentyear)
-SELECT 1, date_part('year', CURRENT_DATE)
-WHERE NOT EXISTS (SELECT id FROM document_number_counter);
+INSERT INTO
+  document_number_counter (nextnumber, currentyear)
+SELECT
+  1,
+  date_part('year', CURRENT_DATE)
+WHERE
+  NOT EXISTS (
+    SELECT
+      id
+    FROM
+      document_number_counter
+  );
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,35 +1,42 @@
-CREATE TABLE IF NOT EXISTS doc_unit (
+CREATE TABLE
+  IF NOT EXISTS doc_unit (
     id BIGSERIAL NOT NULL PRIMARY KEY,
     uuid uuid NOT NULL UNIQUE,
     documentnumber VARCHAR(14) NOT NULL UNIQUE,
-    creationtimestamp TIMESTAMP WITH TIME ZONE NOT NULL,
-    fileuploadtimestamp TIMESTAMP WITH TIME ZONE,
-    s3path VARCHAR(255),
-    filetype VARCHAR(30),
-    filename VARCHAR(255),
-    aktenzeichen VARCHAR(255),
-    gerichtstyp VARCHAR(255),
-    dokumenttyp VARCHAR(255),
-    vorgang VARCHAR(255),
-    ecli VARCHAR(255),
-    spruchkoerper VARCHAR(255),
-    entscheidungsdatum VARCHAR(255),
-    gerichtssitz VARCHAR(255),
-    rechtskraft VARCHAR(255),
-    eingangsart VARCHAR(255),
-    dokumentationsstelle VARCHAR(255),
-    region VARCHAR(255),
-    entscheidungsname TEXT,
-    titelzeile TEXT,
-    leitsatz TEXT,
-    orientierungssatz TEXT,
-    tenor TEXT,
-    gruende TEXT,
-    tatbestand TEXT,
-    entscheidungsgruende TEXT
-);
-CREATE TABLE IF NOT EXISTS document_number_counter (
+    creationtimestamp TIMESTAMP
+    WITH
+      TIME ZONE NOT NULL,
+      fileuploadtimestamp TIMESTAMP
+    WITH
+      TIME ZONE,
+      s3path VARCHAR(255),
+      filetype VARCHAR(30),
+      filename VARCHAR(255),
+      aktenzeichen VARCHAR(255),
+      gerichtstyp VARCHAR(255),
+      dokumenttyp VARCHAR(255),
+      vorgang VARCHAR(255),
+      ecli VARCHAR(255),
+      spruchkoerper VARCHAR(255),
+      entscheidungsdatum VARCHAR(255),
+      gerichtssitz VARCHAR(255),
+      rechtskraft VARCHAR(255),
+      eingangsart VARCHAR(255),
+      dokumentationsstelle VARCHAR(255),
+      region VARCHAR(255),
+      entscheidungsname TEXT,
+      titelzeile TEXT,
+      leitsatz TEXT,
+      orientierungssatz TEXT,
+      tenor TEXT,
+      gruende TEXT,
+      tatbestand TEXT,
+      entscheidungsgruende TEXT
+  );
+
+CREATE TABLE
+  IF NOT EXISTS document_number_counter (
     id SERIAL NOT NULL PRIMARY KEY,
     nextnumber INT NOT NULL,
     currentyear INT NOT NULL
-);
+  );
