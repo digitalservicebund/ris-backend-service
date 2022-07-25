e6c6394772843fe6e862506ba384e57775ca6fcd
Klaus Hartl <klaus.hartl@gmail.com>
2022-07-25 13:02:13 +0200

    Add postgres setup to security integration test
    If the number of tests needing this are growing we should implement an
    abstraction.

--- src/test/java/de/bund/digitalservice/ris/SecurityIntegrationTest.java
+++ src/test/java/de/bund/digitalservice/ris/SecurityIntegrationTest.java
@@ -4,7 +4,12 @@ import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest(
     webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
@@ -14,7 +19,20 @@ import org.springframework.test.web.reactive.server.WebTestClient;
       "local.file-storage=.local-storage"
     })
 @Tag("integration")
+@Testcontainers(disabledWithoutDocker = true)
 class SecurityIntegrationTest {
+
+  @Container
+  static PostgreSQLContainer postgreSQLContainer = new PostgreSQLContainer<>("postgres:12");
+
+  @DynamicPropertySource
+  static void registerDynamicProperties(DynamicPropertyRegistry registry) {
+    registry.add("database.user", () -> postgreSQLContainer.getUsername());
+    registry.add("database.password", () -> postgreSQLContainer.getPassword());
+    registry.add("database.host", () -> postgreSQLContainer.getHost());
+    registry.add("database.port", () -> postgreSQLContainer.getFirstMappedPort());
+    registry.add("database.database", () -> postgreSQLContainer.getDatabaseName());
+  }
 
   @Autowired WebTestClient webTestClient;
 
