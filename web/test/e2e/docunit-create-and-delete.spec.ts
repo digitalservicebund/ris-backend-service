81dfa0ccd4c18bc4bc4e8e9d97e25bee56567d18
Klaus Hartl <klaus.hartl@gmail.com>
2022-07-28 15:23:49 +0200

    Migrate create-and-delete E2E test for new fixture

--- web/test/e2e/docunit-create-and-delete.spec.ts
+++ web/test/e2e/docunit-create-and-delete.spec.ts
@@ -1,29 +1,35 @@
-import { test, expect } from "@playwright/test"
-import { generateDocUnit, deleteDocUnit } from "./e2e-utils"
+import { expect, test } from "@playwright/test"
+import { testWithDocUnit } from "./fixtures"
 
 test.describe("create a doc unit and delete it again", () => {
-  test("generate doc unit", async ({ page }) => {
-    const documentNumber = await generateDocUnit(page)
-    await deleteDocUnit(page, documentNumber)
+  test("create new doc unit", async ({ page, request }) => {
+    await page.goto("/")
+    await page.locator("button >> text=Neue Dokumentationseinheit").click()
+    await page.waitForSelector("text=Festplatte durchsuchen")
+    await expect(page).toHaveURL(
+      /\/jurisdiction\/docunit\/[A-Z0-9]{14}\/files$/
+    )
+
+    // Given the earlier expectation we can assume that the regex will match...
+    const documentNumber = (/jurisdiction\/docunit\/(.*)\/files/g.exec(
+      page.url()
+    ) || [])[1]
+    const response = await request.get("api/v1/docunits")
+    const units = await response.json()
+    await Promise.all(
+      units
+        .filter((unit) => unit.documentnumber === documentNumber)
+        .map((unit) => request.delete(`api/v1/docunits/${unit.uuid}`))
+    )
   })
 
-  test("delete doc unit", async ({ page }) => {
-    const documentNumber = await generateDocUnit(page)
+  test("delete doc unit", async ({ page, request }) => {
+    const response = await request.post("api/v1/docunits", {
+      data: { documentationCenterAbbreviation: "foo", documentType: "X" },
+    })
+    const { documentnumber: documentNumber } = await response.json()
+
     await page.goto("/")
-
-    await expect(
-      page.locator(`a[href*="/jurisdiction/docunit/${documentNumber}/files"]`)
-    ).toBeVisible()
-
-    await deleteDocUnit(page, documentNumber)
-    await expect(
-      page.locator(`a[href*="/jurisdiction/docunit/${documentNumber}/files"]`)
-    ).not.toBeVisible()
-  })
-  test("cancel delete doc unit", async ({ page }) => {
-    const documentNumber = await generateDocUnit(page)
-    await page.goto("/")
-
     await expect(
       page.locator(`a[href*="/jurisdiction/docunit/${documentNumber}/files"]`)
     ).toBeVisible()
@@ -33,9 +39,29 @@ test.describe("create a doc unit and delete it again", () => {
       })
       .locator("[aria-label='Dokumentationseinheit löschen']")
       .click()
-    await page.locator('button:has-text("Abbrechen")').click()
+    await page.locator('button:has-text("Löschen")').click()
     await expect(
       page.locator(`a[href*="/jurisdiction/docunit/${documentNumber}/files"]`)
-    ).toBeVisible()
+    ).not.toBeVisible()
   })
+
+  testWithDocUnit(
+    "cancel delete doc unit",
+    async ({ page, documentNumber }) => {
+      await page.goto("/")
+      await expect(
+        page.locator(`a[href*="/jurisdiction/docunit/${documentNumber}/files"]`)
+      ).toBeVisible()
+      await page
+        .locator("tr", {
+          hasText: documentNumber,
+        })
+        .locator("[aria-label='Dokumentationseinheit löschen']")
+        .click()
+      await page.locator('button:has-text("Abbrechen")').click()
+      await expect(
+        page.locator(`a[href*="/jurisdiction/docunit/${documentNumber}/files"]`)
+      ).toBeVisible()
+    }
+  )
 })
