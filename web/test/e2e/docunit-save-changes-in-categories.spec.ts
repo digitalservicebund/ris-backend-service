d4a996aa48b2988305cfca0f2118dc8e7c46ebb7
Leonie Koch <lk.leonie.koch@gmail.com>
2022-08-01 12:00:32 +0200

    Use custom fixture to speed up test
    RISDEV-178

--- web/test/e2e/docunit-save-changes-in-categories.spec.ts
+++ web/test/e2e/docunit-save-changes-in-categories.spec.ts
@@ -30,358 +30,152 @@ test.describe("save changes in core data and texts and verify it persists", () =
     expect(await page.inputValue("[aria-label='Aktenzeichen']")).toBe("abc")
   })
 
-  test("test bold text input", async ({ page, documentNumber }) => {
-    await navigateToCategories(page, documentNumber)
-
-    let editorField = await page.locator(
-      "[aria-label='Entscheidungsname Editor Feld'] >> div"
-    )
-
+  test("test bold text input", async ({ page, editorField }) => {
     await editorField.click()
 
     const boldButton = await page
       .locator("[aria-label='Entscheidungsname Editor Button Leiste'] >> div")
       .nth(2)
     await boldButton.click()
-
     await editorField.type("this is bold text")
-    await page
-      .locator("[aria-label='Kurz- und Langtexte Speichern Button']")
-      .click()
 
-    await page.goto("/")
-
-    // verify that the change is visible in the Kurz- und Langtexte field
-    await navigateToCategories(page, documentNumber)
-    editorField = await page.locator(
-      "[aria-label='Entscheidungsname Editor Feld'] >> div"
-    )
     expect(await editorField.innerHTML()).toBe(
-      "<p><strong>this is bold text</strong></p>"
+      "<p>this is text<strong>this is bold text</strong></p>"
     )
   })
 
-  test("test italic test input", async ({ page, documentNumber }) => {
-    await navigateToCategories(page, documentNumber)
-
-    let editorField = await page.locator(
-      "[aria-label='Entscheidungsname Editor Feld'] >> div"
-    )
-
+  test("test italic test input", async ({ page, editorField }) => {
     await editorField.click()
 
     const italicButton = await page
       .locator("[aria-label='Entscheidungsname Editor Button Leiste'] >> div")
       .nth(3)
     await italicButton.click()
-
     await editorField.type("this is italic text")
-    await page
-      .locator("[aria-label='Kurz- und Langtexte Speichern Button']")
-      .click()
 
-    await page.goto("/")
-
-    // verify that the change is visible in the Kurz- und Langtexte field
-    await navigateToCategories(page, documentNumber)
-    editorField = await page.locator(
-      "[aria-label='Entscheidungsname Editor Feld'] >> div"
-    )
     expect(await editorField.innerHTML()).toBe(
-      "<p><em>this is italic text</em></p>"
+      "<p>this is text<em>this is italic text</em></p>"
     )
   })
 
-  test("test underlined test input", async ({ page, documentNumber }) => {
-    await navigateToCategories(page, documentNumber)
-
-    let editorField = await page.locator(
-      "[aria-label='Entscheidungsname Editor Feld'] >> div"
-    )
-
+  test("test underlined test input", async ({ page, editorField }) => {
     await editorField.click()
 
     const underlineButton = await page
       .locator("[aria-label='Entscheidungsname Editor Button Leiste'] >> div")
       .nth(4)
     await underlineButton.click()
-
     await editorField.type("this is underlined text")
-    await page
-      .locator("[aria-label='Kurz- und Langtexte Speichern Button']")
-      .click()
 
-    await page.goto("/")
-
-    // verify that the change is visible in the Kurz- und Langtexte field
-    await navigateToCategories(page, documentNumber)
-    editorField = await page.locator(
-      "[aria-label='Entscheidungsname Editor Feld'] >> div"
-    )
     expect(await editorField.innerHTML()).toBe(
-      "<p><u>this is underlined text</u></p>"
+      "<p>this is text<u>this is underlined text</u></p>"
     )
   })
 
-  test("test strike test input", async ({ page, documentNumber }) => {
-    await navigateToCategories(page, documentNumber)
-
-    let editorField = await page.locator(
-      "[aria-label='Entscheidungsname Editor Feld'] >> div"
-    )
-
+  test("test strike test input", async ({ page, editorField }) => {
     await editorField.click()
 
     const strikeButton = await page
       .locator("[aria-label='Entscheidungsname Editor Button Leiste'] >> div")
       .nth(5)
     await strikeButton.click()
-
     await editorField.type("this is striked text")
-    await page
-      .locator("[aria-label='Kurz- und Langtexte Speichern Button']")
-      .click()
-
-    await page.goto("/")
-
-    // verify that the change is visible in the Kurz- und Langtexte field
-    await navigateToCategories(page, documentNumber)
-    editorField = await page.locator(
-      "[aria-label='Entscheidungsname Editor Feld'] >> div"
-    )
     expect(await editorField.innerHTML()).toBe(
-      "<p><s>this is striked text</s></p>"
+      "<p>this is text<s>this is striked text</s></p>"
     )
   })
 
-  test("test superscript test input", async ({ page, documentNumber }) => {
-    await navigateToCategories(page, documentNumber)
-
-    let editorField = await page.locator(
-      "[aria-label='Entscheidungsname Editor Feld'] >> div"
-    )
-
+  test("test superscript test input", async ({ page, editorField }) => {
     await editorField.click()
 
     const moreButton = await page.locator(
       "[aria-label='Entscheidungsname Editor Button Leiste'] >> div:has-text('more_horiz')"
     )
     await moreButton.click()
-    // There are 2 icons of superscript button
-    // The frist one shows only in screen-xl
-    // The second one shows when more button clicked
     const superScriptButton = await page.locator("text=superscript").nth(1)
     await superScriptButton.click()
 
     await editorField.type("this is superscript text")
-    await page
-      .locator("[aria-label='Kurz- und Langtexte Speichern Button']")
-      .click()
-
-    await page.goto("/")
-
-    // verify that the change is visible in the Kurz- und Langtexte field
-    await navigateToCategories(page, documentNumber)
-    editorField = await page.locator(
-      "[aria-label='Entscheidungsname Editor Feld'] >> div"
-    )
     expect(await editorField.innerHTML()).toBe(
-      "<p><sup>this is superscript text</sup></p>"
+      "<p>this is text<sup>this is superscript text</sup></p>"
     )
   })
 
-  test("test subscript test input", async ({ page, documentNumber }) => {
-    await navigateToCategories(page, documentNumber)
-
-    let editorField = await page.locator(
-      "[aria-label='Entscheidungsname Editor Feld'] >> div"
-    )
-
+  test("test subscript test input", async ({ page, editorField }) => {
     await editorField.click()
 
     const moreButton = await page.locator(
       "[aria-label='Entscheidungsname Editor Button Leiste'] >> div:has-text('more_horiz')"
     )
     await moreButton.click()
-    // There are 2 icons of subscript button
-    // The frist one shows only in screen-xl
-    // The second one shows when more button clicked
     const subScriptButton = await page.locator("text=subscript").nth(1)
     await subScriptButton.click()
-
     await editorField.type("this is subscript text")
-    await page
-      .locator("[aria-label='Kurz- und Langtexte Speichern Button']")
-      .click()
 
-    await page.goto("/")
-
-    // verify that the change is visible in the Kurz- und Langtexte field
-    await navigateToCategories(page, documentNumber)
-    editorField = await page.locator(
-      "[aria-label='Entscheidungsname Editor Feld'] >> div"
-    )
     expect(await editorField.innerHTML()).toBe(
-      "<p><sub>this is subscript text</sub></p>"
+      "<p>this is text<sub>this is subscript text</sub></p>"
     )
   })
-  test("test right alignment text input", async ({ page, documentNumber }) => {
-    await navigateToCategories(page, documentNumber)
-
-    let editorField = await page.locator(
-      "[aria-label='Entscheidungsname Editor Feld'] >> div"
-    )
-
-    await editorField.click()
-    await editorField.type("this text has right aligment")
-
+  test("test right alignment text input", async ({ page, editorField }) => {
     const dropdownShowAlignmentButton = await page
       .locator(
         "[aria-label='Entscheidungsname Editor Button Leiste'] >> div:has-text('format_align_leftarrow_drop_down')"
       )
       .nth(0)
     await dropdownShowAlignmentButton.click()
-    // There are 2 icons of subscript button
-    // The frist one shows only in dropdown
-    // The second one shows in screen-xl
     const rightAligmentButton = await page
       .locator("text=format_align_right")
       .nth(0)
     await rightAligmentButton.click()
-
-    await page
-      .locator("[aria-label='Kurz- und Langtexte Speichern Button']")
-      .click()
-
-    await page.goto("/")
-
-    // verify that the change is visible in the Kurz- und Langtexte field
-    await navigateToCategories(page, documentNumber)
-    editorField = await page.locator(
-      "[aria-label='Entscheidungsname Editor Feld'] >> div"
-    )
     expect(await editorField.innerHTML()).toBe(
-      '<p style="text-align: right">this text has right aligment</p>'
+      '<p style="text-align: right">this is text</p>'
     )
   })
-  test("test center alignment text input", async ({ page, documentNumber }) => {
-    await navigateToCategories(page, documentNumber)
 
-    let editorField = await page.locator(
-      "[aria-label='Entscheidungsname Editor Feld'] >> div"
-    )
-
-    await editorField.click()
-    await editorField.type("this text has center aligment")
-
+  test("test center alignment text input", async ({ page, editorField }) => {
     const dropdownShowAlignmentButton = await page
       .locator(
         "[aria-label='Entscheidungsname Editor Button Leiste'] >> div:has-text('format_align_leftarrow_drop_down')"
       )
       .nth(0)
     await dropdownShowAlignmentButton.click()
-    // There are 2 icons of subscript button
-    // The frist one shows only in dropdown
-    // The second one shows in screen-xl
     const centerAligmentButton = await page
       .locator("text=format_align_center")
       .nth(0)
     await centerAligmentButton.click()
 
-    await page
-      .locator("[aria-label='Kurz- und Langtexte Speichern Button']")
-      .click()
-
-    await page.goto("/")
-
-    // verify that the change is visible in the Kurz- und Langtexte field
-    await navigateToCategories(page, documentNumber)
-    editorField = await page.locator(
-      "[aria-label='Entscheidungsname Editor Feld'] >> div"
-    )
     expect(await editorField.innerHTML()).toBe(
-      '<p style="text-align: center">this text has center aligment</p>'
+      '<p style="text-align: center">this is text</p>'
     )
   })
-  test("test left alignment text input", async ({ page, documentNumber }) => {
-    await navigateToCategories(page, documentNumber)
-
-    let editorField = await page.locator(
-      "[aria-label='Entscheidungsname Editor Feld'] >> div"
-    )
-
-    await editorField.click()
-    await editorField.type("this text has left aligment")
-
+  test("test left alignment text input", async ({ page, editorField }) => {
     const dropdownShowAlignmentButton = await page
       .locator(
         "[aria-label='Entscheidungsname Editor Button Leiste'] >> div:has-text('format_align_leftarrow_drop_down')"
       )
       .nth(0)
     await dropdownShowAlignmentButton.click()
-    // There are 2 icons of subscript button
-    // The frist one shows only in dropdown
-    // The second one shows in screen-xl
     const leftAligmentButton = await page
       .locator("text=format_align_left")
       .nth(0)
     await leftAligmentButton.click()
 
-    await page
-      .locator("[aria-label='Kurz- und Langtexte Speichern Button']")
-      .click()
-
-    await page.goto("/")
-
-    // verify that the change is visible in the Kurz- und Langtexte field
-    await navigateToCategories(page, documentNumber)
-    editorField = await page.locator(
-      "[aria-label='Entscheidungsname Editor Feld'] >> div"
-    )
-    expect(await editorField.innerHTML()).toBe(
-      "<p>this text has left aligment</p>"
-    )
+    expect(await editorField.innerHTML()).toBe("<p>this is text</p>")
   })
-  test("test justify alignment text input", async ({
-    page,
-    documentNumber,
-  }) => {
-    await navigateToCategories(page, documentNumber)
-
-    let editorField = await page.locator(
-      "[aria-label='Entscheidungsname Editor Feld'] >> div"
-    )
-
-    await editorField.click()
-    await editorField.type("this text has justify aligment")
-
+  test("test justify alignment text input", async ({ page, editorField }) => {
     const dropdownShowAlignmentButton = await page
       .locator(
         "[aria-label='Entscheidungsname Editor Button Leiste'] >> div:has-text('format_align_leftarrow_drop_down')"
       )
       .nth(0)
     await dropdownShowAlignmentButton.click()
-    // There are 2 icons of subscript button
-    // The frist one shows only in dropdown
-    // The second one shows in screen-xl
     const justifyAligmentButton = await page
       .locator("text=format_align_justify")
       .nth(0)
     await justifyAligmentButton.click()
 
-    await page
-      .locator("[aria-label='Kurz- und Langtexte Speichern Button']")
-      .click()
-
-    await page.goto("/")
-
-    // verify that the change is visible in the Kurz- und Langtexte field
-    await navigateToCategories(page, documentNumber)
-    editorField = await page.locator(
-      "[aria-label='Entscheidungsname Editor Feld'] >> div"
-    )
     expect(await editorField.innerHTML()).toBe(
-      '<p style="text-align: justify">this text has justify aligment</p>'
+      '<p style="text-align: justify">this is text</p>'
     )
   })
 })
--- web/test/e2e/fixtures.ts
+++ web/test/e2e/fixtures.ts
@@ -1,6 +1,13 @@
 import { test } from "@playwright/test"
+import { navigateToCategories } from "./e2e-utils"
 
-export const testWithDocUnit = test.extend<{ documentNumber: string }>({
+// Declare the types of your fixtures.
+type MyFixtures = {
+  documentNumber: string
+  editorField
+}
+
+export const testWithDocUnit = test.extend<MyFixtures>({
   documentNumber: async ({ request }, use) => {
     const response = await request.post("api/v1/docunits", {
       data: { documentationCenterAbbreviation: "foo", documentType: "X" },
@@ -10,5 +17,16 @@ export const testWithDocUnit = test.extend<{ documentNumber: string }>({
     await use(documentNumber)
 
     await request.delete(`api/v1/docunits/${uuid}`)
+  },
+
+  editorField: async ({ page, documentNumber }, use) => {
+    await navigateToCategories(page, documentNumber)
+    const editorField = await page.locator(
+      "[aria-label='Entscheidungsname Editor Feld'] >> div"
+    )
+    await editorField.click()
+    await editorField.type("this is text")
+
+    await use(editorField)
   },
 })
