f6238f867ff7d71b0bbfdb1de211943609f71aec
Hien Tran <me@hientran.dev>
2022-08-17 14:53:33 +0200

    Repair component test NavbarSide

--- web/test/components/navbarSide.spec.ts
+++ web/test/components/navbarSide.spec.ts
@@ -7,31 +7,38 @@ import * as directives from "vuetify/directives"
 import NavbarSide from "../../src/components/NavbarSide.vue"
 
 const OPEN_SIDEBAR_BTN_TEXT = "Menü"
-const OPEN_SIDEBAR_TEXES = [
-  "Rubriken",
-  "Stammdaten",
-  "Kurz- & Langtexte",
-  "Rechtszug",
-  "Dokumente",
-  "Bearbeitungsstand",
-  "docx --> html",
-]
+
 describe("navbar side", async () => {
+  const DOCUMENT_NR = "KORE2022000003"
   const vuetify = createVuetify({ components, directives })
   const router = createRouter({
     history: createWebHistory(),
     routes: [
       {
-        path: "",
+        path: "/jurisdiction",
+        name: "jurisdiction",
+        component: {},
+      },
+      {
+        path: `/jurisdiction/docunit/${DOCUMENT_NR}/files`,
         name: "jurisdiction-docUnit-:documentNumber-files",
+        component: {},
+      },
+      {
+        path: `/jurisdiction/docunit/${DOCUMENT_NR}/categories`,
+        name: "jurisdiction-docUnit-:documentNumber-categories",
+        component: {},
+      },
+      {
+        path: `/jurisdiction/docunit/${DOCUMENT_NR}/publication`,
+        name: "jurisdiction-docUnit-:documentNumber-publication",
         component: {},
       },
     ],
   })
-
   test("navbar side is closed", async () => {
     const { emitted } = render(NavbarSide, {
-      props: { documentNumber: "KORE2022000003", visible: false },
+      props: { documentNumber: DOCUMENT_NR, visible: false },
       global: { plugins: [vuetify, router] },
     })
     const openSidebarBtn = screen.getByText(OPEN_SIDEBAR_BTN_TEXT)
@@ -41,17 +48,33 @@ describe("navbar side", async () => {
   })
 
   test("navbar side is opened", async () => {
-    render(NavbarSide, {
-      props: { documentNumber: "KORE2022000003", visible: false },
-      global: { plugins: [vuetify, router] },
+    const { getByText } = render(NavbarSide, {
+      props: { documentNumber: DOCUMENT_NR, visible: true },
+      global: {
+        plugins: [vuetify, router],
+      },
     })
-    const openSidebarBtn = screen.getByText(OPEN_SIDEBAR_BTN_TEXT)
-    await fireEvent.click(openSidebarBtn)
-    OPEN_SIDEBAR_TEXES.forEach(async (textToMatch) => {
-      const sidebarText = await screen.findByText(textToMatch, {
-        exact: false,
-      })
-      expect(sidebarText).toBeTruthy()
-    })
+    getByText("Rubriken", { exact: false })
+    getByText("Stammdaten", { exact: false })
+    getByText("Kurz- & Langtexte", { exact: false })
+    getByText("Rechtszug", { exact: false })
+    getByText("Dokumente", { exact: false })
+    getByText("Bearbeitungsstand", { exact: false })
+    getByText("Veröffentlichen", { exact: false })
+    expect(
+      screen.getByText("Rubriken").closest("a")?.getAttribute("href")
+    ).toEqual(`/jurisdiction/docunit/${DOCUMENT_NR}/categories`)
+    expect(
+      screen.getByText("Stammdaten").closest("a")?.getAttribute("href")
+    ).toEqual(`/jurisdiction/docunit/${DOCUMENT_NR}/categories#coreData`)
+    expect(
+      screen.getByText("Kurz- & Langtexte").closest("a")?.getAttribute("href")
+    ).toEqual(`/jurisdiction/docunit/${DOCUMENT_NR}/categories#texts`)
+    expect(
+      screen.getByText("Dokumente").closest("a")?.getAttribute("href")
+    ).toEqual(`/jurisdiction/docunit/${DOCUMENT_NR}/files`)
+    expect(
+      screen.getByText("Veröffentlichen").closest("a")?.getAttribute("href")
+    ).toEqual(`/jurisdiction/docunit/${DOCUMENT_NR}/publication`)
   })
 })
