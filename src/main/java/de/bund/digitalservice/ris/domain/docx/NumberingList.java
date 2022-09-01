2409c33ec5e89cf803ec1c8c3bbd35f7532740c3
Hien Tran <me@hientran.dev>
2022-09-01 22:47:03 +0200

    Fix talisha warning
    RISDEV-145

--- src/main/java/de/bund/digitalservice/ris/domain/docx/NumberingList.java
+++ src/main/java/de/bund/digitalservice/ris/domain/docx/NumberingList.java
@@ -179,8 +179,11 @@ public class NumberingList implements DocUnitDocx {
   }
 
   private String intToLatin(int number) {
-    String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZÄÖÜß";
-    return number >= alphabet.length() ? "A" : String.valueOf(alphabet.charAt(number));
+    String[] alphabet = {
+      "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S",
+      "T", "U", "V", "W", "X", "Y", "Z", "Ä", "Ö", "Ü", "ß"
+    };
+    return alphabet[number % alphabet.length];
   }
 
   private String getBulletHexCode(String rFont) {
