0515bfd0e4f77ab1e54ceb58ffc93cb1d97d2368
Florian <florian.zechmeister@digitalservice.bund.de>
2022-09-02 20:22:18 +0200

    Remove more fixed timeouts
    Addresses RISDEV-322

--- web/test/e2e/docunit-save-behavior.spec.ts
+++ web/test/e2e/docunit-save-behavior.spec.ts
@@ -2,93 +2,69 @@ import { expect } from "@playwright/test"
 import { navigateToCategories } from "./e2e-utils"
 import { testWithDocUnit as test } from "./fixtures"
 
-test.beforeEach(async ({ page, documentNumber }) => {
-  await navigateToCategories(page, documentNumber)
-})
+test.describe("autosave on documentation units", () => {
+  test("test save button status change", async ({ page, documentNumber }) => {
+    navigateToCategories(page, documentNumber)
+    await page.locator("[aria-label='Stammdaten Speichern Button']").click()
+    await expect(
+      page.locator("text=Daten werden gespeichert").nth(0)
+    ).toBeVisible()
+    await expect(
+      page.locator("text=Daten werden gespeichert").nth(1)
+    ).toBeVisible()
 
-test("test save button status change", async ({ page }) => {
-  await page.locator("[aria-label='Stammdaten Speichern Button']").click()
-  let coreDataSaveButtonStatus = await page
-    .locator("text='Daten werden gespeichert'")
-    .nth(0)
-  let textSaveButtonStatus = await page
-    .locator("text='Daten werden gespeichert'")
-    .nth(1)
-  expect(coreDataSaveButtonStatus).toBeVisible()
-  expect(textSaveButtonStatus).toBeVisible()
-
-  await page.waitForTimeout(1000)
-  coreDataSaveButtonStatus = await page
-    .locator("text='Zuletzt gespeichert um'")
-    .nth(0)
-  textSaveButtonStatus = await page
-    .locator("text='Zuletzt gespeichert um'")
-    .nth(1)
-  const regEx = /^Zuletzt gespeichert um.*Uhr$/
-  expect(coreDataSaveButtonStatus).toBeVisible()
-  expect(textSaveButtonStatus).toBeVisible()
-  expect((await coreDataSaveButtonStatus.innerText()).match(regEx)?.index).toBe(
-    0
-  )
-  expect((await textSaveButtonStatus.innerText()).match(regEx)?.index).toBe(0)
-})
-
-test("test could not update docunit", async ({ page }) => {
-  await page.route("**/api/v1/docunits/*/docx", async (route) => {
-    route.fulfill({
-      status: 400,
-      contentType: "text/plain",
-      body: "Not Found!",
-    })
+    await expect(
+      page.locator("text=Zuletzt gespeichert um").nth(0)
+    ).toBeVisible()
+    await expect(
+      page.locator("text=Zuletzt gespeichert um").nth(1)
+    ).toBeVisible()
   })
-  await page.locator("[aria-label='Stammdaten Speichern Button']").click()
-  let coreDataSaveButtonStatus = await page
-    .locator("text='Daten werden gespeichert'")
-    .nth(0)
-  let textSaveButtonStatus = await page
-    .locator("text='Daten werden gespeichert'")
-    .nth(1)
-  expect(coreDataSaveButtonStatus).toBeVisible()
-  expect(textSaveButtonStatus).toBeVisible()
 
-  await page.waitForTimeout(1000)
-  coreDataSaveButtonStatus = await page
-    .locator("text='Fehler beim Speichern'")
-    .nth(0)
-  textSaveButtonStatus = await page
-    .locator("text='Fehler beim Speichern'")
-    .nth(1)
+  test("test could not update docunit", async ({ page, documentNumber }) => {
+    await page.route("**/api/v1/docunits/*/docx", async (route) => {
+      route.fulfill({
+        status: 400,
+        contentType: "text/plain",
+        body: "Not Found!",
+      })
+    })
+    navigateToCategories(page, documentNumber)
 
-  expect(coreDataSaveButtonStatus).toBeVisible()
-  expect(textSaveButtonStatus).toBeVisible()
-})
+    await page.locator("[aria-label='Stammdaten Speichern Button']").click()
+    await expect(
+      page.locator("text='Daten werden gespeichert'").nth(0)
+    ).toBeVisible()
+    await expect(
+      page.locator("text='Daten werden gespeichert'").nth(1)
+    ).toBeVisible()
 
-test("test automatic save docunit", async ({ page, editorField }) => {
-  test.setTimeout(50000)
-  await editorField.click()
-  await editorField.type("this is a change")
-  await page.waitForTimeout(30000)
-  let coreDataSaveButtonStatus = await page
-    .locator("text='Daten werden gespeichert'")
-    .nth(0)
-  let textSaveButtonStatus = await page
-    .locator("text='Daten werden gespeichert'")
-    .nth(1)
-  expect(coreDataSaveButtonStatus).toBeVisible()
-  expect(textSaveButtonStatus).toBeVisible()
+    await expect(
+      page.locator("text='Fehler beim Speichern'").nth(0)
+    ).toBeVisible()
+    await expect(
+      page.locator("text='Fehler beim Speichern'").nth(1)
+    ).toBeVisible()
+  })
 
-  await page.waitForTimeout(1000)
-  coreDataSaveButtonStatus = await page
-    .locator("text='Zuletzt gespeichert um'")
-    .nth(0)
-  textSaveButtonStatus = await page
-    .locator("text='Zuletzt gespeichert um'")
-    .nth(1)
-  const regEx = /^Zuletzt gespeichert um.*Uhr$/
-  expect(coreDataSaveButtonStatus).toBeVisible()
-  expect(textSaveButtonStatus).toBeVisible()
-  expect((await coreDataSaveButtonStatus.innerText()).match(regEx)?.index).toBe(
-    0
-  )
-  expect((await textSaveButtonStatus.innerText()).match(regEx)?.index).toBe(0)
+  test("test automatic save docunit", async ({ page, editorField }) => {
+    test.setTimeout(50 * 1000) // autosave is supposed to happen every 30s
+    await editorField.click()
+    await editorField.type("this is a change")
+    await page.keyboard.down("Tab")
+
+    await expect(
+      page.locator("text=Daten werden gespeichert").nth(0)
+    ).toBeVisible({ timeout: 31 * 1000 }) // autosave is supposed to happen every 30s
+    await expect(
+      page.locator("text=Daten werden gespeichert").nth(1)
+    ).toBeVisible({ timeout: 31 * 1000 }) // autosave is supposed to happen every 30s
+
+    await expect(
+      page.locator("text=Zuletzt gespeichert um").nth(0)
+    ).toBeVisible()
+    await expect(
+      page.locator("text=Zuletzt gespeichert um").nth(1)
+    ).toBeVisible()
+  })
 })
