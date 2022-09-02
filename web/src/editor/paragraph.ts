51b10a1caa5937c8c38e5382ec24c15e52a2a2c3
Hien Tran <me@hientran.dev>
2022-09-02 13:40:43 +0200

    Dedup style in list element when coppy
    RISDEV-145

--- web/src/editor/paragraph.ts
+++ web/src/editor/paragraph.ts
@@ -5,6 +5,48 @@ export const CustomParagraph = Paragraph.extend({
     return {
       ...this.parent?.(),
       class: { default: null },
+      listIndexStyle: {
+        default: null,
+        parseHTML: (element) => element.getAttribute("style"),
+        renderHTML: (attributes) => {
+          if (attributes.style === null) {
+            return {}
+          }
+          const listIndexStyle = [
+            ...new Set(
+              JSON.stringify(attributes.listIndexStyle)
+                .replace(/"/g, "")
+                .split(";")
+                .map((s) => s.trim())
+                .filter(
+                  (s) =>
+                    s.length > 0 &&
+                    (s.includes("display") || s.includes("text-align"))
+                )
+            ),
+          ]
+          const display = listIndexStyle.filter((style) =>
+            style.includes("display")
+          )
+          const displayStyle =
+            display.length > 0 ? display[display.length - 1] + ";" : ""
+          const textAlign = listIndexStyle.filter((style) =>
+            style.includes("text-align")
+          )
+          const textAlignStyle =
+            !JSON.stringify(attributes).includes("textAlign") &&
+            textAlign.length > 0
+              ? textAlign[textAlign.length - 1] + ";"
+              : ""
+          const indexStyle = displayStyle + textAlignStyle
+          if (indexStyle.length > 0) {
+            return {
+              style: indexStyle,
+            }
+          }
+          return {}
+        },
+      },
     }
   },
 })
