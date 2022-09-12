807e74dd37a769400ba77b7abdc46701884a7b0f
benjaminaaron <benjamin.degenhart@digitalservice.bund.de>
2022-09-12 11:52:58 +0200

    Use sentry for collecting issues
    RISDEV-180

--- src/main/java/de/bund/digitalservice/ris/Application.java
+++ src/main/java/de/bund/digitalservice/ris/Application.java
@@ -1,5 +1,7 @@
 package de.bund.digitalservice.ris;
 
+import io.sentry.Sentry;
+import java.util.Date;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,5 +11,10 @@ public class Application {
   @Generated
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
+    try {
+      throw new Exception("This is a test-exception for Sentry thrown at " + new Date());
+    } catch (Exception e) {
+      Sentry.captureException(e);
+    }
   }
 }
