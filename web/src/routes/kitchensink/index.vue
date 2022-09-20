e7ccd1ae40e2f2a6795b5c553f4b5695ea2b2b0c
Hien Tran <me@hientran.dev>
2022-09-20 17:36:12 +0200

    Add kitchensink page

---
+++ web/src/routes/kitchensink/index.vue
@@ -0,0 +1,221 @@
+<script lang="ts" setup>
+import TextButton from "@/components/TextButton.vue"
+</script>
+
+<template>
+  <div
+    id="kitchensink"
+    class="absolute bg-white box-border h-full left-0 overflow-y-scroll top-0 w-full"
+  >
+    <!-- Header -->
+    <div
+      class="border-b-4 flex flex-nowrap flex-row items-center justify-center w-full"
+    >
+      <div class="flex flex-col flex-nowrap gap-y-10 justify-start py-10 w-2/3">
+        <!-- Title -->
+        <h1 class="font-bold text-64">Kitchensink</h1>
+        <!-- Navbar -->
+        <div
+          class="flex flex-nowrap flex-row gap-x-8 justify-start navbar overflow-x-scroll overflow-y-hidden"
+        >
+          <TextButton button-type="tertiary" label="Index" />
+          <TextButton button-type="tertiary" label="Buttons" />
+          <TextButton button-type="tertiary" label="Form" />
+          <TextButton button-type="tertiary" label="Spinner" />
+          <TextButton button-type="tertiary" label="Help" />
+          <TextButton button-type="tertiary" label="Icons" />
+          <TextButton button-type="tertiary" label="Email Status" />
+        </div>
+      </div>
+    </div>
+    <!-- Container -->
+    <div
+      class="border-b-4 flex flex-nowrap flex-row items-center justify-center overflow-hidden relative w-full"
+    >
+      <div
+        class="flex flex-col flex-nowrap gap-y-10 justify-start py-10 text-justify w-2/3"
+      >
+        <!-- Title -->
+        <h1 class="font-bold text-32">Component</h1>
+        <!-- Content -->
+        <div class="content-container flex flex-col">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem
+          iusto autem quasi saepe fuga quis dolores dolor ipsum eligendi dolore
+          aliquid, magni quos excepturi amet, odit ipsa, provident eveniet
+          accusamus. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Sequi, magni culpa dolorem saepe explicabo numquam officia incidunt
+          quis corporis assumenda. Sit maxime consequatur dignissimos aperiam
+          consectetur, dolorum natus fuga numquam. Lorem ipsum dolor sit amet,
+          consectetur adipisicing elit. Porro officiis, reiciendis quisquam
+          voluptatibus sint aperiam atque. Pariatur dolorem error, porro modi
+          maiores dolore dolores animi distinctio sint nesciunt vel eius. Lorem
+          ipsum dolor sit, amet consectetur adipisicing elit. Cupiditate odio
+          placeat sapiente tempora in a voluptas laudantium possimus nisi unde
+          veniam labore, nemo quo. Nisi repudiandae id non eum debitis. Lorem
+          ipsum dolor sit amet consectetur adipisicing elit. Voluptatem iusto
+          autem quasi saepe fuga quis dolores dolor ipsum eligendi dolore
+          aliquid, magni quos excepturi amet, odit ipsa, provident eveniet
+          accusamus. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Sequi, magni culpa dolorem saepe explicabo numquam officia incidunt
+          quis corporis assumenda. Sit maxime consequatur dignissimos aperiam
+          consectetur, dolorum natus fuga numquam. Lorem ipsum dolor sit amet,
+          consectetur adipisicing elit. Porro officiis, reiciendis quisquam
+          voluptatibus sint aperiam atque. Pariatur dolorem error, porro modi
+          maiores dolore dolores animi distinctio sint nesciunt vel eius. Lorem
+          ipsum dolor sit, amet consectetur adipisicing elit. Cupiditate odio
+          placeat sapiente tempora in a voluptas laudantium possimus nisi unde
+          veniam labore, nemo quo. Nisi repudiandae id non eum debitis. Lorem
+          ipsum dolor sit amet consectetur adipisicing elit. Voluptatem iusto
+          autem quasi saepe fuga quis dolores dolor ipsum eligendi dolore
+          aliquid, magni quos excepturi amet, odit ipsa, provident eveniet
+          accusamus. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Sequi, magni culpa dolorem saepe explicabo numquam officia incidunt
+          quis corporis assumenda. Sit maxime consequatur dignissimos aperiam
+          consectetur, dolorum natus fuga numquam. Lorem ipsum dolor sit amet,
+          consectetur adipisicing elit. Porro officiis, reiciendis quisquam
+          voluptatibus sint aperiam atque. Pariatur dolorem error, porro modi
+          maiores dolore dolores animi distinctio sint nesciunt vel eius. Lorem
+          ipsum dolor sit, amet consectetur adipisicing elit. Cupiditate odio
+          placeat sapiente tempora in a voluptas laudantium possimus nisi unde
+          veniam labore, nemo quo. Nisi repudiandae id non eum debitis. Lorem
+          ipsum dolor sit amet consectetur adipisicing elit. Voluptatem iusto
+          autem quasi saepe fuga quis dolores dolor ipsum eligendi dolore
+          aliquid, magni quos excepturi amet, odit ipsa, provident eveniet
+          accusamus. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Sequi, magni culpa dolorem saepe explicabo numquam officia incidunt
+          quis corporis assumenda. Sit maxime consequatur dignissimos aperiam
+          consectetur, dolorum natus fuga numquam. Lorem ipsum dolor sit amet,
+          consectetur adipisicing elit. Porro officiis, reiciendis quisquam
+          voluptatibus sint aperiam atque. Pariatur dolorem error, porro modi
+          maiores dolore dolores animi distinctio sint nesciunt vel eius. Lorem
+          ipsum dolor sit, amet consectetur adipisicing elit. Cupiditate odio
+          placeat sapiente tempora in a voluptas laudantium possimus nisi unde
+          veniam labore, nemo quo. Nisi repudiandae id non eum debitis. Lorem
+          ipsum dolor sit amet consectetur adipisicing elit. Voluptatem iusto
+          autem quasi saepe fuga quis dolores dolor ipsum eligendi dolore
+          aliquid, magni quos excepturi amet, odit ipsa, provident eveniet
+          accusamus. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Sequi, magni culpa dolorem saepe explicabo numquam officia incidunt
+          quis corporis assumenda. Sit maxime consequatur dignissimos aperiam
+          consectetur, dolorum natus fuga numquam. Lorem ipsum dolor sit amet,
+          consectetur adipisicing elit. Porro officiis, reiciendis quisquam
+          voluptatibus sint aperiam atque. Pariatur dolorem error, porro modi
+          maiores dolore dolores animi distinctio sint nesciunt vel eius. Lorem
+          ipsum dolor sit, amet consectetur adipisicing elit. Cupiditate odio
+          placeat sapiente tempora in a voluptas laudantium possimus nisi unde
+          veniam labore, nemo quo. Nisi repudiandae id non eum debitis. Lorem
+          ipsum dolor sit amet consectetur adipisicing elit. Voluptatem iusto
+          autem quasi saepe fuga quis dolores dolor ipsum eligendi dolore
+          aliquid, magni quos excepturi amet, odit ipsa, provident eveniet
+          accusamus. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Sequi, magni culpa dolorem saepe explicabo numquam officia incidunt
+          quis corporis assumenda. Sit maxime consequatur dignissimos aperiam
+          consectetur, dolorum natus fuga numquam. Lorem ipsum dolor sit amet,
+          consectetur adipisicing elit. Porro officiis, reiciendis quisquam
+          voluptatibus sint aperiam atque. Pariatur dolorem error, porro modi
+          maiores dolore dolores animi distinctio sint nesciunt vel eius. Lorem
+          ipsum dolor sit, amet consectetur adipisicing elit. Cupiditate odio
+          placeat sapiente tempora in a voluptas laudantium possimus nisi unde
+          veniam labore, nemo quo. Nisi repudiandae id non eum debitis. Lorem
+          ipsum dolor sit amet consectetur adipisicing elit. Voluptatem iusto
+          autem quasi saepe fuga quis dolores dolor ipsum eligendi dolore
+          aliquid, magni quos excepturi amet, odit ipsa, provident eveniet
+          accusamus. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Sequi, magni culpa dolorem saepe explicabo numquam officia incidunt
+          quis corporis assumenda. Sit maxime consequatur dignissimos aperiam
+          consectetur, dolorum natus fuga numquam. Lorem ipsum dolor sit amet,
+          consectetur adipisicing elit. Porro officiis, reiciendis quisquam
+          voluptatibus sint aperiam atque. Pariatur dolorem error, porro modi
+          maiores dolore dolores animi distinctio sint nesciunt vel eius. Lorem
+          ipsum dolor sit, amet consectetur adipisicing elit. Cupiditate odio
+          placeat sapiente tempora in a voluptas laudantium possimus nisi unde
+          veniam labore, nemo quo. Nisi repudiandae id non eum debitis. Lorem
+          ipsum dolor sit amet consectetur adipisicing elit. Voluptatem iusto
+          autem quasi saepe fuga quis dolores dolor ipsum eligendi dolore
+          aliquid, magni quos excepturi amet, odit ipsa, provident eveniet
+          accusamus. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Sequi, magni culpa dolorem saepe explicabo numquam officia incidunt
+          quis corporis assumenda. Sit maxime consequatur dignissimos aperiam
+          consectetur, dolorum natus fuga numquam. Lorem ipsum dolor sit amet,
+          consectetur adipisicing elit. Porro officiis, reiciendis quisquam
+          voluptatibus sint aperiam atque. Pariatur dolorem error, porro modi
+          maiores dolore dolores animi distinctio sint nesciunt vel eius. Lorem
+          ipsum dolor sit, amet consectetur adipisicing elit. Cupiditate odio
+          placeat sapiente tempora in a voluptas laudantium possimus nisi unde
+          veniam labore, nemo quo. Nisi repudiandae id non eum debitis. Lorem
+          ipsum dolor sit amet consectetur adipisicing elit. Voluptatem iusto
+          autem quasi saepe fuga quis dolores dolor ipsum eligendi dolore
+          aliquid, magni quos excepturi amet, odit ipsa, provident eveniet
+          accusamus. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Sequi, magni culpa dolorem saepe explicabo numquam officia incidunt
+          quis corporis assumenda. Sit maxime consequatur dignissimos aperiam
+          consectetur, dolorum natus fuga numquam. Lorem ipsum dolor sit amet,
+          consectetur adipisicing elit. Porro officiis, reiciendis quisquam
+          voluptatibus sint aperiam atque. Pariatur dolorem error, porro modi
+          maiores dolore dolores animi distinctio sint nesciunt vel eius. Lorem
+          ipsum dolor sit, amet consectetur adipisicing elit. Cupiditate odio
+          placeat sapiente tempora in a voluptas laudantium possimus nisi unde
+          veniam labore, nemo quo. Nisi repudiandae id non eum debitis. Lorem
+          ipsum dolor sit amet consectetur adipisicing elit. Voluptatem iusto
+          autem quasi saepe fuga quis dolores dolor ipsum eligendi dolore
+          aliquid, magni quos excepturi amet, odit ipsa, provident eveniet
+          accusamus. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Sequi, magni culpa dolorem saepe explicabo numquam officia incidunt
+          quis corporis assumenda. Sit maxime consequatur dignissimos aperiam
+          consectetur, dolorum natus fuga numquam. Lorem ipsum dolor sit amet,
+          consectetur adipisicing elit. Porro officiis, reiciendis quisquam
+          voluptatibus sint aperiam atque. Pariatur dolorem error, porro modi
+          maiores dolore dolores animi distinctio sint nesciunt vel eius. Lorem
+          ipsum dolor sit, amet consectetur adipisicing elit. Cupiditate odio
+          placeat sapiente tempora in a voluptas laudantium possimus nisi unde
+          veniam labore, nemo quo. Nisi repudiandae id non eum debitis. Lorem
+          ipsum dolor sit amet consectetur adipisicing elit. Voluptatem iusto
+          autem quasi saepe fuga quis dolores dolor ipsum eligendi dolore
+          aliquid, magni quos excepturi amet, odit ipsa, provident eveniet
+          accusamus. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Sequi, magni culpa dolorem saepe explicabo numquam officia incidunt
+          quis corporis assumenda. Sit maxime consequatur dignissimos aperiam
+          consectetur, dolorum natus fuga numquam. Lorem ipsum dolor sit amet,
+          consectetur adipisicing elit. Porro officiis, reiciendis quisquam
+          voluptatibus sint aperiam atque. Pariatur dolorem error, porro modi
+          maiores dolore dolores animi distinctio sint nesciunt vel eius. Lorem
+          ipsum dolor sit, amet consectetur adipisicing elit. Cupiditate odio
+          placeat sapiente tempora in a voluptas laudantium possimus nisi unde
+          veniam labore, nemo quo. Nisi repudiandae id non eum debitis. Lorem
+          ipsum dolor sit amet consectetur adipisicing elit. Voluptatem iusto
+          autem quasi saepe fuga quis dolores dolor ipsum eligendi dolore
+          aliquid, magni quos excepturi amet, odit ipsa, provident eveniet
+          accusamus. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Sequi, magni culpa dolorem saepe explicabo numquam officia incidunt
+          quis corporis assumenda. Sit maxime consequatur dignissimos aperiam
+          consectetur, dolorum natus fuga numquam. Lorem ipsum dolor sit amet,
+          consectetur adipisicing elit. Porro officiis, reiciendis quisquam
+          voluptatibus sint aperiam atque. Pariatur dolorem error, porro modi
+          maiores dolore dolores animi distinctio sint nesciunt vel eius. Lorem
+          ipsum dolor sit, amet consectetur adipisicing elit. Cupiditate odio
+          placeat sapiente tempora in a voluptas laudantium possimus nisi unde
+          veniam labore, nemo quo. Nisi repudiandae id non eum debitis. Lorem
+          ipsum dolor sit amet consectetur adipisicing elit. Voluptatem iusto
+          autem quasi saepe fuga quis dolores dolor ipsum eligendi dolore
+          aliquid, magni quos excepturi amet, odit ipsa, provident eveniet
+          accusamus. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Sequi, magni culpa dolorem saepe explicabo numquam officia incidunt
+          quis corporis assumenda. Sit maxime consequatur dignissimos aperiam
+          consectetur, dolorum natus fuga numquam. Lorem ipsum dolor sit amet,
+          consectetur adipisicing elit. Porro officiis, reiciendis quisquam
+          voluptatibus sint aperiam atque. Pariatur dolorem error, porro modi
+          maiores dolore dolores animi distinctio sint nesciunt vel eius. Lorem
+          ipsum dolor sit, amet consectetur adipisicing elit. Cupiditate odio
+          placeat sapiente tempora in a voluptas laudantium possimus nisi unde
+          veniam labore, nemo quo. Nisi repudiandae id non eum debitis.
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+* {
+  scrollbar-width: none;
+}
+</style>
