c5cd8366d77970d5a1e1bd5017120496be875587
Hien Tran <me@hientran.dev>
2022-09-29 14:56:42 +0200

    Add slot for list index if user doesnt coppy it
    RISDEV-431
    
    A list item has an index and a content text, both components should be display as table cell, to make sure the size of all indexes equally, so if user does not coppy a index of list item, browser will change the size of last idex item to equal its above content. So adding a empty slot for missing index to avoid this situation

--- web/src/editor/listItem.ts
+++ web/src/editor/listItem.ts
@@ -1,16 +1,35 @@
 import { ListItem } from "@tiptap/extension-list-item"
 
+const addListIndex = (element: HTMLLIElement) => {
+  const isFristChildP =
+    !!element.firstChild &&
+    (element.firstChild as HTMLElement).tagName.toLowerCase() === "p"
+  const isSecondChildP =
+    !!element.childNodes[1] &&
+    (element.childNodes[1] as HTMLElement).tagName.toLowerCase() === "p"
+
+  if (isFristChildP && !isSecondChildP) {
+    const index = document.createElement("p")
+    index.style.display = "table-cell"
+    index.textContent = ""
+    element.insertBefore(index, element.firstChild)
+  }
+}
+
 export const CustomListItem = ListItem.extend({
   addAttributes() {
     return {
       ...this.parent?.(),
       style: {
         default: null,
-        parseHTML: (element) => element.getAttribute("style"),
-        renderHTML: (attributes) => {
-          return {
-            style: attributes.style,
+        parseHTML: (element) => {
+          if (element.tagName.toLowerCase() === "li") {
+            addListIndex(element as HTMLLIElement)
           }
+          return element.getAttribute("style")
+        },
+        renderHTML: (attributes) => {
+          return { style: attributes.style }
         },
       },
     }
