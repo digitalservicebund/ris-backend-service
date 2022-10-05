1b15d24b7e53d9c7b2ec633eba5dd3d9de7eec65
Florian <florian.zechmeister@digitalservice.bund.de>
2022-10-05 18:53:51 +0200

    Run a11y tests only in one browser locally
    Addresses RISDEV-322

--- web/test/a11y/playwright.config.ts
+++ web/test/a11y/playwright.config.ts
@@ -1,5 +1,12 @@
+import { devices } from "@playwright/test"
 import config from "../../playwright.config"
 
 config.testDir = "."
+config.projects = [
+  {
+    name: "chromium",
+    use: { ...devices["Desktop Chrome"], channel: "chrome" },
+  },
+]
 
 export default config
