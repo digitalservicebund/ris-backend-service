5d154e387d2f11dbcbf10a02ad5ddc67e322458d
Leonie Koch <lk.leonie.koch@gmail.com>
2022-10-06 19:48:27 +0200

    Add vue transition to expandable content
    RISDEV-433

--- web/src/components/ExpandableContent.vue
+++ web/src/components/ExpandableContent.vue
@@ -7,11 +7,9 @@ interface Props {
   openIconName?: string
   closeIconName?: string
 }
-
 interface Emits {
   (event: "update:isExpanded", value: boolean): void
 }
-
 const props = withDefaults(defineProps<Props>(), {
   header: undefined,
   isExpanded: false,
@@ -19,25 +17,24 @@ const props = withDefaults(defineProps<Props>(), {
   closeIconName: "horizontal_rule",
 })
 const emit = defineEmits<Emits>()
-
+const expandableContainer = ref()
+const containerHeight = ref(0)
 const isExpanded = ref(false)
 const iconName = computed(() =>
   isExpanded.value ? props.closeIconName : props.openIconName
 )
-
 function toggleContentVisibility(): void {
   isExpanded.value = !isExpanded.value
+  containerHeight.value =
+    expandableContainer.value.getBoundingClientRect().height
 }
-
 watch(
   () => props.isExpanded,
   () => (isExpanded.value = props.isExpanded ?? false),
   { immediate: true }
 )
-
 watch(isExpanded, () => emit("update:isExpanded", isExpanded.value))
 </script>
-
 <template>
   <div class="expandable-content">
     <button
@@ -48,19 +45,47 @@ watch(isExpanded, () => emit("update:isExpanded", isExpanded.value))
       <slot name="header">
         <span>{{ header }}</span>
       </slot>
-
       <span class="icon material-icons">{{ iconName }}</span>
     </button>
-
-    <v-expand-transition>
+    <transition
+      ref="expandableContainer"
+      :class="{ expanded: isExpanded }"
+      name="expand"
+      :style="{ height: containerHeight.valueOf + 'px' }"
+    >
       <div v-show="isExpanded">
         <slot />
       </div>
-    </v-expand-transition>
+    </transition>
   </div>
 </template>
-
 <style lang="scss" scoped>
+.expand-enter-from {
+  max-height: 0;
+}
+
+.expand-enter-to {
+  max-height: 600px;
+}
+
+.expand-enter-active {
+  overflow: hidden;
+  transition: all 0.5s ease-in-out;
+}
+
+.expand-leave-from {
+  max-height: 600px;
+}
+
+.expand-leave-to {
+  max-height: 0;
+}
+
+.expand-leave-active {
+  overflow: hidden;
+  transition: all 0.5s ease-in-out;
+}
+
 .expandable-content {
   width: 100%;
 
