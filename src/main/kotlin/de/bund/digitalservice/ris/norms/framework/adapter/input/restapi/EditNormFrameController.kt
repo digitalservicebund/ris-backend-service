9943e3c9d68a69aa622226234dfabc1488b703d6
Thore Strassburg <thore@weilbier.net>
2022-11-09 16:05:42 +0100

    Feat(backend): add edit norms frame controller
    Add another HTTP controller as input adapter to drive the edit norm
    frame use-case.

---
+++ src/main/kotlin/de/bund/digitalservice/ris/norms/framework/adapter/input/restapi/EditNormFrameController.kt
@@ -0,0 +1,33 @@
+package de.bund.digitalservice.ris.norms.framework.adapter.input.restapi
+
+import de.bund.digitalservice.ris.norms.application.port.input.EditNormFrameUseCase
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PatchMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import reactor.core.publisher.Mono
+import java.util.UUID
+
+@RestController
+@RequestMapping(ApiConfiguration.API_BASE_PATH)
+class EditNormFrameController(private val editNormFrameService: EditNormFrameUseCase) {
+
+    @PatchMapping(path = ["/{guid}"])
+    fun editNormFrame(
+        @PathVariable guid: String,
+        @RequestBody request: EditNormFrameRequestSchema
+    ): Mono<ResponseEntity<Void>> {
+        val command = EditNormFrameUseCase.Command(UUID.fromString(guid), request.longTitle)
+
+        return editNormFrameService
+            .editNormFrame(command)
+            .map({ ResponseEntity.noContent().build<Void>() })
+            .onErrorReturn(ResponseEntity.internalServerError().build())
+    }
+
+    class EditNormFrameRequestSchema {
+        lateinit var longTitle: String
+    }
+}
