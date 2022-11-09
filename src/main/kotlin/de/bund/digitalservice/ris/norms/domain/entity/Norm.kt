a6cd1aca5ba07a6859ff14ba7d21829b6067bdba
Thore Strassburg <thore@weilbier.net>
2022-11-09 15:06:12 +0100

    Feat(backend): allow to update norm long title
    The long title of a norm must be able to be updated by the documentary.
    So far there is no additional business logic related to this update and
    it can be applied directly on the entity property. Therefore it is
    feasible to simply make this property a variable. In future this will
    become a setter function that includes the additional checks and logic.

--- src/main/kotlin/de/bund/digitalservice/ris/norms/domain/entity/Norm.kt
+++ src/main/kotlin/de/bund/digitalservice/ris/norms/domain/entity/Norm.kt
@@ -2,4 +2,4 @@ package de.bund.digitalservice.ris.norms.domain.entity
 
 import java.util.UUID
 
-data class Norm(val guid: UUID, val longTitle: String, val articles: List<Article> = listOf())
+data class Norm(val guid: UUID, var longTitle: String, val articles: List<Article> = listOf())
