325ca6a1887e3f06946d7d14d651172e3cc1cfb8
Thore Strassburg <thore@weilbier.net>
2022-11-13 12:15:59 +0100

    Feat(frontend): use child routes for singular norm
    As for caselaw, we have a group of routes that are available to interact
    with one specific norm resource. If the user has selected a norm, he
    gets to a view with a sidebar navigation on the left, and some content
    on the right. The content is always related to this loaded norm and can
    be controlled with the navigation on the right.
    This is a typical use-case for child routes with a child router view.
    Thereby the "root" route of the norm includes the whole setup. This
    includes the navigation, layout and loading the data of the norm. If
    then has a router view in its layout where all child routes get rendered
    into. Each of these child routes gets the norm data passed as property
    and must not load the data by itself.
    This also removes the need for a wrapper component for the layout as it
    is done for caselaw right now. This "new" approach is much more simple
    to use and more powerful, using native features of the Vue router.

---
+++ web/src/routes/norms/norm/[:normGuid].vue
@@ -0,0 +1,46 @@
+<script lang="ts" setup>
+import { toRefs, ref, watch } from "vue"
+import { useRoute, useRouter } from "vue-router"
+import NavbarSide from "@/components/NavbarSide.vue"
+import SideToggle from "@/components/SideToggle.vue"
+import { useNormMenuItems } from "@/composables/useNormMenuItems"
+import { useToggleStateInRouteQuery } from "@/composables/useToggleStateInRouteQuery"
+import { Norm } from "@/domain/Norm"
+import { getNormByGuid } from "@/services/normsService"
+
+const props = defineProps<{ normGuid: string }>()
+
+const route = useRoute()
+const router = useRouter()
+const { normGuid } = toRefs(props)
+const menuItems = useNormMenuItems(normGuid, route)
+const goBackRoute = { name: "norms" }
+const navigationIsOpen = useToggleStateInRouteQuery(
+  "showNavBar",
+  route,
+  router.replace
+)
+
+const norm = ref<Norm | undefined>()
+
+async function loadNormByGuid() {
+  norm.value = (await getNormByGuid(props.normGuid)).data
+}
+
+watch(() => props.normGuid, loadNormByGuid, { immediate: true })
+</script>
+<template>
+  <div class="flex grow w-screen">
+    <SideToggle v-model:is-expanded="navigationIsOpen" label="Navigation">
+      <NavbarSide
+        go-back-label="Zur Übersicht"
+        :go-back-route="goBackRoute"
+        :menu-items="menuItems"
+      />
+    </SideToggle>
+
+    <div class="bg-gray-100 p-64 w-full">
+      <router-view :norm="norm" />
+    </div>
+  </div>
+</template>
