98eb9b83015b451ef62f40b873d0281b3d24237c
Florian <florian.zechmeister@digitalservice.bund.de>
2022-11-16 08:46:23 +0100

    Extract generator from similar test cases

--- web/test/components/documentUnitList.spec.ts
+++ web/test/components/documentUnitList.spec.ts
@@ -2,29 +2,31 @@ import { fireEvent, render, screen } from "@testing-library/vue"
 import DocumentUnitList from "@/components/DocumentUnitList.vue"
 import DocumentUnit from "@/domain/documentUnit"
 
+function renderComponent(
+  documentUnits: DocumentUnit[] = [
+    new DocumentUnit("1", {
+      coreData: {
+        fileNumber: "foo",
+      },
+    }),
+  ]
+) {
+  return render(DocumentUnitList, {
+    props: {
+      documentUnits: documentUnits,
+    },
+  })
+}
+
 describe("documentUnit list", () => {
   test("renders fallback if no documentUnits found", async () => {
-    render(DocumentUnitList, {
-      props: {
-        documentUnits: [],
-      },
-    })
+    renderComponent([])
 
     await screen.findByText("Keine Dokumentationseinheiten gefunden")
   })
 
   test("renders documentUnits", async () => {
-    const documentUnit = new DocumentUnit("1", {
-      coreData: {
-        fileNumber: "foo",
-      },
-    })
-
-    render(DocumentUnitList, {
-      props: {
-        documentUnits: [documentUnit],
-      },
-    })
+    renderComponent()
 
     await screen.findByText("foo")
     expect(
@@ -33,17 +35,7 @@ describe("documentUnit list", () => {
   })
 
   test("delete emits event", async () => {
-    const documentUnit = new DocumentUnit("1", {
-      coreData: {
-        fileNumber: "foo",
-      },
-    })
-
-    const { emitted } = render(DocumentUnitList, {
-      props: {
-        documentUnits: [documentUnit],
-      },
-    })
+    const { emitted } = renderComponent()
 
     await fireEvent.click(
       screen.getByLabelText("Dokumentationseinheit löschen")
