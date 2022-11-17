dd7bdeff2ddb2e1bd6d9560225acefc3fe3d0655
Thore Strassburg <thore@weilbier.net>
2022-11-17 16:59:49 +0100

    Test(frontend): fix router issues for navbar
    The `NavbarSide` component is "heavily" integrating with the
    `vue-router` plugin. So far it is only the `<router-link>`, but we know
    already that with the next planned features we also need the composables
    like `useRoute`.
    Mocking the complete router with its composables and also stub the link
    component is quite hard. Or rather we still have a remaining issue of
    some Vue warnings during the tests where we don't understand where they
    are coming from.
    Therefore it is currently the strategy to do some kind of integration
    tests for this component with a real router instance. As a side-effect,
    this requires us to define all the routes that the navigation component
    uses. That is because the link component of the router does some
    "special" checks. This means while doing the tests we do not only need
    to define the menu items, but also the related routes. To makes this
    more simple for the test cases themselves, some utility functions have
    been added for this test suite that automatically creates the routes
    according to the menu items. Additionally it also adds the required root
    route.
    While this turned out a bit verbose, it is very simple to use, the tests
    themselves are good to read and we do some good integration testing
    here.

--- web/test/components/navbarSide.spec.ts
+++ web/test/components/navbarSide.spec.ts
@@ -1,98 +1,126 @@
 import { render } from "@testing-library/vue"
 import { createRouter, createWebHistory } from "vue-router"
+import type { Router, RouteRecordRaw, RouteLocationRaw } from "vue-router"
 import NavbarSide from "@/components/NavbarSide.vue"
+import { generateString } from "~/test-helper/dataGenerators"
 
-function renderComponent(options?: {
-  goBackLabel?: string
-  goBackRoute?: { path: string }
-  menuItems?: { name: string; route: { path: string }; isDisabled?: boolean }[]
-}) {
-  const props = {
-    goBackLabel: options?.goBackLabel ?? "go back label",
-    goBackRoute: options?.goBackRoute ?? { path: "go-back-route" },
-    menuItems: options?.menuItems ?? [],
-  }
-  const router = createRouter({ routes: [], history: createWebHistory() })
-  const global = { plugins: [router] }
-  return render(NavbarSide, { props, global })
-}
-
-describe("navbarSide", () => {
+describe("NavbarSide", () => {
   it("displays the go back label with related route", () => {
     const { queryByText } = renderComponent({
       goBackLabel: "return",
-      goBackRoute: { path: "origin-route" },
+      goBackRoute: "/origin-route",
     })
 
-    const goBackItem = queryByText("return")
-    const goBackLink = goBackItem?.closest("a")
+    const goBackItem = queryByText("return")?.closest("a")
 
     expect(goBackItem).toBeVisible()
-    expect(goBackLink).toHaveAttribute("href")
-    expect(goBackLink?.getAttribute("href")).toBe("/origin-route")
+    expect(goBackItem?.getAttribute("href")).toBe("/origin-route")
   })
 
   it("shows a router link for each configured menu item", () => {
     const menuItems = [
-      { name: "first item", route: { path: "first-route" } },
-      { name: "second item", route: { path: "second-route" } },
+      { name: "first item", route: "/first-route" },
+      { name: "second item", route: "/second-route" },
     ]
 
     const { queryByText } = renderComponent({ menuItems })
-
-    const firstItem = queryByText("first item")
-    const firstLink = firstItem?.closest("a")
-
-    const secondItem = queryByText("second item")
-    const secondLink = secondItem?.closest("a")
+    const firstItem = queryByText("first item")?.closest("a")
+    const secondItem = queryByText("second item")?.closest("a")
 
     expect(firstItem).toBeVisible()
-    expect(firstLink).toHaveAttribute("href")
-    expect(firstLink?.getAttribute("href")).toBe("/first-route")
+    expect(firstItem?.getAttribute("href")).toBe("/first-route")
 
     expect(secondItem).toBeVisible()
-    expect(secondLink).toHaveAttribute("href")
-    expect(secondLink?.getAttribute("href")).toBe("/second-route")
+    expect(secondItem?.getAttribute("href")).toBe("/second-route")
   })
 
   it("allows to render parent item with child entries", () => {
     const menuItems = [
       {
         name: "parent item",
-        route: { path: "parent-route" },
+        route: "/parent",
         children: [
-          { name: "first child", route: { path: "first-child" } },
-          { name: "second child", route: { path: "second-child" } },
+          { name: "first child", route: "/first-child" },
+          { name: "second child", route: "/second-child" },
         ],
       },
     ]
 
     const { queryByText } = renderComponent({ menuItems })
-
-    const firstChild = queryByText("first child")
-    const firstLink = firstChild?.closest("a")
-
-    const secondChild = queryByText("second child")
-    const secondLink = secondChild?.closest("a")
+    const firstChild = queryByText("first child")?.closest("a")
+    const secondChild = queryByText("second child")?.closest("a")
 
     expect(firstChild).toBeVisible()
-    expect(firstLink).toHaveAttribute("href")
-    expect(firstLink?.getAttribute("href")).toBe("/first-child")
+    expect(firstChild?.getAttribute("href")).toBe("/first-child")
 
     expect(secondChild).toBeVisible()
-    expect(secondLink).toHaveAttribute("href")
-    expect(secondLink?.getAttribute("href")).toBe("/second-child")
+    expect(secondChild?.getAttribute("href")).toBe("/second-child")
   })
 
   it("allows to disable a menu item", () => {
     const menuItems = [
-      { name: "disabled item", route: { path: "" }, isDisabled: true },
+      { name: "disabled item", route: "/route", isDisabled: true },
     ]
 
     const { queryByText } = renderComponent({ menuItems })
-
     const disabledItem = queryByText("disabled item")
 
     expect(disabledItem?.getAttribute("disabled")).toBeDefined()
   })
 })
+
+interface MenuItem {
+  name: string
+  route: RouteLocationRaw
+  children?: MenuItem[]
+  isDisabled?: boolean
+}
+
+function renderComponent(options?: {
+  goBackLabel?: string
+  goBackRoute?: RouteLocationRaw
+  menuItems?: MenuItem[]
+}) {
+  const goBackRoute = options?.goBackRoute ?? "/go-back-route"
+  const menuItems = options?.menuItems ?? []
+  const router = buildRouter(goBackRoute, menuItems)
+  const global = { plugins: [router] }
+  const props = {
+    goBackLabel: options?.goBackLabel ?? "go back label",
+    goBackRoute,
+    menuItems: options?.menuItems ?? [],
+  }
+  return render(NavbarSide, { props, global })
+}
+
+function buildRouter(
+  goBackRoute: RouteLocationRaw,
+  menuItems: MenuItem[]
+): Router {
+  const routes = []
+  routes.push(generateRouterRoute({ name: "root", path: "/" }))
+  routes.push(generateRouterRoute(goBackRoute))
+
+  for (const item of menuItems) {
+    routes.push(generateRouterRoute(item.route))
+
+    for (const child of item.children ?? []) {
+      routes.push(generateRouterRoute(child.route))
+    }
+  }
+
+  return createRouter({ routes, history: createWebHistory() })
+}
+
+function generateRouterRoute(routeLocation: RouteLocationRaw): RouteRecordRaw {
+  if (typeof routeLocation === "string") {
+    return { path: routeLocation, component: {} }
+  } else {
+    return {
+      ...routeLocation,
+      path:
+        "path" in routeLocation ? routeLocation.path : generateString("/path-"),
+      component: {},
+    }
+  }
+}
