1ba2e231688e3088f9a2091f6f052588fa8bcd27
Victor del Campo <victor.delcampo@digitalservice.bund.de>
2022-11-17 12:18:51 +0100

    Moved optional fields | Added tests

--- src/main/kotlin/de/bund/digitalservice/ris/norms/domain/entity/Norm.kt
+++ src/main/kotlin/de/bund/digitalservice/ris/norms/domain/entity/Norm.kt
@@ -5,28 +5,27 @@ import java.util.UUID
 
 data class Norm(
     val guid: UUID,
-    var longTitle: String,
-    val articles: List<Article> = listOf()
-) {
-    val officialShortTitle: String? = null
-    val officialAbbreviation: String? = null
-    val referenceNumber: String? = null
-    val publicationDate: LocalDate? = null
-    val announcementDate: LocalDate? = null
-    val citationDate: LocalDate? = null
-    val frameKeywords: String? = null
-    val authorEntity: String? = null
-    val authorDecidingBody: String? = null
-    val authorIsResolutionMajority: Boolean? = null
-    val leadJurisdiction: String? = null
-    val leadUnit: String? = null
-    val participationType: String? = null
-    val participationInstitution: String? = null
-    val documentTypeName: String? = null
-    val documentNormCategory: String? = null
-    val documentTemplateName: String? = null
-    val subjectFna: String? = null
-    val subjectPreviousFna: String? = null
-    val subjectGesta: String? = null
-    val subjectBgb3: String? = null
-}
+    val longTitle: String,
+    val articles: List<Article> = listOf(),
+    var officialShortTitle: String? = "",
+    var officialAbbreviation: String? = "",
+    var referenceNumber: String? = "",
+    var publicationDate: LocalDate? = LocalDate.EPOCH,
+    var announcementDate: LocalDate? = LocalDate.EPOCH,
+    var citationDate: LocalDate? = LocalDate.EPOCH,
+    var frameKeywords: String? = "",
+    var authorEntity: String? = "",
+    var authorDecidingBody: String? = "",
+    var authorIsResolutionMajority: Boolean? = false,
+    var leadJurisdiction: String? = "",
+    var leadUnit: String? = "",
+    var participationType: String? = "",
+    var participationInstitution: String? = "",
+    var documentTypeName: String? = "",
+    var documentNormCategory: String? = "",
+    var documentTemplateName: String? = "",
+    var subjectFna: String? = "",
+    var subjectPreviousFna: String? = "",
+    var subjectGesta: String? = "",
+    var subjectBgb3: String? = ""
+)
--- src/test/kotlin/norms/domain/entity/NormTest.kt
+++ src/test/kotlin/norms/domain/entity/NormTest.kt
@@ -2,11 +2,12 @@ package de.bund.digitalservice.ris.norms.domain.entity
 
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
+import java.time.LocalDate
 import java.util.UUID
 
 class NormTest {
     @Test
-    fun `can create a simple norm instance`() {
+    fun `can create a norm with only mandatory fields`() {
         val paragraph = Paragraph(UUID.randomUUID(), "marker", "text")
         val article = Article(UUID.randomUUID(), "title", "marker", listOf(paragraph))
         val guid = UUID.randomUUID()
@@ -15,5 +16,57 @@ class NormTest {
         assertTrue(norm.guid == guid)
         assertTrue(norm.longTitle == "long title")
         assertTrue(norm.articles == listOf(article))
+        assertTrue(norm.officialShortTitle == "")
+        assertTrue(norm.officialAbbreviation == "")
+        assertTrue(norm.frameKeywords == "")
+    }
+
+    @Test
+    fun `can create a norm with some optional string fields`() {
+        val paragraph = Paragraph(UUID.randomUUID(), "marker", "text")
+        val article = Article(UUID.randomUUID(), "title", "marker", listOf(paragraph))
+        val guid = UUID.randomUUID()
+        val norm = Norm(
+            guid,
+            "long title",
+            listOf(article),
+            officialShortTitle = "short title",
+            officialAbbreviation = "ABC",
+            frameKeywords = "keywords"
+        )
+
+        assertTrue(norm.guid == guid)
+        assertTrue(norm.longTitle == "long title")
+        assertTrue(norm.articles == listOf(article))
+        assertTrue(norm.officialShortTitle == "short title")
+        assertTrue(norm.officialAbbreviation == "ABC")
+        assertTrue(norm.frameKeywords == "keywords")
+    }
+
+    @Test
+    fun `can create a norm with optional date and boolean fields`() {
+        val publicationDate = LocalDate.of(2022, 11, 17)
+        val announcementDate = LocalDate.of(2022, 11, 18)
+        val citationDate = LocalDate.of(2022, 11, 19)
+        val paragraph = Paragraph(UUID.randomUUID(), "marker", "text")
+        val article = Article(UUID.randomUUID(), "title", "marker", listOf(paragraph))
+        val guid = UUID.randomUUID()
+        val norm = Norm(
+            guid,
+            "long title",
+            listOf(article),
+            publicationDate = publicationDate,
+            announcementDate = announcementDate,
+            citationDate = citationDate,
+            authorIsResolutionMajority = true
+        )
+
+        assertTrue(norm.guid == guid)
+        assertTrue(norm.longTitle == "long title")
+        assertTrue(norm.articles == listOf(article))
+        assertTrue(norm.publicationDate == publicationDate)
+        assertTrue(norm.announcementDate == announcementDate)
+        assertTrue(norm.citationDate == citationDate)
+        assertTrue(norm.authorIsResolutionMajority == true)
     }
 }
