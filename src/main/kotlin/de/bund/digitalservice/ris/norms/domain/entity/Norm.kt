e92d351440204656cfd4ecb181b06e7fcffcbd2a
Victor del Campo <victor.delcampo@digitalservice.bund.de>
2022-11-17 11:44:13 +0100

    Remove optional fields from primary constructor

--- src/main/kotlin/de/bund/digitalservice/ris/norms/domain/entity/Norm.kt
+++ src/main/kotlin/de/bund/digitalservice/ris/norms/domain/entity/Norm.kt
@@ -6,26 +6,27 @@ import java.util.UUID
 data class Norm(
     val guid: UUID,
     var longTitle: String,
-    val articles: List<Article> = listOf(),
-    val officialShortTitle: String?,
-    val officialAbbreviation: String?,
-    val referenceNumber: String?,
-    val publicationDate: LocalDate?,
-    val announcementDate: LocalDate?,
-    val citationDate: LocalDate?,
-    val frameKeywords: String?,
-    val authorEntity: String?,
-    val authorDecidingBody: String?,
-    val authorIsResolutionMajority: Boolean?,
-    val leadJurisdiction: String?,
-    val leadUnit: String?,
-    val participationType: String?,
-    val participationInstitution: String?,
-    val documentTypeName: String?,
-    val documentNormCategory: String?,
-    val documentTemplateName: String?,
-    val subjectFna: String?,
-    val subjectPreviousFna: String?,
-    val subjectGesta: String?,
-    val subjectBgb3: String?
-)
+    val articles: List<Article> = listOf()
+) {
+    val officialShortTitle: String? = null
+    val officialAbbreviation: String? = null
+    val referenceNumber: String? = null
+    val publicationDate: LocalDate? = null
+    val announcementDate: LocalDate? = null
+    val citationDate: LocalDate? = null
+    val frameKeywords: String? = null
+    val authorEntity: String? = null
+    val authorDecidingBody: String? = null
+    val authorIsResolutionMajority: Boolean? = null
+    val leadJurisdiction: String? = null
+    val leadUnit: String? = null
+    val participationType: String? = null
+    val participationInstitution: String? = null
+    val documentTypeName: String? = null
+    val documentNormCategory: String? = null
+    val documentTemplateName: String? = null
+    val subjectFna: String? = null
+    val subjectPreviousFna: String? = null
+    val subjectGesta: String? = null
+    val subjectBgb3: String? = null
+}
