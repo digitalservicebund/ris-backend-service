74151063014947faea0fa9ffad7509fd88a164f0
Thore Strassburg <thore@weilbier.net>
2022-11-17 17:58:28 +0100

    Refactor(frontend): improve navigation setup
    The `NavbarSide` component has become a little ugly with the two
    different levels of menu items and the special classes for different
    situations. And we already know that more features are coming which
    makes this even worse.
    Therefor this is an approach to organize the code a little better and
    extract the logic from the template to the setup function. This results
    into a much simpler template, which is always hard to read with too much
    JavaScript code inside. Furthermore it allows to be more expressive with
    proper variable and function names in the setup function. So it is now
    clear that the last sibling of a level one's level two items gets
    a special margin.
    What is important to keep in mind is that the `NavbarSide` component
    remains mounted while the child routes in its adjacent route view
    change. This puts extra requirements on the reactivity of this
    component. This also means it is not possible to calls functions from
    the template `v-for` loops as these would be only re-evaluated if the
    menu items change. But this will no more be the case as we will have
    side-effects by other reactive factors like the current active route
    etc. Therefore it has been decided to use a computed value that enhances
    the menu items. This works well because this is the exact same hierarchy
    that the template goes through and accessing "additional"/enriched
    properties is straight forward this way.
    To keep the code readable, we use separated functions to generate
    specific enhancement properties which are then used by the computed
    value that puts it all together. Thereby the callback of the computed
    value has only a single responsibility and remains somehow readable.

--- web/src/components/NavbarSide.vue
+++ web/src/components/NavbarSide.vue
@@ -1,4 +1,5 @@
 <script lang="ts" setup>
+import { computed } from "vue"
 import type { RouteLocationRaw } from "vue-router"
 
 interface Props {
@@ -7,7 +8,44 @@ interface Props {
   goBackRoute: RouteLocationRaw
 }
 
-defineProps<Props>()
+const props = defineProps<Props>()
+
+const enhencedMenuItems = computed(() =>
+  props.menuItems.map((levelOneItem) => ({
+    ...levelOneItem,
+    classes: getClassesForLevelOne(levelOneItem),
+    children: levelOneItem.children?.map((levelTwoItem) => ({
+      ...levelTwoItem,
+      classes: getClassesForLevelTwo(levelTwoItem, levelOneItem),
+    })),
+  }))
+)
+
+function getClassesForLevelOne(
+  levelOneItem: LevelOneMenuItem
+): Record<string, boolean> {
+  const { isDisabled, children } = levelOneItem
+  const hasChildren = children !== undefined && children.length > 0
+  return {
+    disabled: isDisabled ?? false,
+    "mb-0": hasChildren,
+  }
+}
+
+function getClassesForLevelTwo(
+  levelTwoItem: LevelTwoMenuItem,
+  levelOneItem: LevelOneMenuItem
+): Record<string, boolean> {
+  const { isDisabled } = levelTwoItem
+  const siblings = levelOneItem.children ?? []
+  const lastSibling = siblings[siblings.length - 1]
+  const isLastSibling = siblings.length > 0 && levelTwoItem == lastSibling
+
+  return {
+    disabled: isDisabled ?? false,
+    "mb-24": isLastSibling,
+  }
+}
 </script>
 
 <script lang="ts">
@@ -36,17 +74,14 @@ export interface LevelTwoMenuItem {
     </router-link>
 
     <div
-      v-for="levelOneItem in menuItems"
+      v-for="levelOneItem in enhencedMenuItems"
       :key="levelOneItem.label"
       class="border-gray-400 border-t-2"
     >
       <router-link
         active-class="bg-blue-200"
         class="block focus:bg-blue-200 focus:underline hover:bg-blue-200 hover:underline label-02-bold my-10 px-8 py-12"
-        :class="{
-          disabled: levelOneItem.isDisabled,
-          'mb-0': levelOneItem.children,
-        }"
+        :class="levelOneItem.classes"
         exact-path
         :to="levelOneItem.route"
       >
@@ -54,10 +89,10 @@ export interface LevelTwoMenuItem {
       </router-link>
 
       <router-link
-        v-for="(levelTwoItem, index) in levelOneItem.children"
+        v-for="levelTwoItem in levelOneItem.children"
         :key="levelTwoItem.label"
         class="block focus:bg-blue-200 focus:underline hover:bg-blue-200 hover:underline label-02-reg p-10 pl-16"
-        :class="{ 'mb-24': index + 1 == levelOneItem.children?.length }"
+        :class="levelTwoItem.classes"
         :to="levelTwoItem.route"
       >
         {{ levelTwoItem.label }}
