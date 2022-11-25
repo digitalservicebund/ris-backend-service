0148cc324bfb828247ebd3ae7b21e3d7b8dcf075
Thore Strassburg <thore@weilbier.net>
2022-11-25 23:29:43 +0100

    Refactor(backend): unify norm controller encoding
    For more unified code, all `fromUseCase` methods for the data mapper
    classes take a parameter called `data`.
    
    Moreover some old "manual" encoding of GUIDs have been adapted.
    
    Finally a data mapper class could have been improved by using values
    instead of variables without defaults.

--- src/main/kotlin/de/bund/digitalservice/ris/norms/framework/adapter/input/restapi/LoadNormController.kt
+++ src/main/kotlin/de/bund/digitalservice/ris/norms/framework/adapter/input/restapi/LoadNormController.kt
@@ -4,13 +4,14 @@ import de.bund.digitalservice.ris.norms.application.port.input.LoadNormUseCase
 import de.bund.digitalservice.ris.norms.domain.entity.Article
 import de.bund.digitalservice.ris.norms.domain.entity.Norm
 import de.bund.digitalservice.ris.norms.domain.entity.Paragraph
+import encodeGuid
+import encodeLocalDate
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import reactor.core.publisher.Mono
-import java.time.LocalDate
 import java.util.UUID
 
 @RestController
@@ -34,56 +35,56 @@ class LoadNormController(private val loadNormService: LoadNormUseCase) {
         val guid: String,
         val longTitle: String,
         val articles: List<ArticleResponseSchema>,
-        var officialShortTitle: String? = null,
-        var officialAbbreviation: String? = null,
-        var referenceNumber: String? = null,
-        var publicationDate: String? = null,
-        var announcementDate: String? = null,
-        var citationDate: String? = null,
-        var frameKeywords: String? = null,
-        var authorEntity: String? = null,
-        var authorDecidingBody: String? = null,
-        var authorIsResolutionMajority: Boolean? = null,
-        var leadJurisdiction: String? = null,
-        var leadUnit: String? = null,
-        var participationType: String? = null,
-        var participationInstitution: String? = null,
-        var documentTypeName: String? = null,
-        var documentNormCategory: String? = null,
-        var documentTemplateName: String? = null,
-        var subjectFna: String? = null,
-        var subjectPreviousFna: String? = null,
-        var subjectGesta: String? = null,
-        var subjectBgb3: String? = null
+        val officialShortTitle: String?,
+        val officialAbbreviation: String?,
+        val referenceNumber: String?,
+        val publicationDate: String?,
+        val announcementDate: String?,
+        val citationDate: String?,
+        val frameKeywords: String?,
+        val authorEntity: String?,
+        val authorDecidingBody: String?,
+        val authorIsResolutionMajority: Boolean?,
+        val leadJurisdiction: String?,
+        val leadUnit: String?,
+        val participationType: String?,
+        val participationInstitution: String?,
+        val documentTypeName: String?,
+        val documentNormCategory: String?,
+        val documentTemplateName: String?,
+        val subjectFna: String?,
+        val subjectPreviousFna: String?,
+        val subjectGesta: String?,
+        val subjectBgb3: String?
     ) {
         companion object {
-            fun fromUseCaseData(norm: Norm): NormResponseSchema {
-                val articles = norm.articles.map { ArticleResponseSchema.fromUseCaseData(it) }
+            fun fromUseCaseData(data: Norm): NormResponseSchema {
+                val articles = data.articles.map { ArticleResponseSchema.fromUseCaseData(it) }
                 return NormResponseSchema(
-                    guid = norm.guid.toString(),
-                    longTitle = norm.longTitle,
+                    guid = encodeGuid(data.guid),
+                    longTitle = data.longTitle,
                     articles = articles,
-                    officialShortTitle = norm.officialShortTitle,
-                    officialAbbreviation = norm.officialAbbreviation,
-                    referenceNumber = norm.referenceNumber,
-                    publicationDate = encodeLocalDate(norm.publicationDate),
-                    announcementDate = encodeLocalDate(norm.announcementDate),
-                    citationDate = encodeLocalDate(norm.citationDate),
-                    frameKeywords = norm.frameKeywords,
-                    authorEntity = norm.authorEntity,
-                    authorDecidingBody = norm.authorDecidingBody,
-                    authorIsResolutionMajority = norm.authorIsResolutionMajority,
-                    leadJurisdiction = norm.leadJurisdiction,
-                    leadUnit = norm.leadUnit,
-                    participationType = norm.participationType,
-                    participationInstitution = norm.participationInstitution,
-                    documentTypeName = norm.documentTypeName,
-                    documentNormCategory = norm.documentNormCategory,
-                    documentTemplateName = norm.documentTemplateName,
-                    subjectFna = norm.subjectFna,
-                    subjectPreviousFna = norm.subjectPreviousFna,
-                    subjectGesta = norm.subjectGesta,
-                    subjectBgb3 = norm.subjectBgb3
+                    officialShortTitle = data.officialShortTitle,
+                    officialAbbreviation = data.officialAbbreviation,
+                    referenceNumber = data.referenceNumber,
+                    publicationDate = encodeLocalDate(data.publicationDate),
+                    announcementDate = encodeLocalDate(data.announcementDate),
+                    citationDate = encodeLocalDate(data.citationDate),
+                    frameKeywords = data.frameKeywords,
+                    authorEntity = data.authorEntity,
+                    authorDecidingBody = data.authorDecidingBody,
+                    authorIsResolutionMajority = data.authorIsResolutionMajority,
+                    leadJurisdiction = data.leadJurisdiction,
+                    leadUnit = data.leadUnit,
+                    participationType = data.participationType,
+                    participationInstitution = data.participationInstitution,
+                    documentTypeName = data.documentTypeName,
+                    documentNormCategory = data.documentNormCategory,
+                    documentTemplateName = data.documentTemplateName,
+                    subjectFna = data.subjectFna,
+                    subjectPreviousFna = data.subjectPreviousFna,
+                    subjectGesta = data.subjectGesta,
+                    subjectBgb3 = data.subjectBgb3
                 )
             }
         }
@@ -97,26 +98,19 @@ class LoadNormController(private val loadNormService: LoadNormUseCase) {
         val paragraphs: List<ParagraphResponseSchema>
     ) {
         companion object {
-            fun fromUseCaseData(article: Article): ArticleResponseSchema {
-                val paragraphs = article.paragraphs.map { ParagraphResponseSchema.fromUseCaseData(it) }
-                return ArticleResponseSchema(
-                    article.guid.toString(),
-                    article.title,
-                    article.marker,
-                    paragraphs
-                )
+            fun fromUseCaseData(data: Article): ArticleResponseSchema {
+                val paragraphs = data.paragraphs.map { ParagraphResponseSchema.fromUseCaseData(it) }
+                return ArticleResponseSchema(encodeGuid(data.guid), data.title, data.marker, paragraphs)
             }
         }
     }
 
     data class ParagraphResponseSchema
-    private constructor(val guid: String, var marker: String? = null, val text: String) {
+    private constructor(val guid: String, val marker: String? = null, val text: String) {
         companion object {
-            fun fromUseCaseData(paragraph: Paragraph): ParagraphResponseSchema {
-                return ParagraphResponseSchema(paragraph.guid.toString(), paragraph.marker, paragraph.text)
+            fun fromUseCaseData(data: Paragraph): ParagraphResponseSchema {
+                return ParagraphResponseSchema(encodeGuid(data.guid), data.marker, data.text)
             }
         }
     }
 }
-
-private fun encodeLocalDate(date: LocalDate?): String? = if (date != null) date.toString() else null
