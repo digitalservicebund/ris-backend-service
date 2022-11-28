fbccd8adeaa7d8d5fbc8ba1b651781bb44d1b686
Thore Strassburg <thore@weilbier.net>
2022-11-28 11:12:38 +0100

    Test(frontend): add data generator for norms
    This will become very handy as the `Norm` dataset has become quite big
    and is inconvenient to test with. Therefore we make use of our test data
    generator concepts and add some functions for the norms related domain
    data structures.

--- web/test/test-helper/dataGenerators.ts
+++ web/test/test-helper/dataGenerators.ts
@@ -5,8 +5,10 @@ import type {
   TextInputAttributes,
   TextInputField,
 } from "@/domain"
+import { Article, Norm, Paragraph } from "@/domain/Norm"
 
 const ALPHABET_CHARACTERS = "abcdefghijklmnopqrstuvwxyz"
+const HEXADECIMAL_CHARACTERS = "0123456789abcdef"
 
 export function generateString(options?: {
   characterSet?: string
@@ -65,5 +67,83 @@ export function generateTextInputField(
     type: InputType.TEXT,
     inputAttributes: generateTextInputAttributes(),
     ...partialField,
+  }
+}
+export function generateGuid(): string {
+  const first = generateString({
+    length: 8,
+    characterSet: HEXADECIMAL_CHARACTERS,
+  })
+  const second = generateString({
+    length: 4,
+    characterSet: HEXADECIMAL_CHARACTERS,
+  })
+  const third = generateString({
+    length: 4,
+    characterSet: HEXADECIMAL_CHARACTERS,
+  })
+  const fourth = generateString({
+    length: 4,
+    characterSet: HEXADECIMAL_CHARACTERS,
+  })
+  const fith = generateString({
+    length: 12,
+    characterSet: HEXADECIMAL_CHARACTERS,
+  })
+  return `${first}-${second}-${third}-${fourth}-${fith}`
+}
+
+export function generateParagraph(
+  partialParagraph?: Partial<Paragraph>
+): Paragraph {
+  return {
+    guid: generateGuid(),
+    marker: generateString({ prefix: "marker " }),
+    text: generateString({ prefix: "text", length: 40 }),
+    ...partialParagraph,
+  }
+}
+
+export function generateArticle(partialArticle?: Partial<Article>): Article {
+  return {
+    guid: generateGuid(),
+    title: generateString({ prefix: "title " }),
+    marker: generateString({ prefix: "marker " }),
+    paragraphs: [generateParagraph()],
+    ...partialArticle,
+  }
+}
+
+export function generateNorm(partialNorm?: Partial<Norm>): Norm {
+  return {
+    guid: generateGuid(),
+    articles: [generateArticle()],
+    longTitle: generateString(),
+    officialShortTitle: generateString(),
+    officialAbbreviation: generateString(),
+    referenceNumber: generateString(),
+    publicationDate: generateString(),
+    announcementDate: generateString(),
+    citationDate: generateString(),
+    frameKeywords: generateString(),
+    authorEntity: generateString(),
+    authorDecidingBody: generateString(),
+    authorIsResolutionMajority: false,
+    leadJurisdiction: generateString(),
+    leadUnit: generateString(),
+    participationType: generateString(),
+    participationInstitution: generateString(),
+    documentTypeName: generateString(),
+    documentNormCategory: generateString(),
+    documentTemplateName: generateString(),
+    subjectFna: generateString(),
+    subjectPreviousFna: generateString(),
+    subjectGesta: generateString(),
+    subjectBgb3: generateString(),
+    unofficialTitle: generateString(),
+    unofficialShortTitle: generateString(),
+    unofficialAbbreviation: generateString(),
+    risAbbreviation: generateString(),
+    ...partialNorm,
   }
 }
