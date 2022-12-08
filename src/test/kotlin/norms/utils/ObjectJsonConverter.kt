a673928fa376faeb2f39f6aaab0e92be552cccd3
Victor del Campo <victor.delcampo@digitalservice.bund.de>
2022-12-08 15:35:02 +0100

    Extend load use case

--- src/test/kotlin/norms/utils/ObjectJsonConverter.kt
+++ src/test/kotlin/norms/utils/ObjectJsonConverter.kt
@@ -1,8 +1,30 @@
 package norms.utils
 
 import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import com.google.gson.JsonElement
+import com.google.gson.JsonPrimitive
+import com.google.gson.JsonSerializationContext
+import com.google.gson.JsonSerializer
+import de.bund.digitalservice.ris.norms.domain.entity.Norm
 import de.bund.digitalservice.ris.norms.framework.adapter.input.restapi.EditNormFrameController
+import encodeLocalDate
+import java.lang.reflect.Type
+import java.time.LocalDate
 
 fun convertNormFrameRequestSchemaToJson(normFramePropertiesRequestSchema: EditNormFrameController.NormFramePropertiesRequestSchema): String {
     return Gson().toJson(normFramePropertiesRequestSchema)
+}
+
+fun convertNormToJson(norm: Norm): String {
+    class LocalDateSerializer : JsonSerializer<LocalDate> {
+        override fun serialize(src: LocalDate?, typeOfSrc: Type?, context: JsonSerializationContext?): JsonElement {
+            return JsonPrimitive(encodeLocalDate(src))
+        }
+    }
+    val gson: Gson = GsonBuilder().registerTypeAdapter(
+        LocalDate::class.java,
+        LocalDateSerializer()
+    ).create()
+    return gson.toJson(norm)
 }
