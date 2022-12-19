digitalservicebund/ris-backend-service

commit 0d8b45d81d7b9f0eaff41c19dbbe028ed12161bb

Fix checkInputValueType in DropdownInput

web/src/components/DropdownInput.vue
@@ -48,11 +48,18 @@ function isCourt(input?: DropdownInputModelType): input is Court {
 
 function checkInputValueType() {
   if (isCourt(inputValue.value)) {
+    // currently only court (Gericht)
     inputText.value = inputValue.value.label
   } else {
-    inputText.value = props.dropdownItems?.find(
-      (item) => item.value === inputValue.value
-    )?.text
+    if (props.endpoint) {
+      // currently only category (Dokumenttyp)
+      inputText.value = inputValue.value as string
+    } else {
+      // currently only legalEffect (Rechtskraft)
+      inputText.value = props.dropdownItems?.find(
+        (item) => item.value === inputValue.value
+      )?.text
+    }
   }
 }
 

commit f7b3d0862ea451ffb9cfa72d428f93b730f6a563

Add unit test for DropdownInput
Corresponds with fixing a functionality that was accidentally broken and then restored in 0d8b45d8

web/test/components/dropdownInput.spec.ts
@@ -138,6 +138,27 @@ describe("Dropdown Element", () => {
     expect(dropdownItems[0]).toHaveTextContent("Kein passender Eintrag")
   })
 
+  it("Dropdown item should be visible after selecting", async () => {
+    renderComponent({ isCombobox: true })
+
+    const input = screen.getByLabelText("test label") as HTMLInputElement
+    await user.type(input, "testItem2")
+
+    const dropdownItems = screen.getAllByLabelText("dropdown-option")
+    expect(screen.getAllByLabelText("dropdown-option")).toHaveLength(1)
+    expect(dropdownItems[0]).toHaveTextContent("testItem2")
+
+    await user.click(dropdownItems[0])
+    expect(screen.queryByLabelText("dropdown-option")).not.toBeInTheDocument()
+    expect(input).toHaveValue("testItem2")
+
+    const openDropdownContainer = screen.getByLabelText(
+      "Dropdown öffnen"
+    ) as HTMLElement
+    await user.click(openDropdownContainer)
+    expect(screen.getAllByLabelText("dropdown-option")).toHaveLength(3)
+  })
+
   it("Dropdown uses endpoint to fetch all DocumentType items", async () => {
     const fetchSpy = vi
       .spyOn(dropdownInputService, "fetch")