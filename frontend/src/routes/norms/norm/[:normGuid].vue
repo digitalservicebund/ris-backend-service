de4bc4943300117e874fae419f06f8f2ba3ea431
Thore Strassburg <thore@weilbier.net>
2023-01-26 17:09:44 +0100

    Feat(norms): add info panel for norms
    Please note that the name of the used component has still a caselaw
    specific background related to their domain entity name `DocumentUnit`.
    Unfortunately does design call this component internally
    `DocUnitHeader`. As that names brings some issues with it, it stays as
    it for now until we found a well fitting alternative.

--- frontend/src/routes/norms/norm/[:normGuid].vue
+++ frontend/src/routes/norms/norm/[:normGuid].vue
@@ -1,7 +1,9 @@
 <script lang="ts" setup>
+import dayjs from "dayjs"
 import { storeToRefs } from "pinia"
-import { toRefs, watchEffect, onUnmounted } from "vue"
-import { useRoute, useRouter } from "vue-router"
+import { toRefs, watchEffect, onUnmounted, computed } from "vue"
+import { RouterView, useRoute, useRouter } from "vue-router"
+import DocumentUnitInfoPanel from "@/components/DocumentUnitInfoPanel.vue"
 import NavbarSide from "@/components/NavbarSide.vue"
 import SideToggle from "@/components/SideToggle.vue"
 import { useNormMenuItems } from "@/composables/useNormMenuItems"
@@ -23,9 +25,38 @@ const navigationIsOpen = useToggleStateInRouteQuery(
 const store = useLoadedNormStore()
 const { loadedNorm } = storeToRefs(store)
 
+const ENTRY_INTO_FORCE_DATE_MAP: Record<string, string> = {
+  UNDEFINED_UNKNOWN: "unbestimmt (unbekannt)",
+  UNDEFINED_FUTURE: "unbestimmt (zukünftig)",
+  UNDEFINED_NOT_PRESENT: "nicht vorhanden",
+}
+
+const announcementInfo = computed(() => {
+  const gazette = loadedNorm.value?.printAnnouncementGazette
+  const page = loadedNorm.value?.printAnnouncementPage
+  return gazette && page ? `${gazette} S. ${page}` : undefined
+})
+
+const entryIntoForceInfo = computed(() => {
+  if (loadedNorm.value?.entryIntoForceDate) {
+    return dayjs(loadedNorm.value.entryIntoForceDate).format("DD.MM.YYYY")
+  } else if (loadedNorm.value?.entryIntoForceDateState) {
+    return ENTRY_INTO_FORCE_DATE_MAP[loadedNorm.value.entryIntoForceDateState]
+  } else {
+    return undefined
+  }
+})
+
+const propertyInfos = computed(() => [
+  { label: "Fundstelle", value: announcementInfo.value },
+  { label: "FNA", value: loadedNorm.value?.subjectFna },
+  { label: "Inkrafttreten", value: entryIntoForceInfo.value },
+])
+
 watchEffect(() => store.load(props.normGuid))
 onUnmounted(() => (loadedNorm.value = undefined))
 </script>
+
 <template>
   <div class="flex grow overflow-hidden w-screen">
     <SideToggle v-model:is-expanded="navigationIsOpen" label="Navigation">
@@ -36,9 +67,17 @@ onUnmounted(() => (loadedNorm.value = undefined))
       />
     </SideToggle>
 
-    <div class="bg-gray-100 border-gray-400 border-l-1 p-48 w-full">
-      <router-view v-if="loadedNorm" />
-      <span v-else>Lade Norm...</span>
+    <div
+      v-if="loadedNorm"
+      class="bg-gray-100 border-gray-400 border-l-1 w-full"
+    >
+      <DocumentUnitInfoPanel
+        :heading="loadedNorm.risAbbreviation"
+        :property-infos="propertyInfos"
+      />
+      <RouterView class="p-48" />
     </div>
+
+    <span v-else>Lade Norm...</span>
   </div>
 </template>
