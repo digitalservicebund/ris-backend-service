1daa2e652f80acd6b4a5ff1c4deddf5b0d9d8685
Florian <florian.zechmeister@digitalservice.bund.de>
2023-02-06 19:06:43 +0100

    Revert "Remember location if redirected to authentication"
    This reverts commit 3c83f7c84fe6be05fff75738e8f894bb364ce93b.

--- frontend/src/services/authService.ts
+++ frontend/src/services/authService.ts
@@ -1,21 +1,27 @@
 import httpClient, { ServiceResponse } from "./httpClient"
 
-export const loginEndpoint = "/oauth2/authorization/oidcclient"
+function redirectToLogin() {
+  location.href = "/oauth2/authorization/oidcclient"
+}
 
-export async function isAuthenticated(): Promise<boolean> {
+async function isAuthenticated(): Promise<boolean> {
   const response = await httpClient.get("auth/me")
   if (response.status === 401 || response.status === 403) {
+    redirectToLogin()
     return false
   }
+
   return true
 }
 
-export async function getName(): Promise<ServiceResponse<string>> {
+async function getName(): Promise<ServiceResponse<string>> {
   const response = await httpClient.get<string>("auth/me")
-  if (response.status != 200) {
+  if (response.status >= 300) {
     response.error = {
       title: "Name konnte nicht geladen werden.",
     }
   }
   return response
 }
+
+export default { isAuthenticated, getName }
