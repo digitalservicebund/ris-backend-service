e7088902f3e0edc0c8edbaf41e5c0bc57c337ea2
Magdalena Noffke <magdalena.noffke@digitalservice.bund.de>
2023-02-09 17:14:54 +0100

    Add import page

--- frontend/src/routes/norms/index.vue
+++ frontend/src/routes/norms/index.vue
@@ -1,14 +1,23 @@
 <script lang="ts" setup>
 import { ref } from "vue"
+import { useRouter } from "vue-router"
 import NormsList from "@/components/NormsList.vue"
+import TextButton from "@/components/TextButton.vue"
 import { getAllNorms } from "@/services/normsService"
 
 const norms = ref((await getAllNorms()).data)
+const router = useRouter()
 </script>
 
 <template>
   <div class="bg-gray-100 flex flex-col gap-16">
-    <h1 class="heading-02-regular p-16 pl-64">Dokumentationseinheiten</h1>
+    <div class="flex justify-between p-16 pl-64">
+      <h1 class="heading-02-regular">Dokumentationseinheiten</h1>
+      <TextButton
+        label="Neue Dokumentationseinheit"
+        @click="router.push({ name: 'norms-norm-import' })"
+      />
+    </div>
     <NormsList
       v-if="norms && norms.length !== 0"
       class="grow max-w-screen-lg"
