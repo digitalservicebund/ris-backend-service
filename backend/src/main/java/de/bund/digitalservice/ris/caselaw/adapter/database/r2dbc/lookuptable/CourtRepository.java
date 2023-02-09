bf44b29586193d089137589ba72949629628e7ac
benjaminaaron <benjamin.degenhart@digitalservice.bund.de>
2023-02-09 22:33:07 +0100

    Restructure SQL query to follow standard
    RISDEV-1018
    
    Co-Authored-By: rvp-c <104769777+rvp-c@users.noreply.github.com>

--- backend/src/main/java/de/bund/digitalservice/ris/caselaw/adapter/database/r2dbc/lookuptable/CourtRepository.java
+++ backend/src/main/java/de/bund/digitalservice/ris/caselaw/adapter/database/r2dbc/lookuptable/CourtRepository.java
@@ -14,30 +14,33 @@ public interface CourtRepository extends R2dbcRepository<CourtDTO, Long> {
   Flux<CourtDTO> findAllByOrderByCourttypeAscCourtlocationAsc();
 
   /*
-  CONCAT(courttype, ' ', courtlocation) will be displayed in the dropdown in the frontend
+  The query gets all rows where searchStr is anywhere in the label.
+  The CASE statements are used to order the results into 3 priority classes:
 
-  The query gets all rows where searchStr is anywhere in CONCAT. The CASE statements are then
-  used to order the results into 3 priority classes.
-  Within a priority class, ordering is alphabetical.
-
-  ^ marks the beginning --> searchStr is start of entire string
-  \m is a word boundary --> searchStr is start of a word
-  .* is any character --> searchStr is anywhere in the string
+  1. searchStr is start of label
+  2. searchStr is start of a word within label: indicated by being after a space or a dash
+  3. that leaves the else case: searchStr is anywhere in the string
 
   The order of the CASE statements is important. If the 3rd would be first, there would only be
   results of priority 3.
-
-  Note: ~* is case-insensitive, ~ would be case-sensitive
+  Within a priority class, ordering is alphabetical.
   */
   @Query(
-      "SELECT *, "
-          + "CASE "
-          + "  WHEN CONCAT(courttype, ' ', courtlocation) ~* CONCAT('^', :searchStr) THEN 1 "
-          + "  WHEN CONCAT(courttype, ' ', courtlocation) ~* CONCAT('\\m', :searchStr) THEN 2 "
-          + "  WHEN CONCAT(courttype, ' ', courtlocation) ~* CONCAT('.*', :searchStr, '.*') THEN 3 "
-          + "  END AS priority "
-          + "FROM lookuptable_court "
-          + "WHERE CONCAT(courttype, ' ', courtlocation) ~* CONCAT('.*', :searchStr, '.*') "
-          + "ORDER BY priority, CONCAT(courttype, ' ', courtlocation)")
+      "WITH label_added AS (SELECT courttype, "
+          + "                            courtlocation, "
+          + "                            UPPER(CONCAT(courttype, ' ', courtlocation)) AS label "
+          + "                     from lookuptable_court) "
+          + "SELECT courttype, "
+          + "       courtlocation, "
+          + "       label, "
+          + "       CASE "
+          + "           WHEN label LIKE UPPER(:searchStr||'%') THEN 1 "
+          + "           WHEN label LIKE UPPER('% '||:searchStr||'%') THEN 2 "
+          + "           WHEN label LIKE UPPER('%-'||:searchStr||'%') THEN 2 "
+          + "           ELSE 3 "
+          + "           END AS weight "
+          + "FROM label_added "
+          + "WHERE label LIKE UPPER('%'||:searchStr||'%') "
+          + "ORDER BY weight, label")
   Flux<CourtDTO> findBySearchStr(String searchStr);
 }
