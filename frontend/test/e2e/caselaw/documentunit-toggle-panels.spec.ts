d038eb77ea1183c58575f2a8780efc8d207a6f4f
Florian <florian.zechmeister@digitalservice.bund.de>
2023-02-09 11:27:00 +0100

    Add more assertions

--- frontend/test/e2e/caselaw/documentunit-toggle-panels.spec.ts
+++ frontend/test/e2e/caselaw/documentunit-toggle-panels.spec.ts
@@ -52,6 +52,7 @@ test.describe("test the different layout options", () => {
     await page
       .locator("span:text-is('Originaldokument öffnen') + button")
       .click()
+    await expect(page).toHaveURL(/showDocPanel=true/)
 
     await page.locator("span:text-is('Navigation schließen') + button").click()
     await expect(page).toHaveURL(/showNavBar=false/)
