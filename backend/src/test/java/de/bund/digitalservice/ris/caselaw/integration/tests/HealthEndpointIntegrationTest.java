53c1db11aaa0597d6c8a8711b1c91351b7d2037a
Hanna Prinz <hanna.prinz@digitalservice.bund.de>
2023-02-10 11:56:16 +0100

    Added test and fixed config
    RISDEV-997

--- backend/src/test/java/de/bund/digitalservice/ris/caselaw/integration/tests/HealthEndpointIntegrationTest.java
+++ backend/src/test/java/de/bund/digitalservice/ris/caselaw/integration/tests/HealthEndpointIntegrationTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.reactive.server.WebTestClient;
@@ -19,7 +20,11 @@ import org.testcontainers.utility.DockerImageName;
       "otc.obs.bucket-name=testBucket",
       "otc.obs.endpoint=testUrl",
       "local.file-storage=.local-storage",
-      "mail.from.address=test@test.com"
+      "mail.from.address=test@test.com",
+      "management.endpoint.health.probes.enabled=true",
+      "management.health.livenessState.enabled=true",
+      "management.health.readinessState.enabled=true",
+      "management.endpoint.health.group.readiness.include=readinessState,db,redis"
     })
 @Tag("integration")
 @Testcontainers(disabledWithoutDocker = true)
@@ -42,6 +47,7 @@ class HealthEndpointIntegrationTest {
 
     registry.add("spring.data.redis.host", () -> redis.getHost());
     registry.add("spring.data.redis.port", () -> redis.getFirstMappedPort());
+    registry.add("spring.data.redis.timeout", () -> "200");
   }
 
   @Autowired WebTestClient webTestClient;
@@ -49,5 +55,24 @@ class HealthEndpointIntegrationTest {
   @Test
   void shouldExposeHealthEndpoint() {
     webTestClient.get().uri("/actuator/health").exchange().expectStatus().isOk();
+    webTestClient.get().uri("/actuator/health/liveness").exchange().expectStatus().isOk();
+  }
+
+  @Test
+  void shouldBeUnhealthyWithoutRedis() {
+    redis.stop();
+    webTestClient
+        .get()
+        .uri("/actuator/health")
+        .exchange()
+        .expectStatus()
+        .isEqualTo(HttpStatus.SERVICE_UNAVAILABLE);
+    webTestClient.get().uri("/actuator/health/liveness").exchange().expectStatus().isOk();
+    webTestClient
+        .get()
+        .uri("/actuator/health/readiness")
+        .exchange()
+        .expectStatus()
+        .isEqualTo(HttpStatus.SERVICE_UNAVAILABLE);
   }
 }
