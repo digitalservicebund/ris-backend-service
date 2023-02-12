6918d516b1f10c6b539c20d805fb99314b6ea19c
benjaminaaron <benjamin.degenhart@digitalservice.bund.de>
2023-02-12 17:29:24 +0100

    Extract SubjectNode into own component
    RISDEV-1034

---
+++ frontend/src/components/SubjectNode.vue
@@ -0,0 +1,32 @@
+<script lang="ts" setup>
+import { SubjectNode } from "@/domain/SubjectTree"
+
+interface Props {
+  node: SubjectNode
+}
+
+const props = defineProps<Props>()
+
+const emits = defineEmits<{
+  (e: "node:toggle", nodeId: string): void
+}>()
+</script>
+
+<template>
+  <div :style="{ 'padding-left': `${props.node.depth * 40}px` }">
+    <button
+      class="align-middle pr-4 text-blue-800"
+      @click="emits('node:toggle', node.id)"
+    >
+      <span
+        aria-label="Sachgebietsbaum aufklappen"
+        class="bg-blue-200 material-icons rounded-full w-icon"
+        >{{ props.node.isExpanded ? "remove" : "add" }}</span
+      >
+    </button>
+    <span v-if="props.node.id !== 'root'" class="pl-6">{{
+      props.node.id
+    }}</span>
+    <span class="pl-6 text-blue-800 text-sm">{{ props.node.stext }}</span>
+  </div>
+</template>
--- frontend/src/components/SubjectTree.vue
+++ frontend/src/components/SubjectTree.vue
@@ -1,5 +1,6 @@
 <script lang="ts" setup>
 import { onMounted, ref } from "vue"
+import SubjectNode from "@/components/SubjectNode.vue"
 import SubjectTree from "@/domain/SubjectTree"
 import SubjectsService from "@/services/subjectsService"
 
@@ -21,22 +22,10 @@ onMounted(fetchTree)
 
 <template>
   <h1 class="heading-03-regular pb-8">Sachgebietsbaum</h1>
-  <div
+  <SubjectNode
     v-for="node in tree?.getOrderedNodes()"
     :key="node.id"
-    :style="{ 'padding-left': `${node.depth * 40}px` }"
-  >
-    <button
-      class="align-middle pr-4 text-blue-800"
-      @click="handleNodeClick(node.id)"
-    >
-      <span
-        aria-label="Sachgebietsbaum aufklappen"
-        class="bg-blue-200 material-icons rounded-full w-icon"
-        >{{ node.isExpanded ? "remove" : "add" }}</span
-      >
-    </button>
-    <span v-if="node.id !== 'root'" class="pl-6">{{ node.id }}</span>
-    <span class="pl-6 text-blue-800 text-sm">{{ node.stext }}</span>
-  </div>
+    :node="node"
+    @node:toggle="handleNodeClick"
+  />
 </template>
