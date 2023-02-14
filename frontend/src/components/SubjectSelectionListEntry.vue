617e3a9abf60d1a9106deac79af1c8fd5d0a7d64
Ralph von Petersdorff-Campen <ralph.vonpetersdorffcampen@digitalservice.bund.de>
2023-02-14 09:34:30 +0100

    Some layout improvments for subject list

--- frontend/src/components/SubjectSelectionListEntry.vue
+++ frontend/src/components/SubjectSelectionListEntry.vue
@@ -16,14 +16,11 @@ function handleRemoveFromList() {
     <div>
       <span class="material-icons p-6 text-blue-800">open_in_new</span>
     </div>
-    <div class="flex-1 pt-8">
-      <span class="leading-{22px} text-blue-800 text-{16px}">{{
-        props.subject.id
-      }}</span
-      ><br />
-      <span class="leading-{18px} text-blue-800 text-{14px}">
+    <div class="flex-1 flex-col pt-8">
+      <div class="label-02-reg text-blue-800">{{ props.subject.id }}</div>
+      <div class="label-03-reg text-blue-800">
         {{ props.subject.stext }}
-      </span>
+      </div>
     </div>
     <div>
       <button
