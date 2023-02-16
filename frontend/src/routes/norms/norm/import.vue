3520adfc26d75b18053eac3623eddbf1ebad34fe
Magdalena Noffke <magdalena.noffke@digitalservice.bund.de>
2023-02-16 17:15:29 +0100

    Add import service to component

--- frontend/src/routes/norms/norm/import.vue
+++ frontend/src/routes/norms/norm/import.vue
@@ -1,5 +1,32 @@
 <script lang="ts" setup>
+import { ref } from "vue"
+import { useRouter } from "vue-router"
 import FileUpload from "@/components/FileUpload.vue"
+import { ResponseError } from "@/services/httpClient"
+import { importNorm } from "@/services/normsService"
+
+const error = ref<ResponseError>()
+const isUploading = ref(false)
+
+const router = useRouter()
+
+async function upload(file: File) {
+  isUploading.value = true
+
+  try {
+    const response = await importNorm(file)
+    if (response.status === 201 && response.data) {
+      await router.replace({
+        name: "norms-norm-:normGuid",
+        params: { normGuid: response.data },
+      })
+    } else {
+      error.value = response.error
+    }
+  } finally {
+    isUploading.value = false
+  }
+}
 </script>
 
 <template>
@@ -12,7 +39,11 @@ import FileUpload from "@/components/FileUpload.vue"
           erstellen.
         </p>
       </div>
-      <FileUpload />
+      <FileUpload
+        :error="error"
+        :is-loading="isUploading"
+        @file-selected="(file) => upload(file)"
+      />
     </div>
   </div>
 </template>
