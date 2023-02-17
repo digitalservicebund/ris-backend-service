3519d97188f96aad387ddcdb9ffe8df3024a0c85
Thore Strassburg <thore@weilbier.net>
2023-02-17 17:05:51 +0100

    Chore: pipeline debugging commit

--- frontend/test/e2e/norms/e2e-utils.ts
+++ frontend/test/e2e/norms/e2e-utils.ts
@@ -27,18 +27,18 @@ async function downloadJurisTestFile(
   const username = process.env.GH_READ_JURIS_TEST_FILES_USER
   const password = process.env.GH_READ_JURIS_TEST_FILES_TOKEN
   console.warn(
-    `username is defined? `,
-    username !== undefined,
-    username!.length > 0,
-    username!.substring(0, 5)
+    `process.envusername is defined? `,
+    process.env.GH_READ_JURIS_TEST_FILES_USER !== undefined
   )
+  console.warn(`username is defined? `, username !== undefined)
+  console.warn(`username is: `, username?.substring(0, 5))
   console.warn(
-    `password is defined? `,
-    password !== undefined,
-    password!.length > 0,
-    password!.substring(0, 5)
+    `process.env.password is defined? `,
+    process.env.GH_READ_JURIS_TEST_FILES_TOKEN !== undefined
   )
-  const remoteUrl = `https://${username}:${password}@${REMOTE_JURIS_TEST_FILE_FOLDER_URL}/${fileName}`
+  console.warn(`password is defined? `, password !== undefined)
+  console.warn(`password is: `, password?.substring(0, 5))
+  const remoteUrl = `https://${process.env.GH_READ_JURIS_TEST_FILES_USER}:${process.env.GH_READ_JURIS_TEST_FILES_TOKEN}@${REMOTE_JURIS_TEST_FILE_FOLDER_URL}/${fileName}`
   console.warn(remoteUrl)
   const response = await request.get(remoteUrl)
 
