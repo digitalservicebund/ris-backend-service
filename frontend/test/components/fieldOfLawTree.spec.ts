00fc05c82da26adb7822a2596d6b4fa0f2e213a1
benjaminaaron <benjamin.degenhart@digitalservice.bund.de>
2023-02-20 20:38:56 +0100

    Add unit tests for FieldOfLawTree
    RISDEV-1034 RISDEV-1089

--- frontend/test/components/fieldOfLawTree.spec.ts
+++ frontend/test/components/fieldOfLawTree.spec.ts
@@ -1,5 +1,6 @@
 /* eslint-disable jest-dom/prefer-in-document */
-import { render } from "@testing-library/vue"
+import userEvent from "@testing-library/user-event"
+import { render, screen } from "@testing-library/vue"
 import FieldOfLawTreeVue from "@/components/FieldOfLawTree.vue"
 import { FieldOfLawNode } from "@/domain/fieldOfLawTree"
 import FieldOfLawService from "@/services/fieldOfLawService"
@@ -18,6 +19,8 @@ function renderComponent(
 }
 
 describe("SubjectTree", () => {
+  const user = userEvent.setup()
+
   const fetchSpy = vi
     .spyOn(FieldOfLawService, "getChildrenOf")
     .mockImplementation(() =>
@@ -25,22 +28,68 @@ describe("SubjectTree", () => {
         status: 200,
         data: [
           {
-            subjectFieldNumber: "branch",
-            subjectFieldText: "Alle Sachgebiete",
+            subjectFieldNumber: "AB-01",
+            subjectFieldText: "Text for AB",
             children: [],
             depth: 1,
             isExpanded: false,
-            isLeaf: true,
+            isLeaf: false,
+          },
+          {
+            subjectFieldNumber: "CD-02",
+            subjectFieldText: "And text for CD with link to AB-01",
+            children: [],
+            linkedFields: ["AB-01"],
+            depth: 1,
+            isExpanded: false,
+            isLeaf: false,
           },
         ],
       })
     )
 
-  it("Is closed", async () => {
+  it("Tree is fully closed upon at start", async () => {
     renderComponent()
     expect(fetchSpy).toBeCalledTimes(0)
-    // expect(screen.queryByText("Alle Sachgebiete anzeigen")).toBeInTheDocument()
-    // expect(screen.queryByText("TE-Subject")).not.toBeInTheDocument()
-    // expect(screen.queryByText("TE-Subject-01")).not.toBeInTheDocument()
+    expect(screen.getByText("Alle Sachgebiete anzeigen")).toBeInTheDocument()
+    const expandIcons = screen.getAllByLabelText("Sachgebietsbaum aufklappen")
+    expect(expandIcons).toHaveLength(1)
+    expect(screen.queryByText("Text for AB")).not.toBeInTheDocument()
+    expect(screen.queryByText("And text for CD")).not.toBeInTheDocument()
+  })
+
+  it("Tree opens top level nodes upon root click", async () => {
+    renderComponent()
+
+    await user.click(
+      screen.getAllByLabelText("Sachgebietsbaum aufklappen")[0] as HTMLElement
+    )
+
+    const expandIcons = screen.getAllByLabelText("Sachgebietsbaum aufklappen")
+
+    expect(fetchSpy).toBeCalledTimes(1)
+    expect(expandIcons).toHaveLength(3)
+    expect(screen.getByText("Text for AB")).toBeInTheDocument()
+    expect(screen.getByText("And text for CD with link to")).toBeInTheDocument()
+    expect(screen.getByText("Alle Sachgebiete anzeigen")).toBeInTheDocument()
+  })
+
+  it("Linked node gets displayed as link in stext", async () => {
+    renderComponent()
+
+    await user.click(
+      screen.getAllByLabelText("Sachgebietsbaum aufklappen")[0] as HTMLElement
+    )
+
+    const node1ids = screen.getAllByText("AB-01")
+    const nonLinkText = screen.getByText("And text for CD with link to")
+
+    expect(node1ids).toHaveLength(2)
+    expect(node1ids[1] as HTMLElement).toHaveAttribute("class", "linked-field")
+    expect(nonLinkText as HTMLElement).not.toHaveAttribute(
+      "class",
+      "linked-field"
+    )
+    // expect(emitted()["linkedField:clicked"]).toHaveLength(1)
   })
 })
