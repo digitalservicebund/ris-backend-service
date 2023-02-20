65c5a123146e3c298189149e5af659ab6357d9bb
Thore Strassburg <thore@weilbier.net>
2023-02-20 17:47:27 +0100

    Test(norms): add e2e test to import by file upload
    Note that we only have a very few expectations to verify if the import
    was successful. Namely we got redirected to a norm and that the title is
    matching the expected one.
    All other data that gets extracted during the import is already covered
    by other test cases.

---
+++ frontend/test/e2e/norms/import-norm-by-file-upload.spec.ts
@@ -0,0 +1,50 @@
+import { expect, test } from "@playwright/test"
+import { createDataTransfer } from "../shared/e2e-utils"
+import { loadJurisTestFile } from "./e2e-utils"
+import normData from "./testdata/norm_basic.json"
+
+test.describe("import a norm by uploading a file", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto("/norms/import")
+  })
+
+  test("upload Juris Zip file per file chooser", async ({ page, request }) => {
+    const { filePath } = await loadJurisTestFile(
+      request,
+      normData.jurisZipFileName
+    )
+
+    const fileChooserEvent = page.waitForEvent("filechooser")
+    await page.locator("text=oder Datei auswählen").click()
+    const fileChooser = await fileChooserEvent
+    await fileChooser.setFiles(filePath)
+
+    await page.waitForURL("/norms/norm/*")
+
+    await expect(
+      page.locator(`text=${normData.officialLongTitle}`)
+    ).toBeVisible()
+  })
+
+  test("it can upload Juris Zip file via drag and drop", async ({
+    page,
+    request,
+  }) => {
+    const fileName = normData.jurisZipFileName
+    const { fileContent } = await loadJurisTestFile(request, fileName)
+    const dataTransfer = await createDataTransfer(
+      page,
+      fileContent,
+      fileName,
+      "application/zip"
+    )
+
+    await page.dispatchEvent(".upload-drop-area", "drop", { dataTransfer })
+
+    await page.waitForURL("/norms/norm/*")
+
+    await expect(
+      page.locator(`text=${normData.officialLongTitle}`)
+    ).toBeVisible()
+  })
+})
