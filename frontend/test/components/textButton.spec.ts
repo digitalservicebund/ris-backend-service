eb9e585cef3bf4573fcf2241e4a4c2892c411fff
Thore Strassburg <thore@weilbier.net>
2023-02-21 12:18:10 +0100

    Test: add more tests for the button component
    This also adds the missing tests for the recently added functionality
    that a button can act as a link.

--- frontend/test/components/textButton.spec.ts
+++ frontend/test/components/textButton.spec.ts
@@ -10,4 +10,19 @@ describe("TextButton", () => {
     })
     expect(wrapper.props().label).toBe("foo")
   })
+
+  it("renders a button element per default", () => {
+    const wrapper = mount(TextButton)
+
+    expect(wrapper.find("button").exists()).toBeTruthy()
+  })
+
+  it("renders an anchor element if the hypertext reference property is set", () => {
+    const wrapper = mount(TextButton, {
+      props: { href: "https://test.org" },
+    })
+
+    expect(wrapper.find("a").exists()).toBeTruthy()
+    expect(wrapper.find("button").exists()).toBeFalsy()
+  })
 })
