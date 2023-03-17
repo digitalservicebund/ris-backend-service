24e8dd32009583dae2d10ff52737c9c5a927fcca
Thore Strassburg <thore@weilbier.net>
2023-03-17 17:02:02 +0100

    Fix(norms): fix flaky norms comparison in tests
    The loading of list data from the database is unsorted. This means if
    a norm gets loaded from the database, the order of the articles, file
    references, paragraphs and metadata can be sorted slightly differently
    every time. This is due to the reactive access.
    Anyway, even if they would be strictly sorted based on any order, the
    order of the same data as created (free by hand) can differ from the
    sorting of the database.
    In result, the comparison of two norm instance with exactly the same
    data, but different orders, leads to (flaky) failing tests.
    
    To fix this problem, a quite generic assertion functions was written
    that uses reflection and recognized collections to apply a set on them
    before comparison. Unfortunately it is pretty hard to do this on
    a nested level where paragraphs within the articles are sorted
    differently.
    To have a solid solution for now, a function sorts all known list data
    of a norm first and then compares them. The huge disadvantage is that
    this is not generic and changes to the data structure can make it
    ineffective again.

--- backend/src/test/kotlin/utils/ReflectionAssertor.kt
+++ backend/src/test/kotlin/utils/ReflectionAssertor.kt
@@ -1,11 +1,16 @@
 package utils
 
 import de.bund.digitalservice.ris.norms.application.port.input.EditNormFrameUseCase
+import de.bund.digitalservice.ris.norms.domain.entity.Article
+import de.bund.digitalservice.ris.norms.domain.entity.FileReference
+import de.bund.digitalservice.ris.norms.domain.entity.Metadatum
 import de.bund.digitalservice.ris.norms.domain.entity.Norm
+import de.bund.digitalservice.ris.norms.domain.entity.Paragraph
 import de.bund.digitalservice.ris.norms.framework.adapter.input.restapi.controller.EditNormFrameController
 import de.bund.digitalservice.ris.norms.framework.adapter.input.restapi.decodeLocalDate
 import org.assertj.core.api.Assertions.assertThat
 import java.time.LocalDate
+import kotlin.comparisons.compareBy
 import kotlin.reflect.full.memberProperties
 
 fun assertEditNormFrameProperties(
@@ -54,23 +59,32 @@ fun assertNormAndEditNormFrameProperties(
     }
 }
 
+/**
+ * ATTENTION: File references are excluded atm., as they do not work properly,
+ * yet. We NEED to fix this!
+ */
 fun assertNormsAreEqual(norm1: Norm, norm2: Norm) {
-    val members = Norm::class.memberProperties
-    val supportedMembers = members.filter { it.name != "files" }
-
-    supportedMembers.forEach { member ->
-        val norm1Value = member(norm1)
-        val norm2Value = member(norm2)
-
-        when (norm1Value) {
-            is Iterable<*> ->
-                assertThat(norm1Value.toSet()).isEqualTo((norm2Value as Iterable<*>).toSet())
-
-            is LocalDate ->
-                assertThat(norm1Value).isEqualTo(decodeLocalDate(norm2Value.toString()))
-
-            else ->
-                assertThat(norm1Value).isEqualTo(norm2Value)
-        }
-    }
+    val sortedNorm1 = getNormWithSortedListProperties(norm1).copy(files = emptyList())
+    val sortedNorm2 = getNormWithSortedListProperties(norm2).copy(files = emptyList())
+    assertThat(sortedNorm1.articles).isEqualTo(sortedNorm2.articles)
 }
+
+fun getNormWithSortedListProperties(norm: Norm): Norm {
+    val metadata = norm.metadata.toMutableList().sortedWith(metadatumComparator)
+    val fileReferences = norm.files.toMutableList().sortedWith(fileReferenceComparator)
+    var articles = norm.articles.toMutableList().sortedWith(articleComparator)
+
+    articles = articles.map {
+        it.copy(paragraphs = it.paragraphs.toMutableList().sortedWith(paragraphComparator))
+    }
+
+    return norm.copy(metadata = metadata, files = fileReferences, articles = articles)
+}
+
+// As there is n natural sorting for our domain classes, the comparators try to
+// be exhaustive on the properties as possible. Else it could happen to easily
+// with random/stupid test data that the sorting is not definitely.
+private val metadatumComparator = compareBy<Metadatum<*>>({ it.type }, { it.order })
+private val fileReferenceComparator = compareBy<FileReference>({ it.hash }, { it.name }, { it.createdAt })
+private val articleComparator = compareBy<Article>({ it.guid }, { it.marker }, { it.title })
+private val paragraphComparator = compareBy<Paragraph>({ it.guid }, { it.marker }, { it.text })
