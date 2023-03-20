cefb219422dd11378c75a0c7a68584c53f5c939e
benjaminaaron <benjamin.degenhart@digitalservice.bund.de>
2023-03-20 16:55:12 +0100

    Fix failing style check
    RISDEV-1182

--- frontend/src/components/FieldOfLawDirectInputSearch.vue
+++ frontend/src/components/FieldOfLawDirectInputSearch.vue
@@ -1,6 +1,7 @@
 <script lang="ts" setup>
 import ComboboxInput from "@/components/ComboboxInput.vue"
 import TextButton from "@/components/TextButton.vue"
+import { ComboboxInputModelType } from "@/domain"
 import { FieldOfLawComboboxItem } from "@/domain/fieldOfLaw"
 import ComboboxItemService from "@/services/comboboxItemService"
 
@@ -8,8 +9,9 @@ const emit = defineEmits<{
   (event: "add-to-list", identifier: string): void
 }>()
 
-function handleUpdateModelValue(item: FieldOfLawComboboxItem | undefined) {
-  if (item) emit("add-to-list", item.label)
+function handleUpdateModelValue(item: ComboboxInputModelType | undefined) {
+  if (!item) return
+  emit("add-to-list", (item as FieldOfLawComboboxItem).label)
 }
 </script>
 
