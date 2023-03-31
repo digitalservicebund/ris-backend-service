e98cbae0a2f51933cefaaa89b1ba511f5b7bdbc2
Thore Strassburg <thore@weilbier.net>
2023-03-31 16:19:40 +0200

    Feat: add expandable data set component
    This component is intended to be re-used by the new design for all the
    complex metadata sections of norms. It has a small feature set, but is
    quite flexible.
    
    There are some issues with the transition of the underlying
    `ExpandableContent` component which become more visible with the new
    intend usage. We might have to fix that soon.

---
+++ frontend/src/components/ExpandableDataSet.vue
@@ -0,0 +1,44 @@
+<script lang="ts" setup>
+import { ref } from "vue"
+import type { Component } from "vue"
+import ExpandableContent from "@/components/ExpandableContent.vue"
+import DataSetSummary from "@/shared/components/DataSetSummary.vue"
+import TextButton from "@/shared/components/input/TextButton.vue"
+
+interface Props {
+  title: string
+  dataSet: any // eslint-disable-line @typescript-eslint/no-explicit-any
+  summaryComponent?: Component
+}
+
+withDefaults(defineProps<Props>(), {
+  summaryComponent: DataSetSummary,
+})
+
+const isExpanded = ref(false)
+
+function collapse(): void {
+  isExpanded.value = false
+}
+</script>
+
+<template>
+  <ExpandableContent
+    v-model:is-expanded="isExpanded"
+    class="p-16"
+    close-icon-name="expand_less"
+    open-icon-name="expand_more"
+  >
+    <template #header>
+      <div class="flex">
+        <h2 class="label-02-bold min-w-[20rem] text-left">{{ title }}</h2>
+        <Component :is="summaryComponent" v-if="!isExpanded" :data="dataSet" />
+      </div>
+    </template>
+
+    <div class="flex flex-col gap-32 items-start mt-24">
+      <slot />
+      <TextButton aria-label="Fertig" label="Fertig" @click="collapse" />
+    </div>
+  </ExpandableContent>
+</template>
---
+++ frontend/test/components/expandableDataSet.spec.ts
@@ -0,0 +1,129 @@
+import userEvent from "@testing-library/user-event"
+import { render, screen } from "@testing-library/vue"
+import { Component, defineComponent, markRaw } from "vue"
+import ExpandableDataSet from "@/components/ExpandableDataSet.vue"
+
+const JsonStringifySummary = defineComponent({
+  props: {
+    data: {
+      default: undefined,
+      validator: () => true,
+    },
+  },
+  template: "<span>{{ JSON.stringify(data) }}</span>",
+})
+
+function renderComponent(options?: {
+  title?: string
+  dataSet?: any // eslint-disable-line @typescript-eslint/no-explicit-any
+  summaryComponent?: Component
+  defaultSlot?: string
+}) {
+  const props = {
+    title: options?.title ?? "title",
+    dataSet: options?.dataSet ?? [],
+    summaryComponent: markRaw(
+      options?.summaryComponent ?? JsonStringifySummary
+    ),
+  }
+
+  const slots = {
+    default: options?.defaultSlot ?? "",
+  }
+
+  render(ExpandableDataSet, { props, slots })
+}
+
+describe("ExpandableDataSet", () => {
+  beforeAll(() => {
+    global.ResizeObserver = require("resize-observer-polyfill")
+  })
+
+  it("shows the given title", () => {
+    renderComponent({ title: "test title" })
+
+    const title = screen.queryByText("test title")
+
+    expect(title).toBeVisible()
+  })
+
+  it("shows a summary with the given dataset on initial render", () => {
+    renderComponent({
+      dataSet: ["foo", "bar"],
+      summaryComponent: JsonStringifySummary,
+    })
+
+    const summary = screen.queryByText('["foo","bar"]')
+
+    expect(summary).toBeVisible()
+  })
+
+  it("hides the default slot and close button on initial render", () => {
+    renderComponent({ defaultSlot: "special" })
+
+    const slot = screen.queryByText("special")
+    const button = screen.queryByText("Fertig")
+
+    expect(slot).not.toBeVisible()
+    expect(button).not.toBeVisible()
+  })
+
+  it("toggles the default slot and close button when clicking on title", async () => {
+    const user = userEvent.setup()
+    renderComponent({ title: "test title", defaultSlot: "content" })
+    const title = screen.getByText("test title")
+
+    await user.click(title)
+
+    expect(screen.queryByText("content")).toBeVisible()
+    expect(screen.queryByText("Fertig")).toBeVisible()
+
+    await user.click(title)
+
+    expect(screen.queryByText("content")).not.toBeVisible()
+    expect(screen.queryByText("Fertig")).not.toBeVisible()
+  })
+
+  it("shows the default slot and close button when clicking on the summary", async () => {
+    const user = userEvent.setup()
+    renderComponent({
+      dataSet: ["foo", "bar"],
+      summaryComponent: JsonStringifySummary,
+      defaultSlot: "content",
+    })
+    const summary = screen.getByText('["foo","bar"]')
+
+    await user.click(summary)
+
+    expect(screen.queryByText("content")).toBeVisible()
+    expect(screen.queryByText("Fertig")).toBeVisible()
+  })
+
+  it("hides the summary when opening data set", async () => {
+    const user = userEvent.setup()
+    renderComponent({
+      dataSet: ["foo", "bar"],
+      summaryComponent: JsonStringifySummary,
+      defaultSlot: "content",
+    })
+
+    const summary = screen.getByText('["foo","bar"]')
+    await user.click(summary)
+
+    expect(summary).not.toBeVisible()
+  })
+
+  it("it hides the default slot and close button when clicking the latter", async () => {
+    const user = userEvent.setup()
+    renderComponent({ title: "test title", defaultSlot: "content" })
+    const title = screen.getByText("test title")
+    await user.click(title)
+    const button = screen.getByText("Fertig")
+
+    await user.click(button)
+    const slot = screen.queryByText("content")
+
+    expect(button).not.toBeVisible()
+    expect(slot).not.toBeVisible()
+  })
+})
