97f1742827217fd2b3905474b3a8cfac9b3293b4
Thore Strassburg <thore@weilbier.net>
2023-03-31 16:32:25 +0200

    Feat: replace expandable data field component
    The new and more slim component `ExpandableDataSet` is the new version
    which serves the same purpose as the former one.
    Updates all the related parts to show it on the kitchensink.

--- frontend/src/kitchensink/components/KitchensinkBody.vue
+++ frontend/src/kitchensink/components/KitchensinkBody.vue
@@ -8,7 +8,7 @@ import DocumentUnitInfoPanelView from "@/kitchensink/views/DocumentUnitInfoPanel
 import DropdownView from "@/kitchensink/views/DropdownView.vue"
 import ErrorView from "@/kitchensink/views/ErrorView.vue"
 import ExpandableContentView from "@/kitchensink/views/ExpandableContentView.vue"
-import ExpandableFieldSetView from "@/kitchensink/views/ExpandableFieldSetView.vue"
+import ExpandableDataSetView from "@/kitchensink/views/ExpandableDataSetView.vue"
 import FileUploadView from "@/kitchensink/views/FileUploadView.vue"
 import FileViewerView from "@/kitchensink/views/FileViewerView.vue"
 import InfoModalView from "@/kitchensink/views/InfoModalView.vue"
@@ -34,8 +34,8 @@ const component = computed(() => {
       return InfoModalView
     case "ExpandableContentView":
       return ExpandableContentView
-    case "ExpandableFieldSetView":
-      return ExpandableFieldSetView
+    case "ExpandableDataSetView":
+      return ExpandableDataSetView
     case "FileUploadView":
       return FileUploadView
     case "FileViewerView":
---
+++ frontend/src/kitchensink/views/ExpandableDataSetView.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup>
+import ExpandableDataSet from "@/components/ExpandableDataSet.vue"
+</script>
+
+<template>
+  <ExpandableDataSet
+    :data-set="['First Value', 'SecondValue']"
+    title="Short Section"
+  >
+    <div class="h-64">Content</div>
+  </ExpandableDataSet>
+
+  <ExpandableDataSet :data-set="['Single Value']" title="Long Section Name">
+    <div class="h-64">Content</div>
+  </ExpandableDataSet>
+</template>
