19951cdfbe6cb1a184bce590590d47254a352a3c
Leonie Koch <lk.leonie.koch@gmail.com>
2023-04-06 16:49:28 +0200

    Add stylings to search results
    RISDEV-1408

--- frontend/src/components/proceedingDecisions/SearchResultList.vue
+++ frontend/src/components/proceedingDecisions/SearchResultList.vue
@@ -22,19 +22,25 @@ export type SearchResults = {
     <div
       v-for="searchResult in searchResults"
       :key="searchResult.decision.uuid"
-      class="link-01-bold mb-24 mt-12 table-row underline"
+      class="mb-24 mt-12 table-row"
     >
-      <div class="table-cell">
+      <div class="link-01-bold table-cell">
         <InlineDecision :decision="searchResult.decision" />
-        <span v-if="searchResult.isLinked"> - Bereits hinzugefügt</span>
       </div>
       <div class="p-8 table-cell">
         <TextButton
           aria-label="Treffer übernehmen"
+          class="ml-24"
+          :disabled="searchResult.isLinked"
           label="Übernehmen"
           @click="emits('linkDecision', searchResult.decision.uuid as string)"
         />
       </div>
+      <span
+        v-if="searchResult.isLinked"
+        class="bg-green-700 label-03-reg ml-24 px-24 py-4 rounded-full text-white"
+        >Bereits hinzugefügt</span
+      >
     </div>
   </div>
 </template>
