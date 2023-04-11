37623af574d8f35811b877aa3d27de267843ba8e
Haytham Mones <haythameyd@gmail.com>
2023-04-11 10:21:38 +0200

    Chore: Fix timezone of the app to Europe/Berlin

--- backend/src/main/java/de/bund/digitalservice/ris/Application.java
+++ backend/src/main/java/de/bund/digitalservice/ris/Application.java
@@ -2,11 +2,17 @@ package de.bund.digitalservice.ris;
 
 import io.sentry.Sentry;
 import java.util.Date;
+import java.util.TimeZone;
+import javax.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
+  @PostConstruct
+  public void init() {
+    TimeZone.setDefault(TimeZone.getTimeZone("Europe/Berlin"));
+  }
 
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
