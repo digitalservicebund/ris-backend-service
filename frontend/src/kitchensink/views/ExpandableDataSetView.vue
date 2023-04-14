846132ac40cf6309603323aea024e94e74644991
Tarek Hamaoui <tarek.hamaoui@digitalservice.bund.de>
2023-04-14 14:49:30 +0200

    RISDEV-1209 Adds print annoncement input component

--- frontend/src/kitchensink/views/ExpandableDataSetView.vue
+++ frontend/src/kitchensink/views/ExpandableDataSetView.vue
@@ -1,10 +1,18 @@
 <script lang="ts" setup>
-import { h } from "vue"
+import { h, ref } from "vue"
+import AgeIndicationInputGroup from "@/components/AgeIndicationInputGroup.vue"
+import CitationDateInputGroup from "@/components/CitationDateInputGroup.vue"
 import ExpandableDataSet from "@/components/ExpandableDataSet.vue"
+import LeadInputGroup from "@/components/LeadInputGroup.vue"
+import ParticipatingInstitutionsInputGroup from "@/components/ParticipatingInstitutionInputGroup.vue"
+import PrintAnnouncementInputGroup from "@/components/PrintAnnouncementInputGroup.vue"
+import SubjectAreaInputGroup from "@/components/SubjectAreaInputGroup.vue"
+import { MetadatumType } from "@/domain/Norm"
 import DataSetSummary, {
   withSummarizer,
   defaultSummarizer,
 } from "@/shared/components/DataSetSummary.vue"
+import EditableList from "@/shared/components/EditableList.vue"
 
 function chipSummarizer(dataEntry: undefined) {
   return h(
@@ -13,6 +21,24 @@ function chipSummarizer(dataEntry: undefined) {
     defaultSummarizer(dataEntry)
   )
 }
+
+const leadData = ref<string[]>([])
+const subjectData = ref<string[]>([])
+const participationData = ref<string[]>([])
+const citationDateData = ref<string[]>([])
+const ageIndicationData = ref<string[]>([])
+const printAnnouncementData = ref<string[]>([])
+
+const defaultLead = { jurisdiction: "", unit: "" }
+const defaultSubject = { fna: "", previousFna: "", gesta: "", bgb3: "" }
+const defaultParticipation = { type: "", institution: "" }
+const defaultAgeIndication = {
+  start: "",
+  finish: "",
+  startUnitValue: "",
+  endUnitValue: "",
+}
+const defaultPrintAnnouncement = { type: "", institution: "" }
 
 const ChipSummary = withSummarizer(chipSummarizer)
 </script>
@@ -39,5 +65,56 @@ const ChipSummary = withSummarizer(chipSummarizer)
     title="Special Section"
   >
     <div class="h-64">Content</div>
+  </ExpandableDataSet>
+
+  <ExpandableDataSet :data-set="leadData" title="Lead">
+    <EditableList
+      v-model="leadData"
+      :default-value="defaultLead"
+      :edit-component="LeadInputGroup"
+    />
+  </ExpandableDataSet>
+
+  <ExpandableDataSet :data-set="subjectData" title="Subject">
+    <EditableList
+      v-model="subjectData"
+      :default-value="defaultSubject"
+      :edit-component="SubjectAreaInputGroup"
+    />
+  </ExpandableDataSet>
+
+  <ExpandableDataSet :data-set="participationData" title="Participation">
+    <EditableList
+      v-model="participationData"
+      :default-value="defaultParticipation"
+      :edit-component="ParticipatingInstitutionsInputGroup"
+    />
+  </ExpandableDataSet>
+
+  <ExpandableDataSet :data-set="citationDateData" title="Zitierdatum">
+    <EditableList
+      v-model="citationDateData"
+      :default-value="{}"
+      :edit-component="CitationDateInputGroup"
+    />
+  </ExpandableDataSet>
+
+  <ExpandableDataSet :data-set="ageIndicationData" title="Altersangabe">
+    <EditableList
+      v-model="ageIndicationData"
+      :default-value="defaultAgeIndication"
+      :edit-component="AgeIndicationInputGroup"
+    />
+  </ExpandableDataSet>
+
+  <ExpandableDataSet
+    :data-set="printAnnouncementData"
+    title="Amtliche Fundstelle"
+  >
+    <EditableList
+      v-model="printAnnouncementData"
+      :default-value="defaultAgeIndication"
+      :edit-component="PrintAnnouncementInputGroup"
+    />
   </ExpandableDataSet>
 </template>
