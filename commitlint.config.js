cb1df4dbd3c2d25ca1839c3ca6cb28511768b20c
Thore Strassburg <thore@weilbier.net>
2023-04-20 08:31:44 +0200

    Chore: add commit lint rule for ticket numbers
    The team has decided that we always want to include the related ticket
    number in each commit message. This allows our stakeholder to get more
    insights into our work. To support ourselves here to not forget this
    convention, the commit linting rules take care of it.
    
    RISDEV-0000

--- commitlint.config.js
+++ commitlint.config.js
@@ -5,5 +5,11 @@ module.exports = {
     "header-case": [2, "always", "sentence-case"],
     "header-full-stop": [2, "never", "."],
     "header-max-length": [2, "always", 50],
+    "references-empty": [2, "never"],
+  },
+  parserPreset: {
+    parserOpts: {
+      issuePrefixes: ["RISDEV-"],
+    },
   },
 };
