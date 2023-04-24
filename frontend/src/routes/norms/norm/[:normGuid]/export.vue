3122f9e5c3832ed489577b6aa2cfcd44e5fce5a4
Haytham Mones <haythameyd@gmail.com>
2023-04-24 14:02:37 +0200

    RISDEV-1217 | Fix timezone for export page

--- frontend/src/routes/norms/norm/[:normGuid]/export.vue
+++ frontend/src/routes/norms/norm/[:normGuid]/export.vue
@@ -33,7 +33,7 @@ const downloadUrl = computed(() =>
 const downloadIsPossible = computed(() => downloadUrl.value != undefined)
 
 const formatDateTime = function (date: string): string {
-  return dayjs(date).tz("Europe/Berlin").format("DD.MM.YYYY HH:mm")
+  return dayjs.utc(date).tz(dayjs.tz.guess()).format("DD.MM.YYYY HH:mm")
 }
 
 async function getFileLink() {
