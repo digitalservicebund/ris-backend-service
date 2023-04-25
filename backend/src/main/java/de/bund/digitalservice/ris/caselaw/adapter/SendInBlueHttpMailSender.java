f8c801213f9976c8abef6fd9e5ce0f84786128a9
Florian <florian.zechmeister@digitalservice.bund.de>
2023-04-25 18:11:17 +0200

    Revert "Test reply-to"
    This reverts commit 631baeda6b0bb12fb443dd973211057222086d72.

--- backend/src/main/java/de/bund/digitalservice/ris/caselaw/adapter/SendInBlueHttpMailSender.java
+++ backend/src/main/java/de/bund/digitalservice/ris/caselaw/adapter/SendInBlueHttpMailSender.java
@@ -15,7 +15,6 @@ import sendinblue.auth.ApiKeyAuth;
 import sibApi.TransactionalEmailsApi;
 import sibModel.SendSmtpEmail;
 import sibModel.SendSmtpEmailAttachment;
-import sibModel.SendSmtpEmailReplyTo;
 import sibModel.SendSmtpEmailSender;
 import sibModel.SendSmtpEmailTo;
 
@@ -63,11 +62,6 @@ public class SendInBlueHttpMailSender implements HttpMailSender {
     sendSmtpEmail.setSubject(subject);
     sendSmtpEmail.setAttachment(attachmentList);
     sendSmtpEmail.setTags(tags);
-
-    SendSmtpEmailReplyTo replyTo = new SendSmtpEmailReplyTo();
-    replyTo.setEmail("florian.zechmeister@digitalservice.bund.de");
-    replyTo.setName("Test Florian");
-    sendSmtpEmail.setReplyTo(replyTo);
 
     try {
       api.sendTransacEmail(sendSmtpEmail);
