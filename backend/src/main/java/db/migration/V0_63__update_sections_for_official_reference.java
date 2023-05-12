a03a901043c3f8577a961586d2861f854ba4d950
Haytham Mones <haythameyd@gmail.com>
2023-05-12 15:01:27 +0200

    RISDEV-1209 | Moved old sections to new

---
+++ backend/src/main/java/db/migration/V0_63__update_sections_for_official_reference.java
@@ -0,0 +1,50 @@
+package db.migration;
+
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.UUID;
+import org.flywaydb.core.api.migration.BaseJavaMigration;
+import org.flywaydb.core.api.migration.Context;
+
+public class V0_63__update_sections_for_official_reference extends BaseJavaMigration {
+  public void migrate(Context context) throws Exception {
+    try (Statement add = context.getConnection().createStatement()) {
+      try (ResultSet rows =
+          add.executeQuery(
+              "SELECT * FROM metadata_sections WHERE name IN ('PRINT_ANNOUNCEMENT', 'DIGITAL_ANNOUNCEMENT', 'EU_ANNOUNCEMENT', 'OTHER_OFFICIAL_ANNOUNCEMENT') AND section_id IS NULL")) {
+        while (rows.next()) {
+          int childSectionId = rows.findColumn("id");
+          int normId = rows.findColumn("norm_id");
+          int order = rows.findColumn("order_number");
+
+          UUID parentGuid = UUID.randomUUID();
+
+          try (Statement change = context.getConnection().createStatement()) {
+            change.execute(
+                "INSERT INTO metadata_sections (name, norm_id, section_id, order_number, guid) VALUES ('OFFICIAL_REFERENCE', "
+                    + normId
+                    + ", NULL, "
+                    + order
+                    + ", '"
+                    + parentGuid
+                    + "');");
+          }
+
+          try (Statement findParent = context.getConnection().createStatement()) {
+            try (ResultSet parent =
+                findParent.executeQuery("SELECT * metadata_sections WHERE guid=" + parentGuid)) {
+              parent.next();
+              try (Statement update = context.getConnection().createStatement()) {
+                update.execute(
+                    "UPDATE metadata_sections SET section_id='"
+                        + parentGuid
+                        + "', order_number=1 WHERE id="
+                        + childSectionId);
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
