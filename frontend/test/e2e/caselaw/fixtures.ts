188269ad2e9d7788e96414ee6db55eaa08653cfb
Florian <florian.zechmeister@digitalservice.bund.de>
2023-05-16 17:06:16 +0200

    Use isoString in fixture
    Addresses RISDEV-1488

--- frontend/test/e2e/caselaw/fixtures.ts
+++ frontend/test/e2e/caselaw/fixtures.ts
@@ -44,7 +44,7 @@ export const testWithDocumentUnit = test.extend<MyFixtures>({
             },
             fileNumbers: [generateString()],
             documentType: { jurisShortcut: "AnU", label: "Anerkenntnisurteil" },
-            decisionDate: dayjs("2020-01-01", "YYYY-MM-DD").format(),
+            decisionDate: dayjs("2020-01-01", "YYYY-MM-DD").toISOString(),
           },
         },
       }
