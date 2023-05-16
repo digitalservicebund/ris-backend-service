b3ca24640b5204907cdcd37e2b7c149452e21ffc
benjaminaaron <benjamin.degenhart@digitalservice.bund.de>
2023-05-16 13:47:07 +0200

    Manual undo of accidental merge changes
    RISDEV-0000

--- frontend/src/kitchensink/views/ExpandableDataSetView.vue
+++ frontend/src/kitchensink/views/ExpandableDataSetView.vue
@@ -1,13 +1,10 @@
 <script lang="ts" setup>
-import { h, ref } from "vue"
-import AnnouncementInputGroup from "@/components/AnnouncementGroup.vue"
+import { h } from "vue"
 import ExpandableDataSet from "@/components/ExpandableDataSet.vue"
-import { Metadata, MetadataSections } from "@/domain/Norm"
 import DataSetSummary, {
   withSummarizer,
   defaultSummarizer,
 } from "@/shared/components/DataSetSummary.vue"
-import EditableList from "@/shared/components/EditableList.vue"
 
 function chipSummarizer(dataEntry: undefined) {
   return h(
@@ -16,26 +13,6 @@ function chipSummarizer(dataEntry: undefined) {
     defaultSummarizer(dataEntry)
   )
 }
-
-function AnnouncementSummarizer(
-  data: Metadata,
-  sections: MetadataSections
-): string {
-  console.log(data)
-  if (!data) return ""
-
-  const printGazette = data.ANNOUNCEMENT_GAZETTE?.[0]
-  const printYear = data.YEAR?.[0]
-  const printNumber = data.NUMBER?.[0]
-  const printPage = data.PAGE_NUMBER?.[0]
-  const section = sections.PRINT_ANNOUNCEMENT
-
-  return `${section}, ${printGazette}, ${printYear}, ${printNumber}, ${printPage}`
-}
-
-const announcementData = ref<string[]>([])
-
-const defaultAnnouncement = { type: "", institution: "" }
 
 const ChipSummary = withSummarizer(chipSummarizer)
 </script>
@@ -62,14 +39,5 @@ const ChipSummary = withSummarizer(chipSummarizer)
     title="Special Section"
   >
     <div class="h-64">Content</div>
-  </ExpandableDataSet>
-
-  <ExpandableDataSet :data-set="announcementData" title="Amtliche Fundstelle">
-    <EditableList
-      v-model="announcementData"
-      :default-value="defaultAnnouncement"
-      :edit-component="AnnouncementInputGroup"
-      :summary-component="AnnouncementSummarizer"
-    />
   </ExpandableDataSet>
 </template>
