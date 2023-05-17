fdfc9db36c82c761a4e7cc3087899f36557ab287
benjaminaaron <benjamin.degenhart@digitalservice.bund.de>
2023-05-17 10:42:48 +0200

    Remove unnecessary proceedingDecisionFields
    RISDEV-0000
    
    Co-Authored-By: zechmeister <2386669+zechmeister@users.noreply.github.com>

--- frontend/src/fields/caselaw/proceedingDecisionFields.ts
+++ frontend/src/fields/caselaw/proceedingDecisionFields.ts
@@ -1,53 +1,6 @@
-import { InputType } from "../../shared/components/input/types"
-import type { InputField } from "../../shared/components/input/types"
-import comboboxItemService from "@/services/comboboxItemService"
-
-export const proceedingDecisionFields: InputField[] = [
-  {
-    name: "court",
-    type: InputType.COMBOBOX,
-    label: "Gericht",
-    required: true,
-    inputAttributes: {
-      ariaLabel: "Gericht Rechtszug",
-      placeholder: "Gerichtstyp Gerichtsort",
-      itemService: comboboxItemService.getCourts,
-    },
-  },
-  {
-    name: "date",
-    type: InputType.CUSTOMDATE,
-    label: "Entscheidungsdatum",
-    required: true,
-    inputAttributes: {
-      ariaLabel: "Entscheidungsdatum Rechtszug",
-    },
-  },
-  {
-    name: "fileNumber",
-    type: InputType.TEXT,
-    label: "Aktenzeichen",
-    required: true,
-    inputAttributes: {
-      ariaLabel: "Aktenzeichen Rechtszug",
-    },
-  },
-  {
-    name: "documentType",
-    type: InputType.COMBOBOX,
-    label: "Dokumenttyp",
-    inputAttributes: {
-      ariaLabel: "Dokumenttyp Rechtszug",
-      placeholder: "Bitte auswählen",
-      itemService: comboboxItemService.getDocumentTypes,
-    },
-  },
-]
-
-export const proceedingDecisionFieldLabels: { [name: string]: string } =
-  Object.assign(
-    {},
-    ...proceedingDecisionFields.map((field) => ({
-      [field.name]: field.label as string,
-    }))
-  )
+export const proceedingDecisionFieldLabels: { [name: string]: string } = {
+  court: "Gericht",
+  date: "Entscheidungsdatum",
+  fileNumber: "Aktenzeichen",
+  documentType: "Dokumenttyp",
+}
