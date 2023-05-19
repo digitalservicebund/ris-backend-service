5a6c7ae7242db7e638ce74fe6cddb223dee9c13f
Florian <florian.zechmeister@digitalservice.bund.de>
2023-05-19 16:25:08 +0200

    Add postgres container for test
    Addresses RISDEV-1587

--- backend/src/test/java/de/bund/digitalservice/ris/caselaw/integration/tests/AuthIntegrationTest.java
+++ backend/src/test/java/de/bund/digitalservice/ris/caselaw/integration/tests/AuthIntegrationTest.java
@@ -14,12 +14,28 @@ import java.util.Collections;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.test.web.reactive.server.SecurityMockServerConfigurers.OidcLoginMutator;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
 
 @RISIntegrationTest(
     imports = {FlywayConfig.class, PostgresConfig.class, KeycloakUserService.class},
     controllers = {AuthController.class})
 class AuthIntegrationTest {
+
+  @Container
+  static PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>("postgres:12");
+
+  @DynamicPropertySource
+  static void registerDynamicProperties(DynamicPropertyRegistry registry) {
+    registry.add("database.user", () -> postgreSQLContainer.getUsername());
+    registry.add("database.password", () -> postgreSQLContainer.getPassword());
+    registry.add("database.host", () -> postgreSQLContainer.getHost());
+    registry.add("database.port", () -> postgreSQLContainer.getFirstMappedPort());
+    registry.add("database.database", () -> postgreSQLContainer.getDatabaseName());
+  }
 
   @Autowired private WebTestClient webClient;
 
