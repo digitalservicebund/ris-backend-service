a495894035a5fddb0e33163d39348db78512cdc2
Ralph von Petersdorff-Campen <ralph.vonpetersdorffcampen@digitalservice.bund.de>
2023-06-13 11:58:51 +0200

    Use right migration script
    RISDEV-1846

--- backend/src/main/resources/db/migration/V0.86__caselaw_migrate_proceeding_decision_table.sql
+++ backend/src/main/resources/db/migration/V0.86__caselaw_migrate_proceeding_decision_table.sql
@@ -1,18 +1,14 @@
-CREATE TABLE
+INSERT INTO
   documentation_unit_link (
-    id BIGSERIAL NOT NULL PRIMARY KEY,
-    parent_documentation_unit_uuid UUID NOT NULL,
-    child_documentation_unit_uuid UUID NOT NULL,
+    parent_documentation_unit_uuid,
+    child_documentation_unit_uuid,
     type
-      VARCHAR(30) NOT NULL,
-      UNIQUE (
-        parent_documentation_unit_uuid,
-        child_documentation_unit_uuid,
-        type
-      ),
-      FOREIGN KEY (parent_documentation_unit_uuid) REFERENCES doc_unit (uuid),
-      FOREIGN KEY (child_documentation_unit_uuid) REFERENCES doc_unit (uuid)
-  );
-
-CREATE INDEX
-  IF NOT EXISTS documentation_uuid_idx ON doc_unit (uuid);
+  )
+SELECT
+  dp.uuid,
+  dc.uuid,
+  'PREVIOUS_DECISION'
+FROM
+  proceeding_decision_link pdl
+  INNER JOIN doc_unit dc ON pdl.child_document_unit_id = dc.id
+  INNER JOIN doc_unit dp ON pdl.parent_document_unit_id = dp.id;
