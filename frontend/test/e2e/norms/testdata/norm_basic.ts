425ba89867a61f512b91c528e85e88b06fad9cfd
Tarek Hamaoui <tarek.hamaoui@digitalservice.bund.de>
2023-06-14 09:22:33 +0200

    Updates e2e test
    Adds prefix FNA and GESTA to the norm_basic.
    
    RISDEV-0000

--- frontend/test/e2e/norms/testdata/norm_basic.ts
+++ frontend/test/e2e/norms/testdata/norm_basic.ts
@@ -59,10 +59,10 @@ export const normData: NormData = {
       },
     ],
     SUBJECT_AREA: [
-      { SUBJECT_FNA: ["315-12"] },
-      { SUBJECT_FNA: ["671-34"] },
-      { SUBJECT_GESTA: ["123-45"] },
-      { SUBJECT_GESTA: ["678-90"] },
+      { SUBJECT_FNA: ["FNA 315-12"] },
+      { SUBJECT_FNA: ["FNA 671-34"] },
+      { SUBJECT_GESTA: ["GESTA 123-45"] },
+      { SUBJECT_GESTA: ["GESTA 678-90"] },
     ],
     LEAD: [
       { LEAD_JURISDICTION: ["BMVBS"], LEAD_UNIT: ["RS III 2"] },
