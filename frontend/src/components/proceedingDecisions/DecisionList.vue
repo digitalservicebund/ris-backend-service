a7f27fffa4906b0d0a8dde7eca015c60231bbc66
Leonie Koch <lk.leonie.koch@gmail.com>
2023-06-14 08:44:43 +0200

    Remove sorting from proceeding decision list
    RISDEV-1727

--- frontend/src/components/proceedingDecisions/DecisionList.vue
+++ frontend/src/components/proceedingDecisions/DecisionList.vue
@@ -1,28 +1,20 @@
 <script setup lang="ts">
-import dayjs from "dayjs"
-import { computed } from "vue"
 import InlineDecision from "@/components/proceedingDecisions/InlineDecision.vue"
 import ProceedingDecision from "@/domain/proceedingDecision"
 
-const props = defineProps<{
+defineProps<{
   decisions: ProceedingDecision[]
 }>()
 
 const emit = defineEmits<{
   (e: "removeLink", decision: ProceedingDecision): Promise<void>
 }>()
-
-const sortedDecisions = computed(() =>
-  [...props.decisions].sort((a, b) =>
-    dayjs(b.decisionDate).diff(a.decisionDate)
-  )
-)
 </script>
 
 <template>
   <div class="flex flex-col items-start text-start w-full">
     <div
-      v-for="decision in sortedDecisions"
+      v-for="decision in decisions"
       :key="decision.uuid"
       class="border-b-1 border-b-blue-500 first:pt-0 flex flex-start justify-between label-02-reg last:border-none last:pb-0 py-10 w-full"
     >
