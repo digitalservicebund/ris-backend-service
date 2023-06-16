e4b5122ce2826cc06972188b737a70270fc7c5c9
Tarek Hamaoui <tarek.hamaoui@digitalservice.bund.de>
2023-06-16 11:19:45 +0200

    Dropping old columns for Digital Evidence
    RISDEV-1843

---
+++ backend/src/main/resources/db/migration/V0.89__norms_remove_digital_evidence_columns_from_norms_table.sql
@@ -0,0 +1,10 @@
+ALTER TABLE
+  norms
+DROP COLUMN
+  digital_evidence_link,
+DROP COLUMN
+  digital_evidence_related_data,
+DROP COLUMN
+  digital_evidence_external_data_note,
+DROP COLUMN
+  digital_evidence_appendix;
