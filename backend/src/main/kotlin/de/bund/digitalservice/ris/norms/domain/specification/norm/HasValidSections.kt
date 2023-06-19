a09ac090b2455b754c3509689e9124a49ab0730d
Tarek Hamaoui <tarek.hamaoui@digitalservice.bund.de>
2023-06-19 11:08:13 +0200

    Removes child sections from hasValidSections
    RISDEV-1212

--- backend/src/main/kotlin/de/bund/digitalservice/ris/norms/domain/specification/norm/HasValidSections.kt
+++ backend/src/main/kotlin/de/bund/digitalservice/ris/norms/domain/specification/norm/HasValidSections.kt
@@ -10,10 +10,7 @@ import de.bund.digitalservice.ris.norms.domain.value.MetadataSectionName.DIVERGE
 import de.bund.digitalservice.ris.norms.domain.value.MetadataSectionName.DIVERGENT_ENTRY_INTO_FORCE_UNDEFINED
 import de.bund.digitalservice.ris.norms.domain.value.MetadataSectionName.DIVERGENT_EXPIRATION
 import de.bund.digitalservice.ris.norms.domain.value.MetadataSectionName.DIVERGENT_EXPIRATION_UNDEFINED
-import de.bund.digitalservice.ris.norms.domain.value.MetadataSectionName.DOCUMENT_OTHER
-import de.bund.digitalservice.ris.norms.domain.value.MetadataSectionName.DOCUMENT_STATUS
 import de.bund.digitalservice.ris.norms.domain.value.MetadataSectionName.DOCUMENT_STATUS_SECTION
-import de.bund.digitalservice.ris.norms.domain.value.MetadataSectionName.DOCUMENT_TEXT_PROOF
 import de.bund.digitalservice.ris.norms.domain.value.MetadataSectionName.DOCUMENT_TYPE
 import de.bund.digitalservice.ris.norms.domain.value.MetadataSectionName.ENTRY_INTO_FORCE
 import de.bund.digitalservice.ris.norms.domain.value.MetadataSectionName.EXPIRATION
@@ -52,9 +49,6 @@ val hasValidSections =
                 DIGITAL_EVIDENCE,
                 FOOTNOTES,
                 DOCUMENT_STATUS_SECTION,
-                DOCUMENT_STATUS,
-                DOCUMENT_TEXT_PROOF,
-                DOCUMENT_OTHER,
             )
         }
     }
