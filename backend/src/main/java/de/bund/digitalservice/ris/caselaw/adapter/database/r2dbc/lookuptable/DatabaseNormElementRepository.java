0bed16aa387c47679a909c9cca1e450ee73178f1
benjaminaaron <benjamin.degenhart@digitalservice.bund.de>
2023-06-29 14:29:03 +0200

    Implement single norm validation logic
    RISDEV-1845

--- backend/src/main/java/de/bund/digitalservice/ris/caselaw/adapter/database/r2dbc/lookuptable/DatabaseNormElementRepository.java
+++
@@ -1,6 +0,0 @@
-package de.bund.digitalservice.ris.caselaw.adapter.database.r2dbc.lookuptable;
-
-import java.util.UUID;
-import org.springframework.data.r2dbc.repository.R2dbcRepository;
-
-public interface DatabaseNormElementRepository extends R2dbcRepository<NormElementDTO, UUID> {}
