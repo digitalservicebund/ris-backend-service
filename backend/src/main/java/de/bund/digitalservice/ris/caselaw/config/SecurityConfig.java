e05913c17f4ef240e149869aab7d3539619bcb16
Hanna Prinz <hanna.prinz@digitalservice.bund.de>
2023-07-06 10:00:31 +0200

    Bump Spring to 3.1.1 and Netty to 1.1.8
    RISDEV-0000

--- backend/src/main/java/de/bund/digitalservice/ris/caselaw/config/SecurityConfig.java
+++ backend/src/main/java/de/bund/digitalservice/ris/caselaw/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package de.bund.digitalservice.ris.caselaw.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
@@ -15,23 +16,25 @@ import org.springframework.security.web.server.authentication.HttpStatusServerEn
 public class SecurityConfig {
   @Bean
   public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
-    return http.authorizeExchange()
-        .pathMatchers("/actuator/**", "/api/v1/open/norms/**", "/admin/webhook")
-        .permitAll()
-        .anyExchange()
-        .authenticated()
-        .and()
-        .oauth2Login()
-        .and()
-        .exceptionHandling()
-        .authenticationEntryPoint(new HttpStatusServerEntryPoint(HttpStatus.UNAUTHORIZED))
-        .and()
-        .csrf()
-        .disable()
+    return http.authorizeExchange(
+            exchanges ->
+                exchanges
+                    .pathMatchers("/actuator/**", "/api/v1/open/norms/**", "/admin/webhook")
+                    .permitAll()
+                    .anyExchange()
+                    .authenticated())
+        .oauth2Login(Customizer.withDefaults())
+        .exceptionHandling(
+            handling ->
+                handling.authenticationEntryPoint(
+                    new HttpStatusServerEntryPoint(HttpStatus.UNAUTHORIZED)))
+        .csrf(ServerHttpSecurity.CsrfSpec::disable)
         .headers(
             headers ->
                 headers.contentSecurityPolicy(
-                    "default-src 'self'; img-src 'self' data:; style-src 'self' 'unsafe-inline'"))
+                    customizer ->
+                        customizer.policyDirectives(
+                            "default-src 'self'; img-src 'self' data:; style-src 'self' 'unsafe-inline'")))
         .build();
   }
 }
