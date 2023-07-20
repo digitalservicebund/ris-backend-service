a40aa2fe6360c7abc0bddd64f55e6a4594643db7
Fabio Tacke <fabio.tacke@digitalservice.bund.de>
2023-07-20 14:05:10 +0200

    Refactor single norm validation
    RISDEV-2139

---
+++ backend/src/main/java/de/bund/digitalservice/ris/caselaw/domain/InvalidSingleNormValue.java
@@ -0,0 +1,8 @@
+package de.bund.digitalservice.ris.caselaw.domain;
+
+public class InvalidSingleNormValue extends RuntimeException {
+
+  public InvalidSingleNormValue(String message) {
+    super(message);
+  }
+}
--- backend/src/main/java/de/bund/digitalservice/ris/caselaw/domain/validator/SingleNormValidator.java
+++ backend/src/main/java/de/bund/digitalservice/ris/caselaw/domain/validator/SingleNormValidator.java
@@ -1,5 +1,6 @@
 package de.bund.digitalservice.ris.caselaw.domain.validator;
 
+import de.bund.digitalservice.ris.caselaw.domain.InvalidSingleNormValue;
 import de.bund.digitalservice.ris.caselaw.domain.NormElement;
 import de.bund.digitalservice.ris.caselaw.domain.NormElementRepository;
 import de.bund.digitalservice.ris.caselaw.domain.SingleNormValidationInfo;
@@ -7,7 +8,7 @@ import de.bund.digitalservice.ris.caselaw.domain.lookuptable.NormCode;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 import java.util.Map;
-import java.util.function.Function;
+import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -26,34 +27,50 @@ public class SingleNormValidator
       return true;
     }
 
-    Map<String, NormElement> labelMap = generateNormAbbreviationLabelMap(value.normAbbreviation());
+    Map<String, Boolean> labelNumberDesignationMap =
+        generateNormAbbreviationLabelNumberDesignationMap(value.normAbbreviation());
 
-    String[] parts = value.singleNorm().replaceAll("\\p{Z}", " ").split(" ");
+    String normalizedString = value.singleNorm().replaceAll("\\p{Z}", " ");
 
-    if (parts.length == 0) {
-      return true;
+    try {
+      validateSingleNormString(normalizedString, labelNumberDesignationMap);
+    } catch (InvalidSingleNormValue e) {
+      return false;
     }
 
-    boolean lastWasDesignation = false;
-    boolean isLabel = false;
-    for (int i = parts.length - 1; i >= 0; i--) {
-      isLabel = labelMap.containsKey(parts[i]);
-
-      if (isLabel) {
-        boolean withDesignation = labelMap.get(parts[i]).hasNumberDesignation();
-
-        if (withDesignation ^ lastWasDesignation) {
-          return false;
-        }
-      }
-
-      lastWasDesignation = !isLabel;
-    }
-
-    return isLabel;
+    return true;
   }
 
-  private Map<String, NormElement> generateNormAbbreviationLabelMap(String normAbbreviation) {
+  private void validateSingleNormString(String s, Map<String, Boolean> labelNumberDesignationMap)
+      throws InvalidSingleNormValue {
+    String remainingString = s;
+
+    while (remainingString.length() > 0) {
+      String label = longestMatchingPrefix(remainingString, labelNumberDesignationMap.keySet());
+
+      if (label == null) {
+        throw new InvalidSingleNormValue(
+            "Expected norm label but found unexpected character sequence.");
+      }
+
+      remainingString = stripPrefix(remainingString, label.length());
+
+      if (Boolean.TRUE.equals(labelNumberDesignationMap.get(label))) {
+        remainingString = stripOneLeadingWhitespace(remainingString, false);
+
+        remainingString =
+            stripNumberDesignation(remainingString, labelNumberDesignationMap.keySet());
+        remainingString = stripOneLeadingWhitespace(remainingString, true);
+      } else {
+        if (remainingString.length() > 0) {
+          remainingString = stripOneLeadingWhitespace(remainingString, false);
+        }
+      }
+    }
+  }
+
+  private Map<String, Boolean> generateNormAbbreviationLabelNumberDesignationMap(
+      String normAbbreviation) {
     Stream<NormElement> stream = normElementRepository.findAllByDocumentCategoryLabelR().stream();
 
     return stream
@@ -62,6 +79,59 @@ public class SingleNormValidator
                 // for "EinigVtr" normAbbreviation, only get EINIGUNGS_VERTRAG codes
                 "EinigVtr".equals(normAbbreviation)
                     == NormCode.EINIGUNGS_VERTRAG.name().equals(normElement.normCode()))
-        .collect(Collectors.toMap(NormElement::label, Function.identity()));
+		.collect(Collectors.toMap(NormElement::label, NormElement::hasNumberDesignation));
+  }
+
+  private String stripPrefix(String s, int length) throws InvalidSingleNormValue {
+    if (s.length() < length) {
+      throw new InvalidSingleNormValue("Prefix longer than string.");
+    }
+
+    if (s.length() == length) {
+      return "";
+    }
+
+    return s.substring(length);
+  }
+
+  private String stripOneLeadingWhitespace(String s, boolean relaxed) {
+    if (s.startsWith(" ")) {
+      return s.substring(1);
+    } else {
+      if (relaxed) {
+        return s;
+      } else {
+        throw new InvalidSingleNormValue("Expected whitespace character missing.");
+      }
+    }
+  }
+
+  private String stripNumberDesignation(String s, Set<String> normLabels)
+      throws InvalidSingleNormValue {
+    int numberDesignationEndIndex = s.length() - 1;
+    for (String nextLabel : normLabels) {
+      int labelIndex = s.indexOf(nextLabel);
+      if (labelIndex > -1) {
+        numberDesignationEndIndex = Integer.min(numberDesignationEndIndex, labelIndex - 1);
+      }
+    }
+
+    if (numberDesignationEndIndex > -1) {
+      return stripPrefix(s, numberDesignationEndIndex + 1);
+    } else {
+      throw new InvalidSingleNormValue("Expected number designation but found a norm label.");
+    }
+  }
+
+  private String longestMatchingPrefix(String s, Set<String> prefixes) {
+    String longestPrefix = null;
+    for (String prefix : prefixes) {
+      if (s.startsWith(prefix)
+          && (longestPrefix == null || longestPrefix.length() < prefix.length())) {
+        longestPrefix = prefix;
+      }
+    }
+
+    return longestPrefix;
   }
 }
