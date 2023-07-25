8c6579c7b0e466198c594c4edc9be3ceb8845783
Ralph von Petersdorff-Campen <ralph.vonpetersdorffcampen@digitalservice.bund.de>
2023-07-25 16:20:44 +0200

    Fix validation check on publication
    Add the DTD error messages
    
    RISDEV-2183

--- frontend/src/shared/components/InfoModal.vue
+++ frontend/src/shared/components/InfoModal.vue
@@ -61,7 +61,8 @@ const ariaLabelIcon = props.title + " icon"
 
     <div class="flex flex-col">
       <span class="ds-label-02-bold">{{ title }}</span>
-      <span class="ds-body-01-reg">{{ description }}</span>
+      <!-- eslint-disable vue/no-v-html -->
+      <span class="ds-body-01-reg" v-html="description"></span>
     </div>
   </div>
 </template>
