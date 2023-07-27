c63c26fcaaef218557e162a80e7c609d3e51d6d9
Leonie Koch <leonie.koch@digitalservice.bund.de>
2023-07-27 12:53:11 +0200

    Add unit tests for norm references
    RISDEV-1504

---
+++ frontend/test/components/normReferenceInput.spec.ts
@@ -0,0 +1,169 @@
+import userEvent from "@testing-library/user-event"
+import { render, screen } from "@testing-library/vue"
+import NormReferenceInput from "@/components/NormReferenceInput.vue"
+import { NormAbbreviation } from "@/domain/normAbbreviation"
+import NormReference from "@/domain/normReference"
+import comboboxItemService from "@/services/comboboxItemService"
+import { ComboboxItem } from "@/shared/components/input/types"
+
+function renderComponent(options?: { modelValue?: NormReference }) {
+  const user = userEvent.setup()
+  const props = {
+    modelValue: new NormReference({ ...options?.modelValue }),
+  }
+  const utils = render(NormReferenceInput, { props })
+  return { screen, user, props, ...utils }
+}
+
+describe("NormReferenceEntry", () => {
+  const normAbbreviation: NormAbbreviation = {
+    abbreviation: "1000g-BefV",
+  }
+  const dropdownAbbreviationItems: ComboboxItem[] = [
+    {
+      label: normAbbreviation.abbreviation,
+      value: normAbbreviation,
+    },
+  ]
+  vi.spyOn(comboboxItemService, "getRisAbbreviations").mockImplementation(() =>
+    Promise.resolve({ status: 200, data: dropdownAbbreviationItems }),
+  )
+  it("render empty norm input group on initial load", () => {
+    renderComponent()
+    expect(screen.getByLabelText("RIS-Abkürzung der Norm")).toBeInTheDocument()
+    expect(screen.getByLabelText("Einzelnorm der Norm")).toBeInTheDocument()
+    expect(screen.getByLabelText("Fassungsdatum der Norm")).toBeInTheDocument()
+    expect(screen.getByLabelText("Jahr der Norm")).toBeInTheDocument()
+    expect(screen.getByLabelText("Jahr der Norm")).toBeInTheDocument()
+    expect(screen.getByLabelText("Norm speichern")).toBeDisabled()
+    expect(
+      screen.queryByText(/Pflichtfeld nicht befüllt/),
+    ).not.toBeInTheDocument()
+  })
+
+  it("render values if given", () => {
+    const { screen } = renderComponent({
+      modelValue: {
+        normAbbreviation: { id: "123", abbreviation: "ABC" },
+        singleNorm: "12",
+        dateOfVersion: "2022-01-31T23:00:00Z",
+        dateOfRelevance: "2023",
+      } as NormReference,
+    })
+
+    const abbreviationField = screen.getByLabelText("RIS-Abkürzung der Norm")
+
+    const singleNormField = screen.getByLabelText("Einzelnorm der Norm")
+
+    const versionField = screen.getByLabelText("Fassungsdatum der Norm")
+
+    const relevanceField = screen.getByLabelText("Jahr der Norm")
+
+    expect(abbreviationField).toHaveValue("ABC")
+    expect(singleNormField).toHaveValue("12")
+    expect(versionField).toHaveValue("31.01.2022")
+    expect(relevanceField).toHaveValue("2023")
+    expect(screen.getByLabelText("Norm speichern")).toBeEnabled()
+  })
+
+  it("Add norm without valid single norm not possible", async () => {
+    const { user, emitted } = renderComponent({
+      modelValue: {
+        singleNorm: "12",
+      } as NormReference,
+    })
+
+    const singleNormInput = await screen.findByLabelText("Einzelnorm der Norm")
+    expect(singleNormInput).toHaveValue("12")
+    const button = screen.getByLabelText("Norm speichern")
+    await user.click(button)
+
+    await screen.findByText(/Inhalt nicht valide/)
+    expect(emitted("update:modelValue")).toEqual(undefined)
+  })
+
+  it("Add norm without all required fields filled possible", async () => {
+    const { user, emitted } = renderComponent({
+      modelValue: {
+        dateOfVersion: "2022-01-31T23:00:00Z",
+        dateOfRelevance: "2023",
+      } as NormReference,
+    })
+
+    const button = screen.getByLabelText("Norm speichern")
+    await user.click(button)
+
+    expect(emitted("addEntry")).toBeTruthy()
+  })
+
+  it("New input removes error message", async () => {
+    const { user } = renderComponent({
+      modelValue: {
+        singleNorm: "12",
+      } as NormReference,
+    })
+
+    const singleNormInput = await screen.findByLabelText("Einzelnorm der Norm")
+    expect(singleNormInput).toHaveValue("12")
+    const button = screen.getByLabelText("Norm speichern")
+    await user.click(button)
+
+    await screen.findByText(/Inhalt nicht valide/)
+
+    await user.type(singleNormInput, "{backspace}")
+
+    expect(screen.queryByText(/Inhalt nicht valide/)).not.toBeInTheDocument()
+  })
+
+  it("correctly updates the value of ris abbreviation input", async () => {
+    const { user, emitted } = renderComponent()
+    const abbreviationField = screen.getByLabelText("RIS-Abkürzung der Norm")
+
+    await user.type(abbreviationField, "1000")
+    const dropdownItems = screen.getByLabelText("dropdown-option")
+    expect(dropdownItems).toBeInTheDocument()
+    expect(dropdownItems[0]).toHaveTextContent("1000g-BefV")
+    await user.click(dropdownItems[0])
+
+    const button = screen.getByLabelText("Norm speichern")
+    await user.click(button)
+    expect(emitted("update:modelValue")).toEqual([
+      [
+        {
+          dateOfRelevance: undefined,
+          dateOfVersion: undefined,
+          normAbbreviation: {
+            abbreviation: "1000g-BefV",
+          },
+          singleNorm: undefined,
+        },
+      ],
+    ])
+  })
+
+  it("correctly updates the value of the single norm input", async () => {
+    const { user } = renderComponent()
+    const singleNormInput = await screen.findByLabelText("Einzelnorm der Norm")
+
+    await user.type(singleNormInput, "§ 123")
+    expect(singleNormInput).toHaveValue("§ 123")
+  })
+
+  it("correctly updates the value of the version date input", async () => {
+    const { user } = renderComponent()
+
+    const versionField = screen.getByLabelText("Fassungsdatum der Norm")
+    await user.type(versionField, "31.01.2022")
+
+    expect(versionField).toHaveValue("31.01.2022")
+  })
+
+  it("correctly updates the value of the version date input", async () => {
+    const { user } = renderComponent()
+
+    const relevanceField = screen.getByLabelText("Jahr der Norm")
+    await user.type(relevanceField, "2023")
+
+    expect(relevanceField).toHaveValue("2023")
+  })
+})
---
+++ frontend/test/components/normReferences.spec.ts
@@ -0,0 +1,159 @@
+import userEvent from "@testing-library/user-event"
+import { render, screen } from "@testing-library/vue"
+import NormReferences from "@/components/NormReferences.vue"
+import { NormAbbreviation } from "@/domain/normAbbreviation"
+import NormReference from "@/domain/normReference"
+import comboboxItemService from "@/services/comboboxItemService"
+import { ComboboxItem } from "@/shared/components/input/types"
+
+function renderComponent(options?: { modelValue?: NormReference[] }) {
+  const props = {
+    modelValue: options?.modelValue ? options?.modelValue : [],
+  }
+
+  const user = userEvent.setup()
+  return {
+    user,
+    ...render(NormReferences, {
+      props,
+      global: {
+        stubs: { routerLink: { template: "<a><slot/></a>" } },
+      },
+    }),
+  }
+}
+
+function generateNormReference(options?: {
+  normAbbreviation?: NormAbbreviation
+  singleNorm?: string
+  dateOfVersion?: string
+  dateOfRelevance?: string
+}) {
+  const normReference = new NormReference({
+    normAbbreviation: options?.normAbbreviation ?? { abbreviation: "ABC" },
+    singleNorm: options?.singleNorm ?? "",
+    dateOfVersion: options?.dateOfVersion ?? "2022-02-01",
+    dateOfRelevance: options?.dateOfRelevance ?? "2022",
+  })
+  return normReference
+}
+
+describe("Norm references", async () => {
+  const normAbbreviation: NormAbbreviation = {
+    abbreviation: "1000g-BefV",
+  }
+  const dropdownAbbreviationItems: ComboboxItem[] = [
+    {
+      label: normAbbreviation.abbreviation,
+      value: normAbbreviation,
+    },
+  ]
+  vi.spyOn(comboboxItemService, "getRisAbbreviations").mockImplementation(() =>
+    Promise.resolve({ status: 200, data: dropdownAbbreviationItems }),
+  )
+
+  it("renders empty normreference in edit mode, when no normReferences in list", async () => {
+    renderComponent()
+    expect(screen.getAllByLabelText("Listen Eintrag").length).toBe(1)
+    expect(screen.getByLabelText("RIS-Abkürzung der Norm")).toBeInTheDocument()
+    expect(screen.getByLabelText("Einzelnorm der Norm")).toBeInTheDocument()
+    expect(screen.getByLabelText("Fassungsdatum der Norm")).toBeInTheDocument()
+    expect(screen.getByLabelText("Jahr der Norm")).toBeInTheDocument()
+    expect(screen.getByLabelText("Jahr der Norm")).toBeInTheDocument()
+    expect(screen.getByLabelText("Norm speichern")).toBeDisabled()
+    expect(
+      screen.queryByText(/Pflichtfeld nicht befüllt/),
+    ).not.toBeInTheDocument()
+  })
+
+  it("renders activeCitations as list entries", () => {
+    const modelValue: NormReference[] = [
+      generateNormReference({ singleNorm: "§ 123" }),
+      generateNormReference({ singleNorm: "§ 345" }),
+    ]
+    renderComponent({ modelValue })
+
+    expect(screen.getAllByLabelText("Eintrag bearbeiten").length).toBe(2)
+    expect(screen.getAllByLabelText("Eintrag löschen").length).toBe(2)
+    expect(screen.getAllByLabelText("Listen Eintrag").length).toBe(2)
+    expect(
+      screen.queryByLabelText("RIS-Abkürzung der Norm"),
+    ).not.toBeInTheDocument()
+    expect(screen.getByText(/§ 123/)).toBeInTheDocument()
+    expect(screen.getByText(/§ 345/)).toBeInTheDocument()
+  })
+
+  it("click on edit icon, opens the list entry in edit mode", async () => {
+    const { user } = renderComponent({
+      modelValue: [
+        generateNormReference({
+          singleNorm: "§ 123",
+        }),
+      ],
+    })
+    const button = screen.getByLabelText("Eintrag bearbeiten")
+    await user.click(button)
+
+    expect(screen.getByLabelText("RIS-Abkürzung der Norm")).toBeInTheDocument()
+    expect(screen.getByLabelText("Einzelnorm der Norm")).toBeInTheDocument()
+    expect(screen.getByLabelText("Fassungsdatum der Norm")).toBeInTheDocument()
+    expect(screen.getByLabelText("Jahr der Norm")).toBeInTheDocument()
+  })
+
+  it("correctly deletes norm reference", async () => {
+    const { user } = renderComponent({
+      modelValue: [generateNormReference(), generateNormReference()],
+    })
+
+    expect(screen.getAllByLabelText("Listen Eintrag").length).toBe(2)
+    const buttonList = screen.getAllByLabelText("Eintrag löschen")
+    await user.click(buttonList[0])
+    expect(screen.getAllByLabelText("Listen Eintrag").length).toBe(1)
+  })
+
+  it("correctly deletes active citations added by search", async () => {
+    const modelValue: NormReference[] = [
+      generateNormReference(),
+      generateNormReference(),
+    ]
+    const { user } = renderComponent({ modelValue })
+    const activeCitations = screen.getAllByLabelText("Listen Eintrag")
+    expect(activeCitations.length).toBe(2)
+    const buttonList = screen.getAllByLabelText("Eintrag löschen")
+    await user.click(buttonList[0])
+    expect(screen.getAllByLabelText("Listen Eintrag").length).toBe(1)
+  })
+
+  it("click on 'Weitere Angabe' adds new emptry list entry", async () => {
+    const modelValue: NormReference[] = [
+      generateNormReference(),
+      generateNormReference(),
+    ]
+    const { user } = renderComponent({ modelValue })
+    const activeCitations = screen.getAllByLabelText("Listen Eintrag")
+    expect(activeCitations.length).toBe(2)
+    const button = screen.getByLabelText("Weitere Angabe")
+    await user.click(button)
+    expect(screen.getAllByLabelText("Listen Eintrag").length).toBe(3)
+  })
+
+  it("displays error in list and edit component when fields missing", async () => {
+    const modelValue: NormReference[] = [generateNormReference()]
+    const { user } = renderComponent({ modelValue })
+    await screen.findByText(/ABC, 01.02.2022, 2022/)
+    const editButton = screen.getByLabelText("Eintrag bearbeiten")
+    await user.click(editButton)
+
+    const abbreviationInput = await screen.findByLabelText(
+      "RIS-Abkürzung der Norm",
+    )
+    await screen.getAllByLabelText("Auswahl zurücksetzen")[1].click()
+    await user.clear(abbreviationInput)
+    expect(abbreviationInput).toHaveValue("")
+    await user.click(screen.getByLabelText("Norm speichern"))
+    await screen.findByText(/01.02.2022, 2022/)
+    expect(screen.getByLabelText(/Fehlerhafte Eingabe/)).toBeInTheDocument()
+    await user.click(editButton)
+    expect(screen.getAllByText(/Pflichtfeld nicht befüllt/).length).toBe(1)
+  })
+})
