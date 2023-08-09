267234d971081b5568c9dba9aba21d4759a95c9f
Haytham Mones <haythameyd@gmail.com>
2023-08-09 15:29:23 +0200

    Fixed deserialization of juris norm
    RISDEV-2172

--- backend/src/main/kotlin/de/bund/digitalservice/ris/norms/application/port/input/MigrateNormUseCase.kt
+++ backend/src/main/kotlin/de/bund/digitalservice/ris/norms/application/port/input/MigrateNormUseCase.kt
@@ -1,5 +1,13 @@
 package de.bund.digitalservice.ris.norms.application.port.input
 
+import com.fasterxml.jackson.core.JsonParser
+import com.fasterxml.jackson.core.TreeNode
+import com.fasterxml.jackson.databind.DeserializationContext
+import com.fasterxml.jackson.databind.JsonDeserializer
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize
+import com.fasterxml.jackson.module.kotlin.KotlinFeature
+import com.fasterxml.jackson.module.kotlin.KotlinModule
 import de.bund.digitalservice.ris.norms.juris.converter.model.Norm
 import java.util.*
 import reactor.core.publisher.Mono
@@ -9,7 +17,25 @@ fun interface MigrateNormUseCase {
 
   data class Command(val norms: ConverterNorms)
 
+  @JsonDeserialize(using = ConverterNormsDeserializer::class)
   data class ConverterNorms(val norms: List<ConverterNorm>)
 
   data class ConverterNorm(val guid: UUID, val norm: Norm)
+
+  class ConverterNormsDeserializer : JsonDeserializer<ConverterNorms>() {
+    override fun deserialize(
+        parser: JsonParser?,
+        context: DeserializationContext?
+    ): ConverterNorms {
+      val kotlinModule: KotlinModule =
+          KotlinModule.Builder().configure(KotlinFeature.StrictNullChecks, true).build()
+      val mapper = ObjectMapper().registerModule(kotlinModule)
+      val schema: TreeNode = mapper.readTree(parser)
+      val norms =
+          mapper
+              .readerForListOf(ConverterNorm::class.java)
+              .readValue<List<ConverterNorm>>(schema.get("norms").toString())
+      return ConverterNorms(norms)
+    }
+  }
 }
--- backend/src/test/kotlin/integration/framework/adapter/input/restapi/controller/MigrateNormControllerIntegrationTest.kt
+++ backend/src/test/kotlin/integration/framework/adapter/input/restapi/controller/MigrateNormControllerIntegrationTest.kt
@@ -1,6 +1,7 @@
 package de.bund.digitalservice.ris.norms.framework.adapter.input.restapi.controller
 
 import de.bund.digitalservice.ris.caselaw.config.FlywayConfig
+import de.bund.digitalservice.ris.caselaw.config.JacksonConfig
 import de.bund.digitalservice.ris.norms.application.port.output.GetNormByGuidOutputPort
 import de.bund.digitalservice.ris.norms.application.service.MigrateNormService
 import de.bund.digitalservice.ris.norms.domain.value.MetadataSectionName
@@ -9,8 +10,6 @@ import de.bund.digitalservice.ris.norms.framework.adapter.output.database.NormsS
 import de.bund.digitalservice.ris.norms.framework.adapter.output.database.PostgresTestcontainerIntegrationTest
 import de.bund.digitalservice.ris.norms.framework.adapter.output.database.dto.NormDto
 import de.bund.digitalservice.ris.norms.framework.adapter.output.juris.JurisConverter
-import de.bund.digitalservice.ris.norms.juris.converter.model.Norm
-import de.bund.digitalservice.ris.norms.juris.converter.model.NormProvider
 import java.time.Duration
 import java.util.*
 import org.assertj.core.api.Assertions.assertThat
@@ -39,6 +38,7 @@ import org.springframework.web.reactive.function.BodyInserters
     MigrateNormService::class,
     NormsService::class,
     JurisConverter::class,
+    JacksonConfig::class,
 )
 @WebFluxTest(controllers = [MigrateNormController::class])
 @WithMockUser
@@ -67,46 +67,38 @@ class MigrateNormControllerIntegrationTest : PostgresTestcontainerIntegrationTes
     template.delete(NormDto::class.java).all().block(Duration.ofSeconds(1))
   }
 
-  companion object {
-    private val data =
-        ConverterNormsSchema(
-            norms =
-                listOf(
-                    ConverterNormSchema(
-                        UUID.randomUUID(),
-                        Norm(
-                            officialLongTitle = "Test",
-                            normProviderList = listOf(NormProvider("entity", "decidingBody", true)),
-                        ))))
-  }
-
   @Test
   fun `it correctly saves a metadata with sections then calls it back via api`() {
+    val guid = UUID.randomUUID()
+    val dataJson =
+        "{\"norms\":[{\"guid\":\"$guid\",\"norm\":{\"text\": null, \"articles\": [{\"title\": null, \"marker\": \"§ 1\", \"paragraphs\": [{\"text\": \"Auf den Schienenpersonenverkehr der öffentlichen Eisenbahnen sind die Vorschriften der Verordnung (EG) Nr. 1371/2007 des Europäischen Parlaments und des Rates vom 23. Oktober 2007 über die Rechte und Pflichten der Fahrgäste im Eisenbahnverkehr (ABl. EU Nr. L 315 S. 14) anzuwenden.<Rec></Rec> Das gilt nach Maßgabe des Artikels 2 Abs. 5 der Verordnung (EG) Nr. 1371/2007 nicht, soweit auf Grund des § 26 Abs. 1 Satz 1 Nr. 1a des Allgemeinen Eisenbahngesetzes vom 27. Dezember 1993 (BGBl. I S. 2378, 2396; 1994 I S. 2439), das zuletzt durch Artikel 2 des Gesetzes vom 26. Mai 2009 (BGBl. I S. 1146) geändert worden ist, für die Beförderung im Schienenpersonennahverkehr etwas anderes bestimmt ist oder soweit es sich um Verkehrsdienste des Schienenpersonennahverkehrs handelt, die hauptsächlich aus Gründen historischen Interesses oder zu touristischen Zwecken betrieben werden.<Rec></Rec>\", \"marker\": \"\"}]}, {\"title\": null, \"marker\": \"§ 2\", \"paragraphs\": [{\"text\": \"Dieses Gesetz tritt mit Ablauf des 2. Dezember 2009 außer Kraft.<Rec></Rec>\", \"marker\": \"\"}]}], \"leadList\": [{\"unit\": \"III A 4\", \"jurisdiction\": \"BMJ\"}], \"footnotes\": [{\"reference\": null, \"footnoteEuLaw\": [], \"otherFootnote\": [{\"first\": 1, \"second\": \"*T\\n(+++ Textnachweis ab: 29.7.2009 +++)\\n(+++ Amtlicher Hinweis des Normgebers auf EG-Recht:\\n     Durchführung der\\n       EGV 1371/2007           (CELEX Nr: 307R1371) +++)\"}, {\"first\": 2, \"second\": \"*/\\nDas G wurde als Artikel 1 G v. 26.5.2009 I 1146 vom Bundestag mit Zustimmung \\ndes Bundesrates beschlossen*. Es tritt gem. Art. 4 Satz 1 dieses G am 29.7.2009\\n in Kraft*.\"}], \"footnoteChange\": [], \"footnoteComment\": [], \"footnoteDecision\": [], \"footnoteStateLaw\": []}], \"repealList\": [\"Dieses G tritt nach seinem § 2 mit Ablauf des 2.12.2009 außer Kraft\"], \"statusList\": [], \"celexNumber\": null, \"reissueList\": [], \"documentType\": {\"name\": \"GE\", \"categories\": [\"SN\", \"ÄN\"], \"templateNames\": []}, \"definitionList\": [], \"documentStatus\": [{\"documentStatusDateYear\": null, \"documentStatusWorkNote\": [\"Außerkraft\"], \"documentStatusReference\": null, \"documentStatusDescription\": null}], \"expirationDate\": \"2009-12-02\", \"otherStatusList\": [], \"risAbbreviation\": \"FGRVAnwG\", \"subjectAreaList\": [{\"fna\": \"934-2\", \"gesta\": null}, {\"fna\": null, \"gesta\": \"C177\"}], \"announcementDate\": \"2009-05-29\", \"citationDateList\": [\"2009-05-26\"], \"documentCategory\": \"NR\", \"frameKeywordList\": [\"BGBl I 2009, Nr 028\", \"aufgehobenes Bundesrecht\", \"Anwendung\", \"Verordnung (EG) Nr. 1371/2007\", \"(EG) Nr. 1371/2007\", \"EGV 1371/2007\", \"Durchführung EG-Recht\", \"Durchführung EGV 1371/2007\", \"Pflicht\", \"Fahrgast\", \"Eisenbahnverkehr\", \"Fahrgastrechteverordnung\", \"Anwendungsgesetz\"], \"normProviderList\": [{\"entity\": \"DEU\", \"decidingBody\": \"BT\", \"isResolutionMajority\": true}], \"validityRuleList\": [], \"documentTextProof\": null, \"officialLongTitle\": \"Gesetz über die Anwendung der Verordnung (EG) Nr. 1371/2007 des Europäischen Parlaments und des Rates vom 23. Oktober 2007 über die Rechte und Pflichten der Fahrgäste im Eisenbahnverkehr\", \"participationList\": [{\"type\": \"EZ\", \"institution\": \"BR\"}], \"entryIntoForceDate\": \"2009-07-29\", \"officialShortTitle\": \"Fahrgastrechteverordnung-Anwendungsgesetz\", \"expirationDateState\": null, \"referenceNumberList\": [], \"applicationScopeArea\": null, \"officialAbbreviation\": null, \"categorizedReferences\": [{\"text\": \"&A 21 &B § 2 &E X &G 2009-12-03\"}, {\"text\": \"&A 84 &E EGV 1371/2007\"}], \"printAnnouncementList\": [{\"page\": \"1146\", \"year\": \"2009\", \"gazette\": \"BGBl I\"}], \"ageIndicationStartList\": [], \"applicationScopeEndDate\": null, \"digitalAnnouncementList\": [], \"divergentDocumentNumber\": \"BJNR114610009\", \"entryIntoForceDateState\": null, \"principleExpirationDate\": \"2009-12-02\", \"unofficialLongTitleList\": [], \"unofficialReferenceList\": [], \"divergentExpirationsList\": [], \"unofficialShortTitleList\": [], \"applicationScopeStartDate\": null, \"unofficialAbbreviationList\": [], \"ageOfMajorityIndicationList\": [], \"divergentEntryIntoForceList\": [], \"principleEntryIntoForceDate\": \"2009-07-29\", \"principleExpirationDateState\": null, \"principleEntryIntoForceDateState\": null, \"risAbbreviationInternationalLawList\": []}}]}"
+
     webClient
         .mutateWith(csrf())
         .post()
         .uri("/api/v1/norms/migrate")
         .contentType(MediaType.APPLICATION_JSON)
-        .body(BodyInserters.fromValue(data))
+        .body(BodyInserters.fromValue(dataJson))
         .exchange()
         .expectStatus()
         .isCreated
 
-    val norm = normService.getNormByGuid(GetNormByGuidOutputPort.Query(data.norms[0].guid)).block()
+    val norm = normService.getNormByGuid(GetNormByGuidOutputPort.Query(guid)).block()
 
     assertThat(
             norm
                 ?.getFirstMetadatum(MetadataSectionName.NORM, MetadatumType.OFFICIAL_LONG_TITLE)
                 ?.value)
-        .isEqualTo("Test")
+        .isEqualTo(
+            "Gesetz über die Anwendung der Verordnung (EG) Nr. 1371/2007 des Europäischen Parlaments und des Rates vom 23. Oktober 2007 über die Rechte und Pflichten der Fahrgäste im Eisenbahnverkehr")
     assertThat(
             norm?.getFirstMetadatum(MetadataSectionName.NORM_PROVIDER, MetadatumType.ENTITY)?.value)
-        .isEqualTo("entity")
+        .isEqualTo("DEU")
     assertThat(
             norm
                 ?.getFirstMetadatum(MetadataSectionName.NORM_PROVIDER, MetadatumType.DECIDING_BODY)
                 ?.value)
-        .isEqualTo("decidingBody")
+        .isEqualTo("BT")
     assertThat(
             norm
                 ?.getFirstMetadatum(
@@ -114,8 +106,4 @@ class MigrateNormControllerIntegrationTest : PostgresTestcontainerIntegrationTes
                 ?.value)
         .isEqualTo(true)
   }
-
-  data class ConverterNormsSchema(var norms: List<ConverterNormSchema> = emptyList())
-
-  data class ConverterNormSchema(val guid: UUID, val norm: Norm)
 }
