b0e06be3ba61b24db51265a608af52456f295949
Victor del Campo <victor.delcampo@digitalservice.bund.de>
2023-08-29 10:17:46 +0200

    Readd manual sorting of articles
    RISDEV-0000

--- frontend/src/routes/norms/norm/[normGuid]/index.vue
+++ frontend/src/routes/norms/norm/[normGuid]/index.vue
@@ -1,9 +1,56 @@
 <script lang="ts" setup>
 import { storeToRefs } from "pinia"
+import { SectionElement } from "@/domain/norm"
 import { useLoadedNormStore } from "@/stores/loadedNorm"
 
 const store = useLoadedNormStore()
 const { loadedNorm } = storeToRefs(store)
+if (loadedNorm.value !== undefined) {
+  const preamble: SectionElement[] = loadedNorm.value.sections.filter(
+    (section) => section.designation === "Eingangsformel",
+  )
+
+  const close: SectionElement[] = loadedNorm.value.sections.filter((section) =>
+    ["Schlussformel", "Schlußformel"].includes(section.designation),
+  )
+
+  const articlesWithoutPreambleAndClose: SectionElement[] =
+    loadedNorm.value.sections.filter(
+      (section) =>
+        !["Eingangsformel", "Schlussformel", "Schlußformel"].includes(
+          section.designation,
+        ),
+    )
+
+  articlesWithoutPreambleAndClose.sort((a, b) => {
+    if (a.designation.includes("Art")) {
+      return Number(a.designation.substring(4)) <
+        Number(b.designation.substring(4))
+        ? -1
+        : 1
+    } else {
+      return Number(a.designation.substring(2)) <
+        Number(b.designation.substring(2))
+        ? -1
+        : 1
+    }
+  })
+
+  loadedNorm.value.sections = preamble
+    .concat(articlesWithoutPreambleAndClose)
+    .concat(close)
+
+  loadedNorm.value.sections.forEach((section) => {
+    if (section.paragraphs.filter((f) => f.marker == null).length == 0) {
+      section.paragraphs.sort((a, b) =>
+        Number(a?.marker?.substring(1, a.marker?.length - 1) ?? 0) <
+        Number(b?.marker?.substring(1, b.marker?.length - 1) ?? 0)
+          ? -1
+          : 1,
+      )
+    }
+  })
+}
 </script>
 
 <template>
