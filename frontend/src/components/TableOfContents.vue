f9a7bc4c9a59c397c04fb75bc42fd3517b8dc557
Victor del Campo <victor.delcampo@digitalservice.bund.de>
2023-09-01 11:39:07 +0200

    WIP
    RISDEV-2335

--- frontend/src/components/TableOfContents.vue
+++ frontend/src/components/TableOfContents.vue
@@ -3,29 +3,56 @@ import ExpandableContent from "@/components/ExpandableContent.vue"
 import { Article, DocumentSection, isDocumentSection } from "@/domain/norm"
 
 const props = defineProps<{
-  documentSections: (Article | DocumentSection)[] | undefined
+  documentSections: (Article | DocumentSection)[]
+  marginLeft: number
 }>()
+
+const articleMargins: { [key: number]: string } = {
+  0: "ml-[0px]",
+  20: "ml-[8px]",
+  40: "ml-[28px]",
+  60: "ml-[48px]",
+  80: "ml-[68px]",
+  100: "ml-[88px]",
+  120: "ml-[108px]",
+  140: "ml-[128px]",
+  160: "ml-[148px]",
+}
 </script>
 
 <template>
-  <div v-for="doc in props.documentSections" :key="doc.guid">
+  <div
+    v-for="doc in props.documentSections"
+    :key="doc.guid"
+    class="border-t border-gray-400"
+  >
     <ExpandableContent
       v-if="isDocumentSection(doc)"
-      class="bg-blue-200 p-16"
+      class="bg-blue-200 pt-4"
       close-icon-name="expand_less"
       icons-on-left
       is-expanded
+      :margin-left="marginLeft"
       open-icon-name="expand_more"
     >
       <template #header>
-        <h2 class="ds-label-02-bold w-[15rem] flex-none text-left">
+        <h2 class="ds-label-02-bold ml-4 w-full text-left">
           {{ doc.marker }} {{ doc.heading }}
         </h2>
       </template>
-      <TableOfContents :document-sections="doc.documentation" />
+      <TableOfContents
+        v-if="doc.documentation"
+        :document-sections="doc.documentation"
+        :margin-left="marginLeft + 20"
+      />
     </ExpandableContent>
-    <div v-else class="border-b border-t border-gray-400 pb-4 pt-4">
-      <p class="pl-20">{{ doc.marker }} {{ doc.heading }}</p>
+    <div v-else class="bg-gray-100 pb-4 pt-4">
+      <p
+        class="ds-label-02-reg pl-20 pr-20"
+        :class="`${articleMargins[marginLeft]}`"
+      >
+        {{ doc.marker }} {{ doc.heading }}
+      </p>
     </div>
   </div>
 </template>
--- frontend/src/routes/norms/norm/[normGuid]/content.vue
+++ frontend/src/routes/norms/norm/[normGuid]/content.vue
@@ -8,8 +8,12 @@ const { loadedNorm } = storeToRefs(loadedNormStore)
 </script>
 
 <template>
-  <div class="max-w-screen-lg">
-    <h2 class="ds-label-01-bold mb-44">Nichtamtliches Inhaltsverzeichnis</h2>
-    <TableOfContents :document-sections="loadedNorm?.documentation" />
+  <div class="w-5/6 max-w-screen-lg">
+    <h2 class="ds-label-01-bold mb-32">Nichtamtliches Inhaltsverzeichnis</h2>
+    <TableOfContents
+      v-if="loadedNorm?.documentation"
+      :document-sections="loadedNorm?.documentation"
+      :margin-left="0"
+    />
   </div>
 </template>
