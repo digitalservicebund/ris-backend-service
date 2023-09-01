806dd6add1087935cf9ac2ebdd7419d5cae4b600
Thore Strassburg <thore@weilbier.net>
2023-09-01 13:56:51 +0200

    Fix(norms): use functional interface for use case
    Fix of a code smell.
    
    RISDEV-2153

--- backend/src/main/kotlin/de/bund/digitalservice/ris/norms/application/port/input/ImportTestDataUseCase.kt
+++ backend/src/main/kotlin/de/bund/digitalservice/ris/norms/application/port/input/ImportTestDataUseCase.kt
@@ -3,7 +3,7 @@ package de.bund.digitalservice.ris.norms.application.port.input
 import de.bund.digitalservice.ris.norms.domain.entity.Norm
 import reactor.core.publisher.Mono
 
-interface ImportTestDataUseCase {
+fun interface ImportTestDataUseCase {
   fun importTestData(command: Command): Mono<Boolean>
 
   data class Command(val norm: Norm)
