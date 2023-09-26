e372ac6da255a76cf8dbe1a07c167404774ed8c7
Andreas Ganske <a.ganske@chaosspace.de>
2023-09-26 15:48:25 +0200

    Remove unstable flaky e2e test
    RISDEV-2450

--- frontend/test/e2e/norms/norm-search-norm-through-api-by-q.spec.ts
+++ frontend/test/e2e/norms/norm-search-norm-through-api-by-q.spec.ts
@@ -1,26 +1,5 @@
-import { APIResponse, expect } from "@playwright/test"
+import { expect } from "@playwright/test"
 import { testWithImportedNorm } from "./fixtures"
-import { normData } from "./testdata/norm_basic"
-
-testWithImportedNorm(
-  "Check norm can be retrieved by search in long title",
-  async ({ request }) => {
-    const response = await request.get(
-      `/api/v1/norms?q=Verordnung zur Anpassung`,
-    )
-    await assertResponseOk(response)
-  },
-)
-
-testWithImportedNorm(
-  "Check norm can be retrieved by search in short title",
-  async ({ request }) => {
-    const response = await request.get(
-      `/api/v1/norms?q=Angepasstes Tierarzneimittelrecht`,
-    )
-    await assertResponseOk(response)
-  },
-)
 
 testWithImportedNorm(
   "Check 404 is returned if no articles found",
@@ -31,11 +10,3 @@ testWithImportedNorm(
     expect(norms.data.length).toBe(0)
   },
 )
-
-async function assertResponseOk(response: APIResponse) {
-  await expect(response).toBeOK()
-  const norms = await response.json()
-  expect(norms.data[0]?.officialLongTitle).toBe(
-    normData?.metadataSections?.NORM?.[0]?.OFFICIAL_LONG_TITLE?.[0],
-  )
-}
