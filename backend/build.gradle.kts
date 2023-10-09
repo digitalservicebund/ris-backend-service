6876403708f1b4eba6b9ba72593473c2255a6cb1
Niklas Schröder <niklas.schroeder@digitalservice.bund.de>
2023-10-09 17:15:20 +0200

    Add uuid-ossp for ER-diagram
    RISDEV-0000

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -263,7 +263,9 @@ dependencies {
     // 1.0.23 as that is affected by CVE-2022-31684
     implementation("io.projectreactor.netty:reactor-netty-http:1.1.8")
 
-    implementation("org.flywaydb:flyway-core:9.21.1")
+    var flywayCore = "org.flywaydb:flyway-core:9.21.1"
+    implementation(flywayCore)
+    "migrationImplementation"(flywayCore)
 
     implementation("org.jetbrains.kotlin:kotlin-reflect")
 
