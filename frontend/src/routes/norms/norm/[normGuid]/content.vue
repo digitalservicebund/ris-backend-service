0064890a5e196a2ab69fc79b8a1572f014778f4b
Andreas Philippi <andreas.philippi@digitalservice.bund.de>
2023-10-16 14:09:19 +0200

    Feat: support linebreaks in titles
    This commit:
    
    - Updates the converter version to the most recent version. This version
      imports line breaks in titles as `<br/>` tags
    - Displays line breaks in the norm title in the frontend
    - Displays line breaks as white space in section titles in the table of
      contents
    
    RISDEV-2581 RISDEV-2546 RISDEV-2545

--- frontend/src/routes/norms/norm/[normGuid]/content.vue
+++ frontend/src/routes/norms/norm/[normGuid]/content.vue
@@ -1,18 +1,26 @@
 <script lang="ts" setup>
 import { storeToRefs } from "pinia"
+import { computed } from "vue"
 import TableOfContents from "@/components/tableOfContents/TableOfContents.vue"
+import { sanitizeNormTitle } from "@/helpers/sanitizer"
 import { useLoadedNormStore } from "@/stores/loadedNorm"
 
 const loadedNormStore = useLoadedNormStore()
 const { loadedNorm } = storeToRefs(loadedNormStore)
+
+const title = computed(() =>
+  loadedNorm.value?.metadataSections?.NORM?.[0]?.OFFICIAL_LONG_TITLE?.[0]
+    ? sanitizeNormTitle(
+        loadedNorm.value?.metadataSections?.NORM?.[0]?.OFFICIAL_LONG_TITLE?.[0],
+      )
+    : "",
+)
 </script>
 
 <template>
   <div class="w-5/6 max-w-screen-lg">
-    <h1 class="ds-heading-02-reg mb-32">
-      {{
-        loadedNorm?.metadataSections?.NORM?.[0]?.OFFICIAL_LONG_TITLE?.[0] ?? ""
-      }}
+    <h1 class="ds-heading-02-reg mb-32 whitespace-pre-wrap">
+      {{ title }}
     </h1>
     <h1 class="ds-label-01-bold mb-32">Nichtamtliches Inhaltsverzeichnis</h1>
 
