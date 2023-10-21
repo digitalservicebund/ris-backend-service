5a46595410a5ae063c6f8d565cb0e8cc698bf5aa
Hanna Prinz <hanna.prinz@digitalservice.bund.de>
2023-10-21 08:31:25 +0200

    Remove foreign key to allow tests to run
    RISDEV-2517

--- backend/src/main/resources/db/migration/V0.131__caselaw_add_legacy_doc_unit_column_in_migration_norm_reference.sql
+++ backend/src/main/resources/db/migration/V0.131__caselaw_add_legacy_doc_unit_column_in_migration_norm_reference.sql
@@ -2,8 +2,3 @@ ALTER TABLE IF EXISTS
   incremental_migration.norm_reference
 ADD COLUMN IF NOT EXISTS
   legacy_doc_unit_id uuid;
-
-ALTER TABLE IF EXISTS
-  incremental_migration.norm_reference
-ADD
-  CONSTRAINT fk_legacy_doc_unit FOREIGN KEY (legacy_doc_unit_id) REFERENCES public.doc_unit (uuid) MATCH SIMPLE ON UPDATE NO ACTION ON DELETE NO ACTION;
