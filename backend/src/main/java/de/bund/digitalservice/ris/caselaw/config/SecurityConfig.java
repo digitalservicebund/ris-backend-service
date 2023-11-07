f8ea06939c376eebc17af0a117bf7601984d956e
Victor del Campo <victor.delcampo@digitalservice.bund.de>
2023-11-07 13:05:55 +0100

    Readd HTTP headers
    RISDEV-1644

--- backend/src/main/java/de/bund/digitalservice/ris/caselaw/config/SecurityConfig.java
+++ backend/src/main/java/de/bund/digitalservice/ris/caselaw/config/SecurityConfig.java
@@ -12,6 +12,9 @@ import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
 import org.springframework.security.oauth2.jwt.ReactiveJwtDecoders;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.security.web.server.authentication.HttpStatusServerEntryPoint;
+import org.springframework.security.web.server.header.ReferrerPolicyServerHttpHeadersWriter;
+import org.springframework.security.web.server.header.XContentTypeOptionsServerHttpHeadersWriter;
+import org.springframework.security.web.server.header.XFrameOptionsServerHttpHeadersWriter;
 
 @Configuration
 @EnableWebFluxSecurity
@@ -43,23 +46,31 @@ public class SecurityConfig {
         .csrf(ServerHttpSecurity.CsrfSpec::disable)
         .headers(
             headers ->
-                headers.contentSecurityPolicy(
-                    customizer ->
-                        customizer.policyDirectives(
-                            "default-src 'self'; img-src 'self' data:; style-src 'self' 'unsafe-inline'"))
-            //                    .writer(new XContentTypeOptionsServerHttpHeadersWriter())
-            //                    .frameOptions(
-            //                        frameOptions ->
-            //
-            // frameOptions.mode(XFrameOptionsServerHttpHeadersWriter.Mode.SAMEORIGIN))
-            //                    .referrerPolicy(
-            //                        referrerPolicySpec ->
-            //                            referrerPolicySpec.policy(
-            //                                ReferrerPolicyServerHttpHeadersWriter.ReferrerPolicy
-            //                                    .STRICT_ORIGIN_WHEN_CROSS_ORIGIN))
-            //                    .permissionsPolicy(permissionsPolicySpec ->
-            // permissionsPolicySpec.policy("*"))
-            )
+                headers
+                    .contentSecurityPolicy(
+                        customizer ->
+                            customizer.policyDirectives(
+                                "default-src 'self'; img-src 'self' data:; style-src 'self' 'unsafe-inline'"))
+                    .writer(new XContentTypeOptionsServerHttpHeadersWriter())
+                    .frameOptions(
+                        frameOptions ->
+                            frameOptions.mode(XFrameOptionsServerHttpHeadersWriter.Mode.SAMEORIGIN))
+                    .referrerPolicy(
+                        referrerPolicySpec ->
+                            referrerPolicySpec.policy(
+                                ReferrerPolicyServerHttpHeadersWriter.ReferrerPolicy
+                                    .STRICT_ORIGIN_WHEN_CROSS_ORIGIN))
+                    .permissionsPolicy(
+                        permissionsPolicySpec ->
+                            permissionsPolicySpec.policy(
+                                "accelerometer=(), ambient-light-sensor=(), autoplay=(), battery=(), camera=(), cross-origin-isolated=(), "
+                                    + "display-capture=(), document-domain=(), encrypted-media=(), execution-while-not-rendered=(), "
+                                    + "execution-while-out-of-viewport=(), fullscreen=(), geolocation=(), gyroscope=(), keyboard-map=(), "
+                                    + "magnetometer=(), microphone=(), midi=(), navigation-override=(), payment=(), picture-in-picture=(), "
+                                    + "publickey-credentials-get=(), screen-wake-lock=(), sync-xhr=(), usb=(), web-share=(), xr-spatial-tracking=(), "
+                                    + "clipboard-read=(self), clipboard-write=(self), gamepad=(), speaker-selection=(), conversion-measurement=(), "
+                                    + "focus-without-user-activation=(self), hid=(), idle-detection=(), interest-cohort=(), serial=(), sync-script=(), "
+                                    + "trust-token-redemption=(), unload=(), window-placement=(), vertical-scroll=(self)")))
         .oauth2ResourceServer(jwtCustomizer -> jwtCustomizer.jwt(Customizer.withDefaults()))
         .build();
   }
