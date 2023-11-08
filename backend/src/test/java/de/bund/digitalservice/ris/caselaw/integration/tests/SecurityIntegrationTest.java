5a11f12491ef999688408f87f935e5397b4be4b0
Victor del Campo <victor.delcampo@digitalservice.bund.de>
2023-11-08 09:06:18 +0100

    Add backend tests for HTTP headers
    RISDEV-1644

--- backend/src/test/java/de/bund/digitalservice/ris/caselaw/integration/tests/SecurityIntegrationTest.java
+++ backend/src/test/java/de/bund/digitalservice/ris/caselaw/integration/tests/SecurityIntegrationTest.java
@@ -53,4 +53,53 @@ class SecurityIntegrationTest {
             "Content-Security-Policy",
             "default-src 'self'; img-src 'self' data:; style-src 'self' 'unsafe-inline'; script-src 'self' 'unsafe-eval'; connect-src 'self' *.sentry.io data:");
   }
+
+  @Test
+  void shouldHaveEnabledXFrameOptionsHeader() {
+    webTestClient
+        .get()
+        .uri("/")
+        .exchange()
+        .expectHeader()
+        .valueEquals("X-Frame-Options", "SAMEORIGIN");
+  }
+
+  @Test
+  void shouldHaveEnabledXContentTypeOptionsHeader() {
+    webTestClient
+        .get()
+        .uri("/")
+        .exchange()
+        .expectHeader()
+        .valueEquals("X-Content-Type-Options", "nosniff");
+  }
+
+  @Test
+  void shouldHaveEnabledReferrerPolicyHeader() {
+    webTestClient
+        .get()
+        .uri("/")
+        .exchange()
+        .expectHeader()
+        .valueEquals("Referrer-Policy", "strict-origin-when-cross-origin");
+  }
+
+  @Test
+  void shouldHaveEnabledPermissionsPolicyHeader() {
+    webTestClient
+        .get()
+        .uri("/")
+        .exchange()
+        .expectHeader()
+        .valueEquals(
+            "Permissions-Policy",
+            "accelerometer=(), ambient-light-sensor=(), autoplay=(), battery=(), camera=(), cross-origin-isolated=(), "
+                + "display-capture=(), document-domain=(), encrypted-media=(), execution-while-not-rendered=(), "
+                + "execution-while-out-of-viewport=(), fullscreen=(), geolocation=(), gyroscope=(), keyboard-map=(), "
+                + "magnetometer=(), microphone=(), midi=(), navigation-override=(), payment=(), picture-in-picture=(), "
+                + "publickey-credentials-get=(), screen-wake-lock=(), sync-xhr=(), usb=(), web-share=(), xr-spatial-tracking=(), "
+                + "clipboard-read=(self), clipboard-write=(self), gamepad=(), speaker-selection=(), conversion-measurement=(), "
+                + "focus-without-user-activation=(self), hid=(), idle-detection=(), interest-cohort=(), serial=(), sync-script=(), "
+                + "trust-token-redemption=(), window-placement=(), vertical-scroll=(self)");
+  }
 }
