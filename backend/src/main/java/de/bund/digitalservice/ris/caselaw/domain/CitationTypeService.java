2afe6f5cdc0bae5137d073ea7f0a3fe61c3fd391
Ralph von Petersdorff-Campen <ralph.vonpetersdorffcampen@digitalservice.bund.de>
2023-11-10 09:47:14 +0100

    Citation type again hexagonal
    RISDEV-2671

--- backend/src/main/java/de/bund/digitalservice/ris/caselaw/domain/CitationTypeService.java
+++ backend/src/main/java/de/bund/digitalservice/ris/caselaw/domain/CitationTypeService.java
@@ -1,7 +1,5 @@
 package de.bund.digitalservice.ris.caselaw.domain;
 
-import de.bund.digitalservice.ris.caselaw.adapter.database.jpa.DatabaseCitationTypeRepository;
-import de.bund.digitalservice.ris.caselaw.adapter.transformer.CitationTypeTransformer;
 import de.bund.digitalservice.ris.caselaw.domain.lookuptable.citation.CitationType;
 import java.util.List;
 import java.util.Optional;
@@ -11,22 +9,18 @@ import org.springframework.stereotype.Service;
 @Service
 @Slf4j
 public class CitationTypeService {
-  private final DatabaseCitationTypeRepository citationTypeRepository;
+  private final CitationTypeRepository citationTypeRepository;
 
-  public CitationTypeService(DatabaseCitationTypeRepository citationTypeRepository) {
+  public CitationTypeService(CitationTypeRepository citationTypeRepository) {
 
     this.citationTypeRepository = citationTypeRepository;
   }
 
   public List<CitationType> getCitationStyles(Optional<String> searchStr) {
     if (searchStr.isPresent() && !searchStr.get().isBlank()) {
-      return citationTypeRepository.findBySearchStr(searchStr.get().trim()).stream()
-          .map(CitationTypeTransformer::transformToDomain)
-          .toList();
+      return citationTypeRepository.findAllBySearchStr(searchStr.get().trim());
     }
 
-    return citationTypeRepository.findAllByCitationDocumentCategoryOrderByAbbreviation("R").stream()
-        .map(CitationTypeTransformer::transformToDomain)
-        .toList();
+    return citationTypeRepository.findAllByCitationDocumentCategoryOrderByAbbreviation("R");
   }
 }
