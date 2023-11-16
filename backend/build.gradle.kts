dd353a52aad3e3edc55ab15f16bad8584df30991
Hanna Prinz <hanna.prinz@digitalservice.bund.de>
2023-11-16 12:18:30 +0100

    RISDEV-2733

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -206,7 +206,7 @@ dependencies {
     }
     implementation("org.freehep:freehep-graphicsio-emf:2.4")
     // package served by private repo, requires authentication:
-    implementation("de.bund.digitalservice:neuris-juris-xml-export:0.7.16") {
+    implementation("de.bund.digitalservice:neuris-juris-xml-export:0.7.17") {
         exclude(group = "org.slf4j", module = "slf4j-simple")
     }
     // for local development:
