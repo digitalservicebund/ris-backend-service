3e6a1a9e90a08896d1ef3eaf23a59fd76dbff082
Fabio Tacke <fabio.tacke@digitalservice.bund.de>
2023-11-28 07:09:44 +0100

    Update exporter version
    RISDEV-2732

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -206,7 +206,7 @@ dependencies {
     }
     implementation("org.freehep:freehep-graphicsio-emf:2.4")
     // package served by private repo, requires authentication:
-    implementation("de.bund.digitalservice:neuris-juris-xml-export:0.8.7") {
+    implementation("de.bund.digitalservice:neuris-juris-xml-export:0.8.8") {
         exclude(group = "org.slf4j", module = "slf4j-simple")
     }
     // for local development:
