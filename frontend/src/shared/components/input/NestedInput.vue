dbc2cada8c10bd0298d8f5a272ef6c96d95fa189
Leonie Koch <leonie.koch@digitalservice.bund.de>
2023-12-11 11:28:40 +0100

    Fix tabbing order
    RISDEV-2461

--- frontend/src/shared/components/input/NestedInput.vue
+++ frontend/src/shared/components/input/NestedInput.vue
@@ -54,23 +54,6 @@ function toggleContentVisibility(): void {
 
 <template>
   <div class="relative">
-    <div class="absolute -right-10 top-48 z-10">
-      <button
-        :aria-label="`${ariaLabel} ${
-          localIsExpanded ? 'schließen' : 'anzeigen'
-        }`"
-        class="w-icon rounded-full bg-blue-800 text-white"
-        @click="toggleContentVisibility"
-      >
-        <slot v-if="localIsExpanded" name="close-icon">
-          <IconHorizontalRule />
-        </slot>
-        <slot v-else name="open-icon">
-          <IconAdd />
-        </slot>
-      </button>
-    </div>
-
     <!-- Parent Element -->
     <InputFieldComponent
       :id="fields.parent.name"
@@ -88,6 +71,24 @@ function toggleContentVisibility(): void {
         @update:validation-error="updateValidationError"
       ></InputElement>
     </InputFieldComponent>
+
+    <!-- Toggle Button -->
+    <div class="absolute -right-10 top-48 z-10">
+      <button
+        :aria-label="`${ariaLabel} ${
+          localIsExpanded ? 'schließen' : 'anzeigen'
+        }`"
+        class="w-icon rounded-full bg-blue-800 text-white"
+        @click="toggleContentVisibility"
+      >
+        <slot v-if="localIsExpanded" name="close-icon">
+          <IconHorizontalRule />
+        </slot>
+        <slot v-else name="open-icon">
+          <IconAdd />
+        </slot>
+      </button>
+    </div>
 
     <!-- Child Element -->
     <InputFieldComponent
