dfda2f40f1f1a05d6bb3871db79889997b996c4d
Florian <florian.zechmeister@digitalservice.bund.de>
2023-12-29 16:59:41 +0100

    Redirect 404s to frontend
    Addresses RISDEV-2956

---
+++ backend/src/main/java/de/bund/digitalservice/ris/caselaw/config/ForwardingWebExceptionHandler.java
@@ -0,0 +1,25 @@
+package de.bund.digitalservice.ris.caselaw.config;
+
+import java.net.URI;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebExceptionHandler;
+import reactor.core.publisher.Mono;
+
+public class ForwardingWebExceptionHandler implements WebExceptionHandler {
+
+  @Override
+  public Mono<Void> handle(ServerWebExchange exchange, Throwable ex) {
+    if (ex instanceof ResponseStatusException) {
+      HttpStatusCode status = ((ResponseStatusException) ex).getStatusCode();
+      if (status == HttpStatus.NOT_FOUND) {
+        exchange.getResponse().setStatusCode(HttpStatus.FOUND);
+        exchange.getResponse().getHeaders().setLocation(URI.create("/404.index"));
+        return exchange.getResponse().setComplete();
+      }
+    }
+    return Mono.error(ex);
+  }
+}
