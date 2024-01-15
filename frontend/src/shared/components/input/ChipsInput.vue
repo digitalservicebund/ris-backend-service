286a1055fa6a114b98e896b3e1775a396d654547
Fabio Tacke <fabio.tacke@digitalservice.bund.de>
2024-01-15 07:42:39 +0100

    Revert "Refactor ChipsDateInput"
    This reverts commit fe8127233714e774674fc671bac169b1860d23eb.

--- frontend/src/shared/components/input/ChipsInput.vue
+++ frontend/src/shared/components/input/ChipsInput.vue
@@ -1,6 +1,5 @@
 <script lang="ts" setup>
 import { produce } from "immer"
-import { vMaska } from "maska"
 import { nextTick, ref, watch, watchEffect } from "vue"
 import ChipsList from "@/shared/components/input/ChipsList.vue"
 import IconSubdirectoryArrowLeft from "~icons/ic/baseline-subdirectory-arrow-left"
@@ -10,7 +9,6 @@ interface Props {
   modelValue?: string[]
   ariaLabel?: string
   readOnly?: boolean
-  maska?: string
 }
 
 const props = defineProps<Props>()
@@ -131,7 +129,7 @@ watch(newChipText, async () => {
   <!-- eslint-disable-next-line vuejs-accessibility/no-static-element-interactions -->
   <div
     ref="wrapperEl"
-    class="flex min-h-48 w-full cursor-text flex-wrap items-center overflow-hidden bg-white px-16 py-8 outline-2 -outline-offset-4 autofill:text-inherit autofill:shadow-white autofill:focus:text-inherit autofill:focus:shadow-white"
+    class="min-h-48 flex w-full cursor-text flex-wrap items-center overflow-hidden bg-white px-16 py-8 outline-2 -outline-offset-4 autofill:text-inherit autofill:shadow-white autofill:focus:text-inherit autofill:focus:shadow-white"
     :class="[
       readOnly
         ? 'hover:outline-none'
@@ -160,11 +158,9 @@ watch(newChipText, async () => {
         :id="id"
         ref="chipsInput"
         v-model="newChipText"
-        v-maska
         :aria-describedby="`enter-note-for-${id}`"
         :aria-label="ariaLabel"
         class="peer w-4 min-w-0 border-none bg-transparent outline-none"
-        :data-maska="maska ?? null"
         :style="{ width: inputContentWidth }"
         type="text"
         @blur="addChip"
