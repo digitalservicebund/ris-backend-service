4760ff50c7de3002392a6bdf689a7a17182f8baa
Ralph von Petersdorff-Campen <ralph.vonpetersdorffcampen@digitalservice.bund.de>
2024-01-23 10:55:46 +0100

    Add SourceDTO
    Address RISDEV-0000

--- backend/src/main/java/de/bund/digitalservice/ris/caselaw/adapter/database/jpa/SourceDTO.java
+++ backend/src/main/java/de/bund/digitalservice/ris/caselaw/adapter/database/jpa/SourceDTO.java
@@ -1,3 +1,32 @@
 package de.bund.digitalservice.ris.caselaw.adapter.database.jpa;
 
-public class SourceDTO {}
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(schema = "incremental_migration", name = "source")
+public class SourceDTO {
+  @Id @GeneratedValue private UUID id;
+
+  @NotNull private Integer rank;
+
+  @Size(max = 1000)
+  @NotBlank
+  private String value;
+}
