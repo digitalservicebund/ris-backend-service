bdafb884524776bbb04a9317e29c688859ce9084
Hanna Prinz <hanna.prinz@digitalservice.bund.de>
2024-01-26 08:50:56 +0100

    Show expanded file preview in right panel
    RISDEV-3198

--- frontend/src/components/OriginalFileSidePanel.vue
+++ frontend/src/components/OriginalFileSidePanel.vue
@@ -64,7 +64,7 @@ const uploadFileRoute = computed(() =>
         v-else
         class="h-[65vh] overflow-scroll border-1 border-solid border-gray-400"
       >
-        <TextEditor element-id="odoc" :value="file" />
+        <TextEditor element-id="odoc" expanded :value="file" />
       </div>
     </div>
   </div>
