56d54b80adb913ec7ec5444af15efd0bae28b436
Nadav <nadav.babai@digitalservice.bund.de>
2024-02-02 15:51:57 +0100

    CoreData with leading DecisionNormReferences
    RISDEV-3268

---
+++ frontend/src/shared/components/InputErrorMessages.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import IconErrorOutline from "~icons/ic/baseline-error-outline"
+
+interface Props {
+  errorMessage?: string
+}
+
+const props = defineProps<Props>()
+</script>
+<template>
+  <div v-if="errorMessage" class="mb-10 flex flex-row items-center">
+    <IconErrorOutline class="leading-default text-gray-900" />
+    <p class="ds-label-02-reg m-4 text-gray-900">
+      {{ props.errorMessage }}
+    </p>
+  </div>
+</template>
