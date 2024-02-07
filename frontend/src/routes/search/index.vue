f53ad59eb781af3c2754ef4a75e895e7cf56b8b5
Christian Schuhmann <christian.schuhmann@digitalservice.bund.de>
2024-02-07 12:48:41 +0100

    Rollback search help page
    RISDEV-3217

--- frontend/src/routes/search/index.vue
+++ frontend/src/routes/search/index.vue
@@ -42,77 +42,30 @@ async function handleSearchSubmit() {
 </script>
 
 <template>
-  <header class="container mx-auto flex flex-col gap-16 px-16 py-16">
-    <div
-      aria-label="Infomodal"
-      class="flex w-full gap-[0.625rem] border-l-[0.125rem] border-l-yellow-800 bg-yellow-200 px-[1.25rem] py-[1.125rem]"
-    >
-      <svg
-        aria-label="Es ist ein Fehler aufgetreten icon"
-        class="text-yellow-800"
-        height="1.3333em"
-        viewBox="0 0 24 24"
-        width="1.3333em"
-      >
-        <path
-          d="M11 15h2v2h-2zm0-8h2v6h-2zm.99-5C6.47 2 2 6.48 2 12s4.47 10 9.99 10C17.52 22 22 17.52 22 12S17.52 2 11.99 2zM12 20c-4.42 0-8-3.58-8-8s3.58-8 8-8s8 3.58 8 8s-3.58 8-8 8z"
-          fill="currentColor"
-        />
-      </svg>
-      <div class="flex flex-col">
-        <span class="ds-label-02-bold">
-          Die Suche ist aktuell ein Prototyp
-        </span>
-        <span class="ds-body-01-reg">
-          Die Funktionalität der Suche befindet sich gerade in der Entwicklung
-          und ist zum Testen für
-          <strong>veröffentlichte</strong> Dokumentationseinheiten verfügbar.
-          <router-link
-            class="underline focus:outline-none focus-visible:outline-4 focus-visible:outline-offset-4 focus-visible:outline-blue-800"
-            target="_blank"
-            to="/search/help"
-          >
-            Lesen Sie hier mehr über die aktuelle Funktionalität.
-          </router-link>
-        </span>
-      </div>
-    </div>
-
+  <header class="bg-white px-16 py-16">
     <h1 class="ds-heading-02-reg">Suche</h1>
-
-    <div class="flex flex-col gap-4">
-      <div>
-        <InputField id="searchInput" label="Suche" visually-hide-label>
-          <TextInput
-            id="searchInput"
-            v-model="searchInput"
-            aria-label="Suchanfrage"
-            class="ds-input-medium"
-            :disabled="isLoading"
-            placeholder="Suchanfrage"
-          />
-        </InputField>
-      </div>
-
-      <div class="flex flex-row items-center gap-8">
-        <TextButton
-          aria-label="Suchen"
-          class="self-start"
+    <div class="mt-32">
+      <InputField id="searchInput" label="Suche" visually-hide-label>
+        <TextInput
+          id="searchInput"
+          v-model="searchInput"
+          aria-label="Sucheingabe"
+          class="ds-input-medium"
           :disabled="isLoading"
-          label="Suchen"
-          size="small"
-          @click="handleSearchSubmit"
-        />
-        <router-link
-          class="block underline focus:outline-none focus-visible:outline-4 focus-visible:outline-offset-4 focus-visible:outline-blue-800"
-          target="_blank"
-          to="/search/help"
-        >
-          Hilfe
-        </router-link>
-      </div>
-
-      <p :class="{ 'text-red-700': hasError }">{{ message }}</p>
+          placeholder="Sucheingabe"
+        ></TextInput>
+      </InputField>
     </div>
+    <div class="py-8">
+      <TextButton
+        aria-label="Suchen"
+        class="self-start"
+        :disabled="isLoading"
+        label="Suchen"
+        size="small"
+        @click="handleSearchSubmit"
+      />
+    </div>
+    <p :class="{ 'text-red-700': hasError }">{{ message }}</p>
   </header>
 </template>
