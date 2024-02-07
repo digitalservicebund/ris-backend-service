e3ab9b05ba4a2f18ad1a4a5a34ab365a31e7e6f4
Leonie Koch <leonie.koch@digitalservice.bund.de>
2024-02-07 16:56:14 +0100

    Update editable list story
    RISDEV-0000

--- frontend/src/routes/kitchensink/index/editableList.vue
+++ frontend/src/routes/kitchensink/index/editableList.vue
@@ -1,50 +1,71 @@
 <script lang="ts" setup>
-import { ref } from "vue"
+import { defineComponent, h, ref } from "vue"
 import EditableList from "@/components/EditableListCaselaw.vue"
 import EditableListItem from "@/domain/editableListItem"
 import KitchensinkPage from "@/kitchensink/components/KitchensinkPage.vue"
 import KitchensinkStory from "@/kitchensink/components/KitchensinkStory.vue"
+import { withSummarizer } from "@/shared/components/DataSetSummary.vue"
 import TextInput from "@/shared/components/input/TextInput.vue"
 
-const listWithEntries = ref<EditableListItem[]>([
-  {
-    isReadOnly: false,
-    hasForeignSource: false,
-    isEmpty: false,
-    renderDecision: "Foo",
-    hasMissingRequiredFields: false,
-    missingRequiredFields: [],
-    showSummaryOnEdit: false,
-  },
-  {
-    isReadOnly: false,
-    hasForeignSource: false,
-    isEmpty: false,
-    renderDecision: "Foo",
-    hasMissingRequiredFields: false,
-    missingRequiredFields: [],
-    showSummaryOnEdit: false,
-  },
+class ExtendedEditableListItem implements EditableListItem {
+  public text?: string
+
+  constructor(data: Partial<ExtendedEditableListItem> = {}) {
+    Object.assign(this, data)
+  }
+
+  get renderDecision(): string {
+    return this.text ? this.text : "default text"
+  }
+}
+
+const listWithEntries = ref<ExtendedEditableListItem[]>([
+  new ExtendedEditableListItem({ text: "foo" }),
+  new ExtendedEditableListItem({ text: "bar" }),
 ])
+
+function summerizer(dataEntry: EditableListItem) {
+  return h("div", { class: ["ds-label-01-reg"] }, dataEntry.renderDecision)
+}
+
+const SummaryComponent = withSummarizer(summerizer)
+const EditComponent = defineComponent({
+  //Todo: how to define props and emits in defineComponent
+  // eslint-disable-next-line vue/require-prop-types
+  props: ["modelValue"],
+  emits: ["update:modelValue"],
+  setup(props, { emit }) {
+    return () =>
+      h(TextInput, {
+        id: "testId",
+        ariaLabel: "testAriaLabel",
+        modelValue: props.modelValue.text,
+        class: ["my-24 ds-input-medium"],
+        "onUpdate:modelValue": (value) => emit("update:modelValue", value),
+      })
+  },
+})
+
+const defaultValue = new ExtendedEditableListItem()
 const emptyList = ref([])
 </script>
 
 <template>
-  <KitchensinkPage name="Edtiable list">
+  <KitchensinkPage name="Editable list">
     <KitchensinkStory name="With entries">
       <EditableList
         v-model="listWithEntries"
-        :default-value="listWithEntries[0]"
-        :edit-component="TextInput"
+        :default-value="defaultValue"
+        :edit-component="EditComponent"
+        :summary-component="SummaryComponent"
       />
     </KitchensinkStory>
-
-    <KitchensinkStory name="Without entries">
+    <KitchensinkStory name="With no entries">
       <EditableList
         v-model="emptyList"
-        :default-value="listWithEntries[0]"
-        :edit-component="TextInput"
-        transformer=""
+        :default-value="defaultValue"
+        :edit-component="EditComponent"
+        :summary-component="SummaryComponent"
       />
     </KitchensinkStory>
   </KitchensinkPage>
