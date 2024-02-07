6ee3a418c5e7118449bcb894d5802268bc8ad43e
Leonie Koch <leonie.koch@digitalservice.bund.de>
2024-02-07 17:19:36 +0100

    Fix minor styling issues
    RISDEV-0000

--- frontend/src/routes/kitchensink/index/sideToggle.vue
+++ frontend/src/routes/kitchensink/index/sideToggle.vue
@@ -7,7 +7,7 @@ import SideToggle from "@/shared/components/SideToggle.vue"
   <KitchensinkPage name="Side toggle">
     <div class="flex h-[10rem] justify-between gap-[3rem]">
       <SideToggle from-side="left" label="Inhalt">
-        <div class="flex w-full">
+        <div class="flex w-full p-24">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem
           iusto autem quasi saepe fuga quis dolores dolor ipsum eligendi dolore
           aliquid, magni quos excepturi amet, odit ipsa, provident eveniet
@@ -15,7 +15,7 @@ import SideToggle from "@/shared/components/SideToggle.vue"
         </div>
       </SideToggle>
       <SideToggle from-side="right" label="Inhalt rechts">
-        <div class="flex w-full">
+        <div class="flex w-full p-24">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem
           iusto autem quasi saepe fuga quis dolores dolor ipsum eligendi dolore
           aliquid, magni quos excepturi amet, odit ipsa, provident eveniet
