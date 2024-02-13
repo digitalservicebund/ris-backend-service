95207ba5bec678a24f890d286067e77caa8bc2ed
Hanna Prinz <hanna.prinz@digitalservice.bund.de>
2024-02-13 15:14:45 +0100

    TODOs and comments for draft
    RISDEV-3248

--- backend/src/main/java/de/bund/digitalservice/ris/caselaw/adapter/DatabaseDocumentNumberService.java
+++ backend/src/main/java/de/bund/digitalservice/ris/caselaw/adapter/DatabaseDocumentNumberService.java
@@ -14,6 +14,7 @@ public class DatabaseDocumentNumberService implements DocumentNumberService {
   private final DatabaseDocumentNumberCounterRepository repository;
   private static final String CASELAW_ABBREVIATION = "RE";
 
+  // TODO delete - will be replaced by configuration in application.yaml
   private static final Map<String, String> DOCUMENT_NUMBER_ABBREVIATIONS =
       Map.ofEntries(
           Map.entry("BGH", "KO"),
@@ -38,7 +39,7 @@ public class DatabaseDocumentNumberService implements DocumentNumberService {
 
     DocumentNumberCounterDTO numberCounterDTO =
         repository
-            .findById(1)
+            .findById(1) // findById("bgh")
             .orElse(
                 DocumentNumberCounterDTO.builder().currentYear(currentYear).nextNumber(1).build());
 
@@ -47,13 +48,36 @@ public class DatabaseDocumentNumberService implements DocumentNumberService {
       numberCounterDTO.setNextNumber(1);
     }
 
+    // TODO either string replacement or string format
+
+    // BGH Example
+    // parse docoffice specific format --> for BGH KORE7****YYYY
+    // replace **** with next available number
+    // --> "%s%s%04d%04d"
+    // %s -> KO
+    // %s -> RE7
+    // %04d -> 0001
+    // %04d -> 2024
+
+    // BVerfG Example
+    // parse docoffice specific format --> for BVerfG KVRE*****YY41
+    // replace ***** with next available number
+    // --> "%s%s%05d%02d%s"
+    // %s -> KV
+    // %s -> RE
+    // %05d -> 00001
+    // %02d -> 24
+    // %s -> 41
+
+    // TODO use docnumber counter of doc office
+
     String result =
         String.format(
             "%s%s%04d%05d",
-            DOCUMENT_NUMBER_ABBREVIATIONS.get(documentationOffice.abbreviation()),
-            CASELAW_ABBREVIATION,
-            currentYear,
-            numberCounterDTO.getNextNumber());
+            DOCUMENT_NUMBER_ABBREVIATIONS.get(documentationOffice.abbreviation()), // XX
+            CASELAW_ABBREVIATION, // RE
+            currentYear, // 2024
+            numberCounterDTO.getNextNumber()); // 00001
 
     numberCounterDTO.setNextNumber(numberCounterDTO.getNextNumber() + 1);
     repository.save(numberCounterDTO);
