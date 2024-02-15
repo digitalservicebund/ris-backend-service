e1295102bbc0255a52fb5eb639ec8b1410946d10
Florian <florian.zechmeister@digitalservice.bund.de>
2024-02-15 17:14:15 +0100

    Add unit test for `FooterConverter`
    RISDEV-0000

---
+++ backend/src/test/java/de/bund/digitalservice/ris/caselaw/adapter/converter/docx/FooterConverterTest.java
@@ -0,0 +1,35 @@
+package de.bund.digitalservice.ris.caselaw.adapter.converter.docx;
+
+import static org.mockito.Mockito.mock;
+
+import de.bund.digitalservice.ris.caselaw.domain.docx.ParagraphElement;
+import java.util.Arrays;
+import java.util.List;
+import org.docx4j.wml.P;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
+
+public class FooterConverterTest {
+
+  @Test
+  void testConvert() {
+    DocxConverter mockConverter = mock(DocxConverter.class);
+    P mockP = mock(P.class);
+
+    try (MockedStatic<ParagraphConverter> mockedStatic =
+        Mockito.mockStatic(ParagraphConverter.class)) {
+      ParagraphElement mockParagraphElement = new ParagraphElement();
+      mockedStatic
+          .when(() -> ParagraphConverter.convert(mockP, mockConverter))
+          .thenReturn(mockParagraphElement);
+      List<Object> content = Arrays.asList(mockP);
+
+      ParagraphElement result = FooterConverter.convert(content, mockConverter);
+
+      Assertions.assertNotNull(result);
+      Assertions.assertEquals(mockParagraphElement, result);
+    }
+  }
+}
