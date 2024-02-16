da985ab17e68572e8afc02e05ee0146dcc0fc974
Hanna Prinz <hanna.prinz@digitalservice.bund.de>
2024-02-16 13:13:20 +0100

    Add Test for DocumentTypeController
    RISDEV-0000

--- backend/src/main/java/de/bund/digitalservice/ris/caselaw/domain/lookuptable/documenttype/DocumentType.java
+++ backend/src/main/java/de/bund/digitalservice/ris/caselaw/domain/lookuptable/documenttype/DocumentType.java
@@ -3,6 +3,5 @@ package de.bund.digitalservice.ris.caselaw.domain.lookuptable.documenttype;
 import java.util.UUID;
 import lombok.Builder;
 
-// Todo update domain with uuid and document_category?
 @Builder
 public record DocumentType(UUID uuid, String jurisShortcut, String label) {}
---
+++ backend/src/test/resources/document_types.sql
@@ -0,0 +1,23 @@
+insert into
+  incremental_migration.document_type (id, abbreviation, label)
+values
+  (
+    '0f382996-497f-4c2f-9a30-1c73d8ac0a87',
+    'Beschluss',
+    'Bes'
+  ),
+  (
+    '11defe05-cd4d-43e5-a07e-06c611b81a26',
+    'Urteil',
+    'Ur'
+  ),
+  (
+    '2e5bab9c-8852-49a3-8ed8-08b67399abde',
+    'Anordnung',
+    'Ao'
+  ),
+  (
+    '3fc6c738-7e81-40bd-8aa4-c5426605e9b0',
+    'Amtsrechtliche Anordnung',
+    'AmA'
+  );
