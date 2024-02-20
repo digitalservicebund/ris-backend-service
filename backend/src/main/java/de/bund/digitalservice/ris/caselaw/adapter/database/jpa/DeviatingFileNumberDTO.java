c3ab07edea56a5969fbef56445e8d270289ff740
Leonie Koch <leonie.koch@digitalservice.bund.de>
2024-02-20 16:09:29 +0100

    Fix: order deviating filenumbers
    RISDEV-3289

--- backend/src/main/java/de/bund/digitalservice/ris/caselaw/adapter/database/jpa/DeviatingFileNumberDTO.java
+++ backend/src/main/java/de/bund/digitalservice/ris/caselaw/adapter/database/jpa/DeviatingFileNumberDTO.java
@@ -7,7 +7,6 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
-import jakarta.persistence.Transient;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import java.io.Serializable;
@@ -38,7 +37,7 @@ public class DeviatingFileNumberDTO {
   @NotNull
   private DocumentationUnitDTO documentationUnit;
 
-  @Transient private Long rank;
+  private Long rank;
 }
 
 @AllArgsConstructor
