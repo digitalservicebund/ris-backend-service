7440573e9f22469b659f166933ba2537e45a69f1
Nadav <nadav.babai@digitalservice.bund.de>
2024-02-21 15:51:56 +0100

    Document number pattern getters only
    RISDEV-3248

--- backend/src/main/java/de/bund/digitalservice/ris/caselaw/adapter/DocumentNumberPatternConfig.java
+++ backend/src/main/java/de/bund/digitalservice/ris/caselaw/adapter/DocumentNumberPatternConfig.java
@@ -3,18 +3,19 @@ package de.bund.digitalservice.ris.caselaw.adapter;
 import de.bund.digitalservice.ris.caselaw.domain.DocumentNumberPatternException;
 import java.util.Map;
 import lombok.Getter;
-import lombok.Setter;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ConfigurationProperties(prefix = "neuris")
-@Getter
-@Setter
 public class DocumentNumberPatternConfig implements InitializingBean {
 
-  Map<String, String> documentNumberPatterns;
+  @Getter private final Map<String, String> documentNumberPatterns;
+
+  public DocumentNumberPatternConfig(Map<String, String> documentNumberPatterns) {
+    this.documentNumberPatterns = documentNumberPatterns;
+  }
 
   @Override
   public void afterPropertiesSet() throws DocumentNumberPatternException {
