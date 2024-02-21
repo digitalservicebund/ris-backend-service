b2b722ba4379a297a7f85a365f2de8e7b4d7ad9b
Nadine Kost <nadine.kost@digitalservice.bund.de>
2024-02-21 16:52:53 +0100

    Input rule for border-number-link node with attrs
    RISDEV-3172

--- frontend/src/editor/borderNumberLink.ts
+++ frontend/src/editor/borderNumberLink.ts
@@ -1,5 +1,8 @@
-import { Node } from "@tiptap/vue-3"
+import { Node, nodeInputRule } from "@tiptap/vue-3"
 
+export interface BorderNumberLinkOptions {
+  validNumbers: string[]
+}
 export const BorderNumberLink = Node.create({
   name: "borderNumberLink",
   priority: 1000,
@@ -8,6 +11,23 @@ export const BorderNumberLink = Node.create({
   inline: true,
   selectable: false,
   content: "text*",
+  addAttributes() {
+    return {
+      nr: {
+        default: null,
+        parseHTML: (element) => element.getAttribute("nr"),
+        renderHTML: (attributes) => {
+          if (!attributes.nr) {
+            return {}
+          }
+
+          return {
+            nr: attributes.nr,
+          }
+        },
+      },
+    }
+  },
   parseHTML() {
     return [{ tag: "border-number-link" }]
   },
@@ -19,7 +39,44 @@ export const BorderNumberLink = Node.create({
           "color: #003350; font-weight: bold; text-decoration: underline; padding: 2px; font-style: italic",
         contenteditable: false,
       },
-      `Rn. ${node.textContent}`,
+      node.attrs.nr ? `${node.attrs.nr}` : 0,
+    ]
+  },
+  addKeyboardShortcuts() {
+    return {
+      Backspace: () =>
+        this.editor.commands.command(({ tr, state }) => {
+          let isBorderNumberLink = false
+          const { selection } = state
+          const { empty, anchor } = selection
+
+          if (!empty) {
+            return false
+          }
+
+          state.doc.nodesBetween(anchor - 1, anchor, (node, pos) => {
+            if (node.type.name === this.name) {
+              isBorderNumberLink = true
+              tr.insertText("##" || "", pos, pos + node.nodeSize)
+
+              return false
+            }
+          })
+
+          return isBorderNumberLink
+        }),
+    }
+  },
+  addInputRules() {
+    return [
+      nodeInputRule({
+        find: /(##(\d+))\s$/,
+        type: this.type,
+        getAttributes(match) {
+          console.log(match)
+          return { nr: match[2] }
+        },
+      }),
     ]
   },
 })
