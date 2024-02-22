30621baa5b75d75943fde0a73b75ffa05f1b73f3
Nadine Kost <nadine.kost@digitalservice.bund.de>
2024-02-22 17:06:44 +0100

    [WIP] Render border number link validity
    Shows border number link in blue if valid or red if invalid. Using dummy data for now.
    
    RISDEV-3415 & RISDEV3425

--- frontend/src/editor/borderNumberLinkMark.ts
+++ frontend/src/editor/borderNumberLinkMark.ts
@@ -11,12 +11,12 @@ export interface BorderNumberOptions {
 }
 
 export const BorderNumberLinkMark = Mark.create<BorderNumberOptions>({
-  name: "BorderNumber",
+  name: "BorderNumberLink",
 
   addOptions() {
     return {
       HTMLAttributes: {},
-      validBorderNumbers: [],
+      validBorderNumbers: ["1", "2", "3"],
     }
   },
 
@@ -29,24 +29,33 @@ export const BorderNumberLinkMark = Mark.create<BorderNumberOptions>({
   },
 
   renderHTML({ mark, HTMLAttributes }) {
-    this.options.validBorderNumbers
+    const valid = this.options.validBorderNumbers.includes(mark.attrs.nr)
+    const color = valid ? "text-blue-800" : "text-red-800"
     return [
       "border-number-link",
       mergeAttributes(this.options.HTMLAttributes, HTMLAttributes, {
-        class: "ds-heading-03-bold, ds-text-blue-900",
-        valid: false,
+        // class: "ds-heading-03-bold, ds-text-blue-900",
+        class: `font-bold ${color} italic underline`,
+        // style:
+        //     "color: #003350; font-weight: bold; text-decoration: underline; padding: 2px; font-style: italic",
+        valid: valid,
       }),
-      `Rn. ${mark}`,
+      `Rn. ${mark.attrs.nr}`,
     ]
   },
 
   addInputRules() {
     return [
       markInputRule({
-        find: this.options.validBorderNumbers
-          ? new RegExp(`(##(${"1|2|3"})##)`, "g")
-          : /(?:^|\s)(##(\d+)##)$/,
+        find: /(?:^|\s)((?:#)((?:[^#]+))(?:#))$/,
+        // find: this.options.validBorderNumbers
+        //   ? new RegExp(`(##(${"1|2|3"})##)$`, "g")
+        //   : /(?:^|\s)(##(\d+)##)$/,
         type: this.type,
+        getAttributes(match) {
+          console.log(match)
+          return { nr: match[2] }
+        },
       }),
     ]
   },
@@ -91,6 +100,23 @@ export const BorderNumberLinkMark = Mark.create<BorderNumberOptions>({
       //     return true
       //   })
       // },
+    }
+  },
+  addAttributes() {
+    return {
+      nr: {
+        default: null,
+        parseHTML: (element) => element.getAttribute("nr"),
+        renderHTML: (attributes) => {
+          if (!attributes.nr) {
+            return {}
+          }
+
+          return {
+            nr: attributes.nr,
+          }
+        },
+      },
     }
   },
 })
