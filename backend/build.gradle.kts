4420735f58ae36404e32a4e02768892bd3f51886
Nadine Kost <nadine.kost@digitalservice.bund.de>
2024-03-14 14:18:55 +0100

    Bump exporter version
    RISDEV-3479

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -184,7 +184,7 @@ dependencies {
     implementation("com.icegreen:greenmail:2.0.0")
 
     // package served by private repo, requires authentication:
-    implementation("de.bund.digitalservice:neuris-juris-xml-export:0.8.31") {
+    implementation("de.bund.digitalservice:neuris-juris-xml-export:0.8.32") {
         exclude(group = "org.slf4j", module = "slf4j-simple")
     }
     // for local development:
