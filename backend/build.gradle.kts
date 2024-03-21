bfc1002b65721dfec611aed1f82dffd432c968c9
anne-ds <anne.schumann@digitalservice.bund.de>
2024-03-21 12:00:17 +0100

    Resolve CVE-2023-52428
    Parent is spring-boot-starter-oauth2-client:3.2.3
    RISDEV-0000
    
    Signed-off-by: anne-ds <anne.schumann@digitalservice.bund.de>

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -165,6 +165,8 @@ dependencies {
     implementation("org.apache.commons:commons-compress:1.26.1")
     // CVE-2022-3171
     implementation("com.google.protobuf:protobuf-java:4.26.0")
+    // CVE-2023-52428 in spring-boot-starter-oauth2-client:3.2.3
+    implementation("com.nimbusds:nimbus-jose-jwt:9.37.3")
     // CVE-2023-31582
     implementation("org.bitbucket.b_c:jose4j:0.9.6")
 
