9a0e7b42ed2c4f7e3353fe33284591f5b109a143
Nadav <nadav.babai@digitalservice.bund.de>
2024-04-12 13:42:50 +0200

    Revert "Fix toggle panel did not close"
    This reverts commit 97bf1f9cdcb57b55c36a8deb6f459f90bda0090e.

--- frontend/src/components/DocumentUnitFiles.vue
+++ frontend/src/components/DocumentUnitFiles.vue
@@ -72,7 +72,7 @@ console.log(props.documentUnit)
 
 const handleOnSelect = (index: number) => {
   selectedAttachmentIndex.value = index
-  showDocPanel.value = !showDocPanel.value
+  showDocPanel.value = true
 }
 
 const handleOnDelete = (index: number) => {
