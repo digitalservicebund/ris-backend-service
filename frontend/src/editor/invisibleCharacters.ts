be2489d49457c54fb61e2746ff41c610224f1fb9
Hanna Prinz <hanna.prinz@digitalservice.bund.de>
2024-04-22 09:12:09 +0200

    Add tab as invisible char
    RISDEV-3183

--- frontend/src/editor/invisibleCharacters.ts
+++ frontend/src/editor/invisibleCharacters.ts
@@ -10,12 +10,13 @@ import {
 import { Extension } from "@tiptap/core"
 import { Node } from "prosemirror-model"
 import "@guardian/prosemirror-invisibles/dist/style.css"
-import "./softHyphen.css"
+import "./invisible-characters.css"
 
 // Can be removed (along with the css file and the createInvisibleDecosForCharacter("soft-hyphen", isSoftHyphen) below) as soon as prosemirror-invisibles is updated (latest version includes soft-hyphen)
 const isSoftHyphen = (char: string) => char === "\u00ad"
 // Necessary as TipTap overrides the hardbreak node type name from hard_break to hardBreak
 const isHardbreak = (node: Node): boolean => node.type.name === "hardBreak"
+const isTab = (char: string) => char === "\t"
 
 export const InvisibleCharacters = Extension.create({
   name: "invisible-characters",
@@ -27,6 +28,7 @@ export const InvisibleCharacters = Extension.create({
         nbSpace,
         heading,
         createInvisibleDecosForCharacter("soft-hyphen", isSoftHyphen),
+        createInvisibleDecosForCharacter("tab", isTab),
         createInvisibleDecosForNode("break", (_, pos) => pos, isHardbreak),
       ]),
     ]
