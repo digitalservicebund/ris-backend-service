a62def054adcce41c46d6c27dfe9f79f22d7c7a9
Nadav <nadav.babai@digitalservice.bund.de>
2024-04-23 17:31:32 +0200

    Navigation with query params
    RISDEV-3822

--- frontend/test/e2e/caselaw/toggle-panels.spec.ts
+++ frontend/test/e2e/caselaw/toggle-panels.spec.ts
@@ -1,5 +1,5 @@
 import { expect } from "@playwright/test"
-import { navigateToCategories } from "./e2e-utils"
+import { navigateToCategories, navigateToFiles } from "./e2e-utils"
 import { caselawTest as test } from "./fixtures"
 
 test.describe("test the different layout options", () => {
@@ -17,11 +17,24 @@ test.describe("test the different layout options", () => {
   }) => {
     await navigateToCategories(page, documentNumber)
     await page.getByLabel("Navigation schließen").click()
-    await expect(page).toHaveURL(/showNavBar=false/)
+    await expect(page).toHaveURL(/showNavigationPanel=false/)
     await expect(page.locator("aside", { hasText: "Rubriken" })).toBeHidden()
 
     await page.getByLabel("Navigation öffnen").click()
-    await expect(page).toHaveURL(/showNavBar=true/)
+    await expect(page).toHaveURL(/showNavigationPanel=true/)
     await expect(page.getByRole("link", { name: "Rubriken" })).toBeVisible()
+  })
+
+  test("navigation state passed to other menu item", async ({
+    page,
+    documentNumber,
+  }) => {
+    await navigateToCategories(page, documentNumber)
+    await page.getByLabel("Navigation schließen").click()
+    await expect(page).toHaveURL(/showNavigationPanel=false/)
+
+    await navigateToFiles(page, documentNumber)
+    await expect(page.locator("aside", { hasText: "Rubriken" })).toBeHidden()
+    await expect(page).toHaveURL(/showNavigationPanel=false/)
   })
 })
