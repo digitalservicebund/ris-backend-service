bf03eab319674254a2f2ea39747a06bd5687b7b0
Nadav <nadav.babai@digitalservice.bund.de>
2024-04-23 17:44:10 +0200

    Fix e2e tests
    RISDEV-3822

--- frontend/test/e2e/caselaw/categories/shortAndLongTexts/copy-paste-formatted.spec.ts
+++ frontend/test/e2e/caselaw/categories/shortAndLongTexts/copy-paste-formatted.spec.ts
@@ -39,7 +39,6 @@ test("copy-paste from side panel", async ({ page, documentNumber }) => {
   // Click on "Rubriken" und check if original document loaded
   await navigateToCategories(page, documentNumber)
 
-  await page.getByLabel("Dokumentansicht öffnen").click()
   await expect(page.getByLabel("Ladestatus")).toBeHidden()
   await expect(page.locator(`text=${rightAlignText}`)).toBeVisible()
   await expect(page.locator(`text=${centerAlignText}`)).toBeVisible()
