e3052884ece8cb484c5d6c3b4169fcb2877d4ac4
Nadine Kost <nadine.kost@digitalservice.bund.de>
2024-04-24 18:39:32 +0200

    Test sonar cloud v-html detection
    RISDEV-0000

--- frontend/src/components/InfoModal.vue
+++ frontend/src/components/InfoModal.vue
@@ -83,7 +83,6 @@ const ariaLabelIcon = props.title + " icon"
         </ul>
       </div>
       <!-- eslint-disable vue/no-v-html -->
-      <span v-else class="ds-body-01-reg" v-html="description"></span>
       <span v-if="!isArray" class="ds-body-01-reg" v-html="description"></span>
     </div>
   </div>
