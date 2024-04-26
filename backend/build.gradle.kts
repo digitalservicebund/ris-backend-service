db14428ebba113f28a2ee5703b25ef8bae1371d7
Hanna Prinz <hanna.prinz@digitalservice.bund.de>
2024-04-26 22:52:16 +0200

    Use maven module instead of local file
    RISDEV-3027

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -192,9 +192,9 @@ dependencies {
     // for local development:
 //     implementation(files("../../neuris-juris-xml-export/build/libs/neuris-juris-xml-export-0.8.38.jar"))
 
-//    implementation("de.bund.digitalservice.neuris.caselaw.migration:schema:0.0.1-SNAPSHOT")
+    implementation("de.bund.digitalservice.neuris.caselaw.migration:schema:0.0.1-SNAPSHOT")
     // for local development:
-    implementation(files("../../ris-data-migration/schema/build/libs/schema-0.0.1-SNAPSHOT.jar"))
+//    implementation(files("../../ris-data-migration/schema/build/libs/schema-0.0.1-SNAPSHOT.jar"))
 
     implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-xml:2.17.0")
 
