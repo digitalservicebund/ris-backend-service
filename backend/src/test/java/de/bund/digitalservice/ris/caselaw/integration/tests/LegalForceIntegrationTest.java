367dfcfc1fe6577bc64800d4504c38349883e4f2
Hanna Prinz <hanna.prinz@digitalservice.bund.de>
2024-04-30 08:42:38 +0200

    Fix test
    RISDEV-0000

--- backend/src/test/java/de/bund/digitalservice/ris/caselaw/integration/tests/LegalForceIntegrationTest.java
+++ backend/src/test/java/de/bund/digitalservice/ris/caselaw/integration/tests/LegalForceIntegrationTest.java
@@ -32,6 +32,7 @@ import de.bund.digitalservice.ris.caselaw.adapter.database.jpa.PostgresDocumenta
 import de.bund.digitalservice.ris.caselaw.adapter.database.jpa.PostgresPublicationReportRepositoryImpl;
 import de.bund.digitalservice.ris.caselaw.adapter.database.jpa.RegionDTO;
 import de.bund.digitalservice.ris.caselaw.config.FeatureToggleConfig;
+import de.bund.digitalservice.ris.caselaw.config.FlywayConfig;
 import de.bund.digitalservice.ris.caselaw.config.PostgresJPAConfig;
 import de.bund.digitalservice.ris.caselaw.config.SecurityConfig;
 import de.bund.digitalservice.ris.caselaw.domain.AttachmentService;
@@ -67,6 +68,7 @@ import software.amazon.awssdk.services.s3.S3AsyncClient;
       DatabaseDocumentUnitStatusService.class,
       PostgresDocumentationUnitRepositoryImpl.class,
       PostgresPublicationReportRepositoryImpl.class,
+      FlywayConfig.class,
       PostgresJPAConfig.class,
       SecurityConfig.class,
       AuthService.class,
@@ -83,8 +85,7 @@ import software.amazon.awssdk.services.s3.S3AsyncClient;
 class LegalForceIntegrationTest {
   @Container
   static PostgreSQLContainer<?> postgreSQLContainer =
-      new PostgreSQLContainer<>("postgres:14")
-          .withInitScript("db/create_migration_scheme_and_extensions.sql");
+      new PostgreSQLContainer<>("postgres:14").withInitScript("init_db.sql");
 
   @DynamicPropertySource
   static void registerDynamicProperties(DynamicPropertyRegistry registry) {
