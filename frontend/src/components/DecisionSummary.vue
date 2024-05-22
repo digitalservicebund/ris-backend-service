f2e4c4de460bffadf05c9659919e15a62f9a55e9
nbabai <nadav.babai@digitalservice.bund.de>
2024-05-22 17:27:15 +0200

    Feature/search resutlist (#1512)
    RISDEV-3690

---
+++ frontend/src/components/DecisionSummary.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import DocumentNumberPreviewLink from "@/components/DocumentNumberLink.vue"
+import FlexContainer from "@/components/FlexContainer.vue"
+import RelatedDocumentation from "@/domain/relatedDocumentation"
+
+interface Props {
+  decision: RelatedDocumentation
+}
+
+const props = defineProps<Props>()
+</script>
+
+<template>
+  <FlexContainer flex-direction="flex-row">
+    {{ decision.renderDecisionWithoutDocumentNumber }}
+    <DocumentNumberPreviewLink :decision="props.decision" />
+  </FlexContainer>
+</template>
