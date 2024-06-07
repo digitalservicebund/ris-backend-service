52d2f880c1fc9b79d02e51e3148ffc95063f92e0
Nadine Kost <nadine.kost@digitalservice.bund.de>
2024-06-07 17:05:26 +0200

    Adjust navbar design
    RISDEV-3503

--- frontend/src/components/NavbarTop.vue
+++ frontend/src/components/NavbarTop.vue
@@ -52,11 +52,11 @@ onMounted(async () => {
 
 <template>
   <nav
-    class="flex items-center justify-between border-y border-gray-400 py-16 pe-32 ps-16 print:hidden"
+    class="flex items-center justify-between border-y border-gray-400 py-16 pe-16 print:hidden"
   >
     <div class="flex items-center gap-44">
       <div class="flex items-center">
-        <span class="px-[1rem] text-14 font-bold leading-16">
+        <span class="px-[1rem] text-14 font-bold uppercase leading-16">
           <span aria-hidden="true" :style="{ color: fontColor }">
             Rechtsinformationen</span
           >
