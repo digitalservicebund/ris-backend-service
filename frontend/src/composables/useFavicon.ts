2f81dc1840445b20e5ed7fd13382f45bc0bf7ca8
Nadine Kost <nadine.kost@digitalservice.bund.de>
2024-06-11 12:01:31 +0200

    Import favicon assets properly
    RISDEV-3503

--- frontend/src/composables/useFavicon.ts
+++ frontend/src/composables/useFavicon.ts
@@ -1,15 +1,18 @@
 import { computed, ref } from "vue"
+import productionFavicon from "@/assets/favicon-production.svg"
+import stagingFavicon from "@/assets/favicon-staging.svg"
+import uatFavicon from "@/assets/favicon-uat.svg"
 
 export function useFavicon(env: string | undefined) {
   const favicon = ref()
 
   return computed(() => {
     if (env == "staging") {
-      favicon.value = "/src/assets/favicon-staging.svg"
+      favicon.value = stagingFavicon
     } else if (env == "uat") {
-      favicon.value = "/src/assets/favicon-uat.svg"
+      favicon.value = uatFavicon
     } else {
-      favicon.value = "/src/assets/favicon-production.svg"
+      favicon.value = productionFavicon
     }
 
     return favicon.value
