f752c528430783788567f8c174e507fd8553d406
Anne <158598611+anne-ds@users.noreply.github.com>
2024-06-25 09:13:10 +0200

    Add publication status to small search (#1702)
    * Add publication status to small search
    
    RISDEV-3593
    
    * Fix test
    
    RISDEV-3593
    
    * Add e2e test
    
    RISDEV-3593

--- backend/src/test/java/de/bund/digitalservice/ris/caselaw/integration/tests/JurisXmlExporterWrapperIntegrationTest.java
+++ backend/src/test/java/de/bund/digitalservice/ris/caselaw/integration/tests/JurisXmlExporterWrapperIntegrationTest.java
@@ -157,6 +157,7 @@ class JurisXmlExporterWrapperIntegrationTest {
             PreviousDecision.builder()
                 .uuid(UUID.randomUUID())
                 .documentNumber("documentNumber")
+                .status(Status.builder().build())
                 .uuid(UUID.randomUUID())
                 .court(new Court(UUID.randomUUID(), "courtType", "courtPlace", "courtLabel", null))
                 .decisionDate(LocalDate.parse("2020-04-05"))
