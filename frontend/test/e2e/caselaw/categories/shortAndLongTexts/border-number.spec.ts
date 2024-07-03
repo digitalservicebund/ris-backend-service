1c9608ceaea74c4a9fc3d06e59029b620b197dd8
anne-ds <anne.schumann@digitalservice.bund.de>
2024-07-03 14:09:10 +0200

    Add e2e test annotations
    RISDEV-4119

--- frontend/test/e2e/caselaw/categories/shortAndLongTexts/border-number.spec.ts
+++ frontend/test/e2e/caselaw/categories/shortAndLongTexts/border-number.spec.ts
@@ -18,146 +18,188 @@ test.beforeEach(async ({ page, documentNumber }) => {
 
 /*
     1. Upload document with border numbers
-    2. Copy border numbers into reasons
-    3. Delete all border numbers with button
-    4. Delete only selected border numbers via button
+    2. Copy border Numbers (Randnummern) into reasons
+    3. Delete all border Numbers (Randnummern) with button
+    4. Delete only selected border Numbers (Randnummern) via button
     5. Delete border number via backspace in content
     6. delete border number via backspace in number
 */
 // eslint-disable-next-line playwright/no-skipped-test
-test("delete border numbers via button and backspace", async ({
-  page,
-  documentNumber,
-}) => {
-  // Copy border numbers from side panel into reasons to have reference data
-  const documentOrigin = "Gründe:"
-  const firstReason = "First reason"
-  const secondReason = "Second reason"
-  const thirdReason = "Third reason"
+test.describe(
+  "Remove border numbers (Randnummern)",
+  {
+    annotation: {
+      type: "story",
+      description:
+        "https://digitalservicebund.atlassian.net/browse/RISDEV-4119",
+    },
+  },
+  () => {
+    test("delete border Numbers (Randnummern) via button and backspace", async ({
+      page,
+      documentNumber,
+    }) => {
+      const documentOrigin = "Gründe:"
+      const firstReason = "First reason"
+      const secondReason = "Second reason"
+      const thirdReason = "Third reason"
+      // eslint-disable-next-line playwright/no-conditional-in-test
+      const modifier = (await page.evaluate(() => navigator.platform))
+        .toLowerCase()
+        .includes("mac")
+        ? "Meta"
+        : "Control"
 
-  // upload file
-  await uploadTestfile(page, "some-border-numbers.docx")
-  await expect(page.getByText("some-border-numbers.docx")).toBeVisible()
-  await expect(page.getByLabel("Datei löschen")).toBeVisible()
-  await expect(page.getByText(firstReason)).toBeVisible()
-  await expect(page.getByText(secondReason)).toBeVisible()
-  await expect(page.getByText(thirdReason)).toBeVisible()
+      await test.step("Upload file with border Numbers (Randnummern) (Randnummern)", async () => {
+        await uploadTestfile(page, "some-border-numbers.docx")
+        await expect(page.getByText("some-border-numbers.docx")).toBeVisible()
+        await expect(page.getByLabel("Datei löschen")).toBeVisible()
+        await expect(page.getByText(firstReason)).toBeVisible()
+        await expect(page.getByText(secondReason)).toBeVisible()
+        await expect(page.getByText(thirdReason)).toBeVisible()
+      })
 
-  // Click on "Rubriken" und check if original document loaded
-  await navigateToCategories(page, documentNumber)
-  await expect(page.getByLabel("Ladestatus")).toBeHidden()
-  await expect(page.getByText(firstReason)).toBeVisible()
-  await expect(page.getByText(secondReason)).toBeVisible()
-  await expect(page.getByText(thirdReason)).toBeVisible()
-  const originalFileParagraph = page.getByText(documentOrigin)
-  await expect(originalFileParagraph).toBeVisible()
+      await test.step("Click on 'Rubriken' und check if original document loaded", async () => {
+        await navigateToCategories(page, documentNumber)
+        await expect(page.getByLabel("Ladestatus")).toBeHidden()
+        await expect(page.getByText(firstReason)).toBeVisible()
+        await expect(page.getByText(secondReason)).toBeVisible()
+        await expect(page.getByText(thirdReason)).toBeVisible()
+        await expect(page.getByText(documentOrigin)).toBeVisible()
+      })
 
-  // Selected all text from sidepanel
-  await originalFileParagraph.evaluate((element) => {
-    const originalFile = element.parentElement?.parentElement?.parentElement
+      const editor = page.locator("[data-testid='Gründe']")
+      let inputFieldInnerHTML = await editor.innerText()
 
-    if (!originalFile) {
-      throw new Error("No original file available.")
-    }
+      await test.step("Copy border numbers (Randnummern) from side panel into reasons to have reference data", async () => {
+        // Selected all text from sidepanel
+        await page.getByText(documentOrigin).evaluate((element) => {
+          const originalFile =
+            element.parentElement?.parentElement?.parentElement
 
-    const selection = window.getSelection()
-    const elementChildsLength = originalFile.childNodes.length
-    const range = document.createRange()
-    range.setStart(originalFile.childNodes[0], 0)
-    range.setEnd(originalFile.childNodes[elementChildsLength - 1], 0)
-    selection?.removeAllRanges()
-    selection?.addRange(range)
-  })
+          if (!originalFile) {
+            throw new Error("No original file available.")
+          }
 
-  // copy from sidepanel to clipboard
-  // eslint-disable-next-line playwright/no-conditional-in-test
-  const modifier = (await page.evaluate(() => navigator.platform))
-    .toLowerCase()
-    .includes("mac")
-    ? "Meta"
-    : "Control"
-  await page.keyboard.press(`${modifier}+KeyC`)
+          const selection = window.getSelection()
+          const elementChildsLength = originalFile.childNodes.length
+          const range = document.createRange()
+          range.setStart(originalFile.childNodes[0], 0)
+          range.setEnd(originalFile.childNodes[elementChildsLength - 1], 0)
+          selection?.removeAllRanges()
+          selection?.addRange(range)
+        })
 
-  // paste from clipboard into input field "Entscheidungsgründe"
-  const editor = page.locator("[data-testid='Gründe']")
-  await editor.click()
-  await page.keyboard.press(`${modifier}+KeyV`)
-  let inputFieldInnerHTML = await editor.innerText()
+        // copy from sidepanel to clipboard
+        await page.keyboard.press(`${modifier}+KeyC`)
 
-  // Check all text copied
-  expect(inputFieldInnerHTML.includes("1\n\n" + firstReason)).toBeTruthy()
-  expect(inputFieldInnerHTML.includes("2\n\n" + secondReason)).toBeTruthy()
-  expect(inputFieldInnerHTML.includes("3\n\n" + thirdReason)).toBeTruthy()
+        // paste from clipboard into input field "Entscheidungsgründe"
+        await editor.click()
+        await page.keyboard.press(`${modifier}+KeyV`)
+      })
 
-  // Select all and remove border numbers via button
-  await page.keyboard.press(`${modifier}+KeyA`)
-  await page.getByLabel("borderNumber").click()
+      await checkAllBorderNumbersAreVisible()
 
-  // Check border numbers have gone
-  inputFieldInnerHTML = await editor.innerText()
-  expect(inputFieldInnerHTML.includes("1\n\n" + firstReason)).toBeFalsy()
-  expect(inputFieldInnerHTML.includes("2\n\n" + secondReason)).toBeFalsy()
-  expect(inputFieldInnerHTML.includes("3\n\n" + thirdReason)).toBeFalsy()
+      await test.step("Select all text", async () => {
+        await page.keyboard.press(`${modifier}+KeyA`)
+      })
 
-  // Revert
-  await page.keyboard.press(`${modifier}+KeyV`)
+      await clickBorderNumberButton()
 
-  inputFieldInnerHTML = await editor.innerText()
-  expect(inputFieldInnerHTML.includes("1\n\n" + firstReason)).toBeTruthy()
-  expect(inputFieldInnerHTML.includes("2\n\n" + secondReason)).toBeTruthy()
-  expect(inputFieldInnerHTML.includes("3\n\n" + thirdReason)).toBeTruthy()
+      await test.step("Check all border Numbers (Randnummern) have gone", async () => {
+        inputFieldInnerHTML = await editor.innerText()
+        expect(inputFieldInnerHTML.includes("1\n\n" + firstReason)).toBeFalsy()
+        expect(inputFieldInnerHTML.includes("2\n\n" + secondReason)).toBeFalsy()
+        expect(inputFieldInnerHTML.includes("3\n\n" + thirdReason)).toBeFalsy()
+      })
 
-  // Select third reason
-  await page.keyboard.down("Shift")
-  for (let i = 0; i < 10; i++) {
-    await page.keyboard.press("ArrowLeft")
-  }
-  await page.keyboard.up("Shift")
+      await reinsertAllBorderNumbers()
 
-  // delete border number via button
-  await page.getByLabel("borderNumber").click()
-  inputFieldInnerHTML = await editor.innerText()
-  expect(inputFieldInnerHTML.includes("1\n\n" + firstReason)).toBeTruthy()
-  expect(inputFieldInnerHTML.includes("2\n\n" + secondReason)).toBeTruthy()
-  expect(inputFieldInnerHTML.includes("3\n\n" + thirdReason)).toBeFalsy()
+      await test.step("Select text of last border number (Randnummer)", async () => {
+        await page.keyboard.down("Shift")
+        for (let i = 0; i < 10; i++) {
+          await page.keyboard.press("ArrowLeft")
+        }
+        await page.keyboard.up("Shift")
+      })
 
-  // Revert
-  await page.keyboard.press(`${modifier}+KeyA`)
-  await page.keyboard.press(`${modifier}+KeyV`)
-  inputFieldInnerHTML = await editor.innerText()
-  expect(inputFieldInnerHTML.includes("1\n\n" + firstReason)).toBeTruthy()
-  expect(inputFieldInnerHTML.includes("2\n\n" + secondReason)).toBeTruthy()
-  expect(inputFieldInnerHTML.includes("3\n\n" + thirdReason)).toBeTruthy()
+      await clickBorderNumberButton()
 
-  // Navigate to the beginning of third reason content
-  for (let i = 0; i < 13; i++) {
-    await page.keyboard.press("ArrowLeft")
-  }
+      await checkLastBorderNumberHasGone()
 
-  // Delete border number with backspace
-  await page.keyboard.press("Backspace")
-  inputFieldInnerHTML = await editor.innerText()
-  expect(inputFieldInnerHTML.includes("1\n\n" + firstReason)).toBeTruthy()
-  expect(inputFieldInnerHTML.includes("2\n\n" + secondReason)).toBeTruthy()
-  expect(inputFieldInnerHTML.includes("3\n\n" + thirdReason)).toBeFalsy()
+      await reinsertAllBorderNumbers()
 
-  // Revert
-  await page.keyboard.press(`${modifier}+KeyA`)
-  await page.keyboard.press(`${modifier}+KeyV`)
-  inputFieldInnerHTML = await editor.innerText()
-  expect(inputFieldInnerHTML.includes("1\n\n" + firstReason)).toBeTruthy()
-  expect(inputFieldInnerHTML.includes("2\n\n" + secondReason)).toBeTruthy()
-  expect(inputFieldInnerHTML.includes("3\n\n" + thirdReason)).toBeTruthy()
+      await test.step("Navigate cursor to the start of the last border number content", async () => {
+        for (let i = 0; i < 13; i++) {
+          await page.keyboard.press("ArrowLeft")
+        }
+      })
 
-  // Navigate into the third reason border number
-  for (let i = 0; i < 14; i++) {
-    await page.keyboard.press("ArrowLeft")
-  }
+      await clickBackspace()
 
-  // Delete border number with backspace
-  await page.keyboard.press("Backspace")
-  inputFieldInnerHTML = await editor.innerText()
-  expect(inputFieldInnerHTML.includes("1\n\n" + firstReason)).toBeTruthy()
-  expect(inputFieldInnerHTML.includes("2\n\n" + secondReason)).toBeTruthy()
-  expect(inputFieldInnerHTML.includes("3\n\n" + thirdReason)).toBeFalsy()
-})
+      await checkLastBorderNumberHasGone()
+
+      await reinsertAllBorderNumbers()
+
+      await test.step("Navigate cursor to the last border number number", async () => {
+        for (let i = 0; i < 14; i++) {
+          await page.keyboard.press("ArrowLeft")
+        }
+      })
+
+      await clickBackspace()
+
+      await checkLastBorderNumberHasGone()
+
+      async function clickBackspace() {
+        await test.step("Press Backspace to delete last border number", async () => {
+          await page.keyboard.press("Backspace")
+        })
+      }
+
+      async function clickBorderNumberButton() {
+        await test.step("Click border number button to delete border numbers from selection", async () => {
+          await page.getByLabel("borderNumber").click()
+        })
+      }
+
+      async function reinsertAllBorderNumbers() {
+        await test.step("Reinsert all border numbers (Randnummern)", async () => {
+          await page.keyboard.press(`${modifier}+KeyA`)
+          await page.keyboard.press(`${modifier}+KeyV`)
+          await checkAllBorderNumbersAreVisible()
+        })
+      }
+
+      async function checkAllBorderNumbersAreVisible() {
+        await test.step("Check all border numbers (Randnummern) are visible", async () => {
+          inputFieldInnerHTML = await editor.innerText()
+          expect(
+            inputFieldInnerHTML.includes("1\n\n" + firstReason),
+          ).toBeTruthy()
+          expect(
+            inputFieldInnerHTML.includes("2\n\n" + secondReason),
+          ).toBeTruthy()
+          expect(
+            inputFieldInnerHTML.includes("3\n\n" + thirdReason),
+          ).toBeTruthy()
+        })
+      }
+
+      async function checkLastBorderNumberHasGone() {
+        await test.step("Check the last border Number (Randnummer) has gone", async () => {
+          inputFieldInnerHTML = await editor.innerText()
+          expect(
+            inputFieldInnerHTML.includes("1\n\n" + firstReason),
+          ).toBeTruthy()
+          expect(
+            inputFieldInnerHTML.includes("2\n\n" + secondReason),
+          ).toBeTruthy()
+          expect(
+            inputFieldInnerHTML.includes("3\n\n" + thirdReason),
+          ).toBeFalsy()
+        })
+      }
+    })
+  },
+)
