e1473a7d6f863b7a6341910116e1856cae1d2ae5
Nadav <nadav.babai@digitalservice.bund.de>
2024-07-08 15:52:42 +0200

    Fix document unit info panel tests
    RISDEV-4328

--- frontend/src/components/DocumentUnitInfoPanel.vue
+++ frontend/src/components/DocumentUnitInfoPanel.vue
@@ -4,12 +4,11 @@ import { computed, ref, toRaw, watchEffect } from "vue"
 import IconBadge from "@/components/IconBadge.vue"
 import SaveButton from "@/components/SaveDocumentUnitButton.vue"
 import { useStatusBadge } from "@/composables/useStatusBadge"
-import DocumentUnit from "@/domain/documentUnit"
 import { ServiceResponse } from "@/services/httpClient"
+import { useDocumentUnitStore } from "@/stores/documentUnitStore"
 import IconError from "~icons/ic/baseline-error"
 
 interface Props {
-  documentUnit: DocumentUnit
   heading?: string
   saveCallback?: () => Promise<ServiceResponse<void>>
 }
@@ -19,18 +18,22 @@ const props = withDefaults(defineProps<Props>(), {
   saveCallback: undefined,
 })
 
+const documentUnitStore = useDocumentUnitStore()
+
 const fileNumberInfo = computed(
-  () => props.documentUnit?.coreData.fileNumbers?.[0] || "",
+  () => documentUnitStore.documentUnit?.coreData.fileNumbers?.[0] || "",
 )
 
 const decisionDateInfo = computed(() =>
-  props.documentUnit?.coreData.decisionDate
-    ? dayjs(props.documentUnit.coreData.decisionDate).format("DD.MM.YYYY")
+  documentUnitStore.documentUnit?.coreData.decisionDate
+    ? dayjs(documentUnitStore.documentUnit.coreData.decisionDate).format(
+        "DD.MM.YYYY",
+      )
     : "",
 )
 
 const courtInfo = computed(
-  () => props.documentUnit?.coreData.court?.label || "",
+  () => documentUnitStore.documentUnit?.coreData.court?.label || "",
 )
 
 const formattedInfo = computed(() => {
@@ -42,10 +45,14 @@ const formattedInfo = computed(() => {
   return parts.join(", ")
 })
 
-const statusBadge = ref(useStatusBadge(props.documentUnit?.status).value)
+const statusBadge = ref(
+  useStatusBadge(documentUnitStore.documentUnit?.status).value,
+)
 
 watchEffect(() => {
-  statusBadge.value = useStatusBadge(props.documentUnit?.status).value
+  statusBadge.value = useStatusBadge(
+    documentUnitStore.documentUnit?.status,
+  ).value
 })
 </script>
 
@@ -53,7 +60,7 @@ watchEffect(() => {
   <div
     class="sticky top-0 z-30 flex h-[64px] flex-row items-center border-b border-solid border-gray-400 bg-blue-100 px-24 py-12"
   >
-    <h1 class="text font-bold">{{ heading }}</h1>
+    <h1 class="text font-bold">{{ props.heading }}</h1>
     <span v-if="formattedInfo.length > 0" class="m-4"> | </span>
     <span
       class="overflow-hidden text-ellipsis whitespace-nowrap"
@@ -69,7 +76,7 @@ watchEffect(() => {
       :label="statusBadge.label"
     />
     <IconBadge
-      v-if="props.documentUnit?.status?.withError"
+      v-if="documentUnitStore.documentUnit?.status?.withError"
       background-color="bg-red-300"
       class="ml-12"
       color="text-red-900"
