94e2f00ac66bc4294502214b593ab88533841e15
Kim Kern <kim.kern@digitalservice.bund.de>
2024-07-10 11:47:24 +0200

    Use mouse.wheel in sidepanel e2e test
    RISDEV-1789

--- frontend/test/e2e/caselaw/extra-content-panel.spec.ts
+++ frontend/test/e2e/caselaw/extra-content-panel.spec.ts
@@ -188,23 +188,25 @@ es zu unterlassen, den Kläger für das Einstellen des unter Ziffer 1 genannten 
 
           // TODO: Re-enable when pipeline error is resolved. (Scrolling works only locally.)
 
-          // const scrollTopBeforeScrolling = await page
-          //   .locator("#notesInput")
-          //   .evaluate((el) => el.scrollTop)
-          // await page.locator("#notesInput").focus()
-          // await page.keyboard.press("Home")
-          //
-          // await page.waitForFunction(
-          //   () => document.getElementById("notesInput")?.scrollTop === 0,
-          //   undefined,
-          //   { timeout: 1_000 },
-          // )
-          //
-          // const scrollTopAfterScrolling = await page
-          //   .locator("#notesInput")
-          //   .evaluate((el) => el.scrollTop)
-          // expect(scrollTopBeforeScrolling).toBeGreaterThan(0)
-          // expect(scrollTopAfterScrolling).toBe(0)
+          const scrollTopBeforeScrolling = await page
+            .locator("#notesInput")
+            .evaluate((el) => el.scrollTop)
+          await page.locator("#notesInput").focus()
+          await page.locator("#notesInput").click()
+          await page.keyboard.press("Home")
+          await page.mouse.wheel(0, -2000)
+
+          await page.waitForFunction(
+            () => document.getElementById("notesInput")?.scrollTop === 0,
+            undefined,
+            { timeout: 1_000 },
+          )
+
+          const scrollTopAfterScrolling = await page
+            .locator("#notesInput")
+            .evaluate((el) => el.scrollTop)
+          expect(scrollTopBeforeScrolling).toBeGreaterThan(0)
+          expect(scrollTopAfterScrolling).toBe(0)
 
           await page.reload()
           await expect(page.getByLabel("Notiz Eingabefeld")).toHaveValue(
