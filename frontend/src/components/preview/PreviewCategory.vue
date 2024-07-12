4dcca27c1e14905c8f6e212649c3e12e0c547f7c
Kim Kern <kim.kern@digitalservice.bund.de>
2024-07-12 12:02:15 +0200

    Converge label+content in sidepanel preview
    RISDEV-4254

--- frontend/src/components/preview/PreviewCategory.vue
+++ frontend/src/components/preview/PreviewCategory.vue
@@ -1,8 +1,18 @@
 <script setup lang="ts">
+import { inject } from "vue"
 import FlexItem from "@/components/FlexItem.vue"
+import { previewLayoutInjectionKey } from "@/components/preview/constants"
+
+const layout = inject(previewLayoutInjectionKey) || "wide"
 </script>
 <template>
-  <FlexItem class="ds-body-01-reg w-min min-w-[213px] px-16 py-4 text-gray-900">
+  <FlexItem
+    class="ds-body-01-reg min-w-[213px] px-16 text-gray-900"
+    :class="{
+      'w-min py-4': layout === 'wide',
+      'mb-[-2px] pt-4': layout === 'narrow',
+    }"
+  >
     <slot></slot>
   </FlexItem>
 </template>
