d749268f604716720942ed65776a5cf7bcfcf8c5
Leonie Koch <leonie.koch@digitalservice.bund.de>
2024-07-15 14:17:08 +0200

    Fix DocumentUnitInfoPanel unit test
    RISDEV-4316

--- frontend/test/components/documentUnitInfoPanel.spec.ts
+++ frontend/test/components/documentUnitInfoPanel.spec.ts
@@ -1,53 +1,54 @@
+import { createTestingPinia } from "@pinia/testing"
 import { render, screen } from "@testing-library/vue"
 import DocumentUnitInfoPanel from "@/components/DocumentUnitInfoPanel.vue"
-import DocumentUnit from "@/domain/documentUnit"
+import DocumentUnit, { CoreData } from "@/domain/documentUnit"
+
+function renderComponent(options?: { heading?: string; coreData?: CoreData }) {
+  return {
+    ...render(DocumentUnitInfoPanel, {
+      props: { heading: options?.heading ?? "" },
+      global: {
+        plugins: [
+          createTestingPinia({
+            initialState: {
+              docunitStore: {
+                documentUnit: new DocumentUnit("foo", {
+                  documentNumber: "1234567891234",
+                  coreData: options?.coreData ?? {
+                    court: {
+                      type: "AG",
+                      location: "Test",
+                      label: "AG Test",
+                    },
+                  },
+                }),
+              },
+            },
+          }),
+        ],
+      },
+    }),
+  }
+}
 
 describe("documentUnit InfoPanel", () => {
   it("renders heading if given", async () => {
-    render(DocumentUnitInfoPanel, {
-      props: {
-        heading: "test heading",
-        documentUnit: new DocumentUnit("foo", {
-          documentNumber: "1234567891234",
-          coreData: {
-            court: {
-              type: "AG",
-              location: "Test",
-              label: "AG Test",
-            },
-          },
-          texts: {},
-          previousDecisions: undefined,
-          ensuingDecisions: undefined,
-          contentRelatedIndexing: {},
-        }),
-      },
-    })
+    renderComponent({ heading: "test heading" })
 
     screen.getAllByText("test heading")
   })
 
   it("renders all given property infos in correct order", async () => {
-    render(DocumentUnitInfoPanel, {
-      props: {
-        documentUnit: new DocumentUnit("foo", {
-          documentNumber: "1234567891234",
-          coreData: {
-            decisionDate: "2024-01-01",
-            fileNumbers: ["AZ123"],
-            court: {
-              type: "AG",
-              location: "Test",
-              label: "AG Test",
-            },
-          },
-          texts: {},
-          previousDecisions: undefined,
-          ensuingDecisions: undefined,
-          contentRelatedIndexing: {},
-        }),
+    const coreData = {
+      decisionDate: "2024-01-01",
+      fileNumbers: ["AZ123"],
+      court: {
+        type: "AG",
+        location: "Test",
+        label: "AG Test",
       },
-    })
+    }
+    renderComponent({ coreData: coreData })
 
     expect(
       await screen.findByText("AG Test, AZ123, 01.01.2024"),
@@ -55,24 +56,7 @@ describe("documentUnit InfoPanel", () => {
   })
 
   it("omits incomplete coredata fields from rendering", async () => {
-    render(DocumentUnitInfoPanel, {
-      props: {
-        documentUnit: new DocumentUnit("foo", {
-          documentNumber: "1234567891234",
-          coreData: {
-            court: {
-              type: "AG",
-              location: "Test",
-              label: "AG Test",
-            },
-          },
-          texts: {},
-          previousDecisions: undefined,
-          ensuingDecisions: undefined,
-          contentRelatedIndexing: {},
-        }),
-      },
-    })
+    renderComponent()
 
     expect(await screen.findByText("AG Test")).toBeInTheDocument()
   })
