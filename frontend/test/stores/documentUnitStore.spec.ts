13a6b4643623ab3523958cd0dff58779f4a89a44
Leonie Koch <leonie.koch@digitalservice.bund.de>
2024-07-25 14:42:25 +0200

    Add unit test for user feedback
    RISDEV-4318

--- frontend/test/stores/documentUnitStore.spec.ts
+++ frontend/test/stores/documentUnitStore.spec.ts
@@ -103,6 +103,59 @@ describe("useDocumentUnitStore", () => {
       expect(store.documentUnit?.version).toBe(1)
     })
 
+    it("updates a document unit with newer version of updated path", async () => {
+      const mockedLoadResponse: ServiceResponse<DocumentUnit> = {
+        status: 200,
+        data: new DocumentUnit("123", { version: 0 }),
+        error: undefined,
+      }
+
+      const mockedUpdateResponse: ServiceResponse<RisJsonPatch> = {
+        status: 200,
+        data: {
+          documentationUnitVersion: 1,
+          patch: [{ op: "add", path: "/coreData/ecli", value: "123" }],
+          errorPaths: ["/coreData/ecli"],
+        },
+        error: undefined,
+      }
+
+      const documentUnitServiceLoadMock = vi
+        .spyOn(documentUnitService, "getByDocumentNumber")
+        .mockResolvedValueOnce(mockedLoadResponse)
+
+      const documentUnitServiceUpdateMock = vi
+        .spyOn(documentUnitService, "update")
+        .mockResolvedValueOnce(mockedUpdateResponse)
+
+      const store = useDocumentUnitStore()
+
+      //load docunit to fill "originalDocumentUnit"
+      await store.loadDocumentUnit("123")
+      expect(documentUnitServiceLoadMock).toHaveBeenCalledOnce()
+
+      store.documentUnit = new DocumentUnit("123", {
+        version: 0,
+        coreData: {
+          ecli: "321",
+        },
+      })
+
+      const response = await store.updateDocumentUnit()
+
+      expect(documentUnitServiceUpdateMock).toHaveBeenCalledOnce()
+      expect(response).toEqual({
+        status: 207,
+        data: undefined,
+        error: {
+          title: "ECLI",
+        },
+      })
+      expect(store.documentUnit?.version).toBe(1)
+      // Since last saving, someone else edited this input and this newer version overrides the local docunit
+      expect(store.documentUnit?.coreData?.ecli).toBe("123")
+    })
+
     it("handles no changes in document unit", async () => {
       const mockedLoadResponse: ServiceResponse<DocumentUnit> = {
         status: 200,
