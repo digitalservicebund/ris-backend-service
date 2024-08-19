9e75094cdb0878d7484575076e2c57842c447250
Leonie Koch <leonie.koch@digitalservice.bund.de>
2024-08-19 17:08:23 +0200

    Add search inputs to evaluation
    RISDEV-4560

--- frontend/src/components/legalperiodical/EditionEvaluation.vue
+++ frontend/src/components/legalperiodical/EditionEvaluation.vue
@@ -2,20 +2,42 @@
 import { UUID } from "crypto"
 import { onMounted, ref, computed } from "vue"
 import { useRoute } from "vue-router"
+import { ValidationError } from "../input/types"
+import { Page } from "../Pagination.vue"
 import ComboboxInput from "@/components/ComboboxInput.vue"
+import DateInput from "@/components/input/DateInput.vue"
 import InputField from "@/components/input/InputField.vue"
+import TextButton from "@/components/input/TextButton.vue"
 import TextInput from "@/components/input/TextInput.vue"
 import ErrorPage from "@/components/PageError.vue"
+import { useValidationStore } from "@/composables/useValidationStore"
+import { Court, DocumentType } from "@/domain/documentUnit"
 import LegalPeriodical from "@/domain/legalPeriodical"
 import LegalPeriodicalEdition from "@/domain/legalPeriodicalEdition"
 import Reference from "@/domain/reference"
+import RelatedDocumentation from "@/domain/relatedDocumentation"
 import ComboboxItemService from "@/services/comboboxItemService"
+import documentUnitService from "@/services/documentUnitService"
 import { ResponseError } from "@/services/httpClient"
 import LegalPeriodicalEditionService from "@/services/legalPeriodicalEditionService"
 
+type DocumentUnitSearchParameter = {
+  court?: Court
+  decisionDate?: string
+  fileNumber?: string
+  documentType?: DocumentType
+}
+
+const fields = ["legalPeriodical", "citation", "referenceSupplement"]
 const route = useRoute()
 const edition = ref<LegalPeriodicalEdition>(new LegalPeriodicalEdition())
 const reference = ref<Reference>(new Reference())
+const documentationUnitSearchInput = ref<DocumentUnitSearchParameter>({})
+const searchValidationStore = useValidationStore<(typeof fields)[number]>()
+const pageNumber = ref<number>(0)
+const itemsPerPage = ref<number>(15)
+const isLoading = ref(false)
+const searchResultsCurrentPage = ref<Page<RelatedDocumentation>>()
 
 const responseError = ref<ResponseError>()
 
@@ -42,6 +64,36 @@ const legalPeriodical = computed({
   },
 })
 
+function updateDateFormatValidation(
+  validationError: ValidationError | undefined,
+) {
+  if (validationError)
+    searchValidationStore.add(validationError.message, "decisionDate")
+  else searchValidationStore.remove("decisionDate")
+}
+
+async function search() {
+  isLoading.value = true
+  const documentationUnitRef = new RelatedDocumentation({
+    ...documentationUnitSearchInput.value,
+  })
+
+  const response = await documentUnitService.searchByRelatedDocumentation(
+    pageNumber.value,
+    itemsPerPage.value,
+    documentationUnitRef,
+  )
+  if (response.data) {
+    searchResultsCurrentPage.value = {
+      ...response.data,
+      content: response.data.content.map(
+        (decision) => new RelatedDocumentation({ ...decision }),
+      ),
+    }
+  }
+  isLoading.value = false
+}
+
 onMounted(async () => {
   const uuid = route.params.uuid
   if (uuid) {
@@ -59,7 +111,7 @@ onMounted(async () => {
 <template>
   <div
     v-if="!responseError"
-    class="flex h-full flex-col space-y-24 bg-gray-100 px-16 py-16"
+    class="flex h-full flex-col space-y-24 px-16 py-16"
   >
     <h2 class="ds-heading-03-reg">{{ title }}</h2>
 
@@ -120,6 +172,98 @@ onMounted(async () => {
           </InputField>
         </div>
       </div>
+    </div>
+    <div class="flex flex-col gap-24">
+      <div class="flex justify-between gap-24">
+        <InputField
+          id="courtInput"
+          v-slot="slotProps"
+          label="Gericht *"
+          :validation-error="searchValidationStore.getByField('court')"
+        >
+          <ComboboxInput
+            id="courtInput"
+            v-model="documentationUnitSearchInput.court"
+            aria-label="Gericht Aktivzitierung"
+            clear-on-choosing-item
+            :has-error="slotProps.hasError"
+            :item-service="ComboboxItemService.getCourts"
+            @focus="searchValidationStore.remove('court')"
+          >
+          </ComboboxInput>
+        </InputField>
+        <InputField
+          id="decisionDate"
+          v-slot="slotProps"
+          label="Entscheidungsdatum *"
+          :validation-error="searchValidationStore.getByField('decisionDate')"
+          @update:validation-error="
+            (validationError) => updateDateFormatValidation(validationError)
+          "
+        >
+          <DateInput
+            id="decisionDate"
+            v-model="documentationUnitSearchInput.decisionDate"
+            aria-label="Entscheidungsdatum"
+            class="ds-input-medium"
+            :has-error="slotProps.hasError"
+            @focus="searchValidationStore.remove('decisionDate')"
+            @update:validation-error="slotProps.updateValidationError"
+          ></DateInput>
+        </InputField>
+      </div>
+      <div class="flex justify-between gap-24">
+        <InputField
+          id="activeCitationFileNumber"
+          v-slot="slotProps"
+          label="Aktenzeichen *"
+          :validation-error="searchValidationStore.getByField('fileNumber')"
+        >
+          <TextInput
+            id="activeCitationDocumentType"
+            v-model="documentationUnitSearchInput.fileNumber"
+            aria-label="Aktenzeichen Aktivzitierung"
+            :has-error="slotProps.hasError"
+            size="medium"
+            @focus="searchValidationStore.remove('fileNumber')"
+          ></TextInput>
+        </InputField>
+        <InputField id="activeCitationDecisionDocumentType" label="Dokumenttyp">
+          <ComboboxInput
+            id="activeCitationDecisionDocumentType"
+            v-model="documentationUnitSearchInput.documentType"
+            aria-label="Dokumenttyp Aktivzitierung"
+            :item-service="ComboboxItemService.getDocumentTypes"
+          ></ComboboxInput>
+        </InputField>
+      </div>
+    </div>
+    <div class="flex w-full flex-row justify-between">
+      <div>
+        <div class="flex gap-16">
+          <TextButton
+            aria-label="Nach Entscheidung suchen"
+            button-type="primary"
+            label="Suchen"
+            size="small"
+            @click="search"
+          />
+          <!-- <TextButton
+            aria-label="Abbrechen"
+            button-type="ghost"
+            label="Abbrechen"
+            size="small"
+            @click.stop="emit('cancelEdit')"
+          /> -->
+        </div>
+      </div>
+      <!-- <TextButton
+        aria-label="Eintrag löschen"
+        button-type="destructive"
+        label="Eintrag löschen"
+        size="small"
+        @click.stop="emit('removeEntry', true)"
+      /> -->
     </div>
   </div>
   <ErrorPage
