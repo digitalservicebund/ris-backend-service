4ff6e3620eac4ec8c1b852bd568ec1b90cb031ec
Nadine Kost <nadine.kost@digitalservice.bund.de>
2024-08-22 17:36:44 +0200

    Filter user groups
    RISDEV-4518

--- backend/src/main/java/de/bund/digitalservice/ris/caselaw/adapter/KeycloakUserService.java
+++ backend/src/main/java/de/bund/digitalservice/ris/caselaw/adapter/KeycloakUserService.java
@@ -33,10 +33,11 @@ public class KeycloakUserService implements UserService {
     return getUser(oidcUser).documentationOffice();
   }
 
-  // Todo: Try to avoid duplication in data processing (see getDocuemntationOffice
   public List<DocumentationOfficeUserGroup> getUserGroups(OidcUser oidcUser) {
     return documentationOfficeUserGroupService.getUserGroups().stream()
-        .filter(group -> group.docOffice().equals(getDocumentationOffice(oidcUser)))
+        .filter(
+            group ->
+                group.docOffice().equals(getDocumentationOffice(oidcUser)) && !group.isInternal())
         .toList();
   }
 
