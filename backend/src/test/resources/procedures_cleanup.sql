a6bffb0c9ac305e7ed80e8e0c65c15abde34c064
anne-ds <anne.schumann@digitalservice.bund.de>
2024-08-26 12:00:48 +0200

    Add (un)assign ProcedureIntegrationTest
    RISDEV-4518

--- backend/src/test/resources/procedures_cleanup.sql
+++ backend/src/test/resources/procedures_cleanup.sql
@@ -2,4 +2,8 @@ delete from
   incremental_migration.procedure;
 
 delete from
-  incremental_migration.documentation_unit
+  incremental_migration.documentation_unit;
+
+delete from
+  incremental_migration.documentation_office_user_group;
+
