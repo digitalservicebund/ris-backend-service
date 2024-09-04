c430be4b708792ad775c6fc11b2c57af3fcc93af
Hanna Prinz <hanna.prinz@digitalservice.bund.de>
2024-09-04 17:06:44 +0200

    Use uuid from frontend
    RISDEV-4749

--- backend/src/main/java/de/bund/digitalservice/ris/caselaw/adapter/database/jpa/LegalPeriodicalEditionDTO.java
+++ backend/src/main/java/de/bund/digitalservice/ris/caselaw/adapter/database/jpa/LegalPeriodicalEditionDTO.java
@@ -4,7 +4,6 @@ import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -31,7 +30,7 @@ import org.springframework.data.annotation.CreatedDate;
 @Getter
 @Setter
 public class LegalPeriodicalEditionDTO {
-  @Id @GeneratedValue private UUID id;
+  @Id private UUID id;
 
   @NotNull
   @ManyToOne(fetch = FetchType.EAGER)
