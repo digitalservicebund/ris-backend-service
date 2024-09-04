b89aafe201469c20ed4a6100a194ab93e47a58af
Kim Kern <kim.kern@digitalservice.bund.de>
2024-09-04 17:23:05 +0200

    Reduce e2e flakiness of tiptap keyboard nav
    RISDEV-0000

--- frontend/test/e2e/caselaw/categories/shortAndLongTexts/short-and-long-texts.spec.ts
+++ frontend/test/e2e/caselaw/categories/shortAndLongTexts/short-and-long-texts.spec.ts
@@ -117,6 +117,10 @@ test.describe("short and long texts", () => {
       await page.keyboard.press("Enter")
       await expect(guidingPrincipleInput).toBeFocused()
 
+      // The timeout is needed for the focus, otherwise tabbing back to the menu is flaky. It's unclear why.
+      // eslint-disable-next-line playwright/no-wait-for-timeout
+      await page.waitForTimeout(100)
+
       // Tiptap inserts invisible characters -> input is split into two parts
       expect(await page.getByText("Text input").innerHTML()).toContain(
         "<strong>Text</strong>",
