4a87529ed35afa41798e47737d0c2cca2c7c71c4
nbabai <nadavbabai@outlook.com>
2024-09-09 11:12:43 +0200

    Fixing link font size in decision summary
    RISDEV-4731

--- frontend/src/components/DocumentNumberLink.vue
+++ frontend/src/components/DocumentNumberLink.vue
@@ -28,7 +28,7 @@ const props = defineProps<Props>()
         }"
       >
         <button
-          class="ds-link-03-bold flex flex-row border-b-2 border-blue-800 leading-24 no-underline focus:outline-none focus-visible:outline-4 focus-visible:outline-offset-4 focus-visible:outline-blue-800"
+          class="ds-link-01-bold flex flex-row border-b-2 border-blue-800 leading-24 no-underline focus:outline-none focus-visible:outline-4 focus-visible:outline-offset-4 focus-visible:outline-blue-800"
         >
           <FlexContainer align-items="items-center" flex-direction="flex-row">
             <p>{{ props.decision.documentNumber }}</p>
