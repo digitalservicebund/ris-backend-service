ecefd7fb3cf882a7638892905ff85ef33440e641
Kim Kern <kim.kern@digitalservice.bund.de>
2024-09-11 15:01:10 +0200

    Temporarily let tests fail for debugging
    RISDEV-0000
    I want to test the metabase flaky test reporting. Will revert immediately afterward.

--- frontend/test/e2e/caselaw/e2e-utils.ts
+++ frontend/test/e2e/caselaw/e2e-utils.ts
@@ -53,6 +53,9 @@ export const navigateToCategories = async (
     skipAssert?: boolean
   },
 ) => {
+  if (Math.random() > 0.5) {
+    throw new Error("This will fail")
+  }
   await test.step("Navigate to 'Rubriken'", async () => {
     const queryParams = getAllQueryParamsFromUrl(page)
     const baseUrl =
