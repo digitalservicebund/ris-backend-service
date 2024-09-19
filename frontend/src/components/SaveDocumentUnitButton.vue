2a31be83116acd922412b777e13a6039173464e7
Kim Kern <kim.kern@digitalservice.bund.de>
2024-09-19 11:08:39 +0200

    Fix autosave bug
    RISDEV-0000
    I accidentally disabled autosaving. Sorry

--- frontend/src/components/SaveDocumentUnitButton.vue
+++ frontend/src/components/SaveDocumentUnitButton.vue
@@ -8,7 +8,7 @@ const props = defineProps<{
 }>()
 
 const { saveIsInProgress, triggerSave, lastSaveError, formattedLastSavedOn } =
-  useSaveToRemote(10000000000)
+  useSaveToRemote(10_000)
 
 const getErrorDetails = () =>
   lastSaveError.value?.title ? ": " + lastSaveError.value.title : ""
