725e09d9336f21cfd89ce478b2dd351a3367e251
Leonie Koch <leonie.koch@digitalservice.bund.de>
2024-10-14 18:19:42 +0200

    Remove old keywords chip component
    RISDEV-0000

--- frontend/src/routes/kitchensink/index/chips.vue
+++ frontend/src/routes/kitchensink/index/chips.vue
@@ -4,13 +4,11 @@ import ChipsDateInput from "@/components/input/ChipsDateInput.vue"
 import ChipsInput from "@/components/input/ChipsInput.vue"
 import ChipsYearInput from "@/components/input/ChipsYearInput.vue"
 import InputField from "@/components/input/InputField.vue"
-import ChipsInputBottom from "@/components/input/KeywordsChipsInput.vue"
 import type { ChipsInputModelType } from "@/components/input/types"
 import KitchensinkPage from "@/kitchensink/components/KitchensinkPage.vue"
 import KitchensinkStory from "@/kitchensink/components/KitchensinkStory.vue"
 
 const inlineChips = ref<ChipsInputModelType>(["one", "two"])
-const stackedChips = ref<ChipsInputModelType>(["one", "two"])
 const dateChips = ref<ChipsInputModelType>(["2023-07-20", "2023-07-21"])
 const yearChips = ref<ChipsInputModelType>(["2022", "2023"])
 </script>
@@ -23,16 +21,6 @@ const yearChips = ref<ChipsInputModelType>(["2022", "2023"])
           :id="id"
           v-model="inlineChips"
           aria-label="Inline chips input"
-        />
-      </InputField>
-    </KitchensinkStory>
-
-    <KitchensinkStory class="w-384" name="Stacked">
-      <InputField id="stackedChipsInput" v-slot="{ id }" label="Stacked chips">
-        <ChipsInputBottom
-          :id="id"
-          v-model="stackedChips"
-          aria-label="Stacked chips input"
         />
       </InputField>
     </KitchensinkStory>
