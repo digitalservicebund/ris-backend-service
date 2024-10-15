febb2516fb24dfb7e4cbcde531e8ad723c158a87
Ralph von Petersdorff-Campen <ralph.vonpetersdorffcampen@digitalservice.bund.de>
2024-10-15 08:13:47 +0200

    Ignore AsyncRequestNotUsableException
    RISDEV-0000

--- backend/src/main/java/de/bund/digitalservice/ris/caselaw/adapter/CaselawExceptionHandler.java
+++ backend/src/main/java/de/bund/digitalservice/ris/caselaw/adapter/CaselawExceptionHandler.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.async.AsyncRequestNotUsableException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
@@ -16,6 +17,12 @@ public class CaselawExceptionHandler extends ResponseEntityExceptionHandler {
     ApiError apiError = new ApiError(HttpStatus.BAD_REQUEST, ex.getMessage());
 
     return new ResponseEntity<>(apiError, new HttpHeaders(), HttpStatus.BAD_REQUEST);
+  }
+
+  @ExceptionHandler(AsyncRequestNotUsableException.class)
+  void handleAsyncRequestNotUsableException(AsyncRequestNotUsableException ex) {
+    /* ignore exception because most of the time this exception occurred when the user
+    close the browser without waiting to the result of an api call. */
   }
 
   private record ApiError(HttpStatus status, String message) {}
