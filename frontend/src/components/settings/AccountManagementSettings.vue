035e9dfb70527be96cf30a6bbc70e444beac4377
Nadine Kost <nadine.kost@digitalservice.bund.de>
2024-10-21 18:55:55 +0200

    Use TextButton properly for account mgmt link
    RISDEV-5003

--- frontend/src/components/settings/AccountManagementSettings.vue
+++ frontend/src/components/settings/AccountManagementSettings.vue
@@ -1,12 +1,10 @@
 <script setup lang="ts">
-import { sanitizeUrl } from "@braintree/sanitize-url"
-import { computed, onMounted, ref } from "vue"
+import { onMounted, ref } from "vue"
 import FlexContainer from "@/components/FlexContainer.vue"
 import TextButton from "@/components/input/TextButton.vue"
 import adminService from "@/services/adminService"
 
 const link = ref<string | undefined>(undefined)
-const sanitizedUrl = computed(() => sanitizeUrl(link.value))
 
 async function fetchUrl() {
   link.value = (await adminService.getAccountManagementUrl()).data
@@ -20,8 +18,12 @@ onMounted(() => {
 <template>
   <FlexContainer v-if="link" flex-direction="flex-col">
     <span class="ds-label-02-bold">Kontoverwaltung</span>
-    <a aria-label="Bare.ID öffnen" :href="sanitizedUrl" target="_blank">
-      <TextButton class="mt-16" label="Bare.ID öffnen"> </TextButton>
-    </a>
+    <TextButton
+      class="mt-16"
+      :href="link"
+      label="Bare.ID öffnen"
+      target="_blank"
+    >
+    </TextButton>
   </FlexContainer>
 </template>
