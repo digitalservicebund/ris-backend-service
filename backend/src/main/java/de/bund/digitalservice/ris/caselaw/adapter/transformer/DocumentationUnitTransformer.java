53b2852535346749d3f3bc10df2f8ad0f81b7b96
Hanna Prinz <hanna.prinz@digitalservice.bund.de>
2024-10-24 17:44:15 +0200

    Fix tests
    RISDEV-5198

--- backend/src/main/java/de/bund/digitalservice/ris/caselaw/adapter/transformer/DocumentationUnitTransformer.java
+++ backend/src/main/java/de/bund/digitalservice/ris/caselaw/adapter/transformer/DocumentationUnitTransformer.java
@@ -68,12 +68,6 @@ public class DocumentationUnitTransformer {
 
     DocumentationUnitDTO.DocumentationUnitDTOBuilder builder =
         currentDto.toBuilder()
-            .documentationOffice(
-                DocumentationOfficeTransformer.transformToDTO(
-                    updatedDomainObject.coreData().documentationOffice()))
-            .creatingDocumentationOffice(
-                DocumentationOfficeTransformer.transformToDTO(
-                    updatedDomainObject.coreData().creatingDocOffice()))
             .id(updatedDomainObject.uuid())
             .documentNumber(updatedDomainObject.documentNumber())
             .note(updatedDomainObject.note())
