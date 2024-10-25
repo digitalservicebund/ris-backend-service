604b5959f660622612fddb70ed48186717c69cb1
Kim Kern <kim.kern@digitalservice.bund.de>
2024-10-25 14:02:14 +0200

    Add role to LoadingSpinner div
    RISDEV-5221
    the a11y test is failing in the pipeline.

--- frontend/src/components/LoadingSpinner.vue
+++ frontend/src/components/LoadingSpinner.vue
@@ -10,5 +10,6 @@ const sizeClasses = props.size === "small" ? "h-24 w-24" : "h-32 w-32"
     aria-label="Ladestatus"
     class="inline-block animate-spin rounded-full border-[3px] border-solid border-blue-900 border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
     :class="sizeClasses"
+    role="status"
   ></div>
 </template>
