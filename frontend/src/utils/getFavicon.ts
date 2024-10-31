e38495a30ba74554493d6857af43ec2ff4a3b66a
anne-ds <anne.schumann@digitalservice.bund.de>
2024-10-31 17:55:06 +0100

    Improve visibility of local env by using rainbow
    RISDEV-0000

--- frontend/src/utils/getFavicon.ts
+++ frontend/src/utils/getFavicon.ts
@@ -1,9 +1,13 @@
+import localFavicon from "@/assets/favicon-local.svg"
 import productionFavicon from "@/assets/favicon-production.svg"
 import stagingFavicon from "@/assets/favicon-staging.svg"
 import uatFavicon from "@/assets/favicon-uat.svg"
 
 export const getFavicon = (env?: string) => {
-  if (env == "staging") {
+  const isLocal = import.meta.env.MODE === "development"
+  if (isLocal) {
+    return localFavicon
+  } else if (env == "staging") {
     return stagingFavicon
   } else if (env == "uat") {
     return uatFavicon
