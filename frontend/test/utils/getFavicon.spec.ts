b98237ee5b3cc58e83c1008e7af359856b25b552
Anne <158598611+anne-ds@users.noreply.github.com>
2024-11-01 09:09:18 +0100

    Improve visibility of local env by using rainbow (#2323)
    * Improve visibility of local env by using rainbow
    
    RISDEV-0000
    
    * Make code more robust and add test
    
    RISDEV-0000
    
    * Fix
    
    RISDEV-0000

--- frontend/test/utils/getFavicon.spec.ts
+++ frontend/test/utils/getFavicon.spec.ts
@@ -4,9 +4,11 @@ describe("getFavicon", () => {
   it.each(["staging", "uat", "production"])(
     "returns correct favicon for %s",
     (environment) => {
+      vi.stubEnv("MODE", "production")
       const favicon = getFavicon(environment)
 
       expect(favicon).toBe(`/src/assets/favicon-${environment}.svg`)
+      vi.unstubAllEnvs()
     },
   )
 
@@ -14,5 +16,14 @@ describe("getFavicon", () => {
     const favicon = getFavicon("foo")
 
     expect(favicon).toBe(`/src/assets/favicon-production.svg`)
+  })
+
+  it("returns correct favicon for local", () => {
+    vi.stubEnv("MODE", "development")
+
+    const favicon = getFavicon("staging")
+
+    expect(favicon).toBe(`/src/assets/favicon-local.svg`)
+    vi.unstubAllEnvs()
   })
 })
