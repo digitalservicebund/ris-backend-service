3c9ff1a5df0a8c3e10f9ef3d5c17ebc573378e01
anne-ds <anne.schumann@digitalservice.bund.de>
2024-11-01 08:35:17 +0100

    Make code more robust and add test
    RISDEV-0000

--- frontend/src/utils/getFavicon.ts
+++ frontend/src/utils/getFavicon.ts
@@ -4,10 +4,11 @@ import stagingFavicon from "@/assets/favicon-staging.svg"
 import uatFavicon from "@/assets/favicon-uat.svg"
 
 export const getFavicon = (env?: string) => {
-  const isLocal = import.meta.env.MODE === "development"
-  if (isLocal) {
-    return localFavicon
-  } else if (env == "staging") {
+  if (env == "staging") {
+    const isLocal = import.meta?.env?.MODE === "development"
+    if (isLocal) {
+      return localFavicon
+    }
     return stagingFavicon
   } else if (env == "uat") {
     return uatFavicon
