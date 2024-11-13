24a9a5049bcb7d46ac3ded1912aab6107fc3ecab
nbabai <nadav.babai@digitalservice.bund.de>
2024-11-13 17:32:18 +0100

    Strengthening recycling service conditions (#2353)
    * Strengthening recycling service conditions
    
    RISDEV-5186
    
    * Removing optional from service to throw actual errors
    * Adding and adjusting tests
    * addForRecycling to void
    * Adding custom exception
    * Empty log in either handover or migration service is required to for recycle. This condition is a must on both recycling and saving
    
    * Fixing integration tests errors
    
    RISDEV-5186
    
    * Changing warning to log info
    
    RISDEV-5186

--- backend/src/main/java/de/bund/digitalservice/ris/caselaw/domain/DocumentNumberRecyclingService.java
+++ backend/src/main/java/de/bund/digitalservice/ris/caselaw/domain/DocumentNumberRecyclingService.java
@@ -1,15 +1,27 @@
 package de.bund.digitalservice.ris.caselaw.domain;
 
+import de.bund.digitalservice.ris.caselaw.domain.exception.DocumentNumberPatternException;
+import de.bund.digitalservice.ris.caselaw.domain.exception.DocumentNumberRecyclingException;
+import de.bund.digitalservice.ris.caselaw.domain.exception.DocumentationUnitNotExistsException;
 import java.time.Year;
-import java.util.Optional;
 import java.util.UUID;
 
 public interface DocumentNumberRecyclingService {
 
-  Optional<String> addForRecycling(
-      UUID documentationOfficeId, String documentNumber, String documentationOfficeAbbreviation);
+  void addForRecycling(
+      UUID documentationOfficeId, String documentNumber, String documentationOfficeAbbreviation)
+      throws DocumentNumberPatternException,
+          DocumentationUnitNotExistsException,
+          DocumentNumberRecyclingException;
 
-  Optional<String> findDeletedDocumentNumber(String documentationOfficeAbbreviation, Year year);
+  String recycleFromDeletedDocumentationUnit(String documentationOfficeAbbreviation, Year year)
+      throws DocumentNumberPatternException, DocumentNumberRecyclingException;
+
+  void assertDocumentationUnitHasNeverBeenHandedOverOrMigrated(UUID documentationUnitId)
+      throws DocumentNumberRecyclingException;
+
+  void assertPatternIsValid(String documentationOfficeAbbreviation, String documentationUnitNumber)
+      throws DocumentNumberPatternException;
 
   void delete(String documentNumber);
 }
--- backend/src/test/java/de/bund/digitalservice/ris/caselaw/adapter/DatabaseDocumentNumberRecyclingServiceTest.java
+++ backend/src/test/java/de/bund/digitalservice/ris/caselaw/adapter/DatabaseDocumentNumberRecyclingServiceTest.java
@@ -1,6 +1,8 @@
 package de.bund.digitalservice.ris.caselaw.adapter;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import de.bund.digitalservice.ris.caselaw.adapter.database.jpa.DatabaseDeletedDocumentationIdsRepository;
@@ -9,9 +11,15 @@ import de.bund.digitalservice.ris.caselaw.adapter.database.jpa.DeletedDocumentat
 import de.bund.digitalservice.ris.caselaw.adapter.database.jpa.DocumentationUnitDTO;
 import de.bund.digitalservice.ris.caselaw.adapter.database.jpa.StatusDTO;
 import de.bund.digitalservice.ris.caselaw.domain.DateUtil;
+import de.bund.digitalservice.ris.caselaw.domain.EventRecord;
+import de.bund.digitalservice.ris.caselaw.domain.HandoverEntityType;
+import de.bund.digitalservice.ris.caselaw.domain.HandoverService;
 import de.bund.digitalservice.ris.caselaw.domain.PublicationStatus;
+import de.bund.digitalservice.ris.caselaw.domain.exception.DocumentNumberPatternException;
+import de.bund.digitalservice.ris.caselaw.domain.exception.DocumentNumberRecyclingException;
 import java.time.Instant;
 import java.time.Year;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -40,6 +48,8 @@ class DatabaseDocumentNumberRecyclingServiceTest {
 
   @Autowired DatabaseDocumentNumberRecyclingService service;
 
+  @MockBean HandoverService handoverService;
+
   private static final String DEFAULT_DOCUMENTATION_OFFICE = "BGH";
 
   private static String generateDefaultDocumentNumber() {
@@ -47,7 +57,7 @@ class DatabaseDocumentNumberRecyclingServiceTest {
   }
 
   @Test
-  void shouldNotSaveInvalidPrefix() {
+  void addForRecycling_shouldNotSave_ifPrefixIsInvalid() {
 
     var documentationUnitDTO =
         DocumentationUnitDTO.builder()
@@ -72,16 +82,17 @@ class DatabaseDocumentNumberRecyclingServiceTest {
                 DocumentationUnitDTO.builder().statusHistory(List.of(unpublished)).build()));
     when(repository.save(any())).thenReturn(generateDeletedDocumentationUnitDTO());
 
-    var saved =
-        service.addForRecycling(
-            documentationUnitDTO.getId(),
-            documentationUnitDTO.getDocumentNumber(),
-            DEFAULT_DOCUMENTATION_OFFICE);
-    Assertions.assertTrue(saved.isEmpty());
+    assertThrows(
+        DocumentNumberPatternException.class,
+        () ->
+            service.addForRecycling(
+                documentationUnitDTO.getId(),
+                documentationUnitDTO.getDocumentNumber(),
+                DEFAULT_DOCUMENTATION_OFFICE));
   }
 
   @Test
-  void shouldNotOfferInvalidPrefix() {
+  void recycleFromDeletedDocumentationUnit_shouldNotOfferInvalidPrefix() {
     var outdatedDeletedId =
         DeletedDocumentationUnitDTO.builder()
             .documentNumber("KORE2" + Year.now() + "0037")
@@ -92,84 +103,171 @@ class DatabaseDocumentNumberRecyclingServiceTest {
     when(repository.findFirstByAbbreviationAndYear(DEFAULT_DOCUMENTATION_OFFICE, Year.now()))
         .thenReturn(Optional.of(outdatedDeletedId));
 
-    Assertions.assertTrue(
-        service.findDeletedDocumentNumber(DEFAULT_DOCUMENTATION_OFFICE, Year.now()).isEmpty());
+    assertThrows(
+        DocumentNumberPatternException.class,
+        () ->
+            service.recycleFromDeletedDocumentationUnit(DEFAULT_DOCUMENTATION_OFFICE, Year.now()));
+  }
+
+  @Test
+  void recycleFromDeletedDocumentationUnit_shouldNotOfferUsedDocumentationUnit() {
+    var usedDocumentationUnit =
+        DeletedDocumentationUnitDTO.builder()
+            .documentNumber(generateDefaultDocumentNumber())
+            .year(Year.now())
+            .abbreviation(DEFAULT_DOCUMENTATION_OFFICE)
+            .build();
+
+    when(repository.findFirstByAbbreviationAndYear(DEFAULT_DOCUMENTATION_OFFICE, Year.now()))
+        .thenReturn(Optional.of(usedDocumentationUnit));
+
+    when(documentationUnitRepository.findByDocumentNumber(
+            usedDocumentationUnit.getDocumentNumber()))
+        .thenReturn(Optional.of(generateDocumentationUnitDto()));
+
+    var exception =
+        assertThrows(
+            DocumentNumberRecyclingException.class,
+            () ->
+                service.recycleFromDeletedDocumentationUnit(
+                    DEFAULT_DOCUMENTATION_OFFICE, Year.now()));
+
+    Assertions.assertEquals(
+        exception.getMessage(), "Saved documentation number is currently in use");
   }
 
   @ParameterizedTest
   @EnumSource(
       value = PublicationStatus.class,
       names = {"UNPUBLISHED", "EXTERNAL_HANDOVER_PENDING"})
-  void shouldSaveIfOnly_unpublished(PublicationStatus publicationStatus) {
+  void addForRecycling_shouldSaveIfOnly_unpublished(PublicationStatus publicationStatus) {
     var documentationUnitDTO =
         DocumentationUnitDTO.builder()
             .id(UUID.randomUUID())
             .documentNumber(generateDefaultDocumentNumber())
             .build();
 
+    documentationUnitDTO.setStatus(generateStatus(documentationUnitDTO, publicationStatus));
+
     when(documentationUnitRepository.findById(documentationUnitDTO.getId()))
-        .thenReturn(
-            Optional.of(
-                DocumentationUnitDTO.builder()
-                    .status(generateStatus(documentationUnitDTO, publicationStatus))
-                    .statusHistory(List.of(generateStatus(documentationUnitDTO, publicationStatus)))
-                    .build()));
+        .thenReturn(Optional.of(documentationUnitDTO));
 
     when(repository.save(any())).thenReturn(generateDeletedDocumentationUnitDTO());
 
-    var saved =
-        service.addForRecycling(
-            documentationUnitDTO.getId(),
-            documentationUnitDTO.getDocumentNumber(),
-            DEFAULT_DOCUMENTATION_OFFICE);
+    DocumentNumberRecyclingException exception =
+        assertThrows(
+            DocumentNumberRecyclingException.class,
+            () ->
+                service.addForRecycling(
+                    documentationUnitDTO.getId(),
+                    documentationUnitDTO.getDocumentNumber(),
+                    DEFAULT_DOCUMENTATION_OFFICE));
 
-    Assertions.assertTrue(saved.isPresent());
+    Assertions.assertEquals(
+        "Status has been changed once or neither unpublished nor handover pending",
+        exception.getMessage());
   }
 
   @Test
-  void shouldNotSaveIf_published() {
+  void addForRecycling_shouldNotSave_ifHadedOverOrMigrated() {
     var documentationUnitDTO = generateDocumentationUnitDto();
+    //    var unpublished = generateStatus(documentationUnitDTO, PublicationStatus.UNPUBLISHED);
 
+    when(handoverService.getEventLog(
+            documentationUnitDTO.getId(), HandoverEntityType.DOCUMENTATION_UNIT))
+        .thenReturn(List.of(mock(EventRecord.class)));
+
+    when(documentationUnitRepository.findById(documentationUnitDTO.getId()))
+        .thenReturn(
+            Optional.of(
+                DocumentationUnitDTO.builder().statusHistory(Collections.emptyList()).build()));
+
+    DocumentNumberRecyclingException exception =
+        assertThrows(
+            DocumentNumberRecyclingException.class,
+            () ->
+                service.addForRecycling(
+                    documentationUnitDTO.getId(),
+                    documentationUnitDTO.getDocumentNumber(),
+                    DEFAULT_DOCUMENTATION_OFFICE));
+
+    Assertions.assertEquals(
+        "This Documentation unit has already been handed over or migrated and therefore cannot be reused",
+        exception.getMessage());
+  }
+
+  @Test
+  void addForRecycling_shouldNotSave_IfDocumentationUnitHasBeenPublished() {
+    var documentationUnitDTO = generateDocumentationUnitDto();
     var published = generateStatus(documentationUnitDTO, PublicationStatus.PUBLISHED);
 
     when(documentationUnitRepository.findById(documentationUnitDTO.getId()))
         .thenReturn(
-            Optional.of(DocumentationUnitDTO.builder().statusHistory(List.of(published)).build()));
-
-    when(repository.save(any())).thenReturn(generateDeletedDocumentationUnitDTO());
-
-    var saved =
-        service.addForRecycling(
-            documentationUnitDTO.getId(),
-            documentationUnitDTO.getDocumentNumber(),
-            DEFAULT_DOCUMENTATION_OFFICE);
-
-    Assertions.assertTrue(saved.isEmpty());
-  }
-
-  @Test
-  void shouldNotSaveIf_multipleStatus() {
-    var documentationUnitDto = generateDocumentationUnitDto();
-
-    var unpublished = generateStatus(documentationUnitDto, PublicationStatus.UNPUBLISHED);
-    var published = generateStatus(documentationUnitDto, PublicationStatus.PUBLISHED);
-
-    when(documentationUnitRepository.findById(documentationUnitDto.getId()))
-        .thenReturn(
             Optional.of(
                 DocumentationUnitDTO.builder()
-                    .statusHistory(List.of(published, unpublished))
+                    .status(published)
+                    .statusHistory(List.of(published))
                     .build()));
 
     when(repository.save(any())).thenReturn(generateDeletedDocumentationUnitDTO());
 
-    var saved =
-        service.addForRecycling(
-            documentationUnitDto.getId(),
-            documentationUnitDto.getDocumentNumber(),
-            DEFAULT_DOCUMENTATION_OFFICE);
+    DocumentNumberRecyclingException exception =
+        assertThrows(
+            DocumentNumberRecyclingException.class,
+            () ->
+                service.addForRecycling(
+                    documentationUnitDTO.getId(),
+                    documentationUnitDTO.getDocumentNumber(),
+                    DEFAULT_DOCUMENTATION_OFFICE));
 
-    Assertions.assertTrue(saved.isEmpty());
+    Assertions.assertEquals(
+        "Status has been changed once or neither unpublished nor handover pending",
+        exception.getMessage());
+  }
+
+  @Test
+  void addForRecycling_shouldNotSave_ifMultipleStatusExists() {
+    var documentationUnitDto = generateDocumentationUnitDto();
+
+    var statusList =
+        List.of(
+            generateStatus(documentationUnitDto, PublicationStatus.PUBLISHED),
+            generateStatus(documentationUnitDto, PublicationStatus.UNPUBLISHED));
+    documentationUnitDto.setStatus(statusList.get(1));
+    documentationUnitDto.setStatusHistory(statusList);
+
+    when(documentationUnitRepository.findById(documentationUnitDto.getId()))
+        .thenReturn(Optional.of(documentationUnitDto));
+
+    when(repository.save(any())).thenReturn(generateDeletedDocumentationUnitDTO());
+
+    DocumentNumberRecyclingException exception =
+        assertThrows(
+            DocumentNumberRecyclingException.class,
+            () ->
+                service.addForRecycling(
+                    documentationUnitDto.getId(),
+                    documentationUnitDto.getDocumentNumber(),
+                    DEFAULT_DOCUMENTATION_OFFICE));
+
+    Assertions.assertEquals(
+        "Status has been changed once or neither unpublished nor handover pending",
+        exception.getMessage());
+  }
+
+  @Test
+  void assertStatusHasNeverBeenPublished_shouldReturnErrorIfDocumentHasBeenPublishedOrHandedOver() {
+    var documentationUnitDTO = generateDocumentationUnitDto();
+
+    when(handoverService.getEventLog(
+            documentationUnitDTO.getId(), HandoverEntityType.DOCUMENTATION_UNIT))
+        .thenReturn(List.of(mock(EventRecord.class)));
+
+    assertThrows(
+        DocumentNumberRecyclingException.class,
+        () ->
+            service.assertDocumentationUnitHasNeverBeenHandedOverOrMigrated(
+                documentationUnitDTO.getId()));
   }
 
   private static StatusDTO generateStatus(
