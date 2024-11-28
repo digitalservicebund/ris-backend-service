ecf0dbb5b968b099974b4179d9c9efe2294928f3
Kim Kern <kim.kern@digitalservice.bund.de>
2024-11-28 14:25:43 +0100

    Update backend/src/main/resources/db-scripts/migration/V1.10__shedlock_lock_for_distributed_scheduling.sql
    Co-authored-by: Anne <158598611+anne-ds@users.noreply.github.com>

--- backend/src/main/resources/db-scripts/migration/V1.10__shedlock_lock_for_distributed_scheduling.sql
+++ backend/src/main/resources/db-scripts/migration/V1.10__shedlock_lock_for_distributed_scheduling.sql
@@ -1,2 +1,2 @@
-CREATE TABLE shedlock(name VARCHAR(64) NOT NULL, lock_until TIMESTAMP NOT NULL,
-                      locked_at TIMESTAMP NOT NULL, locked_by VARCHAR(255) NOT NULL, PRIMARY KEY (name));
+CREATE TABLE shedlock(name VARCHAR(64) NOT NULL PRIMARY KEY, lock_until TIMESTAMP NOT NULL,
+                      locked_at TIMESTAMP NOT NULL, locked_by VARCHAR(255) NOT NULL);
