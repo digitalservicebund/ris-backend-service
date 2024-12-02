ff6fac9fa4424303d9a694d799fa86a8bf994167
nbabai <nadav.babai@digitalservice.bund.de>
2024-12-02 16:44:27 +0100

    Revert "Await to dom to render"
    This reverts commit 3e54798847989cfb7951522efd5e05bea42e1ae0.

--- frontend/test/components/periodicalEvaluation/editionReferenceInput.spec.ts
+++ frontend/test/components/periodicalEvaluation/editionReferenceInput.spec.ts
@@ -1,7 +1,6 @@
 import { createTestingPinia } from "@pinia/testing"
 import { userEvent } from "@testing-library/user-event"
 import { render, screen } from "@testing-library/vue"
-import { nextTick } from "vue"
 import { createRouter, createWebHistory } from "vue-router"
 import PeriodicalEditionReferenceInput from "@/components/periodical-evaluation/references/PeriodicalEditionReferenceInput.vue"
 import RelatedDocumentation from "@/domain/relatedDocumentation"
@@ -77,12 +76,10 @@ describe("Legal periodical edition reference input", () => {
 
   test("adding a decision scrolls to reference on validation errors", async () => {
     const { user } = renderComponent()
-
     const scrollIntoViewMock = vi.fn()
     window.HTMLElement.prototype.scrollIntoView = scrollIntoViewMock
 
     await user.click(await screen.findByLabelText("Nach Entscheidung suchen"))
-    await nextTick()
     await user.click(await screen.findByLabelText("Treffer übernehmen"))
     expect(
       scrollIntoViewMock,
