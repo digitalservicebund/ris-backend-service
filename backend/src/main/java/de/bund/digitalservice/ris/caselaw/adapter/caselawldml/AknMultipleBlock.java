535af8c452d7573bec9fc757a6dc1ad995ed8999
andrew.cenkner <andrew.cenkner@digitalservice.bund.de>
2024-12-13 13:46:28 +0100

    RISDEV-5168
    Adjustment structure LDML-R
    *Fixing minor bugs found during re-export of all caselaw in the database

--- backend/src/main/java/de/bund/digitalservice/ris/caselaw/adapter/caselawldml/AknMultipleBlock.java
+++ backend/src/main/java/de/bund/digitalservice/ris/caselaw/adapter/caselawldml/AknMultipleBlock.java
@@ -3,18 +3,20 @@ package de.bund.digitalservice.ris.caselaw.adapter.caselawldml;
 import jakarta.xml.bind.annotation.XmlElement;
 import jakarta.xml.bind.annotation.XmlTransient;
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
+@NoArgsConstructor
 public class AknMultipleBlock {
 
   // Jaxb doesn't handle elements with the same name very well.
   // They need to be in a list with the same class instead of two separate fields.
   // A Map is used with @XmlElement added to a helper getter/setter to facilitate easier use.
-  @XmlTransient private Map<String, AknBlock> blocks;
+  @XmlTransient private Map<String, AknBlock> blocks = new LinkedHashMap<>();
 
   @XmlElement(name = "block", namespace = CaseLawLdml.AKN_NS)
   public void setJaxbBlocks(List<AknBlock> blocks) {
@@ -26,10 +28,6 @@ public class AknMultipleBlock {
   @XmlElement(name = "block", namespace = CaseLawLdml.AKN_NS)
   public List<AknBlock> getJaxbBlocks() {
     return new ArrayList<>(blocks.values());
-  }
-
-  public AknMultipleBlock() {
-    this.blocks = new HashMap<>();
   }
 
   public AknMultipleBlock withBlock(String name, AknBlock block) {
