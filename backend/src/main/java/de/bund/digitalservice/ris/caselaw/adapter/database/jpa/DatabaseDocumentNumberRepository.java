f54336160391eb4dadceff54710cd8bed0a89289
nbabai <nadav.babai@digitalservice.bund.de>
2025-01-06 13:59:33 +0100

    Only one thread can write lock
    RISDEV-6004

--- backend/src/main/java/de/bund/digitalservice/ris/caselaw/adapter/database/jpa/DatabaseDocumentNumberRepository.java
+++ backend/src/main/java/de/bund/digitalservice/ris/caselaw/adapter/database/jpa/DatabaseDocumentNumberRepository.java
@@ -1,13 +1,19 @@
 package de.bund.digitalservice.ris.caselaw.adapter.database.jpa;
 
+import jakarta.persistence.LockModeType;
+import jakarta.validation.constraints.NotBlank;
 import java.time.Year;
 import java.util.Optional;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface DatabaseDocumentNumberRepository extends JpaRepository<DocumentNumberDTO, String> {
 
+  @NotNull
+  @Lock(LockModeType.PESSIMISTIC_WRITE)
   Optional<DocumentNumberDTO> findByDocumentationOfficeAbbreviationAndYear(
-      String documentationOfficeAbbreviation, Year year);
+      @NotBlank String documentationOfficeAbbreviation, @NotNull Year year);
 }
