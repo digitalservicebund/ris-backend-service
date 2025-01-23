a07cc81620aba9e05e87e5ab3a467ae425c45209
nbabai <nadav.babai@digitalservice.bund.de>
2025-01-23 14:50:06 +0100

    Menu styles
    RISDEV-0000

--- frontend/src/components/TextSuggestionsDropdown.vue
+++ frontend/src/components/TextSuggestionsDropdown.vue
@@ -22,10 +22,10 @@ function ignoreSuggestion() {
 
 <template>
   <div
-    class="flex flex-col items-start justify-start border-2 border-solid border-blue-800 bg-white p-24"
+    class="w-max-480 flex-col flex-wrap items-start justify-start border-2 border-solid border-blue-800 bg-white p-24"
   >
     <button
-      class="ds-link-01-bold whitespace-nowrap leading-24 focus:outline-none focus-visible:outline-4 focus-visible:outline-offset-4 focus-visible:outline-blue-800"
+      class="ds-link-01-bold w-full whitespace-nowrap leading-24 focus:outline-none focus-visible:outline-4 focus-visible:outline-offset-4 focus-visible:outline-blue-800"
       @click="ignoreSuggestion"
     >
       Ignorieren
@@ -34,20 +34,21 @@ function ignoreSuggestion() {
     <label>
       {{ match.message }}
     </label>
-    <div
-      v-for="(replacement, i) in match.replacements"
-      :key="i + replacement.value"
-      class="flex w-full flex-row flex-nowrap gap-24"
-    >
-      <TextButton
-        aria-label="Vorschlag übernehmen"
-        button-type="primary"
-        :label="replacement.value"
-        size="small"
-        width="w-max"
-        @click="acceptSuggestion(replacement)"
+    <div class="flex w-full flex-row flex-wrap gap-24">
+      <div
+        v-for="(replacement, i) in match.replacements"
+        :key="i + replacement.value"
       >
-      </TextButton>
+        <TextButton
+          aria-label="Vorschlag übernehmen"
+          button-type="primary"
+          :label="replacement.value"
+          size="small"
+          width="w-max"
+          @click="acceptSuggestion(replacement)"
+        >
+        </TextButton>
+      </div>
     </div>
   </div>
 </template>
