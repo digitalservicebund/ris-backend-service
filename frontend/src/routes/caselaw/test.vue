213a68921b7fa31de4a0785dd93a5eddca9ac7b0
nbabai <nadav.babai@digitalservice.bund.de>
2025-01-23 12:38:09 +0100

    Add test compoenet
    RISDEV-0000

--- frontend/src/routes/caselaw/test.vue
+++ frontend/src/routes/caselaw/test.vue
@@ -2,9 +2,13 @@
 import { ref } from "vue"
 import TextEditor from "@/components/input/TextEditor.vue"
 
-const htmlContent = ref<string>(
-  "<div>LanguageTool ist Ihr intelligenter Schreibassistent für alle gängigen Browser und Textverarbeitungsprogramme. Schreiben sie in diesem Textfeld oder fügen Sie einen Text ein. <noindex>Rechtshcreibfehler</noindex> werden rot markirt, Grammatikfehler werden gelb hervor gehoben und Stilfehler werden, anders wie die anderen Fehler, blau unterstrichen. wussten Sie dass Synonyme per Doppelklick auf ein Wort aufgerufen werden können? Nutzen Sie LanguageTool in allen Lebenslagen, zB. wenn Sie am Donnerstag, dem 13. Mai 2022, einen Basketballkorb in 10 Fuß Höhe montieren möchten. </div>",
-)
+const text = `<div>LanguageTool ist Ihr intelligenter Schreibassistent für alle gängigen Browser und Textverarbeitungsprogramme. Schreiben sie in diesem Textfeld oder fügen Sie einen Text ein. <noindex>Rechtshcreibfehler</noindex> werden rot markirt, Grammatikfehler werden gelb hervor gehoben und Stilfehler werden, anders wie die anderen Fehler, blau unterstrichen. wussten Sie dass Synonyme per Doppelklick auf ein Wort aufgerufen werden können? Nutzen Sie LanguageTool in allen Lebenslagen, zB. wenn Sie am Donnerstag, dem 13. Mai 2022, einen Basketballkorb in 10 Fuß Höhe montieren möchten. </div>`
+
+const htmlContent = ref(text)
+
+const updateContent = (newValue: string) => {
+  htmlContent.value = newValue
+}
 </script>
 <template>
   <TextEditor
@@ -15,5 +19,10 @@ const htmlContent = ref<string>(
     field-size="max"
     plain-border-numbers
     :value="htmlContent"
+    @update-value="updateContent"
   />
+
+  <div>
+    {{ htmlContent }}
+  </div>
 </template>
