030cd705d9948a9b129cf34dad959a0d2fb331a4
nbabai <nadav.babai@digitalservice.bund.de>
2025-01-29 10:35:34 +0100

    Fixing custom rule wildcard issue
    RISDEV-6454

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -1,10 +1,12 @@
 import com.adarshr.gradle.testlogger.theme.ThemeType
+import com.diffplug.spotless.FormatterFunc
 import com.github.jk1.license.filter.DependencyFilter
 import com.github.jk1.license.filter.LicenseBundleNormalizer
 import com.github.jk1.license.render.CsvReportRenderer
 import com.github.jk1.license.render.ReportRenderer
 import io.franzbecker.gradle.lombok.task.DelombokTask
 import org.flywaydb.gradle.task.FlywayMigrateTask
+import java.io.Serializable
 
 plugins {
     java
@@ -75,14 +77,15 @@ spotless {
     java {
         removeUnusedImports()
         googleJavaFormat()
-        custom("Refuse wildcard imports") {
-            // Wildcard imports can't be resolved by spotless itself.
-            // This will require the developer themselves to adhere to best practices.
-            if (it.contains("\nimport .*\\*;".toRegex())) {
-                throw AssertionError("Do not use wildcard imports. 'spotlessApply' cannot resolve this issue.")
+        // Wildcard imports can't be resolved by spotless itself.
+        custom("Refuse wildcard imports", object : Serializable, FormatterFunc {
+            override fun apply(input: String) : String {
+                if (input.contains("\nimport .*\\*;".toRegex())) {
+                    throw GradleException("No wildcard imports allowed.")
+                }
+                return input
             }
-            it
-        }
+        })
     }
     format("misc") {
 
