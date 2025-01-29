f9094fcf5a48b750ae6619896354b1bf9144e889
anne-ds <anne.schumann@digitalservice.bund.de>
2025-01-29 09:08:17 +0100

    Bump schema to 0.0.42
    RISDEV-5912

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -195,7 +195,7 @@ dependencies {
     // or with local gradle project (look also into settings.gradle.kts)
     // implementation(project(":exporter"))
 
-    implementation("de.bund.digitalservice:neuris-caselaw-migration-schema:0.0.41")
+    implementation("de.bund.digitalservice:neuris-caselaw-migration-schema:0.0.42")
     // for local development:
 //     implementation(files("../../ris-data-migration/schema/build/libs/schema-0.0.41.jar"))
 
