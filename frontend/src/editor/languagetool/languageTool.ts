68d42fe7f3d347275624119d8379209617fe6757
nbabai <nadav.babai@digitalservice.bund.de>
2025-02-04 16:49:55 +0100

    init langauge tool with false
    RISDEV-0000

--- frontend/src/editor/languagetool/languageTool.ts
+++ frontend/src/editor/languagetool/languageTool.ts
@@ -55,7 +55,7 @@ export default class LanguageTool {
 
   public proofReadInitially = false
 
-  public languageToolActive: boolean = true
+  public languageToolActive: boolean = false
 
   constructor(decorationSet: DecorationSet) {
     this.decorationSet = decorationSet
