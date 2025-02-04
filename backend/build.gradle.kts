53fa9e00f96dbfd94053a0ab2f1557a3ce367d9e
Nadine Kost <nadine.kost@digitalservice.bund.de>
2025-02-04 17:09:40 +0100

    Use latest schema
    RISDEV-6108

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -198,7 +198,7 @@ dependencies {
     // or with local gradle project (look also into settings.gradle.kts)
     // implementation(project(":exporter"))
 
-    implementation("de.bund.digitalservice:neuris-caselaw-migration-schema:0.0.43")
+    implementation("de.bund.digitalservice:neuris-caselaw-migration-schema:0.0.44")
     // for local development:
 //     implementation(files("../../ris-data-migration/schema/build/libs/schema-0.0.43.jar"))
 
