379cbf7624e8a8485bd37a6a2c78e1903c94c128
Leonie Koch <leonie.koch@digitalservice.bund.de>
2025-02-17 12:59:51 +0100

    Add javadoc
    RISDEV-0000

--- backend/src/main/java/de/bund/digitalservice/ris/caselaw/domain/SourceValue.java
+++ backend/src/main/java/de/bund/digitalservice/ris/caselaw/domain/SourceValue.java
@@ -1,5 +1,14 @@
 package de.bund.digitalservice.ris.caselaw.domain;
 
+/**
+ * Enum representing different source values for decisions. O - Unaufgefordert eingesandtes Original
+ * (Unsolicited original submission) A - Angefordertes Original (Requested original submission) Z -
+ * Zeitschriftenveröffentlichung (Publication in a journal) E - Ohne Vorlage des Originals E-Mail
+ * (Email submission without the original document) L - Ländergerichte, EuG- und EuGH-Entscheidungen
+ * über jDV-Verfahren (Decisions from national courts, the EU General Court (EuG), and the Court of
+ * Justice of the European Union (EuGH) regarding cross-border legal procedures) S - Sonstige (Other
+ * sources)
+ */
 public enum SourceValue {
   O,
   A,
