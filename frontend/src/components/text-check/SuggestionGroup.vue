6fec8de664a6b04ab323bac30ca350f635950968
nbabai <nadav.babai@digitalservice.bund.de>
2025-02-19 12:50:15 +0100

    Reapply "Text checker components changes"
    This reverts commit 0bf15f942eb3e33d9582a981169b3d2f45304192.

--- frontend/src/components/text-check/SuggestionGroup.vue
+++ frontend/src/components/text-check/SuggestionGroup.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import IconBadge from "@/components/IconBadge.vue"
+import MatchLinkingButton from "@/components/text-check/MatchLinkingButton.vue"
 import ReplacementBar from "@/components/text-check/ReplacementBar.vue"
 import { Suggestion } from "@/types/languagetool"
 
@@ -7,6 +8,19 @@ defineProps<{
   suggestion: Suggestion
   isSelected?: boolean
 }>()
+
+const emit = defineEmits<{
+  "suggestion:update": [value: string]
+  "suggestion:ignore": [void]
+}>()
+
+function acceptSuggestion(replacement: string) {
+  emit("suggestion:update", replacement)
+}
+
+function ignoreSuggestion() {
+  emit("suggestion:ignore")
+}
 
 function getAllReplacementValues(suggestion: Suggestion) {
   return [
@@ -24,7 +38,7 @@ function getAllReplacementValues(suggestion: Suggestion) {
     class="flex flex-col gap-4 bg-blue-100 p-24"
     :class="[isSelected ? 'border-4 border-blue-900' : '']"
   >
-    <div class="flex flex-row gap-8">
+    <div class="flex flex-row items-center gap-8">
       <div class="ds-label-01-bold">
         {{ suggestion.word }}
       </div>
@@ -33,9 +47,9 @@ function getAllReplacementValues(suggestion: Suggestion) {
           background-color="bg-red-300"
           color="text-red-900"
           :label="suggestion.matches.length.toString()"
-          :margin-x="4"
         />
       </span>
+      <MatchLinkingButton :category="suggestion.matches[0].category" />
     </div>
 
     <div>
@@ -48,6 +62,8 @@ function getAllReplacementValues(suggestion: Suggestion) {
     <ReplacementBar
       :replacement-mode="suggestion.matches.length > 1 ? 'multiple' : 'single'"
       :replacements="getAllReplacementValues(suggestion)"
+      @suggestion:ignore="ignoreSuggestion"
+      @suggestion:update="acceptSuggestion"
     />
   </div>
 </template>
