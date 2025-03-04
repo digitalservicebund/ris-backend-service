db083d6cc153faffa5c64ba62683f1cdf9d87c92
Leonie Koch <leonie.koch@digitalservice.bund.de>
2025-03-04 14:24:50 +0100

    Add portal controller test
    RISDEV-5631

---
+++ backend/src/test/java/de/bund/digitalservice/ris/caselaw/PortalControllerTestConfig.java
@@ -0,0 +1,18 @@
+package de.bund.digitalservice.ris.caselaw;
+
+import de.bund.digitalservice.ris.caselaw.adapter.DocumentNumberPatternConfig;
+import de.bund.digitalservice.ris.caselaw.adapter.OAuthService;
+import de.bund.digitalservice.ris.caselaw.config.SecurityConfig;
+import de.bund.digitalservice.ris.caselaw.domain.mapper.PatchMapperService;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Import;
+
+@TestConfiguration
+@Import({
+  SecurityConfig.class,
+  OAuthService.class,
+  TestConfig.class,
+  PatchMapperService.class,
+  DocumentNumberPatternConfig.class,
+})
+public class PortalControllerTestConfig {}
