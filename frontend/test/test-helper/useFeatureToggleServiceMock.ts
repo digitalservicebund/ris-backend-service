4a4af51e12a0d20e29966d243ef67851ad5a5c0d
nbabai <nadav.babai@digitalservice.bund.de>
2025-03-12 12:45:02 +0100

    Update useFeatureToggleServiceMock.ts
    RISDEV-0000

--- frontend/test/test-helper/useFeatureToggleServiceMock.ts
+++ frontend/test/test-helper/useFeatureToggleServiceMock.ts
@@ -4,7 +4,7 @@ import featureToggleService from "@/services/featureToggleService"
 /**
  * Mock service call for the feature toggle in vi test
  */
-export const useFeatureToggleServiceMock = () => {
+export function useFeatureToggleServiceMock() {
   vi.spyOn(featureToggleService, "isEnabled").mockResolvedValue({
     status: 200,
     data: true,
