3749a757419976350add4900671e8a0faa68ca33
nbabai <nadav.babai@digitalservice.bund.de>
2025-03-27 10:51:46 +0100

    Adjusting tests
    RISDEV-6206

--- backend/src/main/java/de/bund/digitalservice/ris/caselaw/domain/textcheck/ignored_words/IgnoredTextCheckWord.java
+++ backend/src/main/java/de/bund/digitalservice/ris/caselaw/domain/textcheck/ignored_words/IgnoredTextCheckWord.java
@@ -3,7 +3,9 @@ package de.bund.digitalservice.ris.caselaw.domain.textcheck.ignored_words;
 import jakarta.validation.constraints.Size;
 import java.util.UUID;
 import lombok.Builder;
+import lombok.Getter;
 
+@Getter
 @Builder(toBuilder = true)
 public class IgnoredTextCheckWord {
 
