7508e13b0173d2040a0e9e52e0e74f1ea30139e5
anne-ds <anne.schumann@digitalservice.bund.de>
2025-03-27 12:55:02 +0100

    Trigger logging
    RISDEV-7209

--- backend/src/main/java/de/bund/digitalservice/ris/caselaw/adapter/PublicPortalPublicationService.java
+++ backend/src/main/java/de/bund/digitalservice/ris/caselaw/adapter/PublicPortalPublicationService.java
@@ -121,7 +121,7 @@ public class PublicPortalPublicationService {
   //                    ↓ minute (0-59)
   //                 ↓ second (0-59)
   // Default:        0 30 4 * * * (After migration: CET: 5:30)
-  @Scheduled(cron = "0 50 11 * * *")
+  @Scheduled(cron = "0 30 12 * * *")
   @SchedulerLock(name = "portal-publication-diff-job", lockAtMostFor = "PT15M")
   public void logPortalPublicationSanityCheck() {
     List<String> portalBucketDocumentNumbers =
