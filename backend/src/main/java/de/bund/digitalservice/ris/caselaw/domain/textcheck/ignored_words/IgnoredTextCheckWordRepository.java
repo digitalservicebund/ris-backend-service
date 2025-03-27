f4a531d82619df38fbc1f098f8d5f58dd059d9c1
nbabai <nadav.babai@digitalservice.bund.de>
2025-03-27 11:50:11 +0100

    Add add ignore endpoint
    RISDEV-6206

--- backend/src/main/java/de/bund/digitalservice/ris/caselaw/domain/textcheck/ignored_words/IgnoredTextCheckWordRepository.java
+++ backend/src/main/java/de/bund/digitalservice/ris/caselaw/domain/textcheck/ignored_words/IgnoredTextCheckWordRepository.java
@@ -1,14 +1,14 @@
 package de.bund.digitalservice.ris.caselaw.domain.textcheck.ignored_words;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
-import org.springframework.data.repository.NoRepositoryBean;
-
 import javax.annotation.Nullable;
+import org.springframework.data.repository.NoRepositoryBean;
 
 @NoRepositoryBean
 public interface IgnoredTextCheckWordRepository {
+
+  IgnoredTextCheckWord addIgnoredTextCheckWord(IgnoredTextCheckWord ignoredTextCheckWord);
 
   List<IgnoredTextCheckWord> findAllByDocumentationOfficesOrUnitAndWords(
       List<UUID> documentationOfficeIds, @Nullable UUID documentationUnitId, List<String> words);
