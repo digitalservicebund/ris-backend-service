b0dcb9a8346f2c2115286235162e07c2d5a7b1e8
Leonie Koch <leonie.koch@digitalservice.bund.de>
2025-04-08 12:30:27 +0200

    Add RISUI data table for history log
    RISDEV-7432

--- frontend/src/components/DocumentUnitManagementData.vue
+++ frontend/src/components/DocumentUnitManagementData.vue
@@ -1,5 +1,7 @@
 <script lang="ts" setup>
 import { storeToRefs } from "pinia"
+import Column from "primevue/column"
+import DataTable from "primevue/datatable"
 import DocumentUnitDeleteButton from "@/components/DocumentUnitDeleteButton.vue"
 import DuplicateRelationListItem from "@/components/DuplicateRelationListItem.vue"
 import TitleElement from "@/components/TitleElement.vue"
@@ -7,15 +9,105 @@ import { useDocumentUnitStore } from "@/stores/documentUnitStore"
 import IconCheck from "~icons/ic/baseline-check"
 
 const { documentUnit } = storeToRefs(useDocumentUnitStore())
+
+const data = [
+  {
+    timestamp: "31.03.2025 15:36",
+    name: "NeuRIS",
+    subject: "Status verändert auf „Veröffentlicht“",
+  },
+  {
+    timestamp: "31.03.2025 15:36",
+    name: "BGH (Anna Agensburg)",
+    subject: "Dokument an JDV-Portal übergeben",
+  },
+  {
+    timestamp: "31.03.2025 15:36",
+    name: "NeuRIS",
+    subject: "Status verändert auf „Veröffentlicht“",
+  },
+  {
+    timestamp: "31.03.2025 15:36",
+    name: "BGH (Anna Agensburg)",
+    subject: "Dokument an JDV-Portal übergeben",
+  },
+  {
+    timestamp: "31.03.2025 15:36",
+    name: "NeuRIS",
+    subject: "Status verändert auf „Veröffentlicht“",
+  },
+  {
+    timestamp: "31.03.2025 15:36",
+    name: "BGH (Anna Agensburg)",
+    subject: "Dokument an JDV-Portal übergeben",
+  },
+  {
+    timestamp: "31.03.2025 15:36",
+    name: "NeuRIS",
+    subject: "Status verändert auf „Veröffentlicht“",
+  },
+  {
+    timestamp: "31.03.2025 15:36",
+    name: "BGH (Anna Agensburg)",
+    subject: "Dokument an JDV-Portal übergeben",
+  },
+  {
+    timestamp: "31.03.2025 15:36",
+    name: "NeuRIS",
+    subject: "Status verändert auf „Veröffentlicht“",
+  },
+  {
+    timestamp: "31.03.2025 15:36",
+    name: "BGH (Anna Agensburg)",
+    subject: "Dokument an JDV-Portal übergeben",
+  },
+  {
+    timestamp: "31.03.2025 15:36",
+    name: "NeuRIS",
+    subject: "Status verändert auf „Veröffentlicht“",
+  },
+  {
+    timestamp: "31.03.2025 15:36",
+    name: "BGH (Anna Agensburg)",
+    subject: "Dokument an JDV-Portal übergeben",
+  },
+]
+
+// Dynamically generate the columns based on the keys in the data
+const columns = [
+  { field: "timestamp", header: "Änderung am" },
+  { field: "name", header: "Von" },
+  { field: "subject", header: "Was" },
+]
+const rowClass = () => {
+  return "bg-blue-100"
+}
 </script>
 
 <template>
   <div class="w-full grow p-24">
     <div class="flex flex-col gap-24 bg-white p-24">
       <TitleElement>Verwaltungsdaten</TitleElement>
-      <dl class="my-16">
+      <div class="py-24">
+        <h2 class="ris-body1-bold pb-16">Historie:</h2>
+        <DataTable
+          :row-class="rowClass"
+          scroll-height="250px"
+          scrollable
+          :value="data"
+        >
+          <Column
+            v-for="col in columns"
+            :key="col.field"
+            :field="col.field"
+            :header="col.header"
+            header-class="ris-label2-bold text-gray-900"
+          ></Column>
+        </DataTable>
+      </div>
+      <dl>
         <div class="flex gap-24 px-0">
-          <dt class="ris-label2-bold shrink-0 grow-0 basis-160">
+          <dt class="ris-body1-bold shrink-0 grow-0 basis-160">
             Dublettenverdacht:
           </dt>
           <dd class="ris-body2-regular flex flex-col gap-32">
