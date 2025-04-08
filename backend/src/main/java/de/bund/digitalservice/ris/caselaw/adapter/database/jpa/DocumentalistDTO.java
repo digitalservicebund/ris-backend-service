19e96377ddd761a43879510afc24045e6adeb5ba
Nadine Kost <nadine.kost@digitalservice.bund.de>
2025-04-08 10:03:32 +0200

    Clean up documentalist dto
    RISDEV-6986

--- backend/src/main/java/de/bund/digitalservice/ris/caselaw/adapter/database/jpa/DocumentalistDTO.java
+++ backend/src/main/java/de/bund/digitalservice/ris/caselaw/adapter/database/jpa/DocumentalistDTO.java
@@ -4,8 +4,6 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -34,9 +32,4 @@ public class DocumentalistDTO {
   @Size(max = 255)
   @NotBlank
   private String value;
-
-  @ManyToOne
-  @JoinColumn(name = "documentation_unit_id")
-  @NotNull
-  private DocumentationUnitDTO documentationUnit;
 }
