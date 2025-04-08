01126600406770acfbdfe89b5f23a3dc26e9d26d
Leonie Koch <leonie.koch@digitalservice.bund.de>
2025-04-08 13:18:21 +0200

    Extract datatable into display component
    RISDEV-7432

---
+++ frontend/src/components/management-data/DocumentUnitHistoryLog.vue
@@ -0,0 +1,97 @@
+<script lang="ts" setup>
+import Column from "primevue/column"
+import DataTable from "primevue/datatable"
+
+const data = [
+  {
+    timestamp: "31.03.2025 15:36",
+    name: "NeuRIS",
+    subject: "Status verändert auf „Veröffentlicht“",
+  },
+  {
+    timestamp: "31.03.2025 15:36",
+    name: "BGH (Anna Agensburg)",
+    subject: "Dokument an JDV-Portal übergeben",
+  },
+  {
+    timestamp: "31.03.2025 15:36",
+    name: "NeuRIS",
+    subject: "Status verändert auf „Veröffentlicht“",
+  },
+  {
+    timestamp: "31.03.2025 15:36",
+    name: "BGH (Anna Agensburg)",
+    subject: "Dokument an JDV-Portal übergeben",
+  },
+  {
+    timestamp: "31.03.2025 15:36",
+    name: "NeuRIS",
+    subject: "Status verändert auf „Veröffentlicht“",
+  },
+  {
+    timestamp: "31.03.2025 15:36",
+    name: "BGH (Anna Agensburg)",
+    subject: "Dokument an JDV-Portal übergeben",
+  },
+  {
+    timestamp: "31.03.2025 15:36",
+    name: "NeuRIS",
+    subject: "Status verändert auf „Veröffentlicht“",
+  },
+  {
+    timestamp: "31.03.2025 15:36",
+    name: "BGH (Anna Agensburg)",
+    subject: "Dokument an JDV-Portal übergeben",
+  },
+  {
+    timestamp: "31.03.2025 15:36",
+    name: "NeuRIS",
+    subject: "Status verändert auf „Veröffentlicht“",
+  },
+  {
+    timestamp: "31.03.2025 15:36",
+    name: "BGH (Anna Agensburg)",
+    subject: "Dokument an JDV-Portal übergeben",
+  },
+  {
+    timestamp: "31.03.2025 15:36",
+    name: "NeuRIS",
+    subject: "Status verändert auf „Veröffentlicht“",
+  },
+  {
+    timestamp: "31.03.2025 15:36",
+    name: "BGH (Anna Agensburg)",
+    subject: "Dokument an JDV-Portal übergeben",
+  },
+]
+
+// Dynamically generate the columns based on the keys in the data
+const columns = [
+  { field: "timestamp", header: "Änderung am" },
+  { field: "name", header: "Von" },
+  { field: "subject", header: "Was" },
+]
+const rowClass = () => {
+  return "bg-blue-100"
+}
+</script>
+
+<template>
+  <div class="py-24">
+    <h2 class="ris-body1-bold pb-16">Historie:</h2>
+    <DataTable
+      :row-class="rowClass"
+      scroll-height="250px"
+      scrollable
+      :value="data"
+    >
+      <Column
+        v-for="col in columns"
+        :key="col.field"
+        :field="col.field"
+        :header="col.header"
+        header-class="ris-label2-bold text-gray-900"
+      ></Column>
+    </DataTable>
+  </div>
+</template>
--- frontend/src/components/management-data/DocumentUnitManagementData.vue
+++ frontend/src/components/management-data/DocumentUnitManagementData.vue
@@ -1,8 +1,7 @@
 <script lang="ts" setup>
 import { storeToRefs } from "pinia"
-import Column from "primevue/column"
-import DataTable from "primevue/datatable"
 import DocumentUnitDeleteButton from "@/components/DocumentUnitDeleteButton.vue"
+import DocumentUnitHistoryLog from "@/components/management-data/DocumentUnitHistoryLog.vue"
 import DuplicateRelationListItem from "@/components/management-data/DuplicateRelationListItem.vue"
 import ManagementDataMetadata from "@/components/management-data/ManagementDataMetadata.vue"
 import TitleElement from "@/components/TitleElement.vue"
@@ -11,79 +10,6 @@ import { useDocumentUnitStore } from "@/stores/documentUnitStore"
 import IconCheck from "~icons/ic/baseline-check"
 
 const { documentUnit } = storeToRefs(useDocumentUnitStore())
-
-const data = [
-  {
-    timestamp: "31.03.2025 15:36",
-    name: "NeuRIS",
-    subject: "Status verändert auf „Veröffentlicht“",
-  },
-  {
-    timestamp: "31.03.2025 15:36",
-    name: "BGH (Anna Agensburg)",
-    subject: "Dokument an JDV-Portal übergeben",
-  },
-  {
-    timestamp: "31.03.2025 15:36",
-    name: "NeuRIS",
-    subject: "Status verändert auf „Veröffentlicht“",
-  },
-  {
-    timestamp: "31.03.2025 15:36",
-    name: "BGH (Anna Agensburg)",
-    subject: "Dokument an JDV-Portal übergeben",
-  },
-  {
-    timestamp: "31.03.2025 15:36",
-    name: "NeuRIS",
-    subject: "Status verändert auf „Veröffentlicht“",
-  },
-  {
-    timestamp: "31.03.2025 15:36",
-    name: "BGH (Anna Agensburg)",
-    subject: "Dokument an JDV-Portal übergeben",
-  },
-  {
-    timestamp: "31.03.2025 15:36",
-    name: "NeuRIS",
-    subject: "Status verändert auf „Veröffentlicht“",
-  },
-  {
-    timestamp: "31.03.2025 15:36",
-    name: "BGH (Anna Agensburg)",
-    subject: "Dokument an JDV-Portal übergeben",
-  },
-  {
-    timestamp: "31.03.2025 15:36",
-    name: "NeuRIS",
-    subject: "Status verändert auf „Veröffentlicht“",
-  },
-  {
-    timestamp: "31.03.2025 15:36",
-    name: "BGH (Anna Agensburg)",
-    subject: "Dokument an JDV-Portal übergeben",
-  },
-  {
-    timestamp: "31.03.2025 15:36",
-    name: "NeuRIS",
-    subject: "Status verändert auf „Veröffentlicht“",
-  },
-  {
-    timestamp: "31.03.2025 15:36",
-    name: "BGH (Anna Agensburg)",
-    subject: "Dokument an JDV-Portal übergeben",
-  },
-]
-
-// Dynamically generate the columns based on the keys in the data
-const columns = [
-  { field: "timestamp", header: "Änderung am" },
-  { field: "name", header: "Von" },
-  { field: "subject", header: "Was" },
-]
-const rowClass = () => {
-  return "bg-blue-100"
-}
 </script>
 
 <template>
@@ -94,23 +20,7 @@ const rowClass = () => {
         v-if="documentUnit"
         :document-unit="documentUnit as DocumentUnit"
       />
-      <div class="py-24">
-        <h2 class="ris-body1-bold pb-16">Historie:</h2>
-        <DataTable
-          :row-class="rowClass"
-          scroll-height="250px"
-          scrollable
-          :value="data"
-        >
-          <Column
-            v-for="col in columns"
-            :key="col.field"
-            :field="col.field"
-            :header="col.header"
-            header-class="ris-label2-bold text-gray-900"
-          ></Column>
-        </DataTable>
-      </div>
+      <DocumentUnitHistoryLog />
       <dl>
         <div class="flex gap-24 px-0">
           <dt class="ris-body1-bold shrink-0 grow-0 basis-160">
