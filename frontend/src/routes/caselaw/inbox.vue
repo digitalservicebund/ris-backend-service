eebd1f17807c6485a1bbac74a6a892c6668dd629
Leonie Koch <leonie.koch@digitalservice.bund.de>
2025-05-06 17:28:54 +0200

    Restructure into 'inbox' folder
    RISDEV-7653

--- frontend/src/routes/caselaw/inbox.vue
+++ frontend/src/routes/caselaw/inbox.vue
@@ -5,7 +5,7 @@ import TabPanel from "primevue/tabpanel"
 import TabPanels from "primevue/tabpanels"
 import Tabs from "primevue/tabs"
 import EURLexSearch from "@/components/eurlex/EURLexSearch.vue"
-import PendingHandover from "@/components/pending-handover/PendingHandover.vue"
+import PendingHandover from "@/components/inbox/pending-handover/PendingHandover.vue"
 </script>
 
 <template>
