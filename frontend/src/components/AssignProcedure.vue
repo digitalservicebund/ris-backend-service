982aa59f40ad394b698768e804742b3fc3b75eda
Kim Kern <kim.kern@digitalservice.bund.de>
2025-05-12 18:13:54 +0200

    Refactor grid -> flex
    RISDEV-7374

--- frontend/src/components/AssignProcedure.vue
+++ frontend/src/components/AssignProcedure.vue
@@ -22,29 +22,29 @@ const assignProcedures = () => {
 </script>
 
 <template>
-  <div
-    class="my-16 grid grid-cols-[270px_auto] grid-rows-[auto_auto] gap-x-8 gap-y-4 [grid-template-areas:'input_button''errors_errors']"
-  >
-    <ComboboxInput
-      id="procedure"
-      v-model="procedure"
-      aria-label="Vorgang auswählen"
-      class="[grid-area:input]"
-      :has-error="hasNoProcedureSelectedError"
-      :item-service="ComboboxItemService.getProcedures"
-      manual-entry
-      placeholder="Vorgang auswählen"
-    ></ComboboxInput>
-    <Button
-      aria-label="Zu Vorgang hinzufügen"
-      class="[grid-area:button]"
-      label="Zu Vorgang hinzufügen"
-      severity="secondary"
-      @click="assignProcedures"
-    />
+  <div class="my-16 flex flex-col gap-4 justify-self-end">
+    <div class="flex gap-8">
+      <ComboboxInput
+        id="procedure"
+        v-model="procedure"
+        aria-label="Vorgang auswählen"
+        class="min-w-[270px]"
+        :has-error="hasNoProcedureSelectedError"
+        :item-service="ComboboxItemService.getProcedures"
+        manual-entry
+        placeholder="Vorgang auswählen"
+      ></ComboboxInput>
+      <Button
+        aria-label="Zu Vorgang hinzufügen"
+        class="whitespace-nowrap"
+        label="Zu Vorgang hinzufügen"
+        severity="secondary"
+        @click="assignProcedures"
+      />
+    </div>
     <InputErrorMessages
       v-if="hasNoProcedureSelectedError"
-      class="[grid-area:errors]"
+      class="self-start"
       error-message="Wählen Sie einen Vorgang aus"
     />
   </div>
