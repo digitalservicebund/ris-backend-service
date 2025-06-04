82b5f235e1252dc9fbc031dca64bab70f5a71a58
Hanna Prinz <hanna.prinz@digitalservice.bund.de>
2025-06-04 10:49:17 +0200

    Update spring cloud kubernetes
    RISDEV-8001

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -11,7 +11,7 @@ import java.io.Serializable
 plugins {
     java
     jacoco
-    id("org.springframework.boot") version "3.4.5"
+    id("org.springframework.boot") version "3.5.0"
     id("io.spring.dependency-management") version "1.1.7"
     id("com.diffplug.spotless") version "7.0.4"
     id("org.sonarqube") version "6.2.0.5505"
