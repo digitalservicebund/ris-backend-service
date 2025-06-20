a25f400f626037baf0e996c72e0147c90a3b7b29
Leonie Koch <leonie.koch@digitalservice.bund.de>
2025-06-20 08:28:19 +0200

    Fix integration test
    RISDEV-7932

--- backend/src/test/java/de/bund/digitalservice/ris/caselaw/integration/tests/DocumentTypeIntegrationTest.java
+++ backend/src/test/java/de/bund/digitalservice/ris/caselaw/integration/tests/DocumentTypeIntegrationTest.java
@@ -70,7 +70,7 @@ class DocumentTypeIntegrationTest {
     risWebTestClient
         .withDefaultLogin()
         .get()
-        .uri("/api/v1/caselaw/documenttypes")
+        .uri("/api/v1/caselaw/documenttypes?category=CASELAW")
         .exchange()
         .expectStatus()
         .isOk()
@@ -92,7 +92,7 @@ class DocumentTypeIntegrationTest {
     risWebTestClient
         .withDefaultLogin()
         .get()
-        .uri("/api/v1/caselaw/documenttypes/dependent-literature")
+        .uri("/api/v1/caselaw/documenttypes?category=DEPENDENT_LITERATURE")
         .exchange()
         .expectStatus()
         .isOk()
@@ -112,7 +112,7 @@ class DocumentTypeIntegrationTest {
     risWebTestClient
         .withDefaultLogin()
         .get()
-        .uri("/api/v1/caselaw/documenttypes?q=Anord")
+        .uri("/api/v1/caselaw/documenttypes?q=Anord&category=CASELAW")
         .exchange()
         .expectStatus()
         .isOk()
@@ -132,7 +132,7 @@ class DocumentTypeIntegrationTest {
     risWebTestClient
         .withDefaultLogin()
         .get()
-        .uri("/api/v1/caselaw/documenttypes/dependent-literature?q=Ea")
+        .uri("/api/v1/caselaw/documenttypes?q=Ea&category=DEPENDENT_LITERATURE")
         .exchange()
         .expectStatus()
         .isOk()
