de7b53b9395c5da4d5ffb319768fea3229712d30
nbabai <nadav.babai@digitalservice.bund.de>
2025-07-02 09:25:57 +0200

    Remove wildcard
    RISDEV-7971

--- backend/src/test/java/de/bund/digitalservice/ris/caselaw/adapter/DatabasePatchMapperServiceTest.java
+++ backend/src/test/java/de/bund/digitalservice/ris/caselaw/adapter/DatabasePatchMapperServiceTest.java
@@ -1,7 +1,7 @@
 package de.bund.digitalservice.ris.caselaw.adapter;
 
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
