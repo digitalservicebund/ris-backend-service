154a137f2c03644e0dd5a92860f44b21718c86c8
Kim Kern <kim.kern@digitalservice.bund.de>
2025-07-03 16:07:33 +0200

    Fix unit test
    RISDEV-8382

--- backend/src/test/java/de/bund/digitalservice/ris/caselaw/adapter/transformer/ldml/pendingproceeding/PendingProceedingReducedLdmlTransformerTest.java
+++ backend/src/test/java/de/bund/digitalservice/ris/caselaw/adapter/transformer/ldml/pendingproceeding/PendingProceedingReducedLdmlTransformerTest.java
@@ -228,6 +228,18 @@ class PendingProceedingReducedLdmlTransformerTest {
                  <akn:motivation>
                     <akn:p alternativeTo="textWrapper">Rechtsfrage</akn:p>
                  </akn:motivation>
+                 <akn:introduction>
+                    <akn:block name="Rechtsmittelführer">
+                       <akn:embeddedStructure>
+                          <akn:p alternativeTo="textWrapper">Rechtsmittelführer</akn:p>
+                       </akn:embeddedStructure>
+                    </akn:block>
+                    <akn:block name="Rechtsmittelzulassung">
+                       <akn:embeddedStructure>
+                          <akn:p alternativeTo="textWrapper">Rechtsmittelzulassung</akn:p>
+                       </akn:embeddedStructure>
+                    </akn:block>
+                 </akn:introduction>
               </akn:judgmentBody>
            </akn:judgment>
         </akn:akomaNtoso>
