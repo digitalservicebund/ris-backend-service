13d3d848c8c087da5e0a31dde85038d6f5a9b46a
Nadine Kost <nadine.kost@digitalservice.bund.de>
2025-07-04 09:12:37 +0200

    Bring new folder structure to prototype portal
    RISDEV-7975

--- backend/src/main/java/de/bund/digitalservice/ris/caselaw/adapter/PrototypePortalPublicationService.java
+++ backend/src/main/java/de/bund/digitalservice/ris/caselaw/adapter/PrototypePortalPublicationService.java
@@ -2,9 +2,18 @@ package de.bund.digitalservice.ris.caselaw.adapter;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import de.bund.digitalservice.ris.caselaw.adapter.caselawldml.CaseLawLdml;
+import de.bund.digitalservice.ris.caselaw.adapter.database.jpa.AttachmentDTO;
+import de.bund.digitalservice.ris.caselaw.adapter.database.jpa.AttachmentRepository;
+import de.bund.digitalservice.ris.caselaw.adapter.exception.BucketException;
+import de.bund.digitalservice.ris.caselaw.adapter.exception.LdmlTransformationException;
 import de.bund.digitalservice.ris.caselaw.adapter.exception.PublishException;
+import de.bund.digitalservice.ris.caselaw.domain.Decision;
+import de.bund.digitalservice.ris.caselaw.domain.DocumentationUnit;
 import de.bund.digitalservice.ris.caselaw.domain.DocumentationUnitRepository;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 import java.util.stream.Collectors;
 import lombok.extern.slf4j.Slf4j;
@@ -16,10 +25,14 @@ public class PrototypePortalPublicationService extends CommonPortalPublicationSe
 
   private final RiiService riiService;
   private final DocumentationUnitRepository documentationUnitRepository;
+  private final AttachmentRepository attachmentRepository;
   private final PrototypePortalBucket prototypePortalBucket;
+  private final PortalTransformer ldmlTransformer;
+  private final XmlUtilService xmlUtilService;
 
   public PrototypePortalPublicationService(
       DocumentationUnitRepository documentationUnitRepository,
+      AttachmentRepository attachmentRepository,
       XmlUtilService xmlUtilService,
       PrototypePortalBucket prototypePortalBucket,
       ObjectMapper objectMapper,
@@ -33,7 +46,10 @@ public class PrototypePortalPublicationService extends CommonPortalPublicationSe
         portalTransformer);
     this.riiService = riiService;
     this.documentationUnitRepository = documentationUnitRepository;
+    this.attachmentRepository = attachmentRepository;
     this.prototypePortalBucket = prototypePortalBucket;
+    this.ldmlTransformer = portalTransformer;
+    this.xmlUtilService = xmlUtilService;
   }
 
   @Override
@@ -45,6 +61,67 @@ public class PrototypePortalPublicationService extends CommonPortalPublicationSe
   public void uploadChangelog(
       List<String> publishedDocumentNumbers, List<String> deletedDocumentNumbers) {
     // no-op in prototype - delta changelog uploads currently disabled
+  }
+
+  @Override
+  protected PortalPublicationResult publishToBucket(DocumentationUnit documentationUnit) {
+    if (!(documentationUnit instanceof Decision decision)) {
+      // for now pending proceedings can not be transformed to LDML, so they are ignored.
+      return null;
+    }
+    List<AttachmentDTO> attachments =
+        attachmentRepository.findAllByDocumentationUnitId(documentationUnit.uuid());
+    CaseLawLdml ldml = ldmlTransformer.transformToLdml(decision);
+    Optional<String> fileContent = xmlUtilService.ldmlToString(ldml);
+    if (fileContent.isEmpty()) {
+      throw new LdmlTransformationException("Could not parse transformed LDML as string.", null);
+    }
+
+    return saveToBucket(
+        ldml.getUniqueId() + "/", ldml.getFileName(), fileContent.get(), attachments);
+  }
+
+  protected PortalPublicationResult saveToBucket(
+      String path, String fileName, String fileContent, List<AttachmentDTO> attachments) {
+    try {
+      List<String> existingFiles = prototypePortalBucket.getAllFilenamesByPath(path);
+      List<String> addedFiles = new ArrayList<>();
+
+      prototypePortalBucket.save(path + fileName, fileContent);
+      prototypePortalBucket.save(
+          fileName, fileContent); // save in root as well to not break the live portal
+      addedFiles.add(path + fileName);
+
+      if (!attachments.isEmpty()) {
+        attachments.forEach(
+            attachment -> {
+              prototypePortalBucket.saveBytes(
+                  path + attachment.getFilename(), attachment.getContent());
+              addedFiles.add(path + attachment.getFilename());
+            });
+      }
+
+      // Check for files that are not part of this update and remove them (e.g. removed images)
+      existingFiles.removeAll(addedFiles);
+      existingFiles.forEach(prototypePortalBucket::delete);
+
+      return new PortalPublicationResult(addedFiles, existingFiles);
+    } catch (BucketException e) {
+      throw new PublishException("Could not save LDML to bucket.", e);
+    }
+  }
+
+  @Override
+  public PortalPublicationResult deleteDocumentationUnit(String documentNumber) {
+    try {
+      var deletableFiles = prototypePortalBucket.getAllFilenamesByPath(documentNumber + "/");
+      deletableFiles.forEach(prototypePortalBucket::delete);
+      prototypePortalBucket.delete(
+          documentNumber + ".xml"); // delete in root as well to not break the live portal
+      return new PortalPublicationResult(List.of(), deletableFiles);
+    } catch (BucketException e) {
+      throw new PublishException("Could not delete LDML from bucket.", e);
+    }
   }
 
   //                        ↓ day of month (1-31)
