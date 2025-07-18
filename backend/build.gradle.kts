d1a6666ad2bce1caeb718d1ac39cb54463c646c6
anne-ds <anne.schumann@digitalservice.bund.de>
2025-07-18 14:05:52 +0200

    Update schema version
    RISDEV-8557

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -206,9 +206,9 @@ dependencies {
     // or with local gradle project (look also into settings.gradle.kts)
     // implementation(project(":exporter"))
 
-    // implementation("de.bund.digitalservice:neuris-caselaw-migration-schema:0.0.70")
+    implementation("de.bund.digitalservice:neuris-caselaw-migration-schema:0.0.71")
     // for local development:
-    implementation(files("../../ris-data-migration/schema/build/libs/schema-0.0.71.jar"))
+ //   implementation(files("../../ris-data-migration/schema/build/libs/schema-0.0.71.jar"))
 
     implementation("com.fasterxml.jackson.core:jackson-core:2.19.1")
     implementation("com.fasterxml.jackson.core:jackson-databind:2.19.1")
