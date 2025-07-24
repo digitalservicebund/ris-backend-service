1f1429cc555d1888b52abcca59e7617ea648f6e7
Hanna Prinz <hanna.prinz@digitalservice.bund.de>
2025-07-24 15:12:41 +0200

    Fix integration tests
    RISDEV-8565

--- backend/src/test/resources/doc_office_init.sql
+++ backend/src/test/resources/doc_office_init.sql
@@ -2,18 +2,10 @@
 INSERT INTO
     incremental_migration.jurisdiction_type (id, label, juris_id)
 VALUES
-    ('27e099f9-5b47-4ce9-ac58-b84ca4643bc2', 'Ordentliche Gerichtsbarkeit', '1'),
-    ('33ed4b4c-b4b8-44b1-a6bd-3144b5775f36', 'Sozialgerichtsbarkeit', '2'),
-    ('4eb1e9e5-70e0-42d8-8348-98b1543b9455', 'Verfassungsgerichtsbarkeit', '3') ON CONFLICT
+    ('27e099f9-5b47-4ce9-ac58-b84ca4643bc2', 'Ordentliche Gerichtsbarkeit', '1') ON CONFLICT
     DO
     NOTHING;
 
-INSERT INTO
-    incremental_migration.documentation_office (id, abbreviation, jurisdiction_type_id)
-VALUES
-    ('41e62dbc-e5b6-414f-91e2-0cfe559447d1', 'BGH', '27e099f9-5b47-4ce9-ac58-b84ca4643bc2'),
-    ('f13c2fdb-5323-49aa-bc6d-09fa68c3acb9', 'CC-RIS', null),
-    ('1baf3d1f-b800-4a65-badd-80c84cb38da9', 'BFH', null),
-    ('ba90a851-3c54-4858-b4fa-7742ffbe8f05', 'DS', null) ON CONFLICT
-    DO
-    NOTHING;
+UPDATE incremental_migration.documentation_office
+SET jurisdiction_type_id = '27e099f9-5b47-4ce9-ac58-b84ca4643bc2'
+WHERE id = 'bd350c93-7ff0-4409-9c62-371e3d0c749e';
