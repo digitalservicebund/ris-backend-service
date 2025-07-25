37421c745ea2c4d1840040d02bf485f6886c734b
anne-ds <anne.schumann@digitalservice.bund.de>
2025-07-25 10:21:55 +0200

    Fix fe tests (WIP)
    RISDEV-8679

---
+++ frontend/test/components/foreignLanguageVersions.spec.ts
@@ -0,0 +1,56 @@
+import { createTestingPinia } from "@pinia/testing"
+import { render, screen } from "@testing-library/vue"
+import { describe, it, expect } from "vitest"
+import { createRouter, createWebHistory } from "vue-router"
+import ForeignLanguageVersions from "@/components/ForeignLanguageVersions.vue"
+import { Decision } from "@/domain/decision"
+import ForeignLanguageVersion from "@/domain/foreignLanguageVersion"
+import routes from "~/test-helper/routes"
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: routes,
+})
+
+function renderComponent() {
+  return {
+    ...render(ForeignLanguageVersions, {
+      props: { label: "Fremdsprachige Fassung" },
+      global: {
+        plugins: [
+          [router],
+          [
+            createTestingPinia({
+              initialState: {
+                docunitStore: {
+                  documentUnit: new Decision("123", {
+                    documentNumber: "foo",
+                    contentRelatedIndexing: {
+                      foreignLanguageVersions: [
+                        new ForeignLanguageVersion({
+                          languageCode: { id: "en", label: "Englisch" },
+                          link: "https://link-to-tranlsation.en",
+                        }),
+                      ],
+                    },
+                  }),
+                },
+              },
+            }),
+          ],
+        ],
+      },
+    }),
+  }
+}
+
+describe("ForeignLanguageVersions.vue", () => {
+  it("should display label", () => {
+    renderComponent()
+
+    expect(screen.getByText("Fremdsprachige Fassung")).toBeVisible()
+    expect(
+      screen.getByText("Englisch: https://link-to-tranlsation.en"),
+    ).toBeVisible()
+  })
+})
