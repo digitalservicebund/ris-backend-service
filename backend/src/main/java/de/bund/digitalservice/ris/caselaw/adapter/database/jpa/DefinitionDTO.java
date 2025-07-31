f7b366277de6df1d427872e791a4dc5eb69499c1
Kim Kern <kim.kern@digitalservice.bund.de>
2025-07-31 14:02:07 +0200

    Remove unused hashCode/equals
    RISDEV-6688

--- backend/src/main/java/de/bund/digitalservice/ris/caselaw/adapter/database/jpa/DefinitionDTO.java
+++ backend/src/main/java/de/bund/digitalservice/ris/caselaw/adapter/database/jpa/DefinitionDTO.java
@@ -8,7 +8,6 @@ import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import java.util.Objects;
 import java.util.UUID;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -16,7 +15,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
-import org.hibernate.proxy.HibernateProxy;
 
 @Getter
 @Setter
@@ -39,29 +37,4 @@ public class DefinitionDTO {
   private Long borderNumber;
 
   @Column @NotNull private Long rank;
-
-  @Override
-  @SuppressWarnings("java:S2097") // Class type check is not recognized by Sonar
-  public final boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null) return false;
-    Class<?> oEffectiveClass =
-        o instanceof HibernateProxy hibernateProxy
-            ? hibernateProxy.getHibernateLazyInitializer().getPersistentClass()
-            : o.getClass();
-    Class<?> thisEffectiveClass =
-        this instanceof HibernateProxy hibernateProxy
-            ? hibernateProxy.getHibernateLazyInitializer().getPersistentClass()
-            : this.getClass();
-    if (thisEffectiveClass != oEffectiveClass) return false;
-    DefinitionDTO that = (DefinitionDTO) o;
-    return getId() != null && Objects.equals(getId(), that.getId());
-  }
-
-  @Override
-  public final int hashCode() {
-    return this instanceof HibernateProxy hibernateProxy
-        ? hibernateProxy.getHibernateLazyInitializer().getPersistentClass().hashCode()
-        : getClass().hashCode();
-  }
 }
