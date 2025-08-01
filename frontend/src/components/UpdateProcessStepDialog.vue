3283e8473ee7afd1c51ec7f526064561e2102cf4
Ralph von Petersdorff-Campen <ralph.vonpetersdorffcampen@digitalservice.bund.de>
2025-08-01 16:57:41 +0200

    Add process step drop box
    Add also combobox for user selection
    
    RISDEV-8749

--- frontend/src/components/UpdateProcessStepDialog.vue
+++ frontend/src/components/UpdateProcessStepDialog.vue
@@ -2,11 +2,14 @@
 import { storeToRefs } from "pinia"
 import Button from "primevue/button"
 import Dialog from "primevue/dialog"
+import Select from "primevue/select"
 import { onMounted, Ref, ref } from "vue"
-import IconBadge from "@/components/IconBadge.vue"
-import { useProcessStepBadge } from "@/composables/useProcessStepBadge"
+import ComboboxInput from "@/components/ComboboxInput.vue"
+import InputField from "@/components/input/InputField.vue"
+import { ComboboxItem } from "@/components/input/types"
 import { DocumentationUnit } from "@/domain/documentationUnit"
 import ProcessStep from "@/domain/processStep"
+import ComboboxItemService from "@/services/comboboxItemService"
 import processStepService from "@/services/processStepService"
 import { useDocumentUnitStore } from "@/stores/documentUnitStore"
 import IconApprovalDelegation from "~icons/material-symbols/approval-delegation-outline"
@@ -16,8 +19,13 @@ const { documentUnit } = storeToRefs(store) as {
   documentUnit: Ref<DocumentationUnit | undefined>
 }
 const showDialog = ref(false)
-
+const selectedUser = ref<ComboboxItem>()
+const processSteps = ref<ProcessStep[]>()
 let nextProcessStep: ProcessStep | undefined
+
+onMounted(async () => {
+  processSteps.value = (await processStepService.getProcessSteps()).data
+})
 
 async function triggerUpdateProcessStep(): Promise<void> {
   if (documentUnit.value?.uuid)
@@ -29,7 +37,9 @@ async function triggerUpdateProcessStep(): Promise<void> {
 
 async function updateProcessStep(): Promise<void> {
   if (nextProcessStep)
-    documentUnit.value!.currentProcessStep = { processStep: nextProcessStep }
+    documentUnit.value!.currentProcessStep = {
+      processStep: nextProcessStep,
+    }
   await store.updateDocumentUnit()
   showDialog.value = false
 }
@@ -59,32 +69,49 @@ onMounted(async () => {
     modal
     :visible="showDialog"
   >
-    <div v-if="nextProcessStep" class="mb-12 flex items-center">
-      Nächster Schritt:
-      <IconBadge
-        :background-color="
-          useProcessStepBadge(nextProcessStep).value.backgroundColor
-        "
-        :border-color="useProcessStepBadge(nextProcessStep).value.borderColor"
-        :label="nextProcessStep.name"
-      />
-    </div>
+    <div class="flex w-full flex-col pt-32">
+      <div class="flex gap-32">
+        <div class="flex-1">
+          <InputField id="nextProcessStep" label="Neuer Schritt">
+            <Select
+              v-model="nextProcessStep"
+              aria-label="Neuer Schritt"
+              class="w-full"
+              option-label="name"
+              :options="processSteps"
+            ></Select>
+          </InputField>
+        </div>
+        <div class="flex-1">
+          <InputField id="processStepPerson" label="Neue Person">
+            <ComboboxInput
+              id="processStepPerson"
+              v-model="selectedUser"
+              aria-label="Neue Person"
+              :item-service="ComboboxItemService.getUsersForDocOffice"
+            ></ComboboxInput>
+          </InputField>
+        </div>
+      </div>
 
-    <div class="modal-buttons-container flex w-full flex-row gap-[1rem]">
-      <Button
-        aria-label="Weitergeben"
-        label="Weitergeben"
-        severity="primary"
-        size="small"
-        @click="updateProcessStep"
-      ></Button>
-      <Button
-        aria-label="Abbrechen"
-        label="Abbrechen"
-        severity="secondary"
-        size="small"
-        @click="showDialog = false"
-      ></Button>
+      <div
+        class="modal-buttons-container flex w-full flex-row gap-[1rem] pt-32"
+      >
+        <Button
+          aria-label="Weitergeben"
+          label="Weitergeben"
+          severity="primary"
+          size="small"
+          @click="updateProcessStep"
+        ></Button>
+        <Button
+          aria-label="Abbrechen"
+          label="Abbrechen"
+          severity="secondary"
+          size="small"
+          @click="showDialog = false"
+        ></Button>
+      </div>
     </div>
   </Dialog>
 </template>
