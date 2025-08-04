58c65c7d73b45a5555bce5a45150cd9d06b815b4
Hanna Prinz <hanna.prinz@digitalservice.bund.de>
2025-08-04 16:43:51 +0200

    ProcessStepDialog Refactoring
    RISDEV-8702

--- frontend/src/components/UpdateProcessStepDialog.vue
+++ frontend/src/components/UpdateProcessStepDialog.vue
@@ -3,7 +3,7 @@ import { storeToRefs } from "pinia"
 import Button from "primevue/button"
 import Dialog from "primevue/dialog"
 import Select from "primevue/select"
-import { onMounted, Ref, ref } from "vue"
+import { Ref, ref, watch } from "vue"
 import ComboboxInput from "@/components/ComboboxInput.vue"
 import InputField from "@/components/input/InputField.vue"
 import { ComboboxItem } from "@/components/input/types"
@@ -12,61 +12,58 @@ import ProcessStep from "@/domain/processStep"
 import ComboboxItemService from "@/services/comboboxItemService"
 import processStepService from "@/services/processStepService"
 import { useDocumentUnitStore } from "@/stores/documentUnitStore"
-import IconApprovalDelegation from "~icons/material-symbols/approval-delegation-outline"
+
+const props = defineProps<{
+  showDialog: boolean
+}>()
+
+const emit = defineEmits<{
+  closeDialog: []
+}>()
 
 const store = useDocumentUnitStore()
 const { documentUnit } = storeToRefs(store) as {
-  documentUnit: Ref<DocumentationUnit | undefined>
+  documentUnit: Ref<DocumentationUnit>
 }
-const showDialog = ref(false)
 const selectedUser = ref<ComboboxItem>()
-
-let processSteps: ProcessStep[] | undefined
-let nextProcessStep: ProcessStep | undefined
-
-async function triggerUpdateProcessStep(): Promise<void> {
-  if (documentUnit.value?.uuid) {
-    processSteps = (await processStepService.getProcessSteps()).data
-    nextProcessStep = (
-      await processStepService.getNextProcessStep(documentUnit.value?.uuid)
-    ).data
-  }
-  showDialog.value = true
-}
+const processSteps = ref<ProcessStep[]>()
+const nextProcessStep = ref<ProcessStep>()
 
 async function updateProcessStep(): Promise<void> {
-  if (nextProcessStep)
+  if (nextProcessStep.value)
     documentUnit.value!.currentProcessStep = {
-      processStep: nextProcessStep,
+      processStep: nextProcessStep.value,
     }
   await store.updateDocumentUnit()
-  showDialog.value = false
+  emit("closeDialog")
 }
 
-onMounted(async () => {
-  await store.loadDocumentUnit
-})
+// The logic you want to run every time the dialog is shown
+const fetchData = async () => {
+  processSteps.value = (await processStepService.getProcessSteps()).data
+  nextProcessStep.value = (
+    await processStepService.getNextProcessStep(documentUnit.value.uuid)
+  ).data
+}
+
+watch(
+  () => props.showDialog,
+  async (newValue) => {
+    if (newValue) {
+      await fetchData()
+    }
+  },
+  { immediate: true },
+)
 </script>
 
 <template>
-  <Button
-    v-tooltip.bottom="'Dokumentationseinheit weitergeben'"
-    aria-label="Dokumentationseinheit weitergeben"
-    severity="secondary"
-    size="small"
-    @click="triggerUpdateProcessStep"
-  >
-    <template #icon>
-      <IconApprovalDelegation />
-    </template>
-  </Button>
-
   <Dialog
     class="max-h-[768px] max-w-[1024px]"
     :closable="false"
     header="Dokumentationseinheit weitergeben"
     modal
-    :visible="showDialog"
+    :visible="props.showDialog"
   >
     <div class="flex w-full flex-col pt-32">
       <div class="flex gap-32">
@@ -108,7 +105,7 @@ onMounted(async () => {
           label="Abbrechen"
           severity="secondary"
           size="small"
-          @click="showDialog = false"
+          @click="$emit('closeDialog')"
         ></Button>
       </div>
     </div>
