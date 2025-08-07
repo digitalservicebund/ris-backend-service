0b57fb0f9c7c1059b8558eebe829988f4014f392
nbabai <nadav.babai@digitalservice.bund.de>
2025-08-07 13:40:03 +0200

    Fix tests, extract string method
    RISDEV-8868

--- frontend/src/components/AssigneeBadge.vue
+++ frontend/src/components/AssigneeBadge.vue
@@ -1,23 +1,15 @@
 <script setup lang="ts">
+import StringsUtil from "@/utils/stringsUtil"
 import IconPermIdentity from "~icons/ic/baseline-perm-identity"
 
 defineProps<{
   name?: string
 }>()
-
-function getInitials(name?: string): string {
-  if (!name) return "-"
-
-  const words = name.trim().toUpperCase().split(/\s+/).filter(Boolean)
-  if (words.length === 0) return "-"
-  if (words.length === 1) return words[0].charAt(0)
-  return words[0].charAt(0) + words[words.length - 1].charAt(0)
-}
 </script>
 
 <template>
   <div class="flex flex-row gap-8">
     <IconPermIdentity />
-    {{ getInitials(name) }}
+    {{ StringsUtil.getFirstAndLastChars(name) }}
   </div>
 </template>
