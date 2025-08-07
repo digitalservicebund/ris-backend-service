a2118754da78f1ef9ce80724aaa3b13241072f72
nbabai <nadav.babai@digitalservice.bund.de>
2025-08-07 13:29:25 +0200

    Init name getInitials
    RISDEV-8868

---
+++ frontend/src/components/AssigneeBadge.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import IconPermIdentity from "~icons/ic/baseline-perm-identity"
+
+defineProps<{
+  name?: string
+}>()
+
+function getInitials(name?: string): string {
+  if (!name) return "-"
+
+  const words = name.trim().toUpperCase().split(/\s+/).filter(Boolean)
+  if (words.length === 0) return "-"
+  if (words.length === 1) return words[0].charAt(0)
+  return words[0].charAt(0) + words[words.length - 1].charAt(0)
+}
+</script>
+
+<template>
+  <div class="flex flex-row gap-8">
+    <IconPermIdentity />
+    {{ getInitials(name) }}
+  </div>
+</template>
