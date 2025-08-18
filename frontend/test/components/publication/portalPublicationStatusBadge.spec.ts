561bb57ce96930bcfea19e4a604ec509b8c3b2e6
Kim Kern <kim.kern@digitalservice.bund.de>
2025-08-18 16:00:48 +0200

    Update portal publish badge
    RISDEV-8456

--- frontend/test/components/publication/portalPublicationStatusBadge.spec.ts
+++ frontend/test/components/publication/portalPublicationStatusBadge.spec.ts
@@ -15,6 +15,10 @@ describe("PortalPublicationStatusBadge", () => {
     await renderComponent(PortalPublicationStatus.WITHDRAWN)
     expect(screen.getByText("Zurückgezogen")).toBeInTheDocument()
   })
+  it("should not show anything on empty status", async () => {
+    await renderComponent("Unknown" as unknown as PortalPublicationStatus)
+    expect(screen.getByText("Unbekannter Status")).toBeInTheDocument()
+  })
 })
 
 async function renderComponent(status: PortalPublicationStatus) {
