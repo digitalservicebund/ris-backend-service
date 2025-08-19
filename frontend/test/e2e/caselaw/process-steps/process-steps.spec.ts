b8c831aa42cdfa9397939a7c2da20da424ca1f23
nbabai <nadav.babai@digitalservice.bund.de>
2025-08-19 11:17:51 +0200

    Add tags
    RISDEV-8565

--- frontend/test/e2e/caselaw/process-steps/process-steps.spec.ts
+++ frontend/test/e2e/caselaw/process-steps/process-steps.spec.ts
@@ -2,7 +2,7 @@ import { expect, Page } from "@playwright/test"
 import { caselawTest as test } from "~/e2e/caselaw/fixtures"
 import { navigateToSearch } from "~/e2e/caselaw/utils/e2e-utils"
 
-test.describe("process steps", () => {
+test.describe("process steps", { tag: ["@RISDEV-8565"] }, () => {
   test("rendering initial state, click on 'Weitergeben'", async ({
     pageWithBghUser,
   }) => {
