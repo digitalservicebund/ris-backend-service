d9559464018f53ed247e274c9f2750648721e56c
Leonie Koch <leonie.koch@digitalservice.bund.de>
2025-08-19 17:04:08 +0200

    Revert "Try longer test timeouts"
    This reverts commit 807f82993d4908232ea94befa688d5d565282aae.

--- frontend/test/e2e/caselaw/decision/managementdata/assign-doc-office-in-managementdata.spec.ts
+++ frontend/test/e2e/caselaw/decision/managementdata/assign-doc-office-in-managementdata.spec.ts
@@ -97,7 +97,6 @@ test.describe(
       })
 
       await test.step("Nach erfolgreichem Zuweisen wird zur Startseite weitergeleitet", async () => {
-        await page.getByTestId("document-unit-search-entry-form").waitFor()
         await expect(page).toHaveURL("/caselaw/search")
       })
 
