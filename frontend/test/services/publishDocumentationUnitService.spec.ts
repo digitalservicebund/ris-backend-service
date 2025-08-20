6a733ceb98dead5b2a63ce45ed3f262c0ac6b6f2
Kim Kern <kim.kern@digitalservice.bund.de>
2025-08-20 16:32:55 +0200

    Fix service test
    RISDEV-8456

--- frontend/test/services/publishDocumentationUnitService.spec.ts
+++ frontend/test/services/publishDocumentationUnitService.spec.ts
@@ -14,27 +14,28 @@ vi.mock("@/services/httpClient", () => {
 })
 
 describe("publishDocumentationUnitService", () => {
-  describe("publish", () => {})
-  it("returns error on failure", async () => {
-    putMock.mockReturnValue({
-      status: 400,
-      error: { message: "invalid request" },
+  describe("publish", () => {
+    it("returns error on failure", async () => {
+      putMock.mockReturnValue({
+        status: 400,
+        error: { message: "invalid request" },
+      })
+      const result = await service.publishDocument("123")
+      expect(result.error?.title).toEqual(
+        "Fehler beim Veröffentlichen der Dokumentationseinheit",
+      )
+      expect(result.error?.description).toContain(
+        "Die Dokumentationseinheit konnte nicht veröffentlicht werden",
+      )
     })
-    const result = await service.publishDocument("123")
-    expect(result.error?.title).toEqual(
-      "Fehler beim Veröffentlichen der Dokumentationseinheit",
-    )
-    expect(result.error?.description).toContain(
-      "Die Dokumentationseinheit konnte nicht veröffentlicht werden",
-    )
-  })
 
-  it("returns data on success", async () => {
-    putMock.mockReturnValue({
-      status: 200,
-      data: { message: "success" },
+    it("returns data on success", async () => {
+      putMock.mockReturnValue({
+        status: 200,
+        data: { message: "success" },
+      })
+      const result = await service.publishDocument("123")
+      expect(result.data).toEqual({ message: "success" })
     })
-    const result = await service.publishDocument("123")
-    expect(result.data).toEqual({ message: "success" })
   })
 })
