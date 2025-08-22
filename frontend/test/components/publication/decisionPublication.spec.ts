a4a65cca03a80e59eead50cd192f660a21de4dbf
anne-ds <anne.schumann@digitalservice.bund.de>
2025-08-22 12:01:52 +0200

    Fix fe test
    RISDEV-8843

--- frontend/test/components/publication/decisionPublication.spec.ts
+++ frontend/test/components/publication/decisionPublication.spec.ts
@@ -11,120 +11,100 @@ import { useDocumentUnitStore } from "@/stores/documentUnitStore"
 import { useFeatureToggleServiceMock } from "~/test-helper/useFeatureToggleServiceMock"
 import routes from "~pages"
 
-const getPreviewMock = vi.spyOn(publishDocumentationUnitService, "getPreview")
+const previewMock = vi.spyOn(publishDocumentationUnitService, "getPreview")
 
 describe("DecisionPlausibilityCheck", () => {
   beforeEach(() => {
     setActivePinia(createTestingPinia())
     useFeatureToggleServiceMock()
-  })
-  afterEach(() => {
-    vi.resetAllMocks()
-  })
-
-  it("should not show LDML preview if plausibility check fails", async () => {
-    await renderComponent({ hasPlausibilityCheckPassed: false })
-
-    // Click simulates updating the plausibility check with mock
-    await fireEvent.click(screen.getByText("DecisionPlausibilityCheck"))
-
-    expect(screen.getByText("DecisionPlausibilityCheck")).toBeInTheDocument()
-    expect(
-      screen.getByText("PublicationActions - publishable: false"),
-    ).toBeInTheDocument()
-    expect(screen.queryByText("LDML Vorschau")).not.toBeInTheDocument()
-    expect(getPreviewMock).not.toHaveBeenCalled()
-  })
-
-  it("should render all child components when plausibility check is true", async () => {
-    getPreviewMock.mockResolvedValue({
+    previewMock.mockResolvedValue({
       status: 200,
       data: {
         ldml: "ldml",
         success: true,
       },
     })
-    await renderComponent({ hasPlausibilityCheckPassed: true })
+  })
+  afterEach(() => {
+    vi.resetAllMocks()
+  })
 
-    // Click simulates updating the plausibility check with mock
-    await fireEvent.click(screen.getByText("DecisionPlausibilityCheck"))
+  it("should render all child components when plausibility check is true", async () => {
+    await renderComponent(validDocument)
 
-    expect(screen.getByText("DecisionPlausibilityCheck")).toBeInTheDocument()
+    expect(
+      screen.getByText("Alle Pflichtfelder sind korrekt ausgefüllt."),
+    ).toBeInTheDocument()
     expect(
       screen.getByText("PublicationActions - publishable: true"),
     ).toBeInTheDocument()
-    expect(screen.getByText("LDML Vorschau")).toBeInTheDocument()
+    expect(await screen.findByText("LDML Vorschau")).toBeInTheDocument()
+    expect(previewMock).toHaveBeenCalled()
   })
+
   describe("ldml preview", () => {
     it("should display ldml preview whit plausible data", async () => {
-      getPreviewMock.mockResolvedValue({
-        status: 200,
-        data: {
-          ldml: "ldml",
-          success: true,
-        },
-      })
-      await renderComponent({ hasPlausibilityCheckPassed: true })
+      await renderComponent(validDocument)
 
-      // Click simulates updating the plausibility check with mock
-      await fireEvent.click(screen.getByText("DecisionPlausibilityCheck"))
+      expect(await screen.findByText("LDML Vorschau")).toBeInTheDocument()
+
       // Expand preview
       await fireEvent.click(screen.getByLabelText("Aufklappen"))
-      expect(screen.getByTestId("code-snippet")).toBeInTheDocument()
+      expect(await screen.findByTestId("code-snippet")).toBeInTheDocument()
     })
 
     it("should not display ldml preview with implausible data", async () => {
-      await renderComponent({ hasPlausibilityCheckPassed: false })
+      await renderComponent(invalidDocument)
 
       expect(screen.queryByText("LDML Vorschau")).not.toBeInTheDocument()
-      expect(getPreviewMock).not.toHaveBeenCalled()
+      expect(previewMock).not.toHaveBeenCalled()
     })
 
     it("should show error when ldml preview cannot be loaded", async () => {
       const description =
         "Die LDML-Vorschau konnte nicht geladen werden: Aktuelle Fehlermeldung."
-      getPreviewMock.mockResolvedValue({
+      previewMock.mockResolvedValue({
         status: 422,
         error: {
           title: "Fehler beim Laden der LDML-Vorschau",
           description: description,
         },
       })
-      await renderComponent({ hasPlausibilityCheckPassed: true })
+      await renderComponent(validDocument)
 
-      // Click simulates updating the plausibility check with mock
-      await fireEvent.click(screen.getByText("DecisionPlausibilityCheck"))
-
-      expect(screen.getByText(description)).toBeInTheDocument()
+      expect(await screen.findByText(description)).toBeInTheDocument()
     })
   })
 })
 
-const DecisionPlausibilityCheck = (hasPlausibilityCheckPassed: boolean) => ({
-  template: `<span @click="$emit('update-plausibility-check', ${hasPlausibilityCheckPassed})">DecisionPlausibilityCheck</span>`,
-})
 const PublicationActions = {
   props: ["isPublishable"],
   template: `<span>PublicationActions - publishable: {{ isPublishable }}</span>`,
 }
-function mockDocUnitStore() {
+const invalidDocument = new Decision("q834", {
+  documentNumber: "original",
+})
+const validDocument = new Decision("q834", {
+  documentNumber: "original",
+  coreData: {
+    fileNumbers: ["IZ 1234"],
+    court: { label: "BGH" },
+    decisionDate: "2024-01-01",
+    documentType: { label: "Urteil", jurisShortcut: "U" },
+    legalEffect: "unbestimmt",
+  },
+})
+function mockDocUnitStore(document: Decision) {
   const mockedSessionStore = useDocumentUnitStore()
-  mockedSessionStore.documentUnit = new Decision("q834", {
-    documentNumber: "original",
-  })
-
+  mockedSessionStore.documentUnit = document
   return mockedSessionStore
 }
-async function renderComponent(
-  { hasPlausibilityCheckPassed }: { hasPlausibilityCheckPassed: boolean } = {
-    hasPlausibilityCheckPassed: true,
-  },
-) {
+async function renderComponent(document: Decision) {
   const router = createRouter({
     history: createWebHistory(),
     routes: routes,
   })
-  const store = mockDocUnitStore()
+  const store = mockDocUnitStore(document)
   await router.push({
     name: "caselaw-documentUnit-documentNumber-publication",
     params: { documentNumber: "KORE123412345" },
@@ -132,9 +112,6 @@ async function renderComponent(
   render(DecisionPublication, {
     global: {
       stubs: {
-        DecisionPlausibilityCheck: DecisionPlausibilityCheck(
-          hasPlausibilityCheckPassed,
-        ),
         PublicationActions,
       },
       plugins: [router],
