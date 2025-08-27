5e92c397f8c14abf55cfe7c7f0525b2cb8daceee
nbabai <nadav.babai@digitalservice.bund.de>
2025-08-27 14:53:32 +0200

    Log only user id
    RISDEV-9052

--- backend/src/main/java/de/bund/digitalservice/ris/caselaw/domain/User.java
+++ backend/src/main/java/de/bund/digitalservice/ris/caselaw/domain/User.java
@@ -3,6 +3,7 @@ package de.bund.digitalservice.ris.caselaw.domain;
 import java.util.List;
 import java.util.UUID;
 import lombok.Builder;
+import org.jetbrains.annotations.NotNull;
 
 @Builder
 public record User(
@@ -11,4 +12,11 @@ public record User(
     String email,
     DocumentationOffice documentationOffice,
     List<String> roles,
-    String initials) {}
+    String initials) {
+
+  @NotNull
+  @Override
+  public String toString() {
+    return "User[id=%s]".formatted(id);
+  }
+}
