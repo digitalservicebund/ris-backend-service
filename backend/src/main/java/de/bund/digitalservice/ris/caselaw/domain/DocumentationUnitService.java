c23dd57d99b98635600c8e0b81c6530d82f66d80
nbabai <nadav.babai@digitalservice.bund.de>
2025-08-27 15:00:30 +0200

    Fix duplicate error
    RISDEV-9052

--- backend/src/main/java/de/bund/digitalservice/ris/caselaw/domain/DocumentationUnitService.java
+++ backend/src/main/java/de/bund/digitalservice/ris/caselaw/domain/DocumentationUnitService.java
@@ -19,14 +19,13 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
 import java.util.UUID;
 import java.util.function.BooleanSupplier;
-import java.util.function.Function;
-import java.util.stream.Collectors;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.coyote.BadRequestException;
 import org.springframework.context.annotation.Lazy;
@@ -478,9 +477,10 @@ public class DocumentationUnitService {
   private Slice<DocumentationUnitListItem> retrieveCurrentProcessStepUser(
       Slice<DocumentationUnitListItem> documentationUnitListItems, OidcUser oidcUser) {
 
-    Map<UUID, User> userIdMap =
-        userService.getUsers(oidcUser).stream()
-            .collect(Collectors.toMap(User::id, Function.identity()));
+    Map<UUID, User> userIdMap = new HashMap<>();
+    for (User user : userService.getUsers(oidcUser)) {
+      userIdMap.put(user.id(), user);
+    }
 
     return documentationUnitListItems.map(
         item -> {
