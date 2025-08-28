098a4709edc6a29185b0e62480720e326ddf55b4
nbabai <nadav.babai@digitalservice.bund.de>
2025-08-28 12:45:52 +0200

    Fix styling
    RISDEV-0000

--- frontend/test/e2e/caselaw/process-steps/process-steps.spec.ts
+++ frontend/test/e2e/caselaw/process-steps/process-steps.spec.ts
@@ -1,6 +1,9 @@
 import { expect, Page } from "@playwright/test"
 import { caselawTest as test } from "~/e2e/caselaw/fixtures"
-import { navigateToCategories, navigateToManagementData } from "~/e2e/caselaw/utils/e2e-utils"
+import {
+  navigateToCategories,
+  navigateToManagementData,
+} from "~/e2e/caselaw/utils/e2e-utils"
 
 // eslint-disable-next-line playwright/no-skipped-test
 test.skip("process steps", { tag: ["@RISDEV-8565"] }, () => {
