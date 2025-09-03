3581c542d2fd21f6fc34d15eebf8deaded99246a
Hanna Prinz <hanna.prinz@digitalservice.bund.de>
2025-09-03 12:50:37 +0200

    Review changes
    RISDEV-8923

--- backend/src/main/java/de/bund/digitalservice/ris/caselaw/adapter/DocumentationUnitController.java
+++ backend/src/main/java/de/bund/digitalservice/ris/caselaw/adapter/DocumentationUnitController.java
@@ -1,6 +1,5 @@
 package de.bund.digitalservice.ris.caselaw.adapter;
 
-import com.gravity9.jsonpatch.JsonPatch;
 import de.bund.digitalservice.ris.caselaw.adapter.eurlex.EurLexSOAPSearchService;
 import de.bund.digitalservice.ris.caselaw.adapter.exception.LdmlTransformationException;
 import de.bund.digitalservice.ris.caselaw.adapter.transformer.DocumentationUnitTransformerException;
@@ -372,7 +371,7 @@ public class DocumentationUnitController {
   }
 
   /**
-   * Update a documentation unit with a {@link JsonPatch} object.
+   * Update a documentation unit with a {@link com.gravity9.jsonpatch.JsonPatch} object.
    *
    * @param uuid id of the documentation unit
    * @param patch patch with the change operations
