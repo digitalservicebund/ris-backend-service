66704fc977927ca6b0d7ed8e158c31be2b0c9b23
nbabai <nadav.babai@digitalservice.bund.de>
2025-09-11 13:21:38 +0200

    Bump migration service
    RISDEV-9193

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -218,7 +218,7 @@ dependencies {
     // or with local gradle project (look also into settings.gradle.kts)
     // implementation(project(":exporter"))
 
-    implementation("de.bund.digitalservice:neuris-caselaw-migration-schema:0.0.95")
+    implementation("de.bund.digitalservice:neuris-caselaw-migration-schema:0.0.96")
     // for local development:
     // implementation(files("../../ris-data-migration/schema/build/libs/schema-0.0.95.jar"))
 
