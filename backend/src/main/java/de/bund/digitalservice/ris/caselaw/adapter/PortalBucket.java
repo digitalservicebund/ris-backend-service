eb2439fb738d8d3c228da257a6776fe0575449c6
Nadine Kost <nadine.kost@digitalservice.bund.de>
2025-09-23 16:20:49 +0200

    Revert production bucket changes
    RISDEV-0000

--- backend/src/main/java/de/bund/digitalservice/ris/caselaw/adapter/PortalBucket.java
+++ backend/src/main/java/de/bund/digitalservice/ris/caselaw/adapter/PortalBucket.java
@@ -2,12 +2,10 @@ package de.bund.digitalservice.ris.caselaw.adapter;
 
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 import software.amazon.awssdk.services.s3.S3Client;
 
 @Component
-@Profile("!production")
 public class PortalBucket extends S3Bucket {
 
   public PortalBucket(
