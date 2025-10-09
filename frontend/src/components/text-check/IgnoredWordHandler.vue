f28b89eec05fbd46e40bab424f52ac1afe56cf3a
Dusan <dusan.vorkapic@digitalservice.bund.de>
2025-10-09 19:44:52 +0200

    Remove suggestion bar
    RISDEV-9288

--- frontend/src/components/text-check/IgnoredWordHandler.vue
+++ frontend/src/components/text-check/IgnoredWordHandler.vue
@@ -3,22 +3,32 @@ import Button from "primevue/button"
 import { computed } from "vue"
 import { useFeatureToggle } from "@/composables/useFeatureToggle"
 import { Match } from "@/types/textCheck"
+import IconDescription from "~icons/ic/baseline-description"
+import IconSpellCheck from "~icons/material-symbols/spellcheck"
+import IconBookOpenPageVariant from "~icons/mdi/book-open-page-variant"
+import IconBookOpenVariant from "~icons/mdi/book-open-variant"
 
 const props = defineProps<{
   match: Match
 }>()
 
 const emit = defineEmits<{
+  "ignored-word:add": [void]
   "ignored-word:remove": [string]
   "globally-ignored-word:remove": [string]
   "globally-ignored-word:add": [string]
 }>()
+
+function addIgnoredWord() {
+  emit("ignored-word:add")
+}
 
 async function removeWord() {
   emit("ignored-word:remove", props.match.word)
 }
 
 function addIgnoredWordGlobally() {
+  emit("ignored-word:remove", props.match.word)
   emit("globally-ignored-word:add", props.match.word)
 }
 
@@ -30,67 +40,92 @@ const textCheckGlobal = useFeatureToggle("neuris.text-check-global")
 
 const matchIsIgnoredGlobally = computed(() => {
   return props.match.ignoredTextCheckWords?.some(
-    (ignoredWord) =>
-      ignoredWord.type === "global" || ignoredWord.type === "global_jdv",
+    (ignoredWord) => ignoredWord.type === "global",
+  )
+})
+
+const matchIsIgnoredJDV = computed(() => {
+  return props.match.ignoredTextCheckWords?.some(
+    (ignoredWord) => ignoredWord.type === "global_jdv",
+  )
+})
+
+const matchIsIgnoredInDocument = computed(() => {
+  return props.match.ignoredTextCheckWords?.some(
+    (ignoredWord) => ignoredWord.type === "documentation_unit",
   )
 })
 </script>
 
 <template>
   <div class="flex flex-grow flex-col" data-testid="ignored-word-handler">
-    <div
-      v-if="
-        match.ignoredTextCheckWords?.some(
-          (ignoredWord) => ignoredWord.type === 'global_jdv',
-        )
-      "
-    >
-      Von jDV ignoriert
-    </div>
+    <div v-if="matchIsIgnoredJDV">Von jDV ignoriert</div>
 
-    <Button
-      v-else-if="
-        textCheckGlobal &&
-        match.ignoredTextCheckWords?.some(
-          (ignoredWord) => ignoredWord.type === 'global',
-        )
-      "
-      aria-label="Wort aus globalem Wörterbuch entfernen"
-      data-testid="ignored-word-global-remove-button"
-      label="Aus globalem Wörterbuch entfernen"
-      size="small"
-      text
-      @click="removeWordGlobally"
-    >
-      Aus globalem Wörterbuch entfernen
-    </Button>
-
-    <Button
-      v-if="textCheckGlobal && !matchIsIgnoredGlobally"
-      size="small"
-      text
-      @click="addIgnoredWordGlobally"
-      >Zum globalen Wörterbuch hinzufügen
-    </Button>
-
-    <div>
+    <div class="flex flex-grow flex-col gap-4">
       <Button
         v-if="
-          match.ignoredTextCheckWords?.some(
-            (ignoredWord) => ignoredWord.type === 'documentation_unit',
-          ) &&
-          !match.ignoredTextCheckWords?.some(
-            (ignoredWord) => ignoredWord.type === 'global_jdv',
-          )
+          textCheckGlobal &&
+          !matchIsIgnoredGlobally &&
+          !matchIsIgnoredInDocument &&
+          !matchIsIgnoredJDV
+        "
+        aria-label="Vorschlag ignorieren"
+        data-testid="ignored-word-add-button"
+        label="Ignorieren"
+        severity="secondary"
+        size="small"
+        @click="addIgnoredWord"
+      >
+        <template #icon>
+          <IconDescription />
+        </template>
+      </Button>
+
+      <Button
+        v-if="
+          matchIsIgnoredInDocument &&
+          !matchIsIgnoredJDV &&
+          !matchIsIgnoredGlobally
         "
         aria-label="Wort nicht ignorieren"
         button-type="tertiary"
         data-testid="ignored-word-remove-button"
+        icon="IconDescription"
         label="Nicht ignorieren"
         severity="secondary"
         size="small"
         @click="removeWord"
-      />
+      >
+        <template #icon>
+          <IconSpellCheck />
+        </template>
+      </Button>
+
+      <Button
+        v-if="textCheckGlobal && matchIsIgnoredGlobally && !matchIsIgnoredJDV"
+        aria-label="Wort aus globalem Wörterbuch entfernen"
+        data-testid="ignored-word-global-remove-button"
+        label="Aus globalem Wörterbuch entfernen"
+        size="small"
+        text
+        @click="removeWordGlobally"
+      >
+        <template #icon>
+          <IconBookOpenPageVariant />
+        </template>
+      </Button>
+
+      <Button
+        v-if="textCheckGlobal && !matchIsIgnoredGlobally"
+        label="Zum globalen Wörterbuch hinzufügen"
+        size="small"
+        text
+        @click="addIgnoredWordGlobally"
+      >
+        <template #icon>
+          <IconBookOpenVariant />
+        </template>
+      </Button>
     </div>
   </div>
 </template>
--- frontend/src/components/text-check/TextCheckModal.vue
+++ frontend/src/components/text-check/TextCheckModal.vue
@@ -1,8 +1,7 @@
 <script lang="ts" setup>
 import { computed } from "vue"
 import IgnoredWordHandler from "@/components/text-check/IgnoredWordHandler.vue"
-import ReplacementBar from "@/components/text-check/ReplacementBar.vue"
-import { Match, Replacement } from "@/types/textCheck"
+import { Match } from "@/types/textCheck"
 
 const props = defineProps<{
   match: Match
@@ -13,12 +12,7 @@ const emit = defineEmits<{
   "word:add": [word: string]
   "globalWord:remove": [value: string]
   "globalWord:add": [word: string]
-  "word:replace": [value: string]
 }>()
-
-function acceptSuggestion(replacement: string) {
-  emit("word:replace", replacement)
-}
 
 function addIgnoredWord(word: string) {
   emit("word:add", word)
@@ -34,10 +28,6 @@ function addIgnoredWordGlobally() {
 
 function removeGloballyIgnoredWord(word: string) {
   emit("globalWord:remove", word)
-}
-
-function getValues(replacements: Replacement[]) {
-  return replacements.flatMap((replacement) => replacement.value)
 }
 
 const isMatchIgnored = computed(() => {
@@ -59,21 +49,14 @@ const isMatchIgnored = computed(() => {
       </span>
     </div>
 
+    <p v-if="!isMatchIgnored">{{ match.shortMessage || match.message }}</p>
+
     <IgnoredWordHandler
       :match="match"
       @globally-ignored-word:add="addIgnoredWordGlobally"
       @globally-ignored-word:remove="removeGloballyIgnoredWord(match.word)"
-      @ignored-word:remove="removeIgnoredWord(match.word)"
-    />
-
-    <p v-if="!isMatchIgnored">{{ match.shortMessage || match.message }}</p>
-
-    <ReplacementBar
-      v-if="!isMatchIgnored"
-      replacement-mode="single"
-      :replacements="getValues(match.replacements)"
       @ignored-word:add="addIgnoredWord(match.word)"
-      @suggestion:update="acceptSuggestion"
+      @ignored-word:remove="removeIgnoredWord(match.word)"
     />
   </div>
 </template>
