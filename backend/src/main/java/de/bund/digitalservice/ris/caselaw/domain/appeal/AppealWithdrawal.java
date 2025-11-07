1b7ef6de6c8d33419e92fd6e51e034ec81640de9
Nadine Kost <nadine.kost@digitalservice.bund.de>
2025-11-07 10:15:14 +0100

    Remove humandreadable from enum
    RISDEV-8627

--- backend/src/main/java/de/bund/digitalservice/ris/caselaw/domain/appeal/AppealWithdrawal.java
+++ backend/src/main/java/de/bund/digitalservice/ris/caselaw/domain/appeal/AppealWithdrawal.java
@@ -1,13 +1,7 @@
 package de.bund.digitalservice.ris.caselaw.domain.appeal;
 
 public enum AppealWithdrawal {
-  JA("Ja"),
-  NEIN("Nein"),
-  KEINE_ANGABE("Keine Angabe");
-
-  public final String humanReadable;
-
-  AppealWithdrawal(String humanReadable) {
-    this.humanReadable = humanReadable;
-  }
+  JA,
+  NEIN,
+  KEINE_ANGABE
 }
