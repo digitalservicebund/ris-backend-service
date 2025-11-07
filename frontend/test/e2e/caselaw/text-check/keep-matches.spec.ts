ce806bdcdb53808a533c7f6b6229879f8c110928
Leonie Koch <leonie.koch@digitalservice.bund.de>
2025-11-07 17:34:02 +0100

    Try second fix for firefox
    RISDEV-10063

--- frontend/test/e2e/caselaw/text-check/keep-matches.spec.ts
+++ frontend/test/e2e/caselaw/text-check/keep-matches.spec.ts
@@ -3,6 +3,7 @@ import { navigateToCategories } from "../utils/e2e-utils"
 import { DocumentUnitCategoriesEnum } from "@/components/enumDocumentUnitCategories"
 import { caselawTest as test } from "~/e2e/caselaw/fixtures"
 
+const ERROR_WORD = "Felelr"
 test.describe(
   "text check store",
   {
@@ -11,7 +12,7 @@ test.describe(
   () => {
     test.use({
       decisionToBeCreated: {
-        shortTexts: { headnote: "<p>Text mit Felelr</p>" },
+        shortTexts: { headnote: `<p>Text mit ${ERROR_WORD}</p>` },
       },
     })
     test(
@@ -20,6 +21,16 @@ test.describe(
         tag: ["@RISDEV-9234"],
       },
       async ({ page, decision }) => {
+        const mockResponse = {
+          htmlText: `<p>Text mit <text-check id="1" type="misspelling" ignored="false">${ERROR_WORD}</text-check></p>`,
+          matches: [
+            {
+              id: 1,
+              word: ERROR_WORD,
+              category: "headnote",
+            },
+          ],
+        }
         await test.step("navigate to headnote (Orientierungssatz) in categories", async () => {
           await navigateToCategories(
             page,
@@ -31,7 +42,22 @@ test.describe(
         })
 
         await test.step("Trigger text check in headnote (Orientierungssatz)", async () => {
-          await page.getByTestId("Orientierungssatz").locator("div").click()
+          const headNoteEditor = page.getByTestId("Orientierungssatz")
+          const lock = Promise.withResolvers<void>()
+
+          await page.route(
+            `**/api/v1/caselaw/documentunits/${decision.createdDecision.uuid}/text-check?category=headnote`,
+            async (route) => {
+              await lock.promise
+              await route.fulfill({
+                status: 200,
+                contentType: "application/json",
+                body: JSON.stringify(mockResponse),
+              })
+            },
+          )
+
+          await headNoteEditor.locator("div").click()
 
           await page
             .getByLabel("Orientierungssatz Button")
@@ -42,11 +68,16 @@ test.describe(
             page.getByTestId("text-check-loading-status"),
           ).toHaveText("Rechtschreibprüfung läuft")
 
-          await page
-            .getByTestId("Orientierungssatz")
+          lock.resolve()
+
+          await headNoteEditor
             .locator("text-check")
             .first()
             .waitFor({ state: "visible" })
+
+          await expect(
+            page.getByTestId("text-check-loading-status"),
+          ).toBeHidden()
         })
 
         await test.step("match modal should still appear after navigation", async () => {
@@ -64,7 +95,10 @@ test.describe(
             page.getByTestId("Orientierungssatz").locator("text-check").first(),
           ).toBeVisible()
 
-          await page.locator("text-check").first().click()
+          await page
+            .getByTestId("Orientierungssatz")
+            .getByText("Felelr")
+            .click()
           await expect(page.getByTestId("text-check-modal-word")).toBeVisible()
         })
       },
