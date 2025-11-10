c4d157a19cc156e87b66dafc05749a1433bb02f3
Malte Laukötter <malte.laukoetter@digitalservice.bund.de>
2025-11-10 12:37:12 +0100

    Add endpoint for CollectiveAgreementIndustry
    RISDEV-6687

---
+++ backend/src/test/java/de/bund/digitalservice/ris/caselaw/adapter/CollectiveAgreementIndustryControllerTest.java
@@ -0,0 +1,83 @@
+package de.bund.digitalservice.ris.caselaw.adapter;
+
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import de.bund.digitalservice.ris.caselaw.TestConfig;
+import de.bund.digitalservice.ris.caselaw.config.SecurityConfig;
+import de.bund.digitalservice.ris.caselaw.domain.CollectiveAgreementIndustryService;
+import de.bund.digitalservice.ris.caselaw.webtestclient.RisWebTestClient;
+import java.util.ArrayList;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(controllers = CollectiveAgreementIndustryController.class)
+@Import({SecurityConfig.class, TestConfig.class})
+class CollectiveAgreementIndustryControllerTest {
+
+  @Autowired private RisWebTestClient risWebTestClient;
+
+  @MockitoBean private CollectiveAgreementIndustryService service;
+
+  @MockitoBean private ClientRegistrationRepository clientRegistrationRepository;
+  @MockitoBean private OAuth2AuthorizedClientManager oAuth2AuthorizedClientManager;
+
+  @Test
+  void testGetCollectiveAgreementIndustriesWithoutSize() {
+    when(service.getCollectiveAgreementIndustries(anyString(), anyInt()))
+        .thenReturn(new ArrayList<>());
+
+    risWebTestClient
+        .withDefaultLogin()
+        .get()
+        .uri("/api/v1/caselaw/collective-agreement-industries")
+        .exchange()
+        .expectStatus()
+        .isOk();
+
+    verify(service, times(1)).getCollectiveAgreementIndustries(null, 200);
+  }
+
+  @Test
+  void testGetCollectiveAgreementIndustriesWithSize() {
+    when(service.getCollectiveAgreementIndustries(anyString(), anyInt()))
+        .thenReturn(new ArrayList<>());
+
+    risWebTestClient
+        .withDefaultLogin()
+        .get()
+        .uri("/api/v1/caselaw/collective-agreement-industries?sz=100")
+        .exchange()
+        .expectStatus()
+        .isOk();
+
+    verify(service, times(1)).getCollectiveAgreementIndustries(null, 100);
+  }
+
+  @Test
+  void testGetCollectiveAgreementIndustriesWithQuery() {
+    when(service.getCollectiveAgreementIndustries(anyString(), anyInt()))
+        .thenReturn(new ArrayList<>());
+
+    risWebTestClient
+        .withDefaultLogin()
+        .get()
+        .uri("/api/v1/caselaw/collective-agreement-industries?q=test")
+        .exchange()
+        .expectStatus()
+        .isOk();
+
+    verify(service, times(1)).getCollectiveAgreementIndustries("test", 200);
+  }
+}
