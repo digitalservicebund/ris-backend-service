73631c2ade399bb4391cbb8021fe42bec7dd909a
Kim Kern <kim.kern@digitalservice.bund.de>
2025-11-18 14:19:17 +0100

    Add normen (#3426)
    * Add normen
    
    RISDEV-9358
    
    * Replace Builder type with var
    
    RISDEV-9358
    
    * Remove unused method
    
    RISDEV-9432
    
    * Revert sonar fix
    
    RISDEV-0000
    
    * Refactor isFullLdml flag
    
    RISDEV-9358
    
    * Exclude legalForce from reduced LDML norms
    
    RISDEV-9358
    
    * Remove unused method
    
    RISDEV-9358
    
    * TransformerTest review
    
    RISDEV-9358
    
    * Surpress variable warning
    
    RISDEV-9358
    
    ---------
    
    Co-authored-by: anne-ds <anne.schumann@digitalservice.bund.de>

--- backend/src/main/java/de/bund/digitalservice/ris/caselaw/adapter/transformer/ldml/DocumentationUnitLdmlTransformer.java
+++ backend/src/main/java/de/bund/digitalservice/ris/caselaw/adapter/transformer/ldml/DocumentationUnitLdmlTransformer.java
@@ -3,10 +3,10 @@ package de.bund.digitalservice.ris.caselaw.adapter.transformer.ldml;
 import static de.bund.digitalservice.ris.caselaw.adapter.MappingUtils.nullSafeGet;
 import static de.bund.digitalservice.ris.caselaw.adapter.MappingUtils.validate;
 import static de.bund.digitalservice.ris.caselaw.adapter.MappingUtils.validateNotNull;
+import static org.apache.commons.lang3.StringUtils.isNotBlank;
 
 import de.bund.digitalservice.ris.caselaw.adapter.DateUtils;
 import de.bund.digitalservice.ris.caselaw.adapter.caselawldml.CaseLawLdml;
-import de.bund.digitalservice.ris.caselaw.adapter.caselawldml.RelatedDecision;
 import de.bund.digitalservice.ris.caselaw.adapter.caselawldml.TlcElement;
 import de.bund.digitalservice.ris.caselaw.adapter.caselawldml.header.CourtType;
 import de.bund.digitalservice.ris.caselaw.adapter.caselawldml.header.DocDate;
@@ -15,6 +15,9 @@ import de.bund.digitalservice.ris.caselaw.adapter.caselawldml.header.DocType;
 import de.bund.digitalservice.ris.caselaw.adapter.caselawldml.header.EmbeddedStructure;
 import de.bund.digitalservice.ris.caselaw.adapter.caselawldml.header.Paragraph;
 import de.bund.digitalservice.ris.caselaw.adapter.caselawldml.header.ShortTitle;
+import de.bund.digitalservice.ris.caselaw.adapter.caselawldml.meta.analysis.ImplicitReference;
+import de.bund.digitalservice.ris.caselaw.adapter.caselawldml.meta.analysis.Norm;
+import de.bund.digitalservice.ris.caselaw.adapter.caselawldml.meta.analysis.Rechtszug;
 import de.bund.digitalservice.ris.caselaw.adapter.caselawldml.meta.identification.FrbrAlias;
 import de.bund.digitalservice.ris.caselaw.adapter.caselawldml.meta.identification.FrbrAuthor;
 import de.bund.digitalservice.ris.caselaw.adapter.caselawldml.meta.identification.FrbrCountry;
@@ -22,19 +25,29 @@ import de.bund.digitalservice.ris.caselaw.adapter.caselawldml.meta.identificatio
 import de.bund.digitalservice.ris.caselaw.adapter.caselawldml.meta.identification.FrbrElement;
 import de.bund.digitalservice.ris.caselaw.adapter.caselawldml.meta.identification.FrbrLanguage;
 import de.bund.digitalservice.ris.caselaw.adapter.caselawldml.meta.identification.Identification;
+import de.bund.digitalservice.ris.caselaw.adapter.caselawldml.meta.proprietary.AktenzeichenListe;
+import de.bund.digitalservice.ris.caselaw.adapter.caselawldml.meta.proprietary.DokumentTyp;
+import de.bund.digitalservice.ris.caselaw.adapter.caselawldml.meta.proprietary.Gericht;
 import de.bund.digitalservice.ris.caselaw.adapter.caselawldml.meta.references.References;
+import de.bund.digitalservice.ris.caselaw.domain.ContentRelatedIndexing;
 import de.bund.digitalservice.ris.caselaw.domain.CoreData;
 import de.bund.digitalservice.ris.caselaw.domain.Decision;
 import de.bund.digitalservice.ris.caselaw.domain.DocumentationOffice;
 import de.bund.digitalservice.ris.caselaw.domain.DocumentationUnit;
+import de.bund.digitalservice.ris.caselaw.domain.NormReference;
 import de.bund.digitalservice.ris.caselaw.domain.PendingProceeding;
+import de.bund.digitalservice.ris.caselaw.domain.PreviousDecision;
 import de.bund.digitalservice.ris.caselaw.domain.RelatedDocumentationUnit;
-import de.bund.digitalservice.ris.caselaw.domain.court.Court;
+import de.bund.digitalservice.ris.caselaw.domain.SingleNorm;
 import jakarta.xml.bind.ValidationException;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
+import java.util.stream.Stream;
+import javax.annotation.Nonnull;
+import org.springframework.util.CollectionUtils;
 
 public interface DocumentationUnitLdmlTransformer<T extends DocumentationUnit> {
   Set<String> GERMAN_STATES =
@@ -71,7 +84,7 @@ public interface DocumentationUnitLdmlTransformer<T extends DocumentationUnit> {
                 + getDocOfficeEid(
                     nullSafeGet(documentationUnit.coreData(), CoreData::documentationOffice)));
 
-    List<FrbrAlias> aliases = generateAliases(documentationUnit, isFullLdml);
+    List<FrbrAlias> aliases = generateAliases(documentationUnit);
 
     var elementBuilder =
         FrbrElement.builder()
@@ -84,8 +97,8 @@ public interface DocumentationUnitLdmlTransformer<T extends DocumentationUnit> {
         && documentationUnit.coreData().court().regions() != null
         && !documentationUnit.coreData().court().regions().isEmpty()) {
       elementBuilder.frbrCountry(
-          // The schmema says ISO 3166-1 Alpha-2 code, but we only have Alpha-3 available (for now)
-          // (We have only two regions for a handful of german courts)
+          // The schema says ISO 3166-1 Alpha-2 code, but we only have Alpha-3 available (for now)
+          // (We have only two regions for a handful of German courts)
           new FrbrCountry(getCountry(documentationUnit.coreData().court().regions().getFirst())));
     }
 
@@ -187,22 +200,6 @@ public interface DocumentationUnitLdmlTransformer<T extends DocumentationUnit> {
     }
   }
 
-  default List<RelatedDecision> buildRelatedDecisions(
-      List<? extends RelatedDocumentationUnit> relatedDecisions) {
-    List<RelatedDecision> previousDecision = new ArrayList<>();
-    for (RelatedDocumentationUnit current : relatedDecisions) {
-      RelatedDecision decision =
-          RelatedDecision.builder()
-              .date(DateUtils.toDateString(current.getDecisionDate()))
-              .documentNumber(current.getDocumentNumber())
-              .fileNumber(current.getFileNumber())
-              .courtType(nullSafeGet(current.getCourt(), Court::type))
-              .build();
-      previousDecision.add(decision);
-    }
-    return previousDecision;
-  }
-
   default References buildReferences(DocumentationUnit documentationUnit) {
     References.ReferencesBuilder referencesBuilder = References.builder();
     List<TlcElement> tlcOrganizations = new ArrayList<>();
@@ -267,6 +264,165 @@ public interface DocumentationUnitLdmlTransformer<T extends DocumentationUnit> {
     paragraphs.add(headlineParagraph);
   }
 
+  @Nonnull
+  default List<ImplicitReference> buildCommonImplicitReferences(
+      DocumentationUnit documentationUnit) {
+    return Stream.concat(
+            buildNormen(documentationUnit).stream(),
+            buildVorgehendeEntscheidungen(documentationUnit).stream())
+        .toList();
+  }
+
+  @Nonnull
+  private List<ImplicitReference> buildVorgehendeEntscheidungen(
+      DocumentationUnit documentationUnit) {
+    List<ImplicitReference> vorhergehendeEntscheidungen = new ArrayList<>();
+    var previousDecisions = documentationUnit.previousDecisions();
+    if (previousDecisions != null) {
+      for (PreviousDecision previousDecision : previousDecisions) {
+        if (previousDecision == null) continue;
+
+        var vorgehendBuilder = Rechtszug.Vorgehend.builder();
+        buildCaselawReference(previousDecision, vorgehendBuilder);
+        vorhergehendeEntscheidungen.add(
+            ImplicitReference.builder()
+                .domainTerm("Rechtszug")
+                .vorgehend(vorgehendBuilder.build())
+                .build());
+      }
+    }
+    return vorhergehendeEntscheidungen;
+  }
+
+  @Nonnull
+  private List<ImplicitReference> buildNormen(DocumentationUnit documentationUnit) {
+    List<ImplicitReference> normen = new ArrayList<>();
+
+    var norms =
+        Optional.ofNullable(documentationUnit.contentRelatedIndexing())
+            .map(ContentRelatedIndexing::norms)
+            .orElse(null);
+    if (!CollectionUtils.isEmpty(norms)) {
+      for (NormReference normRef : norms) {
+        if (normRef == null) continue;
+
+        Norm norm = buildNorm(normRef);
+
+        normen.add(ImplicitReference.builder().domainTerm("Norm").norm(norm).build());
+      }
+    }
+    return normen;
+  }
+
+  @SuppressWarnings("java:S1854")
+  default void buildCaselawReference(
+      RelatedDocumentationUnit relatedDocUnit,
+      Rechtszug.CaselawReference.CaselawReferenceBuilder<
+              ?, ? extends Rechtszug.CaselawReference.CaselawReferenceBuilder<?, ?>>
+          builder) {
+    if (relatedDocUnit.getDocumentType() != null
+        && isNotBlank(relatedDocUnit.getDocumentType().label())) {
+      builder.dokumentTyp(
+          DokumentTyp.builder().eId(null).value(relatedDocUnit.getDocumentType().label()).build());
+    }
+    if (relatedDocUnit.getDecisionDate() != null) {
+      builder.datum(
+          Rechtszug.Datum.builder()
+              .value(DateUtils.toDateString(relatedDocUnit.getDecisionDate()))
+              .build());
+    }
+    if (isNotBlank(relatedDocUnit.getDocumentNumber())) {
+      builder.dokumentNummer(
+          Rechtszug.DokumentNummer.builder().value(relatedDocUnit.getDocumentNumber()).build());
+    }
+    if (isNotBlank(relatedDocUnit.getFileNumber())) {
+      builder.aktenzeichen(
+          AktenzeichenListe.Aktenzeichen.builder().value(relatedDocUnit.getFileNumber()).build());
+    }
+    if (relatedDocUnit.getCourt() != null) {
+      Gericht.GerichtBuilder gerichtBuilder = Gericht.builder();
+      if (isNotBlank(relatedDocUnit.getCourt().type())) {
+        gerichtBuilder.typ(
+            Gericht.GerichtTyp.builder().value(relatedDocUnit.getCourt().type()).build());
+      }
+      if (isNotBlank(relatedDocUnit.getCourt().location())) {
+        gerichtBuilder.ort(
+            Gericht.GerichtOrt.builder().value(relatedDocUnit.getCourt().location()).build());
+      }
+
+      builder.gericht(gerichtBuilder.build());
+    }
+  }
+
+  default Norm buildNorm(NormReference normRef) {
+    Norm.NormBuilder normBuilder = Norm.builder();
+    String abbr = null;
+    if (normRef.normAbbreviation() != null
+        && isNotBlank(normRef.normAbbreviation().abbreviation())) {
+      abbr = normRef.normAbbreviation().abbreviation();
+    } else if (isNotBlank(normRef.normAbbreviationRawValue())) {
+      abbr = normRef.normAbbreviationRawValue();
+    }
+    if (isNotBlank(abbr)) {
+      normBuilder.abkuerzung(Norm.Abkuerzung.builder().value(abbr).build());
+    }
+
+    if (normRef.normAbbreviation() != null
+        && isNotBlank(normRef.normAbbreviation().officialLongTitle())) {
+      normBuilder.titel(normRef.normAbbreviation().officialLongTitle());
+    }
+
+    List<Norm.Einzelnorm> einzelNormen = new ArrayList<>();
+    if (!CollectionUtils.isEmpty(normRef.singleNorms())) {
+      for (SingleNorm singleNorm : normRef.singleNorms()) {
+        if (singleNorm == null) continue;
+        Norm.Einzelnorm einzelnorm = buildEinzelnorm(singleNorm);
+        einzelNormen.add(einzelnorm);
+      }
+    }
+    if (!einzelNormen.isEmpty()) {
+      normBuilder.einzelnormen(einzelNormen);
+    }
+    return normBuilder.build();
+  }
+
+  default Norm.Einzelnorm buildEinzelnorm(SingleNorm singleNorm) {
+    Norm.Einzelnorm.EinzelnormBuilder einzelnormBuilder = Norm.Einzelnorm.builder();
+
+    if (isNotBlank(singleNorm.singleNorm())) {
+      einzelnormBuilder.bezeichnung(
+          Norm.Bezeichnung.builder().value(singleNorm.singleNorm()).build());
+    }
+    if (isFullLDML() && singleNorm.legalForce() != null) {
+      Norm.Gesetzeskraft.GesetzeskraftBuilder gkBuilder = Norm.Gesetzeskraft.builder();
+      if (singleNorm.legalForce().type() != null
+          && isNotBlank(singleNorm.legalForce().type().label())) {
+        gkBuilder.typ(
+            Norm.TypDerGesetzeskraft.builder()
+                .value(singleNorm.legalForce().type().label())
+                .build());
+      }
+      if (singleNorm.legalForce().region() != null
+          && isNotBlank(singleNorm.legalForce().region().longText())) {
+        gkBuilder.geltungsbereich(
+            Norm.Geltungsbereich.builder()
+                .value(singleNorm.legalForce().region().longText())
+                .build());
+      }
+      einzelnormBuilder.gesetzeskraft(gkBuilder.build());
+    }
+    if (singleNorm.dateOfVersion() != null) {
+      einzelnormBuilder.datum(
+          Norm.Fassungsdatum.builder()
+              .value(DateUtils.toDateString(singleNorm.dateOfVersion()))
+              .build());
+    }
+    if (isNotBlank(singleNorm.dateOfRelevance())) {
+      einzelnormBuilder.jahr(Norm.Jahr.builder().value(singleNorm.dateOfRelevance()).build());
+    }
+    return einzelnormBuilder.build();
+  }
+
   private String getDocOfficeEid(DocumentationOffice documentationOffice) {
     if (documentationOffice == null) {
       return "dokumentationsstelle";
@@ -327,7 +483,7 @@ public interface DocumentationUnitLdmlTransformer<T extends DocumentationUnit> {
     return "";
   }
 
-  private List<FrbrAlias> generateAliases(DocumentationUnit documentationUnit, boolean isFullLdml) {
+  private List<FrbrAlias> generateAliases(DocumentationUnit documentationUnit) {
     List<FrbrAlias> aliases = new ArrayList<>();
 
     aliases.add(new FrbrAlias("Übergreifende ID", documentationUnit.uuid().toString()));
@@ -339,7 +495,7 @@ public interface DocumentationUnitLdmlTransformer<T extends DocumentationUnit> {
 
     if (documentationUnit.coreData() != null
         && documentationUnit.coreData().celexNumber() != null
-        && isFullLdml) {
+        && this.isFullLDML()) {
       aliases.add(new FrbrAlias("CELEX-Nummer", documentationUnit.coreData().celexNumber()));
     }
 
@@ -360,4 +516,6 @@ public interface DocumentationUnitLdmlTransformer<T extends DocumentationUnit> {
     }
     return aliases;
   }
+
+  boolean isFullLDML();
 }
