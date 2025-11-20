5676acf341f6df424478fa14927c841baa4e1e68
Malte Laukötter <malte.laukoetter@digitalservice.bund.de>
2025-11-20 16:10:29 +0100

    Allow adding edit component via slot
    We need an additional property for it for the corrections. This is not
    possible with a component property. To not need to change the existing
    code the property is still used when the slot is not provided.
    
    RISDEV-8622

--- frontend/test/components/editableList.spec.ts
+++ frontend/test/components/editableList.spec.ts
@@ -32,13 +32,43 @@ async function renderComponent<T>(options?: {
   editComponent?: Component
   summaryComponent?: Component
   modelValue?: T[]
+  editComponentAsSlot?: boolean
 }) {
   const props: EditableListProps<T> = {
-    editComponent: markRaw(options?.editComponent ?? DummyInputGroupVue),
+    editComponent: options?.editComponentAsSlot
+      ? undefined
+      : markRaw(options?.editComponent ?? DummyInputGroupVue),
     summaryComponent: markRaw(options?.summaryComponent ?? SummaryComponent),
     modelValue: options?.modelValue ?? listWithEntries.value,
     createEntry: () => new DummyListItem(),
   }
+  const slots = options?.editComponentAsSlot
+    ? {
+        edit: options?.editComponent
+          ? markRaw(options.editComponent)
+          : `<template
+            #edit="{
+              onAddEntry,
+              onCancelEdit,
+              onRemoveEntry,
+              modelValueList,
+              value,
+              'onUpdate:value': onUpdateValue,
+            }"
+          >
+            <DummyInputGroupVue
+              :model-value="value"
+              :model-value-list="modelValueList"
+              :register-text-editor-ref="registerTextEditorRef"
+              @add-entry="onAddEntry"
+              @cancel-edit="onCancelEdit"
+              @remove-entry="onRemoveEntry"
+              @update:model-value="onUpdateValue"
+            />
+          </template>`,
+      }
+    : undefined
+
   const router = createRouter({
     history: createWebHistory(),
     routes: routes,
@@ -49,8 +79,10 @@ async function renderComponent<T>(options?: {
     user,
     ...render(EditableList, {
       props,
+      slots,
       global: {
         plugins: [[createTestingPinia({})], [router]],
+        components: { DummyInputGroupVue },
       },
     }),
   }
@@ -223,6 +255,118 @@ describe("EditableList", () => {
 
       // Assert
       expect(scrollIntoViewMock).toHaveBeenCalledTimes(1)
+    })
+  })
+
+  describe("edit component as slot", () => {
+    it("shows edit component for default value when adding new new entry via button click", async () => {
+      const { user } = await renderComponent({
+        editComponentAsSlot: true,
+      })
+      expect(screen.queryByLabelText("Editier Input")).not.toBeInTheDocument()
+      expect(screen.getByText("foo")).toBeVisible()
+      expect(screen.getByText("bar")).toBeVisible()
+      await user.click(screen.getByLabelText("Weitere Angabe"))
+      expect(screen.getByLabelText("Editier Input")).toBeVisible()
+    })
+
+    it("shows edit component when list item is clicked", async () => {
+      const { user } = await renderComponent({ editComponentAsSlot: true })
+
+      await user.click(screen.getByTestId("list-entry-0"))
+
+      expect(screen.getByLabelText("Editier Input")).toHaveValue("foo")
+      expect(screen.getByLabelText("Listeneintrag speichern")).toBeVisible()
+      expect(screen.getByLabelText("Abbrechen")).toBeVisible()
+      expect(screen.getByLabelText("Eintrag löschen")).toBeVisible()
+    })
+
+    it("delete button emits modelValue without the deleted entry", async () => {
+      const { user, emitted } = await renderComponent({
+        editComponentAsSlot: true,
+      })
+      await user.click(screen.getByTestId("list-entry-0"))
+      await user.click(screen.getByLabelText("Eintrag löschen"))
+
+      expect(listWithEntries.value.length).toEqual(2)
+
+      expect(emitted()["update:modelValue"]).toEqual([
+        [
+          [
+            {
+              text: "bar",
+              uuid: "124",
+            },
+          ],
+        ],
+      ])
+
+      expect(
+        screen.getByLabelText("Weitere Angabe"),
+        "Deleting did not reset edit entry",
+      ).toBeVisible()
+    })
+
+    it("automatically adds a default entry in edit mode if list is empty on initial render", async () => {
+      await renderComponent({ modelValue: [], editComponentAsSlot: true })
+
+      expect(screen.getByLabelText("Editier Input")).toBeVisible()
+      expect(screen.queryByLabelText("Weitere Angabe")).not.toBeInTheDocument()
+      expect(screen.getByLabelText("Listeneintrag speichern")).toBeVisible()
+      expect(screen.getByLabelText("Listeneintrag speichern")).toBeDisabled()
+
+      //with no inputs, there is no cancel or delete button
+      expect(screen.queryByLabelText("Abbrechen")).not.toBeInTheDocument()
+      expect(screen.queryByLabelText("Eintrag löschen")).not.toBeInTheDocument()
+    })
+
+    it("updates the model value entry on editing it", async () => {
+      const { emitted, user } = await renderComponent({
+        editComponentAsSlot: true,
+      })
+
+      await user.click(screen.getByTestId("list-entry-0"))
+      await user.type(screen.getByLabelText("Editier Input"), "1")
+      await user.click(screen.getByLabelText("Listeneintrag speichern"))
+
+      expect(emitted()["update:modelValue"]).toEqual([
+        [
+          [
+            {
+              text: "foo1",
+              uuid: "123",
+            },
+            {
+              text: "bar",
+              uuid: "124",
+            },
+          ],
+        ],
+      ])
+    })
+
+    it("closes the editing component if user clicks cancel button, changes not saved", async () => {
+      const { user } = await renderComponent({ editComponentAsSlot: true })
+
+      await user.click(screen.getByTestId("list-entry-0"))
+
+      expect(screen.getByLabelText("Editier Input")).toBeVisible()
+      await user.type(screen.getByLabelText("Editier Input"), "1")
+      await user.click(screen.getByLabelText("Abbrechen"))
+      expect(screen.queryByText("foo1")).not.toBeInTheDocument()
+      expect(screen.getByText("foo")).toBeVisible()
+    })
+
+    it("removes the current entry if no inputs made and a different entry gets edited afterwards", async () => {
+      const { user } = await renderComponent({ editComponentAsSlot: true })
+
+      expect(screen.getAllByLabelText("Listen Eintrag").length).toEqual(2)
+      await user.click(screen.getByLabelText("Weitere Angabe"))
+
+      //no inputs made, click in other entry
+      await user.click(screen.getByTestId("list-entry-0"))
+
+      expect(screen.getAllByLabelText("Listen Eintrag").length).toEqual(2)
     })
   })
 })
