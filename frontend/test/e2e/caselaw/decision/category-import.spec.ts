d1301ee5d8ec40731d1e8250e344e15f5df19bf0
Nadine Kost <nadine.kost@digitalservice.bund.de>
2025-12-04 11:00:41 +0100

    Fix category import test
    RISDEV-8712

--- frontend/test/e2e/caselaw/decision/category-import.spec.ts
+++ frontend/test/e2e/caselaw/decision/category-import.spec.ts
@@ -96,7 +96,7 @@ test.describe("category import", () => {
       await test.step("disable import for empty source categories", async () => {
         await navigateToCategoryImport(page, documentNumber)
         await searchForDocumentUnitToImport(page, documentNumber)
-        await expect(page.getByText("Quellrubrik leer")).toHaveCount(34) // total number of importable categories
+        await expect(page.getByText("Quellrubrik leer")).toHaveCount(35) // total number of importable categories
       })
     },
   )
@@ -117,7 +117,7 @@ test.describe("category import", () => {
           prefilledDocumentUnitWithTexts.documentNumber,
         )
 
-        await expect(page.getByText("Zielrubrik ausgefüllt")).toHaveCount(28) // number of non-importable categories, if target category already filled
+        await expect(page.getByText("Zielrubrik ausgefüllt")).toHaveCount(29) // number of non-importable categories, if target category already filled
       })
     },
   )
