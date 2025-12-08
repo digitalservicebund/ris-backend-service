342a991fec40ade4d0318cba5bbbe8c0c24518e0
Nadine Kost <nadine.kost@digitalservice.bund.de>
2025-12-08 18:42:12 +0100

    FIx code smell
    RISDEV-9960

--- backend/src/main/java/de/bund/digitalservice/ris/caselaw/adapter/NormReferenceType.java
+++ backend/src/main/java/de/bund/digitalservice/ris/caselaw/adapter/NormReferenceType.java
@@ -1,6 +1,11 @@
 package de.bund.digitalservice.ris.caselaw.adapter;
 
 public class NormReferenceType {
+
+  private NormReferenceType() {
+    // Private constructor to prevent instantiation of this utility class.
+  }
+
   public static final String NORM = "NORM";
   public static final String NON_APPLICATION_NORM = "NON_APPLICATION_NORM";
 }
