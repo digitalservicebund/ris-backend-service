921150a210379daf615bad9f3a253776bbee0223
Leonie Koch <leonie.koch@digitalservice.bund.de>
2025-12-10 08:14:05 +0100

    Revert "Add trailing node after tables for cursor exit (#3473)"
    This reverts commit 3ed3ff16c296c4c76378433ceb0c690b45bfb99c.

--- frontend/src/components/input/TextEditor.vue
+++ frontend/src/components/input/TextEditor.vue
@@ -43,7 +43,6 @@ import { CustomTableCell } from "@/editor/tableCell"
 import { CustomTableHeader } from "@/editor/tableHeader"
 import { TextCheckExtension } from "@/editor/textCheckExtension"
 import { TextCheckMark } from "@/editor/textCheckMark"
-import { TrailingNode } from "@/editor/trailingNode"
 import { useDocumentUnitStore } from "@/stores/documentUnitStore"
 import { Match } from "@/types/textCheck"
 import IconInfoOutline from "~icons/mdi/information-outline"
@@ -135,7 +134,6 @@ const extensions: AnyExtension[] = [
   TextCheckExtension.configure({
     service: textCheckService,
   }),
-  TrailingNode,
 ]
 
 const editor: Editor = new Editor({
