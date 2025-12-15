0ab3cbee1e5b689200341e556f3777c3131aa926
Dusko <792923+duskox@users.noreply.github.com>
2025-12-15 16:13:01 +0100

    [RISDEV-10446] e2e lists tests (#3573)
    * Sub list style change
    * Ignore lint suggestions
    
    RISDEV-10446

--- frontend/test/e2e/caselaw/decision/categories/shortAndLongTexts/lists.spec.ts
+++ frontend/test/e2e/caselaw/decision/categories/shortAndLongTexts/lists.spec.ts
@@ -1,3 +1,4 @@
+/* eslint-disable jest-dom/prefer-to-have-style */
 import { expect } from "@playwright/test"
 import { caselawTest as test } from "~/e2e/caselaw/fixtures"
 import {
@@ -164,5 +165,333 @@ test.describe(
       inputFieldInnerHTML = await inputField.innerHTML()
       expect(inputFieldInnerHTML).toContain(noList)
     })
+
+    test("Enter text and make it into an ordered list with decimal numbers", async ({
+      page,
+      prefilledDocumentUnit,
+    }) => {
+      const orderedListXMLPreview = `<ol style="list-style-type: decimal;">`
+
+      await navigateToCategories(page, prefilledDocumentUnit.documentNumber!)
+
+      await clickCategoryButton("Gründe", page)
+      const inputField = page.getByTestId("Gründe")
+      await inputField.click()
+      await page.keyboard.type("This is an ordered list")
+      await page
+        .locator(`[aria-label='Nummerierte Liste']:not([disabled])`)
+        .click()
+      await page.getByLabel("Numerisch (1, 2, 3)").click()
+      await page.keyboard.press("Enter")
+      await page.keyboard.type("Second ordered list item")
+
+      // hide invisible characters
+      await page
+        .locator(`[aria-label='Nicht-druckbare Zeichen']:not([disabled])`)
+        .click()
+
+      // verify list structure
+      const orderedList = inputField.locator("ol").first()
+      await expect(orderedList).toHaveAttribute(
+        "style",
+        "list-style-type: decimal;",
+      )
+
+      const listItems = inputField.locator("ol > li")
+      await expect(listItems).toHaveCount(2)
+
+      await expect(
+        inputField.getByText("This is an ordered list"),
+      ).toBeVisible()
+      await expect(
+        inputField.getByText("Second ordered list item"),
+      ).toBeVisible()
+
+      await save(page)
+
+      await navigateToHandover(page, prefilledDocumentUnit.documentNumber!)
+      await expect(page.getByText("XML Vorschau")).toBeVisible()
+      await page.getByText("XML Vorschau").click()
+
+      await expect(page.getByText(orderedListXMLPreview)).toBeVisible()
+    })
+
+    test("Enter text and make it into an ordered list with roman small enumeration", async ({
+      page,
+      prefilledDocumentUnit,
+    }) => {
+      const orderedListXMLPreview = `<ol style="list-style-type: lower-roman;">`
+
+      await navigateToCategories(page, prefilledDocumentUnit.documentNumber!)
+
+      await clickCategoryButton("Gründe", page)
+      const inputField = page.getByTestId("Gründe")
+      await inputField.click()
+      await page.keyboard.type("This is an ordered list")
+      await page
+        .locator(`[aria-label='Nummerierte Liste']:not([disabled])`)
+        .click()
+      await page.getByLabel("Römisch klein (i, ii, iii)").click()
+      await page.keyboard.press("Enter")
+      await page.keyboard.type("Second ordered list item")
+
+      // hide invisible characters
+      await page
+        .locator(`[aria-label='Nicht-druckbare Zeichen']:not([disabled])`)
+        .click()
+
+      // verify list structure
+      const orderedList = inputField.locator("ol").first()
+      await expect(orderedList).toHaveAttribute(
+        "style",
+        "list-style-type: lower-roman;",
+      )
+
+      const listItems = inputField.locator("ol > li")
+      await expect(listItems).toHaveCount(2)
+
+      await expect(
+        inputField.getByText("This is an ordered list"),
+      ).toBeVisible()
+      await expect(
+        inputField.getByText("Second ordered list item"),
+      ).toBeVisible()
+
+      await save(page)
+
+      await navigateToHandover(page, prefilledDocumentUnit.documentNumber!)
+      await expect(page.getByText("XML Vorschau")).toBeVisible()
+      await page.getByText("XML Vorschau").click()
+
+      await expect(page.getByText(orderedListXMLPreview)).toBeVisible()
+    })
+
+    test("Enter text and make it into an ordered list with roman capital enumeration", async ({
+      page,
+      prefilledDocumentUnit,
+    }) => {
+      const orderedListXMLPreview = `<ol style="list-style-type: upper-roman;">`
+
+      await navigateToCategories(page, prefilledDocumentUnit.documentNumber!)
+
+      await clickCategoryButton("Gründe", page)
+      const inputField = page.getByTestId("Gründe")
+      await inputField.click()
+      await page.keyboard.type("This is an ordered list")
+      await page
+        .locator(`[aria-label='Nummerierte Liste']:not([disabled])`)
+        .click()
+      await page.getByLabel("Römisch groß (I, II, III)").click()
+      await page.keyboard.press("Enter")
+      await page.keyboard.type("Second ordered list item")
+
+      // hide invisible characters
+      await page
+        .locator(`[aria-label='Nicht-druckbare Zeichen']:not([disabled])`)
+        .click()
+
+      // verify list structure
+      const orderedList = inputField.locator("ol").first()
+      await expect(orderedList).toHaveAttribute(
+        "style",
+        "list-style-type: upper-roman;",
+      )
+
+      const listItems = inputField.locator("ol > li")
+      await expect(listItems).toHaveCount(2)
+
+      await expect(
+        inputField.getByText("This is an ordered list"),
+      ).toBeVisible()
+      await expect(
+        inputField.getByText("Second ordered list item"),
+      ).toBeVisible()
+
+      await save(page)
+
+      await navigateToHandover(page, prefilledDocumentUnit.documentNumber!)
+      await expect(page.getByText("XML Vorschau")).toBeVisible()
+      await page.getByText("XML Vorschau").click()
+
+      await expect(page.getByText(orderedListXMLPreview)).toBeVisible()
+    })
+
+    test("Enter text and make it into an ordered list with latin small enumeration", async ({
+      page,
+      prefilledDocumentUnit,
+    }) => {
+      const orderedListXMLPreview = `<ol style="list-style-type: lower-alpha;">`
+
+      await navigateToCategories(page, prefilledDocumentUnit.documentNumber!)
+
+      await clickCategoryButton("Gründe", page)
+      const inputField = page.getByTestId("Gründe")
+      await inputField.click()
+      await page.keyboard.type("This is an ordered list")
+      await page
+        .locator(`[aria-label='Nummerierte Liste']:not([disabled])`)
+        .click()
+      await page.getByLabel("Lateinisch klein (a, b, c)").click()
+      await page.keyboard.press("Enter")
+      await page.keyboard.type("Second ordered list item")
+
+      // hide invisible characters
+      await page
+        .locator(`[aria-label='Nicht-druckbare Zeichen']:not([disabled])`)
+        .click()
+
+      // verify list structure
+      const orderedList = inputField.locator("ol").first()
+      await expect(orderedList).toHaveAttribute(
+        "style",
+        "list-style-type: lower-alpha;",
+      )
+
+      const listItems = inputField.locator("ol > li")
+      await expect(listItems).toHaveCount(2)
+
+      await expect(
+        inputField.getByText("This is an ordered list"),
+      ).toBeVisible()
+      await expect(
+        inputField.getByText("Second ordered list item"),
+      ).toBeVisible()
+
+      await save(page)
+
+      await navigateToHandover(page, prefilledDocumentUnit.documentNumber!)
+      await expect(page.getByText("XML Vorschau")).toBeVisible()
+      await page.getByText("XML Vorschau").click()
+
+      await expect(page.getByText(orderedListXMLPreview)).toBeVisible()
+    })
+
+    test("Enter text and make it into an ordered list with latin capital enumeration", async ({
+      page,
+      prefilledDocumentUnit,
+    }) => {
+      const orderedListXMLPreview = `<ol style="list-style-type: upper-alpha;">`
+
+      await navigateToCategories(page, prefilledDocumentUnit.documentNumber!)
+
+      await clickCategoryButton("Gründe", page)
+      const inputField = page.getByTestId("Gründe")
+      await inputField.click()
+      await page.keyboard.type("This is an ordered list")
+      await page
+        .locator(`[aria-label='Nummerierte Liste']:not([disabled])`)
+        .click()
+      await page.getByLabel("Lateinisch groß (A, B, C)").click()
+      await page.keyboard.press("Enter")
+      await page.keyboard.type("Second ordered list item")
+
+      // hide invisible characters
+      await page
+        .locator(`[aria-label='Nicht-druckbare Zeichen']:not([disabled])`)
+        .click()
+
+      // verify list structure
+      const orderedList = inputField.locator("ol").first()
+      await expect(orderedList).toHaveAttribute(
+        "style",
+        "list-style-type: upper-alpha;",
+      )
+
+      const listItems = inputField.locator("ol > li")
+      await expect(listItems).toHaveCount(2)
+
+      await expect(
+        inputField.getByText("This is an ordered list"),
+      ).toBeVisible()
+      await expect(
+        inputField.getByText("Second ordered list item"),
+      ).toBeVisible()
+
+      await save(page)
+
+      await navigateToHandover(page, prefilledDocumentUnit.documentNumber!)
+      await expect(page.getByText("XML Vorschau")).toBeVisible()
+      await page.getByText("XML Vorschau").click()
+
+      await expect(page.getByText(orderedListXMLPreview)).toBeVisible()
+    })
   },
 )
+
+test.describe("Nested lists", () => {
+  test("Positioning on a sub list item and changing list style changes the list style of all items at the same level", async ({
+    page,
+    prefilledDocumentUnit,
+  }) => {
+    await navigateToCategories(page, prefilledDocumentUnit.documentNumber!)
+
+    await clickCategoryButton("Gründe", page)
+    const inputField = page.getByTestId("Gründe")
+    await inputField.click()
+
+    // type the list with two levels
+    await page.keyboard.type("Ordered list item one")
+    await page
+      .locator(`[aria-label='Nummerierte Liste']:not([disabled])`)
+      .click()
+    await page.getByLabel("Griechisch klein (α, β, γ)").click()
+    await page.keyboard.press("Enter")
+    await page.keyboard.press("Tab")
+    await page.keyboard.type("Sub list item one")
+    await page.keyboard.press("Enter")
+    await page.keyboard.type("Sub list item two")
+    await page.keyboard.press("Enter")
+    await page.keyboard.press("Shift+Tab")
+    await page.keyboard.type("Ordered list item two")
+
+    // hide invisible characters
+    await page
+      .locator(`[aria-label='Nicht-druckbare Zeichen']:not([disabled])`)
+      .click()
+
+    // check first list iteration
+    const outerList = inputField.locator("ol").first()
+    await expect(outerList).toHaveAttribute(
+      "style",
+      "list-style-type: lower-greek;",
+    )
+
+    const nestedList = inputField.locator("ol ol").first()
+    await expect(nestedList).toHaveAttribute(
+      "style",
+      "list-style-type: decimal;",
+    )
+
+    await expect(inputField.getByText("Ordered list item one")).toBeVisible()
+    await expect(inputField.getByText("Sub list item one")).toBeVisible()
+    await expect(inputField.getByText("Sub list item two")).toBeVisible()
+    await expect(inputField.getByText("Ordered list item two")).toBeVisible()
+
+    // position cursor on sub list item and change list style
+    const subListSecondElement = inputField.getByText("Sub list item two")
+    await subListSecondElement.click()
+    await page
+      .locator(`[aria-label='Nummerierte Liste']:not([disabled])`)
+      .click()
+    await page.getByLabel("Römisch klein (i, ii, iii)").click()
+
+    // check second list iteration
+    const nestedListAfterChange = inputField.locator("ol ol").first()
+    await expect(nestedListAfterChange).toHaveAttribute(
+      "style",
+      "list-style-type: lower-roman;",
+    )
+
+    const outerListAfterChange = inputField.locator("ol").first()
+    await expect(outerListAfterChange).toHaveAttribute(
+      "style",
+      "list-style-type: lower-greek;",
+    )
+
+    // just checking on validity of XML through button being visible
+    // this is to recognize that juris XML convertion did not break
+    // due to the conversion being sensitive to custom attributes
+    await navigateToHandover(page, prefilledDocumentUnit.documentNumber!)
+    await expect(page.getByText("XML Vorschau")).toBeVisible()
+  })
+})
