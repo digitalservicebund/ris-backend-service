408e6025a2b23c022628eba53d737db0cc253efd
Nadine Kost <nadine.kost@digitalservice.bund.de>
2026-01-08 10:45:45 +0100

    Bump db schema version
    RISDEV-0000

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -205,7 +205,7 @@ dependencies {
     // or with local gradle project (look also into settings.gradle.kts)
     // implementation(project(":exporter"))
 
-    implementation("de.bund.digitalservice:neuris-caselaw-migration-schema:0.0.119")
+    implementation("de.bund.digitalservice:neuris-caselaw-migration-schema:0.0.120")
     // for local development:
 //     implementation(files("../../ris-data-migration/schema/build/libs/schema-0.0.119.jar"))
 
