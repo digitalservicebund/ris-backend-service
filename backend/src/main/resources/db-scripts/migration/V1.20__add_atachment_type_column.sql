0cb4fc819d9fb691f7a69dfdd5c9ab65c07c00a5
Dusan <dusan.vorkapic@digitalservice.bund.de>
2026-01-21 15:27:21 +0100

    No default value att type
    RISDEV-10642

--- backend/src/main/resources/db-scripts/migration/V1.20__add_atachment_type_column.sql
+++ backend/src/main/resources/db-scripts/migration/V1.20__add_atachment_type_column.sql
@@ -4,12 +4,10 @@ UPDATE incremental_migration.attachment
 SET attachment_type =
     CASE
         WHEN upper(format) IN ('DOCX', 'FMX')
-            THEN 'ORIGINATING'
+            THEN 'ORIGINAL'
             ELSE 'OTHER'
     END;
 
-ALTER TABLE incremental_migration.attachment ALTER COLUMN attachment_type SET DEFAULT 'OTHER';
-
 ALTER TABLE incremental_migration.attachment ALTER COLUMN attachment_type SET NOT NULL;
 
-ALTER TABLE incremental_migration.attachment ADD CONSTRAINT attachment_type_check CHECK (attachment_type IN ('ORIGINATING', 'OTHER'));
+ALTER TABLE incremental_migration.attachment ADD CONSTRAINT attachment_type_check CHECK (attachment_type IN ('ORIGINAL', 'OTHER'));
