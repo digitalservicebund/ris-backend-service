c0fb9e5707e2e438690d6f41e5ba260e786b21ee
Dusan <dusan.vorkapic@digitalservice.bund.de>
2026-01-26 17:08:06 +0100

    Update migration version
    RISDEV-10624

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -205,7 +205,7 @@ dependencies {
     // or with local gradle project (look also into settings.gradle.kts)
     // implementation(project(":exporter"))
 
-    implementation("de.bund.digitalservice:neuris-caselaw-migration-schema:0.0.121")
+    implementation("de.bund.digitalservice:neuris-caselaw-migration-schema:0.0.124")
     // for local development:
 //     implementation(files("../../ris-data-migration/schema/build/libs/schema-0.0.121.jar"))
 
