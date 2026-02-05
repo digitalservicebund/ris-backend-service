0a49209d20277c511bb39fae17df96edd0a237b1
Nadine Kost <nadine.kost@digitalservice.bund.de>
2026-02-05 14:41:12 +0100

    Schedule move staging files to unreferenced
    RISDEV-10627

--- backend/src/main/java/de/bund/digitalservice/ris/caselaw/adapter/S3RenamingService.java
+++ backend/src/main/java/de/bund/digitalservice/ris/caselaw/adapter/S3RenamingService.java
@@ -155,8 +155,8 @@ public class S3RenamingService {
    * Moves files in the old s3 path pattern that were not moved in the previous step into the
    * 'unreferenced' prefix
    */
-  @Scheduled(cron = "-", zone = "Europe/Berlin")
-  @SchedulerLock(name = "move-unreferenced", lockAtMostFor = "PT12H")
+  @Scheduled(cron = "0 0 15 5 2 *", zone = "Europe/Berlin")
+  @SchedulerLock(name = "move-unreferenced", lockAtMostFor = "PT1H")
   public void moveRemainingFilesToUnreferenced() {
     if (!env.matchesProfiles("staging")) {
       return;
