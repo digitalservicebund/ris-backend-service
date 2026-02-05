44bfb1488693ec3b4c83d85fa2178dd8b2821817
Dusan <dusan.vorkapic@digitalservice.bund.de>
2026-02-05 13:02:16 +0100

    Delete tables from public schema
    RISDEV-10536

--- backend/src/main/java/de/bund/digitalservice/ris/caselaw/adapter/SchedulingConfig.java
+++ backend/src/main/java/de/bund/digitalservice/ris/caselaw/adapter/SchedulingConfig.java
@@ -22,6 +22,7 @@ public class SchedulingConfig {
   public LockProvider lockProvider(DataSource dataSource) {
     return new JdbcTemplateLockProvider(
         JdbcTemplateLockProvider.Configuration.builder()
+            .withTableName("incremental_migration.shedlock")
             .withJdbcTemplate(new JdbcTemplate(dataSource))
             .usingDbTime()
             .build());
---
+++ backend/src/main/resources/db-scripts/migration/V1.21__delete_public_tables.sql
@@ -0,0 +1,6 @@
+DROP TABLE deleted_documents;
+DROP TABLE document_number;
+DROP TABLE handover_mail_attachments;
+DROP TABLE handover_mail;
+DROP TABLE shedlock;
+DROP TABLE handover_report;
