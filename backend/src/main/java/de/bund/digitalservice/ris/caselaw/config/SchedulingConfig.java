7fade5b3061878251c12494af55b69be4d855d10
Dusan <dusan.vorkapic@digitalservice.bund.de>
2026-02-05 13:04:45 +0100

    Move config to correct package
    RISDEV-10536

--- backend/src/main/java/de/bund/digitalservice/ris/caselaw/config/SchedulingConfig.java
+++ backend/src/main/java/de/bund/digitalservice/ris/caselaw/config/SchedulingConfig.java
@@ -1,4 +1,4 @@
-package de.bund.digitalservice.ris.caselaw.adapter;
+package de.bund.digitalservice.ris.caselaw.config;
 
 import javax.sql.DataSource;
 import net.javacrumbs.shedlock.core.LockProvider;
