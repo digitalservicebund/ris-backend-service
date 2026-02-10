e07edd10169616bece467f98312ff2fc1ba15253
Dusan <dusan.vorkapic@digitalservice.bund.de>
2026-02-10 13:24:45 +0100

    Remove schema hardcode in entities
    RISDEV-11011

--- backend/src/main/java/de/bund/digitalservice/ris/caselaw/adapter/database/jpa/UserDTO.java
+++ backend/src/main/java/de/bund/digitalservice/ris/caselaw/adapter/database/jpa/UserDTO.java
@@ -22,7 +22,7 @@ import lombok.ToString;
 @ToString
 @Builder(toBuilder = true)
 @Entity
-@Table(name = "user")
+@Table(name = "\"user\"")
 public class UserDTO {
 
   @Id @GeneratedValue private UUID id;
