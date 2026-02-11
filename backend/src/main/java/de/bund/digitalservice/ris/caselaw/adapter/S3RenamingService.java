48027b1795b2da9d9317d4dfd252c604b76faa04
Nadine Kost <nadine.kost@digitalservice.bund.de>
2026-02-11 14:03:21 +0100

    Schedule move to unreferenced on uat
    RISDEV-10627

--- backend/src/main/java/de/bund/digitalservice/ris/caselaw/adapter/S3RenamingService.java
+++ backend/src/main/java/de/bund/digitalservice/ris/caselaw/adapter/S3RenamingService.java
@@ -51,7 +51,7 @@ public class S3RenamingService {
    * Adds a document number path prefix and .docx file extension to existing files in the old s3
    * path pattern
    */
-  @Scheduled(cron = "0 45 17 * * *", zone = "Europe/Berlin")
+  @Scheduled(cron = "-", zone = "Europe/Berlin")
   @SchedulerLock(name = "adjust-s3-paths", lockAtMostFor = "PT12H")
   public void moveExistingFilesToNewPaths() {
     if (!env.matchesProfiles("uat")) {
@@ -157,7 +157,7 @@ public class S3RenamingService {
    * Moves files in the old s3 path pattern that were not moved in the previous step into the
    * 'unreferenced' prefix
    */
-  @Scheduled(cron = "-", zone = "Europe/Berlin")
+  @Scheduled(cron = "0 0 15 * * *", zone = "Europe/Berlin")
   @SchedulerLock(name = "move-unreferenced", lockAtMostFor = "PT1H")
   public void moveRemainingFilesToUnreferenced() {
     if (!env.matchesProfiles("uat")) {
