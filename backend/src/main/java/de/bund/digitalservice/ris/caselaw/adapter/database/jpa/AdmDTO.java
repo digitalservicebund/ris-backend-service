8bfdf779f4c0ff00568ddf76fb894b0d5a0f2c5c
Malte Laukötter <malte.laukoetter@digitalservice.bund.de>
2026-03-04 15:58:55 +0100

    Fetch activeCaselawReferences eagerly
    RISDEV-11059

--- backend/src/main/java/de/bund/digitalservice/ris/caselaw/adapter/database/jpa/AdmDTO.java
+++ backend/src/main/java/de/bund/digitalservice/ris/caselaw/adapter/database/jpa/AdmDTO.java
@@ -2,6 +2,7 @@ package de.bund.digitalservice.ris.caselaw.adapter.database.jpa;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
@@ -37,6 +38,6 @@ public class AdmDTO {
   @Column(name = "published_at")
   private Instant publishedAt;
 
-  @OneToMany(mappedBy = "source")
+  @OneToMany(mappedBy = "source", fetch = FetchType.EAGER)
   private List<AdmActiveCaselawReferenceDTO> activeCaselawReferences;
 }
