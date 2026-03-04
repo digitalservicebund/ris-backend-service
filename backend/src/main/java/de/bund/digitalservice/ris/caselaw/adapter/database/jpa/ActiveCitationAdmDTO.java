cb2cef818c1a183642e28c4f132564b3d4096fa6
Malte Laukötter <malte.laukoetter@digitalservice.bund.de>
2026-03-04 15:59:24 +0100

    Fix id generation
    RISDEV-11059

--- backend/src/main/java/de/bund/digitalservice/ris/caselaw/adapter/database/jpa/ActiveCitationAdmDTO.java
+++ backend/src/main/java/de/bund/digitalservice/ris/caselaw/adapter/database/jpa/ActiveCitationAdmDTO.java
@@ -3,6 +3,7 @@ package de.bund.digitalservice.ris.caselaw.adapter.database.jpa;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -23,6 +24,7 @@ import lombok.Setter;
 @Entity
 @Table(name = "active_citation_adm", schema = "incremental_migration")
 public class ActiveCitationAdmDTO {
+  @GeneratedValue
   @Id
   @Column(name = "id", nullable = false)
   private UUID id;
