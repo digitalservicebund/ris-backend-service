b4f86ed55657c7b7343951e57730c54f9cf19040
Malte Laukötter <malte.laukoetter@digitalservice.bund.de>
2026-03-05 14:20:27 +0100

    Add integration test for revoked adm sync job
    RISDEV-11059

---
+++ backend/src/test/resources/adm_revoked_init.sql
@@ -0,0 +1,10 @@
+CREATE SCHEMA IF NOT EXISTS references_schema;
+
+CREATE TABLE IF NOT EXISTS references_schema.revoked_adm (
+                                                              id UUID PRIMARY KEY,
+                                                              doc_unit_id UUID,
+                                                              revoked_at TIMESTAMP
+);
+
+INSERT INTO references_schema.revoked_adm (id, doc_unit_id, revoked_at)
+VALUES ('f0f4d34b-1685-4548-9076-3b68b5d285b2', '41519069-ae24-4dd9-a412-32f2d594fb86', CURRENT_TIMESTAMP);
